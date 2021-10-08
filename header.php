9d3666113b32a4ce4b01a17b18aee5d71a5f8a45
Russell Maier <russ@ecobricks.org>
2021-10-08 18:17:15 +0800

    php

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="description" content="A short description." />
+		<meta name="keywords" content="put, keywords, here" />
+		<meta name="robots" content="noindex" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<title>PHP-MySQL Website from Scratch</title>	
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+
+		<link rel="stylesheet" href="style.css" >
+	</head>
---
+++ index-beta2.php
@@ -0,0 +1,14 @@
+<?php include 'header.php';?>
+	
+	<html>
+		<body>
+		Testing HTML
+		
+
+		
+		<footer class="container">
+		   <?php include 'footer.php'; ?>
+		</footer>
+	  
+		</body>
+	</html>
