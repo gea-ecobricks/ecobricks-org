6661e86b13f9650a4901564e95ff4513c17e8a2b
Russell Maier <russ@ecobricks.org>
2021-11-01 12:31:06 +0800

    meta-focus

---
+++ meta-tags/faqs-en.php
@@ -0,0 +1,20 @@
+
+
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Ecobricks.org | FAQs</title>
+
+<meta name="keywords" content="ecobricks, eco-bricks, ecobrique, ecolladrillos, plastic, plastic management, carbon sequestration, earth ethics, plastic, recycling alternatives, incineration, plastic offsets, plastic solutions, upcycling, plastic sequestration, ecobrick questions, faq questions, frequently asked ecobrick questions, recycling, incineration, pollution, plastic pollution">
+
+<meta name="description" content="Answers to ecobrick concerns, critiques & questions.  Learn how eco bricks relates to plastic, recycling, incineration, sequestration, carbon, offsetting and regenerating out of the petro-capital economy.">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://www.ecobricks.org/en/faqs.php"/>
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="Ecobricks.org | Frequently Asked Questions">
+  	<meta property="og:description"   content="All the answers about ecobrick concerns & critiques.  Learn how eco bricks relates to plastic, recycling, incineration, sequestration, carbon and the petro-capital economy."/>
+  	<meta property="og:image"         content="https://www.ecobricks.org/pngs/question.png"/>
+  	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="1129" />
+  	<meta property="og:image:alt"     content="A floating question mark made with ecobricks"/>
