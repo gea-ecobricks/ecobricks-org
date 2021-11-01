ee512c077d9e719e2fe913e2dd7c66c3bf051edc
Russell Maier <russ@ecobricks.org>
2021-11-01 14:08:21 +0800

    STAR

--- faqs.php
+++ faqs.php
@@ -14,7 +14,7 @@
 <link rel="preload" as="image" href="webp/faq-bottom.webp">
 
 
-<?php include 'meta/faqs_en.php'?>
+<?php include 'meta/faqs_*'?>
 
 <!-- Meta tags for page display and search engine listing
 
