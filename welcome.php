4d5259fe064a91fd1517169dc77303234d6d5154
Russell Maier <russ@ecobricks.org>
2021-11-02 18:53:59 +0800

    revert

--- welcome.php
+++ welcome.php
@@ -1,12 +1,12 @@
-<?php require_once ("lang.php");
-echo <<<_END
-
 <!DOCTYPE html>
 
-<!-- this grabs the language identifier for the page so that it can used in the meta and canonical url variables-->
-<html lang="$lang">
+<html lang="<?php include 'lang.php';?>">
 
 <HEAD>
+
+
+
+
 
 	<link rel="preload" as="image" href="logos/gea-horizontal.svg">
 	<link rel="preload" as="image" href="svgs/cloud-back.svg">
@@ -17,12 +17,26 @@ echo <<<_END
 
 	<!-- Meta tags for page display and search engine listing-->
 
-	_END;?>
 
-	<?php require_once ("meta/welcome_$lang.php");?>
+    <title>Ecobricks.org | Plastic Transition</title>
+
+<meta name="keywords" content="ecobricks, plastic offsets, ecobrick, plastic solved, eco-brick, gobrik, bottle brick, plastic solution, recycling, upcycling, plastic sequestration, plastic transition, petrocapital transition">
+
+<meta name="description" content="Together we can secure plastic out of the biosphere, out of industry and into building the green world for all, that we all long to see.">
 	
-	<?php require_once ("header.php");?>
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
 
+  <meta property="og:url"           content="https://www.ecobricks.org/en/">
+  <meta property="og:type"          content="website">
+ <meta property="og:title"         content="Ecobricks.org | Plastic Transition">
+  <meta property="og:description"   content="Together we can secure plastic out of the biosphere, out of industry and into building the green new world we all long to see."/>
+  <meta property="og:image"         content="https://www.ecobricks.org/pngs/social-banner2.png"/>
+  <meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="638" />
+  <meta property="og:image:alt"     content="A metaphorical road winding into the distance with various ecobrick and earth constructions along side it."/>
+	
+	<?php include 'header.php';?>
 
 <style>
 #clouds {
@@ -282,8 +296,7 @@ echo <<<_END
 	<!--FOOTER STARTS HERE-->
 
 
-
-<?php require_once ("footer.php");?>
+	<?php include 'footer.php'; ?>
 
 
 	<!--FOOTER ENDS HERE-->
