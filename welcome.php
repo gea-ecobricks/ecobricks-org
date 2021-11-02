a0a6640b8d8e120a4f2e40737944ca8370a1390f
Russell Maier <russ@ecobricks.org>
2021-11-02 19:14:55 +0800

    now

--- welcome.php
+++ welcome.php
@@ -1,43 +1,28 @@
+<?php require_once ("lang.php");
+echo <<<_END
 
 <!DOCTYPE html>
 
-<html lang="<?php include 'lang.php';?>">
+<!-- this grabs the language identifier for the page so that it can used in the meta and canonical url variables-->
+
+<html lang="$lang">
+
 
 <HEAD>
 
+<!--Image files to preload that are unique to this page-->
+
+<link rel="preload" as="image" href="logos/gea-horizontal.svg">
+<link rel="preload" as="image" href="svgs/cloud-back.svg">
+<!--<link rel="preload" as="image" href="webp/vision-bottom3b.webp">-->
+<link rel="preload" as="image" href="webp/mountain-top2.webp">
 
 
+_END;?>
 
+<?php require_once ("meta/welcome_$lang.php");?>
 
-	<link rel="preload" as="image" href="logos/gea-horizontal.svg">
-	<link rel="preload" as="image" href="svgs/cloud-back.svg">
-	<!--<link rel="preload" as="image" href="webp/vision-bottom3b.webp">-->
-	<link rel="preload" as="image" href="webp/mountain-top2.webp">
-
-
-
-	<!-- Meta tags for page display and search engine listing-->
-
-
-    <title>Ecobricks.org | Plastic Transition</title>
-
-<meta name="keywords" content="ecobricks, plastic offsets, ecobrick, plastic solved, eco-brick, gobrik, bottle brick, plastic solution, recycling, upcycling, plastic sequestration, plastic transition, petrocapital transition">
-
-<meta name="description" content="Together we can secure plastic out of the biosphere, out of industry and into building the green world for all, that we all long to see.">
-	
-	
-	<!-- Facebook Open Graph Tags for social sharing-->
-
-  <meta property="og:url"           content="https://www.ecobricks.org/en/">
-  <meta property="og:type"          content="website">
- <meta property="og:title"         content="Ecobricks.org | Plastic Transition">
-  <meta property="og:description"   content="Together we can secure plastic out of the biosphere, out of industry and into building the green new world we all long to see."/>
-  <meta property="og:image"         content="https://www.ecobricks.org/pngs/social-banner2.png"/>
-  <meta property="og:image:width" content="1200" />
-<meta property="og:image:height" content="638" />
-  <meta property="og:image:alt"     content="A metaphorical road winding into the distance with various ecobrick and earth constructions along side it."/>
-	
-	<?php include 'header.php';?>
+<?php require_once ("header.php");?>
 
 <style>
 #clouds {
@@ -293,6 +278,7 @@
 		<img src="svgs/3brikcoins.svg" width="30%">
 	</div>
 </div>
+
 
 	<!--FOOTER STARTS HERE-->
 
