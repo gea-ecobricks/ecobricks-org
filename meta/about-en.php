035c0cb76c9ead906562f35794b14da62db64858
Russell <russmaier@gmail.com>
2022-01-25 17:58:02 +0800

    update

--- meta/about-en.php
+++ meta/about-en.php
@@ -1,21 +1,21 @@
 <!-- Meta tags for page display and search engine listing-->
 
-<title>About the GEA | Ecobricks.org</title>
+<title>About Us | Ecobricks.org</title>
 
-<meta name="keywords" content="ecobricks, eco-bricks, eco bricks, eco briks, briks, plastic, plastic management, carbon sequestration, earthen ethics, plastic, recycling alternatives, incineration, plastic offsets, plastic solutions, upcycling, plastic sequestration, recycling alternative, incineration alternative, preventing plastic pollution, plastic pollution, regenerative plastic solution, waste, waste management, plastic waste">
+<meta name="keywords" content="global ecobrick alliance, GEA, gea, ecobricks.org, earth enterprise, about us, ecobrick company, ecobrick organization, ecobrick NGO.">
 
-<meta name="description" content="For keen green enterprises to track, disclose & accelerate their plastic transition.">
+<meta name="description" content="The Global Ecobrick Alliance is an Earth Enterprise accelerating local and global plastic transition.">
 	
 	<!-- Facebook Open Graph Tags for social sharing-->
 
-	<meta property="og:url"           content="https://www.ecobricks.org/en/catalyst.php"/>
+	<meta property="og:url"           content="https://www.ecobricks.org/en/about.php"/>
 	<meta property="og:type"          content="website">
- 	<meta property="og:title"         content="Enterprise Catalyst Program | Ecobricks.org">
-  	<meta property="og:description"   content="For keen green enterprises to track, disclose & accelerate their plastic transition."/>
-  	<meta property="og:image"         content="pngs/catalyst-fb-banner.png"/>
+ 	<meta property="og:title"         content="About Us | Ecobricks.org">
+  	<meta property="og:description"   content="The Global Ecobrick Alliance is an Earth Enterprise accelerating local and global plastic transition."/>
+  	<meta property="og:image"         content="pngs/gea-perspective-1000px.png"/>
   	<meta property="og:image:width" content="1000px" />
-	<meta property="og:image:height" content="1059px" />
-  	<meta property="og:image:alt"     content="Catalyst Program Logo by the Global Ecobrick Alliance."/>
+	<meta property="og:image:height" content="1039px" />
+  	<meta property="og:image:alt"     content="Our GEA seven circle, mandalic, hexagonal logo that resembles a milstein ecobrick module with each color of the spectrum represented and progressing."/>
 	  <meta property="og:locale" content="en_GB" />
 
 	
