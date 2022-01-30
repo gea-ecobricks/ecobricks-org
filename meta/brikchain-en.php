5d738d030ad33e3dbcf67bed7746182e52a91d63
Russell <russmaier@gmail.com>
2022-01-30 23:02:36 +0800

    updload

--- meta/brikchain-en.php
+++ meta/brikchain-en.php
@@ -10,12 +10,12 @@
 
 	<meta property="og:url"           content="https://www.ecobricks.org/en/brikchain.php"/>
 	<meta property="og:type"          content="website">
- 	<meta property="og:title"         content="Brikchain Explorer | Ecobricks.org">
+ 	<meta property="og:title"         content="Plastic Coefficients | Ecobricks.org">
   	<meta property="og:description"   content="Explore the Brikcoin blockchain."/>
-  	<meta property="og:image"         content="https://s3-eu-west-1.amazonaws.com/assets.knack-eu.com/assets/5abaea5dfdbfad4d03858fe2/5b4c1165d44d5804c418a27e/original/brikcoinchaintransparency.png"/>
+  	<meta property="og:image"         content="pngs/gea-perspective-1000px.png"/>
   	<meta property="og:image:width" content="1000px" />
 	<meta property="og:image:height" content="1039px" />
-  	<meta property="og:image:alt"     content="A search for brikcoins."/>
+  	<meta property="og:image:alt"     content="Our GEA seven circle, mandalic, hexagonal logo that resembles a milstein ecobrick module with each color of the spectrum represented and progressing."/>
 	  <meta property="og:locale" content="en_GB" />
 
 	
