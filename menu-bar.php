99d07a287064476a72131a713ae96eecae274b39
Russell <russmaier@gmail.com>
2022-02-06 17:19:03 +0800

    go baby

--- menu-bar.php
+++ menu-bar.php
@@ -15,8 +15,8 @@
                 <a href='https://ecobricks.org/en/<?php echo ($name); ;?>' onclick='javascript:Localize.setLanguage("ed")'>🇬🇧 ENG</a>
                 <a href='https://ecobricks.org/id/<?php echo ($name); ;?>' onclick='javascript:Localize.setLanguage("id")'>🇮🇩 IND</a>
                 <a href='https://ecobricks.org/es/<?php echo ($name); ;?>' onclick='javascript:Localize.setLanguage("es")'>🇪🇸 ESP</a>
-                <a href="https://www.ecobricks.org/index.php?lang=de">🇩🇪 DE</a>
-<!--
+               <!-- <a href="https://www.ecobricks.org/index.php?lang=de">🇩🇪 DE</a>
+
                 <a href='https://www.ecobricks.org/en/' >🇬🇧 ENG</a>
                 <a href='https://www.ecobricks.org/id/'>🇮🇩 IND</a>
                 <a href='https://www.ecobricks.org/es/'>🇪🇸 ESP</a>
