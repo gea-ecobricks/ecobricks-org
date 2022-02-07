8972059a58e90be7733a64284cd72427eb61aa98
Russell <russmaier@gmail.com>
2022-02-07 11:41:34 +0800

    go my friend

--- meta/about-en.php
+++ meta/about-en.php
@@ -2,16 +2,16 @@
 
 <title>About Us | Ecobricks.org</title>
 
-<meta name="keywords" content="global ecobrick alliance, GEA, gea, ecobricks.org, earth enterprise, about us, ecobrick company, ecobrick organization, ecobrick NGO.">
+<meta name="keywords" content="global ecobrick alliance, GEA, gea, ecobricks.org, earth enterprise, about us, ecobrick company, ecobrick organization, ecobrick NGO, earthen, principled.">
 
-<meta name="description" content="The Global Ecobrick Alliance is an Earth Enterprise accelerating local and global plastic transition.">
+<meta name="description" content="The Global Ecobrick Alliance is an Earth Enterprise accelerating local and global plastic transition through earthen principled ecobricking">
 	
 	<!-- Facebook Open Graph Tags for social sharing-->
 
 	<meta property="og:url"           content="https://www.ecobricks.org/en/about.php"/>
 	<meta property="og:type"          content="website">
  	<meta property="og:title"         content="About Us | Ecobricks.org">
-  	<meta property="og:description"   content="The Global Ecobrick Alliance is an Earth Enterprise accelerating local and global plastic transition."/>
+  	<meta property="og:description"   content="The Global Ecobrick Alliance is an Earth Enterprise accelerating local and global plastic transition through earthen principled ecobricking."/>
   	<meta property="og:image"         content="pngs/gea-perspective-1000px.png"/>
   	<meta property="og:image:width" content="1000px" />
 	<meta property="og:image:height" content="1039px" />
