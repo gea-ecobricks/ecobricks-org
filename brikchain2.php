4faaaf0bf08b9b5419e98ace5209149bdd4d179b
Russell <russmaier@gmail.com>
2022-02-08 20:21:23 +0800

    and

---
+++ brikchain2.php
@@ -0,0 +1,318 @@
+<?php require_once ("lang.php");
+echo <<<_END
+
+<!DOCTYPE html>
+
+<!-- this grabs the language identifier for the page so that it can used in the meta and canonical url variables-->
+
+<html lang="$lang">
+
+
+<HEAD>
+
+<!--Image files to preload that are unique to this page-->
+
+<link rel="preload" as="image" href="logos/gea-horizontal.svg">
+<link rel="preload" as="image" href="svgs/eb-blue.svg">
+
+_END;?>
+
+<?php require_once ("meta/brikchain-$lang.php");?>
+
+<?php require_once ("header.php");?>
+
+
+<!-- CUSTOM PAGE SCRIPTS-->
+
+<script src="accordion-scripts.js"></script> 
+
+<STYLE>
+
+
+/*This sets up the the first splash content block that is unique to the top of each page*/
+
+@media screen and (max-width: 700px) { 
+	.splash-content-block {
+		
+		background: url(svgs/cloud-back.svg) top;
+		background-color: #00FFFF;
+		
+		text-align: left;
+		height: 70vh;
+		padding: 50px 5% 25px 5%;
+		margin-bottom: 0px;
+		z-index: 5;
+    	background-repeat: no-repeat;
+		background-size: cover;
+		margin-top: 45px;
+		width: 100%;
+		display: flex;
+ 		box-sizing: border-box;
+		flex-direction: column;
+		position: relative;
+
+	}
+}
+
+
+@media screen and (min-width: 700px) { 
+	.splash-content-block {
+		
+		background: url(svgs/cloud-back.svg) bottom;
+		background-color: #00FFFF;
+		
+		text-align: left;
+		height: 60vh;
+		padding: 50px 7% 50px 7%;
+		z-index: 5;
+		position: relative;
+    	background-repeat: no-repeat;
+		margin: -3px 0 -20px 0;
+		display: flex;
+ 		flex-wrap: wrap;
+ 		box-sizing: border-box;
+		flex-direction: row;
+		width: 100%;
+		background-size: cover;
+		position: relative;
+}
+}
+
+
+
+/*This is the text box on the left of the splash that holds the splash header and sub text*/
+
+
+@media screen and (max-width: 700px) { 
+.splash-text-box {
+		position: relative;
+		flex: 100%;
+		padding: 10px 10px 0px 0px;
+		box-sizing: border-box;
+		text-align: right;
+}
+}
+
+@media screen and (min-width: 700px) { 
+.splash-text-box {
+		z-index: 5;
+		position: relative;
+		flex: 65%;
+		padding: 10px 30px 0px 0px;
+		box-sizing: border-box;
+		text-align: right;
+}
+}
+
+
+@media screen and (max-width: 700px) { 
+.splash-image {
+		z-index: 5;
+		position: relative;
+		text-align: left;
+		flex: 25%;
+		width: 250px;
+		padding: 0px;
+		box-sizing: border-box;
+		margin: 0px 0px 0px 10px;
+}
+}
+
+/*This is the image on the right of the content block*/
+@media screen and (min-width: 700px) { 
+.splash-image {
+		z-index: 5;
+		position: relative;
+		text-align: center;
+		flex: 35%;
+		padding: 0px;
+		box-sizing: border-box;
+		margin-top: 0px;
+}
+}
+
+.splash-heading { 
+  font-family: 'Mulish', Arial, Helvetica, sans-serif;
+  color: white;
+  font-weight: 300;
+  text-shadow: 0 0 8px black;
+
+}
+
+@media screen and (max-width: 700px) {
+	.splash-heading {
+      font-size: 2.0em;
+      line-height: 1.1;
+      margin: 10px 0;
+  }
+}
+
+@media screen and (min-width: 700px) {
+	.splash-heading {
+      font-size: 4.3em;
+      line-height: 1.3;
+      margin: 40px 0 10px;
+  }
+}
+
+.splash-sub {
+  font-family: 'Arvo', Georgia, serif;
+  color: #fff;
+  margin: 15px 0;
+	/*text-shadow: 0px 0px 10px #fff;*/
+}
+
+@media screen and (max-width: 700px) {
+	.splash-sub {
+		font-size: 1.2em;
+		line-height: 1.5;
+		font-weight: 400;
+  }
+}
+@media screen and (min-width: 700px) {
+	.splash-sub {
+		font-size: 1.5em;
+		line-height: 1.3;
+		font-weight: 400;
+  }
+}
+
+
+/*This is the angled bar at the bottom of the intro splash block*/
+
+#splash-bar {
+	margin-top: -50px;
+	width: 100%;
+	background-color: #00FFFF;
+	/*background: url(wp-content/uploads/2020/01/Gray-to-green-catalyst-banner-1.svg) right top;*/
+	height:100px;	
+	box-shadow: 0 8px 7px rgba(85, 84, 84, 0.4);
+	position: relative;
+	z-index: 0;
+	margin-bottom: 40px;
+
+	-webkit-transform: skewY(-3deg);
+ 	 -moz-transform: skewY(-3deg);
+ 	 -ms-transform: skewY(-3deg);
+ 	 -o-transform: skewY(-3deg);
+ 	 transform: skewY(-3deg);
+
+}
+
+#brikchain {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
+
+	
+</style>	
+
+</head>
+							  
+											  
+<BODY id="full-page">
+
+	<div id="load-background">
+
+<!-- MENU BAR-->	
+         
+		<?php include 'menu-bar.php';?>
+
+<!--SPLASH SECTION-->
+
+	
+<div class="splash-content-block">
+	<div class="splash-text-box">
+		<div class="splash-heading"><br>Brikchain Explorer</div>
+		<div class="splash-sub">Search the Full Brikcoin Blockchain</div>
+	</div>
+	<div class="splash-image"><img src="https://s3-eu-west-1.amazonaws.com/assets.knack-eu.com/assets/5abaea5dfdbfad4d03858fe2/5b4c1165d44d5804c418a27e/original/brikcoinchaintransparency.png" style="width: 70%;"></div>	
+</div>
+<div id="splash-bar"></div>
+
+
+
+<!-- PAGE CONTENT-->
+
+<a name="top"></a>
+
+<div id="main-content">
+<!-- The flexible grid (content) -->
+	
+
+			<div class="lead-page-paragraph">
+				
+				<p>The Global Ecobrick Alliance maintains the Brikcoin blockchain and provides the Brikchain Explorer tool as a means to search the full chain of transactions and ecobricks.</p>
+			</div>
+
+			<div class="page-paragraph">
+				  
+
+				<p>For the moment here's a full table of all the current birkcoin transactions.  Not that this feature is still under development!</p>
+
+			</div>
+				
+            <?php include 'db.php';?>
+		
+			<?php
+
+
+				$sql = "SELECT tran_id, tran_name, block_tran_type, individual_amt FROM brk_transaction";
+				$result = $conn->query($sql);
+
+				if ($result->num_rows > 0) {
+				
+					echo'<table id="brikchain">';
+					echo "<tr><th>ID</th><th>Transaction Name</th><th>Block Type</th><th>Amount</th></tr>";
+				// output data of each row
+				while($row = $result->fetch_assoc()) {
+					
+					echo "<tr><td>".$row["tran_id"]."</td><td>".$row["tran_name"]."</td><td>".$row["block_tran_type"]."</td></td>".$row["individual_amt"]."</td></tr>";
+				  }
+				  echo "</table>";
+				} else {
+				  echo "0 results";
+				}
+				$conn->close();
+				?>
+					
+
+					
+		</div>
+
+		
+	
+
+
+
+
+
+	<!--FOOTER STARTS HERE-->
+
+
+	<?php include 'footer.php'; ?>
+
+
+	<!--FOOTER ENDS HERE-->
+</div>
+</body>
+</html>
+
