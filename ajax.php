861643b93aad07bd7207d479428f145c11cd703d
Russell <russmaier@gmail.com>
2022-02-09 20:27:48 +0800

    crazy try

--- ajax.php
+++ ajax.php
@@ -1,25 +1,64 @@
 
-			
-			
-		<?php include 'db.php';?>
-		
+
+
         <?php
-
-
-            $sql = "SELECT tran_id, tran_name, block_tran_type, individual_amt, sender_ecobricker FROM brk_transaction";
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-            
-               
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                
-                echo "<tr><td>".$row["tran_id"]."</td><td>".$row["tran_name"]."</td><td>".$row["sender_ecobricker"]."</td><td>".$row["block_tran_type"]."</td><td>".$row["individual_amt"]."</td></tr>";
-              }
-         
-            } else {
-              echo "0 results";
-            }
-            $conn->close();
-            ?>
+ 
+/*
+ * DataTables example server-side processing script.
+ *
+ * Please note that this script is intentionally extremely simple to show how
+ * server-side processing can be implemented, and probably shouldn't be used as
+ * the basis for a large complex system. It is suitable for simple use cases as
+ * for learning.
+ *
+ * See http://datatables.net/usage/server-side for full details on the server-
+ * side processing requirements of DataTables.
+ *
+ * @license MIT - http://datatables.net/license_mit
+ */
+ 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Easy set variables
+ */
+ 
+// DB table to use
+$table = 'brk_transaction';
+ 
+// Table's primary key
+$primaryKey = 'tran_id';
+ 
+// Array of database columns which should be read and sent back to DataTables.
+// The `db` parameter represents the column name in the database, while the `dt`
+// parameter represents the DataTables column identifier. In this case simple
+// indexes
+$columns = array(
+    array( 'db' => 'tran_name', 'dt' => 0 ),
+    array( 'db' => 'block_tran_type',  'dt' => 1 ),
+    array( 'db' => 'individual_amt',   'dt' => 2 ),
+    array( 'db' => 'sender_ecobricker',     'dt' => 3 ),
+    array(
+        'db'        => 'send_dt',
+        'dt'        => 4,
+        'formatter' => function( $d, $row ) {
+            return date( 'jS M y', strtotime($d));
+        }
+    ),
+    array(
+        'db'        => 'block_amt',
+        'dt'        => 5,
+        'formatter' => function( $d, $row ) {
+            return '$'.number_format($d);
+        }
+    )
+);
+ 
+// SQL server connection information
+$sql_details = array(
+    'user' => 'ecobricks_russs',
+    'pass' => '12345',
+    'db'   => 'ecobricks_gobrik_msql_db',
+    'host' => 'localhost'
+);
+ 
+		
+			
