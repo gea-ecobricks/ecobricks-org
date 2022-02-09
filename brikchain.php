52ae16c560e0fa240cb47ea7651c25f01058903f
Russell <russmaier@gmail.com>
2022-02-09 19:06:24 +0800

    maybe5

--- brikchain.php
+++ brikchain.php
@@ -281,6 +281,7 @@ _END;?>
 				
             <?php include 'db.php';?>
 		
+			<table id="table_id" class="display"><tr><th>ID</th><th>Transaction Name</th><th>Sender</th><th>Block Type</th><th>Amount</th></tr>
 			<?php
 
 
@@ -289,20 +290,21 @@ _END;?>
 
 				if ($result->num_rows > 0) {
 				
-					echo'<table id="table_id" class="display"><tr><th>ID</th><th>Transaction Name</th><th>Sender</th><th>Block Type</th><th>Amount</th></tr>';
+				
 				
 				// output data of each row
 				while($row = $result->fetch_assoc()) {
 					
 					echo "<tr><td>".$row["tran_id"]."</td><td>".$row["tran_name"]."</td><td>".$row["sender_ecobricker"]."</td><td>".$row["block_tran_type"]."</td><td>".$row["individual_amt"]."</td></tr>";
 				  }
-				  echo "</table>";
+			
 				} else {
 				  echo "0 results";
 				}
 				$conn->close();
 				?>
 					
+			</table>
 
 					<br><br>
 
