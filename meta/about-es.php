bab48679649f443b8d7e823f57b4c5dcf9378582
Russell <russmaier@gmail.com>
2022-02-18 17:26:01 +0800

    upload

---
+++ meta/about-es.php
@@ -0,0 +1,21 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Sobre Nosotros | Ecobricks.org</title>
+
+<meta name="keywords" content="global ecobrick alliance, GEA, gea, ecobricks.org, earth enterprise, about us, ecobrick company, ecobrick organization, ecobrick NGO, earthen, principled.">
+
+<meta name="description" content="The Global Ecobrick Alliance is an Earth Enterprise accelerating local and global plastic transition through earthen principled ecobricking">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://www.ecobricks.org/es/about.php"/>
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="Sobre Nosotros | Ecobricks.org">
+  	<meta property="og:description"   content="The Global Ecobrick Alliance is an Earth Enterprise accelerating local and global plastic transition through earthen principled ecobricking."/>
+  	<meta property="og:image"         content="pngs/gea-perspective-1000px.png"/>
+  	<meta property="og:image:width" content="1000px" />
+	<meta property="og:image:height" content="1039px" />
+  	<meta property="og:image:alt"     content="Our GEA seven circle, mandalic, hexagonal logo that resembles a milstein ecobrick module with each color of the spectrum represented and progressing."/>
+	  <meta property="og:locale" content="es_ES" />
+
+	
