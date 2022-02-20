b2df91a8e1a21baeacf2bed85e5dd705e45de546
Russell <russmaier@gmail.com>
2022-02-20 18:38:44 +0800

    buillld

---
+++ meta/build-en.php
@@ -0,0 +1,21 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Ecobrick Applications | Ecobricks.org</title>
+
+<meta name="keywords" content="plastic sequestration, recycling, alternative, sequestration of plastic, plastic offsetting, aes plastic, carbon sequestration.">
+
+<meta name="description" content="The theory, criteria and standards of plastic sequestration.">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://ecobricks.org/en/build.php"/>
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="Ecobrick Applications | Ecobricks.org">
+  	<meta property="og:description"   content="The theory, criteria and standards of plastic sequestration."/>
+  	<meta property="og:image"         content="webp/build-blue-450px.webp"/>
+  	<meta property="og:image:width" content="1000" />
+	<meta property="og:image:height" content="1000" />
+  	<meta property="og:image:alt"     content="A metaphorical road heading far out into the distance of time"/>
+	  <meta property="og:locale" content="en_GB" />
+
+	
