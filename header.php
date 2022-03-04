d17993664531beb7d2ca1702cf38e7c5f986fe83
Russell <russmaier@gmail.com>
2022-03-04 13:46:49 +0800

    arc off

--- header.php
+++ header.php
@@ -95,9 +95,9 @@
 
 	
 
-<!-- Arc CDN Script without delay-->
+<!-- Arc CDN Script without delay
 <script async src="https://arc.io/widget.min.js#Z7EC7Cze"></script>
-
+-->
 
 <!-- Inline styling to lay out the most important part of the page for first load view-->
 
