79b2cc50225992ce6d4f6760c3b50e80af2f3db3
Russell <russmaier@gmail.com>
2022-03-06 14:41:45 +0800

    sdf

--- meta/welcome-en.php
+++ meta/welcome-en.php
@@ -1,5 +1,5 @@
 
-    <title>Ecobrick Plastic Transition  | Ecobricks.org</title>
+    <title>Ecobrick Powered Plastic Transition  | Ecobricks.org</title>
 
 <meta name="keywords" content="eco, brick, ecobricks, eco-bricks, eco bricks, eco briks, briks, plastic, plastic management, carbon sequestration, earthen ethics, plastic, recycling alternatives, incineration, plastic offsets, plastic solutions, upcycling, plastic sequestration, recycling alternative, incineration alternative, preventing plastic pollution, plastic pollution, regenerative plastic solution, waste, waste management, plastic waste, non-capital, zero-waste, net-zero, plastic sequestration"> 
 
@@ -10,7 +10,7 @@
 
   <meta property="og:url"           content="https://www.ecobricks.org/en/welcome.php">
   <meta property="og:type"          content="website">
- <meta property="og:title"         content="Ecobrick Plastic Transition | Ecobricks.org">
+ <meta property="og:title"         content="Ecobrick Powered Plastic Transition | Ecobricks.org">
   <meta property="og:description"   content="Ecobricks prevent plastic from becoming trash, waste, recycling, incineration or pollution.  Eco bricks enable non-capital, zero-waste, net-zero, plastic sequestration."/>
   <meta property="og:image"         content="https://www.ecobricks.org/pngs/social-banner2.png"/>
   <meta property="og:image:width" content="1200" />
--- welcome.php
+++ welcome.php
@@ -252,7 +252,7 @@ _END;?>
 	  
 <div id="content-sect2">
 	<div id="main-content">
-		<div class="lead-graphic"><img src="webp/earth-service-700px.webp" width="100%" height="96%" alt="eco bricks follow the Earth's example." loading="lazy">
+		<div class="lead-graphic"><img src="webp/earth-service-700px.webp" width="100%" height="96%" alt="eco bricks follow the Earth's example." max-width="700px" loading="lazy">
 		</div>
 		<div class="big-header">Follow the Earth's Example</div>
         <!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
@@ -267,7 +267,7 @@ _END;?>
 
 <div id="content-sect3">
 	<div id="main-content">
-		<div class="lead-graphic"><img src="webp/spiral-circular-600px.webp" width="100%" height="90%" alt="eco brick plastic brick building" loading="lazy"></div>
+		<div class="lead-graphic"><img src="webp/spiral-circular-800px.webp" width="100%" max-width="777px" height="90%" alt="eco brick plastic brick building" loading="lazy"></div>
 		<div class="big-header">The Problem is the Solution.</div>
 		<!--<div class="lead-paragraph">Using Ecobricked Plastic we can Build our Greenest Visions.</div>-->
 		<div class="sub-text"><p>Ecobricks turn used plastic into reusable building blocks ideal for circular & spiral building applications.  Use them make modules, home furniture, play parks, and food-forest gardens.  Using the problem, we can build our greenest visions.</p>
