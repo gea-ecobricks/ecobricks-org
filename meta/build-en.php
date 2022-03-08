213c7ef1c28d8ea0100220813e2faa1ac9ac8b26
Russell <russmaier@gmail.com>
2022-03-08 16:59:14 +0800

    update alll

--- meta/build-en.php
+++ meta/build-en.php
@@ -1,8 +1,8 @@
 <!-- Meta tags for page display and search engine listing-->
 
-<title>Applications | Ecobricks.org</title>
+<title>Ecobrick Building Guide | Ecobricks.org</title>
 
-<meta name="keywords" content="plastic sequestration, recycling, alternative, sequestration of plastic, plastic offsetting, aes plastic, carbon sequestration.">
+<meta name="keywords" content="ecobrick, eco brick, eco, brick, building, guide, applications, constructions, modules, earth building, food forest play parks, planters, earth benches, garden walls, earthen structures, plastic sequestration, silicone modules, milstein modules, open spaces, dieleman modules.">
 
 <meta name="description" content="What can you build with ecobricks?">
 	
@@ -10,7 +10,7 @@
 
 	<meta property="og:url"           content="https://ecobricks.org/en/build.php"/>
 	<meta property="og:type"          content="website">
- 	<meta property="og:title"         content="Applications | Ecobricks.org">
+ 	<meta property="og:title"         content="Ecobrick Building Guide | Ecobricks.org">
   	<meta property="og:description"   content="What can you build with Ecobricks"/>
   	<meta property="og:image"         content="pngs/build-banner-1000px.png"/>
   	<meta property="og:image:width" content="1200" />
