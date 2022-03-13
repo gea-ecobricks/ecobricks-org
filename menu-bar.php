921400e181e7295a46a66858790371f3fd477781
Russell <russmaier@gmail.com>
2022-03-13 17:47:29 +0800

    ggg

--- menu-bar.php
+++ menu-bar.php
@@ -12,14 +12,14 @@
         <div class="dropdown-lang">
             <button class="dropbtn-lang">ENG</button>
             <div class="dropdown-content-language">
-               <!-- <a href='https://ecobricks.org/en/<?php echo ($name); ;?>' onclick='javascript:Localize.setLanguage("en")'>🇬🇧 ENG</a>
+               <a href='https://ecobricks.org/en/<?php echo ($name); ;?>' onclick='javascript:Localize.setLanguage("en")'>🇬🇧 ENG</a>
                 <a href='https://ecobricks.org/id/<?php echo ($name); ;?>' onclick='javascript:Localize.setLanguage("id")'>🇮🇩 IND</a>
-                <a href='https://ecobricks.org/es/<?php echo ($name); ;?>' onclick='javascript:Localize.setLanguage("es")'>🇪🇸 ESP</a>-->
+                <a href='https://ecobricks.org/es/<?php echo ($name); ;?>' onclick='javascript:Localize.setLanguage("es")'>🇪🇸 ESP</a>
             
-
+<!--
                 <a href='https://www.ecobricks.org/en/<?php echo ($name); ;?>' >🇬🇧 ENG</a>
                 <a href='https://www.ecobricks.org/id/<?php echo ($name); ;?>'>🇮🇩 IND</a>
-                <a href='https://www.ecobricks.org/es/<?php echo ($name); ;?>'>🇪🇸 ESP</a>
+                <a href='https://www.ecobricks.org/es/<?php echo ($name); ;?>'>🇪🇸 ESP</a>-->
                 
 <a href="javascript:void(0);" style="font-size:15px;" class="icon-lang" onclick="languageMenu()"></a>
                 
