c1fbf4f6a8484f23d6418a074057c08fc75bdd7e
Russell <russmaier@gmail.com>
2022-03-13 19:15:52 +0800

    yikes

--- ajax-ecobricks.php
+++ ajax-ecobricks.php
@@ -2,89 +2,88 @@
 
         <?php
  
- /*
-  * DataTables example server-side processing script.
-  *
-  * Please note that this script is intentionally extremely simple to show how
-  * server-side processing can be implemented, and probably shouldn't be used as
-  * the basis for a large complex system. It is suitable for simple use cases as
-  * for learning.
-  *
-  * See http://datatables.net/usage/server-side for full details on the server-
-  * side processing requirements of DataTables.
-  *
-  * @license MIT - http://datatables.net/license_mit
-  */
-  
- /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  * Easy set variables
-  */
-  
- // DB table to use
- $table = 'tb_brk_transaction';
-  
- // Table's primary key
- $primaryKey = 'tran_id';
-  
+/*
+ * DataTables example server-side processing script.
+ *
+ * Please note that this script is intentionally extremely simple to show how
+ * server-side processing can be implemented, and probably shouldn't be used as
+ * the basis for a large complex system. It is suitable for simple use cases as
+ * for learning.
+ *
+ * See http://datatables.net/usage/server-side for full details on the server-
+ * side processing requirements of DataTables.
+ *
+ * @license MIT - http://datatables.net/license_mit
+ */
  
- // Array of database columns which should be read and sent back to DataTables.
- // The `db` parameter represents the column name in the database, while the `dt`
- // parameter represents the DataTables column identifier. In this case simple
- // indexes
- $columns = array(
-   array( 'db' => 'tran_id',     'dt' => 0 ),
-   array(
-     'db'        => 'send_dt',
-     'dt'        => 1,
-     'formatter' => function( $d, $row ) {
-         return date( 'jS M y', strtotime($d));
-     }
- ),
-     array( 'db' => 'sender_ecobricker',     'dt' => 2 ),
-     array( 'db' => 'receiver_or_receivers',     'dt' => 3 ),
-     array( 'db' => 'block_tran_type', 'dt' => 4 ),
-     array( 'db' => 'ecobrick_serial_no',     'dt' => 5 ),
-    
-     array(
-         'db'        => 'block_amt',
-         'dt'        => 6,
-         'formatter' => function( $d, $row ) {
-             return number_format($d,2).'ß';
-         }
-     ),
-     array(
-         'db'        => 'individual_amt',
-         'dt'        => 7,
-         'formatter' => function( $d, $row ) {
-             return number_format($d,2).'ß';
-         }
-     ),
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Easy set variables
+ */
+ 
+// DB table to use
+$table = 'tb_brk_transaction';
+ 
+// Table's primary key
+$primaryKey = 'tran_id';
+ 
+
+// Array of database columns which should be read and sent back to DataTables.
+// The `db` parameter represents the column name in the database, while the `dt`
+// parameter represents the DataTables column identifier. In this case simple
+// indexes
+$columns = array(
+  array( 'db' => 'tran_id',     'dt' => 0 ),
+  array(
+    'db'        => 'send_dt',
+    'dt'        => 1,
+    'formatter' => function( $d, $row ) {
+        return date( 'jS M y', strtotime($d));
+    }
+),
+    array( 'db' => 'sender_ecobricker',     'dt' => 2 ),
+    array( 'db' => 'receiver_or_receivers',     'dt' => 3 ),
+    array( 'db' => 'block_tran_type', 'dt' => 4 ),
+    array( 'db' => 'ecobrick_serial_no',     'dt' => 5 ),
    
- );
- 
+    array(
+        'db'        => 'block_amt',
+        'dt'        => 6,
+        'formatter' => function( $d, $row ) {
+            return number_format($d,2).'ß';
+        }
+    ),
+    array(
+        'db'        => 'individual_amt',
+        'dt'        => 7,
+        'formatter' => function( $d, $row ) {
+            return number_format($d,2).'ß';
+        }
+    ),
   
- // SQL server connection information
- $sql_details = array(
-     'user' => 'ecobricks_russs',
-     'pass' => '1ecobricks!',
-     'db'   => 'ecobricks_gobrik_msql_db',
-     'host' => 'localhost'
- );
-  
+);
+
  
+// SQL server connection information
+$sql_details = array(
+    'user' => 'ecobricks_russs',
+    'pass' => '1ecobricks!',
+    'db'   => 'ecobricks_gobrik_msql_db',
+    'host' => 'localhost'
+);
  
- /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  * If you just want to use the basic configuration for DataTables with PHP
-  * server-side, there is no need to edit below this line.
-  */
-  
- require( 'ssp.class.php' );
-  
- echo json_encode(
-     SSP::simple( $_GET, $sql_details, $table, $primaryKey, $columns )
- );
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * If you just want to use the basic configuration for DataTables with PHP
+ * server-side, there is no need to edit below this line.
+ */
  
- ?>
-         
-             
+require( 'ssp.class.php' );
  
+echo json_encode(
+    SSP::simple( $_GET, $sql_details, $table, $primaryKey, $columns )
+);
+
+?>
+		
+			
