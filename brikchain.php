7893d2b1ded02e0a4c8606d482ff6d4145934a95
Russell <russmaier@gmail.com>
2022-03-25 22:07:19 +0800

    g

--- brikchain.php
+++ brikchain.php
@@ -217,10 +217,7 @@ _END;?>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
 
 
-
-
 <script>
-
 $(document).ready(function() {
     $('#brikchain').DataTable( {
 		"processing": true,
