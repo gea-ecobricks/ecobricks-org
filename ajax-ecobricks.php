3e140d54cfb812eefff4596e745c602215264787
RakshaEcobricker <72146851+RakshaEcobricker@users.noreply.github.com>
2022-04-30 21:03:43 +0100

    Update ajax-ecobricks.php
    Change to final formatter for new ecobrick-detail serial number selection

--- ajax-ecobricks.php
+++ ajax-ecobricks.php
@@ -87,7 +87,7 @@ $columns = array(
     array( 'db' => 'serial_no',     
         'dt' => 7,
         'formatter' => function( $d, $row ) {
-            return '<a href="brik.php?='.($d).'">'.($d).'</a>';
+            return '<a href="ecobrick-detail.php?serial_no='.($d).'">'.($d).'</a>';
         }
     ),
   
