1acf72411ba096f3deb52ef1c73cc34a62001509
russmaier <russmaier@gmail.com>
2022-05-06 20:37:22 +0800

    gh

--- principles.php
+++ principles.php
@@ -19,7 +19,7 @@ echo <<<_END
 
 _END;?>
 
-<?php require_once ("meta/brikcoins-$lang.php");?>
+<?php require_once ("meta/principles-$lang.php");?>
 
 	<?php require_once ("header.php");?>
 
@@ -29,93 +29,133 @@ _END;?>
 <!-- Arc CDN Script without delay-->
 <script async src="https://arc.io/widget.min.js#Z7EC7Cze"></script>
 
+
+
+
 <STYLE>
 
-/*This sets up the the first splash content block that is unique to the top of each page*/
 
 	
 @media screen and (max-width: 700px) { 
-	#splash-content-block {
+	.splash-content-block {
 		text-align: left;
-		height: 50vh;
-		padding: 10vh 5% 25px 5%;
+		height: 73vh;
+		padding: 50px 5% 25px 5%;
 		margin-bottom: 0px;
 		z-index: 5;
+       
 		background: url(webp/principles-banner-1600px.webp) top;
     	background-repeat: no-repeat;
-    	background-size: contain;
+    	background-size: cover;
 		background-color: #667C09;
-		margin-top: 50px;
 
-		background-repeat: no-repeat;
-		background-size: cover;
 		margin-top: 45px;
 		width: 100%;
 		display: flex;
  		box-sizing: border-box;
 		flex-direction: column;
-		position: relative;
+	position: relative;
 
 	}
 }
 
 
 @media screen and (min-width: 700px) { 
-	#splash-content-block {
+	.splash-content-block {
 		text-align: left;
 		height: 60vh;
-		padding: 20vh 7% 50px 7%;
+		padding: 50px 7% 50px 7%;
 		z-index: 5;
 		position: relative;
+		
+        
 		background: url(webp/principles-banner-1600px.webp) top;
+        background-color: #667C09;
+		background-size: contain;
+		
     	background-repeat: no-repeat;
-    	background-size: contain;
-		background-color: #667C09;
-		margin: 0 0 -20px 0;
-
-		background-repeat: no-repeat;
 		margin: -3px 0 -20px 0;
 		display: flex;
- 		flex-wrap: wrap;
- 		box-sizing: border-box;
-		flex-direction: row;
-		width: 100%;
-		background-size: cover;
-		position: relative;
+ 		 flex-wrap: wrap;
+ 		 box-sizing: border-box;
+		  flex-direction: row;
+		  width: 100%;
+		  background-size: cover;
+
+	position: relative;
+
 
 }
 }
+
+
+@media screen and (min-width: 700px) { 
+.splash-image {
+  z-index: 5;
+  position: relative;
+  text-align: center;
+  flex: 35%;
+	padding: 0px;
+	box-sizing: border-box;
+	margin: auto;
+}
+}
+
+
+@media screen and (min-width: 700px) { 
+.splash-box {
+  z-index: 5;
+  position: relative;
+  flex: 65%;
+	padding: 10px 30px 0px 0px;
+	box-sizing: border-box;
+	text-align: right;
+    margin: auto;
+}
+}
+
 
 
 @media screen and (max-width: 700px) { 
-#splash-box {
+.splash-image {
   z-index: 5;
   position: relative;
-  width: 100%;
   text-align: left;
+  flex: 25%;
+  width: 250px;
+	padding: 0px;
+	box-sizing: border-box;
+	margin: 0px 0px 0px 10px;
 }
 }
 
-@media screen and (min-width: 700px) { 
-#splash-box {
-  z-index: 5;
-  position: relative;
-  width: 80%;
+
+
+@media screen and (max-width: 700px) { 
+.splash-box {
+	position: relative;
+    flex: 100%;
+	padding: 10px 10px 0px 0px;
+	box-sizing: border-box;
+	text-align: right;
+    margin: auto;
 }
 }
+
 
 
 .splash-heading { 
-  font-family: 'Mulish', Arial, Helvetica, sans-serif;
-  color: black;
-  font-weight: 300;
-  text-shadow: 0 0 10px #fff;
+    /*font-family: 'Mulish', Arial, Helvetica, sans-serif;*/
+    font-family: Arvo, serif;
+  color: white;
+  font-weight: 500;
+  text-shadow: 0 0 8px black;
 
 }
 
 @media screen and (max-width: 700px) {
 	.splash-heading {
-      font-size: 3.8em;
+      font-size: 3.0em;
       line-height: 1.1;
       margin: 10px 0;
   }
@@ -123,7 +163,7 @@ _END;?>
 
 @media screen and (min-width: 700px) {
 	.splash-heading {
-      font-size: 5em;
+      font-size: 4em;
       line-height: 1.3;
       margin: 40px 0 10px;
   }
@@ -131,58 +171,58 @@ _END;?>
 
 
 .splash-sub {
-  font-family: 'Arvo', Georgia, serif;
+  font-family: 'Mulish', Arial, Helvetica, sans-serif;
   color: #fff;
   /*text-shadow: 0px 0px 10px #fff;*/
   margin: 15px 0;
-  text-shadow: 0 0 8px black;
+  text-shadow: 0 0 7px black;
 }
 
 @media screen and (max-width: 700px) {
 	.splash-sub {
-		font-size: 1.4em;
-		line-height: 1.5;
+		font-size: 1.9em;
+		line-height: 1.3;
 		font-weight: 400;
   }
 }
 @media screen and (min-width: 700px) {
 	.splash-sub {
-		font-size: 2.3em;
+		font-size: 2.5em;
 		line-height: 1.3;
 		font-weight: 400;
-		width: 60%;
   }
-}
-
-
-@media screen and (max-width: 769px) {
-
-#splash-box img {
-
-	width: 60%;
-	margin-left: 40%;
-}
 }
 
 
 #splash-bar {
 	margin-top: -50px;
 	width: 100%;
-	background-color: #667C09;
-	height:100px;	
-	box-shadow: 0 8px 7px rgba(85, 84, 84, 0.4);
+	
+	height:80px;	
+	
 	position: relative;
-	z-index: 1;
+	z-index: 0;
+
+box-shadow: 0 8px 7px rgba(85, 84, 84, 0.4);
+    background-color: #2A91DA;
+
 	-webkit-transform: skewY(-3deg);
   -moz-transform: skewY(-3deg);
   -ms-transform: skewY(-3deg);
   -o-transform: skewY(-3deg);
   transform: skewY(-3deg);
-
 	margin-bottom: 40px;
+
 }
+
+hr {border: gray;
+border-style: dashed;
+border-width: 1px;
+margin-top: 31px;
+margin-bottom: 31px;}
 	
 </style>	
+	
 
 </head>
 							  
