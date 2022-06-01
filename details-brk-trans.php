ecde6fce5c8d7862f47a7606efcba45fbef7fd39
russmaier <russmaier@gmail.com>
2022-06-01 18:19:59 +0800

    godohng

--- details-brk-trans.php
+++ details-brk-trans.php
@@ -1,314 +1,41 @@
-<?php require_once ("lang.php");
-<<<_END
-<!DOCTYPE html>
-<!-- this grabs the language identifier for the page so that it can used in the meta and canonical url variables test-->
-<!-- https://github.com/gea-ecobricks/ecobricks-org  -->
-<html lang="$lang">
-<HEAD>
-<!--Image files to preload that are unique to this page-->
-<link rel="preload" as="image" href="logos/gea-horizontal.svg">
-<link rel="preload" as="image" href="svgs/eb-blue.svg">
-_END;
-?>
-<!-- PAGE CONTENT  We need a new lang file here -->
-<?php require_once ("meta/ecobrick-detail-$lang.php");?>
+<!-- This calls GEA typography -->
 
-<?php require_once ("header.php");?>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-<!-- CUSTOM PAGE SCRIPTS-->
+<!-- Mulish loads first for first page view-->
 
-<script src="accordion-scripts.js"></script> 
+<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;500&display=swap" rel="stylesheet" media="print" onload="this.media='all'"> 
 
-<STYLE>
+<!-- Arvo loads very last to speed up first load.  See the script in the footer that triggers this  -->
 
-/*This sets up the the first splash content block that is unique to the top of each page*/
-
-@media screen and (max-width: 700px) { 
-	.splash-content-block {
-
-		background: url(svgs/cloud-back.svg) top;
-		background-color: #00FFFF;
-
-		text-align: left;
-		height: 70vh;
-		padding: 50px 5% 25px 5%;
-		margin-bottom: 0px;
-		z-index: 5;
-    	background-repeat: no-repeat;
-		background-size: cover;
-		margin-top: 45px;
-		width: 100%;
-		display: flex;
- 		box-sizing: border-box;
-		flex-direction: column;
-		position: relative;
-
-	}
-}
-
-@media screen and (min-width: 700px) { 
-	.splash-content-block {
-
-		background: url(svgs/cloud-back.svg) bottom;
-		background-color: #00FFFF;
-
-		text-align: left;
-		height: 60vh;
-		padding: 50px 7% 50px 7%;
-		z-index: 5;
-		position: relative;
-    	background-repeat: no-repeat;
-		margin: -3px 0 -20px 0;
-		display: flex;
- 		flex-wrap: wrap;
- 		box-sizing: border-box;
-		flex-direction: row;
-		width: 100%;
-		background-size: cover;
-		position: relative;
-}
-}
-
-/*This is the text box on the left of the splash that holds the splash header and sub text*/
-
-@media screen and (max-width: 700px) { 
-.splash-text-box {
-		position: relative;
-		flex: 100%;
-		padding: 10px 10px 0px 0px;
-		box-sizing: border-box;
-		text-align: right;
-}
-}
-
-@media screen and (min-width: 700px) { 
-.splash-text-box {
-		z-index: 5;
-		position: relative;
-		flex: 65%;
-		padding: 10px 30px 0px 0px;
-		box-sizing: border-box;
-		text-align: right;
-}
-}
-
-@media screen and (max-width: 700px) { 
-.splash-image {
-		z-index: 5;
-		position: relative;
-		text-align: left;
-		flex: 25%;
-		width: 250px;
-		padding: 0px;
-		box-sizing: border-box;
-		margin: 0px 0px 0px 10px;
-}
-}
-
-/*This is the image on the right of the content block*/
-@media screen and (min-width: 700px) { 
-.splash-image {
-		z-index: 5;
-		position: relative;
-		text-align: center;
-		flex: 35%;
-		padding: 0px;
-		box-sizing: border-box;
-		margin-top: 0px;
-}
-}
-
-.splash-heading { 
-  font-family: 'Mulish', Arial, Helvetica, sans-serif;
-  color: white;
-  font-weight: 300;
-  text-shadow: 0 0 8px black;
-
-}
-
-@media screen and (max-width: 700px) {
-	.splash-heading {
-      font-size: 2.0em;
-      line-height: 1.1;
-      margin: 10px 0;
-  }
-}
-
-@media screen and (min-width: 700px) {
-	.splash-heading {
-      font-size: 5em;
-      line-height: 1.3;
-      margin: 40px 0 10px;
-  }
-}
-
-.splash-sub {
-  font-family: 'Arvo', Georgia, serif;
-  color: #fff;
-  margin: 15px 0;
-	/*text-shadow: 0px 0px 10px #fff;*/
-}
-
-@media screen and (max-width: 700px) {
-	.splash-sub {
-		font-size: 1.2em;
-		line-height: 1.5;
-		font-weight: 400;
-  }
-}
-@media screen and (min-width: 700px) {
-	.splash-sub {
-		font-size: 2em;
-		line-height: 1.3;
-		font-weight: 400;
-  }
-}
-
-/*This is the angled bar at the bottom of the intro splash block*/
-
-#splash-bar {
-	margin-top: -50px;
-	width: 100%;
-	background-color: #00FFFF;
-	/*background: url(wp-content/uploads/2020/01/Gray-to-green-catalyst-banner-1.svg) right top;*/
-	height:100px;	
-	box-shadow: 0 8px 7px rgba(85, 84, 84, 0.4);
-	position: relative;
-	z-index: 0;
-	margin-bottom: 40px;
-
-	-webkit-transform: skewY(-3deg);
- 	 -moz-transform: skewY(-3deg);
- 	 -ms-transform: skewY(-3deg);
- 	 -o-transform: skewY(-3deg);
- 	 transform: skewY(-3deg);
-
-}
-
-</style>	
+<noscript>
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Arvo&display=swap" />
+</noscript>
+		
+<!-- This calls our Github hosted stylesheet - if it gets hopelessly cached and doesn't load, up the version number! -->
+		
+<link rel="stylesheet" type="text/css" href="stylesheet-general.css?v0.14">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.css">
-
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
-
-<script>
-
-$(document).ready(function() {
-    $('#brikchain').DataTable( {
-		"processing": true,
-        "serverSide": true,
-		"ajax": "ajax.php"
-
-    } );
-} );
-
-</script>
-<script>
-
-$(document).ready(function() {
-    $('#ecobricks').DataTable( {
-		"processing": true,
-        "serverSide": true,
-		"ajax": "ajax-ecobricks.php"
-
-    } );
-} );
-
-</script>
-
-<style>
-#brikchain {
-  font-family: 'Mulish', Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-  font-weight: 300;
-}
-
-#brikchain td, #brikchain th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-#brikchain tr:nth-child(even){background-color: #f2f2f2;}
-
-#brikchain tr:hover {background-color: #ddd;}
-
-#brikchain th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #E39009;
-  color: white;
-}
-</style>
-
-<style>
-#ecobricks {
-  font-family: 'Mulish', Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-  font-weight: 300;
-}
-
-#ecobricks td, #ecobricks th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-#ecobricks tr:nth-child(even){background-color: #f2f2f2;}
-
-#ecobricks tr:hover {background-color: #ddd;}
-
-#ecobricks th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #E39009;
-  color: white;
-}
-</style>
-
+ 
 </head>
 
+
+
+							  										  
 <BODY id="full-page">
 
 	<div id="load-background">
 
-<!-- MENU BAR-->	
-
-		<?php include 'menu-bar.php';?>
-
-<!--SPLASH SECTION-->
-
-<div class="splash-content-block">
-	<div class="splash-text-box">
-		<div class="splash-heading"><br>Transaction Explorer Detail</div>
-		<div class="splash-sub">Complete Transaction infomation</div>
-	</div>
-	<!-- div class="splash-image"><img src="https://s3-eu-west-1.amazonaws.com/assets.knack-eu.com/assets/5abaea5dfdbfad4d03858fe2/5b4c1165d44d5804c418a27e/original/brikcoinchaintransparency.png" style="width: 70%;"></div -->	
-	<div class="splash-image"><img src="webp/eb-sky-400px.webp" style="width: 70%;"></div>	
-</div>
-
-<div id="splash-bar"></div>
-
-<!-- PAGE CONTENT -->
-
 <a name="top"></a>
 
 <div id="main-content">
-<!-- The flexible grid (content) -->
-			<div class="lead-page-paragraph">
-				<br><br>
-				<p>The Global Ecobrick Alliance maintains the Ecobrick records and provides the Explorer <br> inline with <a href="principles">our principles</a> of transparency and Earth service.</p>
-			</div>
-			<div class="page-paragraph">
-				<p>Here you can view the full Transaction record</p>
-			</div>
-			<hr>
-			<br><br>
 
+			<div class="page-paragraph">
+				<p>The full overview of the transaction on the Brikcoin blockchain.</a>.</p>
+			
 <!-- The DB connection creator -->
 <?php include 'db.php';?>
 <?php
@@ -363,13 +90,11 @@ if ($result->num_rows > 0) {
 $conn->close();
 
 ?>
+	
+<br><br>
+
 </div>
 
-	<!--FOOTER STARTS HERE-->
-
-	<?php include 'footer.php'; ?>
-
-	<!--FOOTER ENDS HERE-->
 </div>
 </body>
 </html>
--- details-ecobrick.php
+++ details-ecobrick.php
@@ -19,11 +19,7 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-
 </head>
-
-
-
 							  										  
 <BODY id="full-page">
 
