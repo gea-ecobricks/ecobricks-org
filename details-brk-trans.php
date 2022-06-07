0fc594249eeee89e6e01d9ab4800cea3648f9fda
russmaier <russmaier@gmail.com>
2022-06-07 20:39:07 +0800

    if statement

--- details-brk-trans.php
+++ details-brk-trans.php
@@ -72,6 +72,11 @@ if ($result->num_rows > 0) {
 		echo " <p><b>Transaction note:</b> <var>" . $array["tran_sender_note"] . "</var></p>" ;
 		echo " <p><b>Product:</b> <var>" . $array["product"] . "</var></p>" ;
 		//echo " <p>Send date = " . $array["send_dt"] . "</p>" ;
+		
+		if ( isset($array["accomp_payment"]) && $array["accomp_payment"] != '' ) {                                              //|AT4737|GEA|DNC|
+			echo " <p><b>Payment:</b> <var>" . $array["accomp_payment"] . "</var></p>" ;
+ }
+		
 		echo " <p><b>Payment:</b> <var>" . $array["accomp_payment"] . "</var></p>" ;
 		
 		echo " <p><b>Expense type:</b> <var>" . $array["expense_type"] . "</var></p>" ;
