45b871eaac322ed47b760c44f00c18f0956bde77
russmaier <russmaier@gmail.com>
2022-06-17 19:12:02 +0800

    bahasa

--- id/welcome2.php
+++ id/welcome2.php
@@ -1,3 +1,5 @@
+<!--PAGE LANGUAGE:  ENGLISH--> 
+
 <!DOCTYPE html>
 <?php require_once ("lang.php");
 echo <<<_END
@@ -11,17 +13,21 @@ _END;?>
 <?php require_once ("meta/welcome-$lang.php");?>
 
 <!--  Set any page specific graphics to preload-->
-<link rel="preload" as="image" href="logos/gea-horizontal.svg">
-<link rel="preload" as="image" href="svgs/cloud-back.svg">
-<link rel="preload" as="image" href="webp/earthday-2022-400px.webp">
-<link rel="preload" as="image" href="webp/mountain-top2.webp">
+<link rel="preload" as="image" href="https://www.ecobricks.org/logos/gea-horizontal.svg">
+<link rel="preload" as="image" href="https://www.ecobricks.org/svgs/cloud-back.svg">
+<link rel="preload" as="image" href="https://www.ecobricks.org/svgs/ecobrickable-square.svg?v0.4">
+<link rel="preload" as="image" href="https://www.ecobricks.org/webp/mountain-top2.webp">
+<link rel="preload" as="image" href="https://www.ecobricks.org/webp/vision-bottom4.webp">
 
 
-<link rel="stylesheet" type="text/css" href="stylesheet-first-load.css?v0.3">
-<link rel="stylesheet" type="text/css" href="stylesheet-landing.css?v0.57">
+
 <!--This loads the page's header-->
 
 <?php require_once ("header.php");?>
+
+<!--This loads the page's header-->
+
+<?php require_once ("styles-landing.php");?>
 
 </head>
 
@@ -47,12 +53,13 @@ _END;?>
 		<div id="sliders">
 
 		<div id="slider1">
-				<div id="slider-image"><img src="webp/faqs-400px.webp" width="92%" height="100%" alt="How to make an ecobrick"></div>
+		<div id="slider-image"><img src="svgs/ecobrickable-square.svg?v0.3" alt="How to make an ecobrick" width="100%" height="102%" style="min-width:175px;"></div>
 					<div id="slider-textbox">
-					<div id="slider-heading">INDONESIA! Ecobricks Guidelines & Standards</div>
-					<div id="slider-body">Learn about what makes an ecobrick an ecobrick!  Did you know there are several kinds of ecobricks?</div>
+					<div id="slider-heading">Ecobrick Guidelines</div>
+					<div id="slider-date">2022 Updated | Standards</div>
+					<div id="slider-body">Did you know there are several kinds of ecobricks? Learn about what makes an ecobrick an ecobrick!  </div>
 					<div id="slider-link"><a href="/what">About Ecobricks</var></a></div>
-					<div id="slider-date">June 16th, 2022 | Site updates</div>
+						
 				</div>
 			</div>
 
@@ -63,10 +70,10 @@ _END;?>
 				<img src="webp/brikmarket-400px.webp" width="92%" height="100%" alt="Our eco brick market place for arranging trades and droping off ecobricks">
 			</div>
 			<div id="slider-textbox">
-				<div id="slider-heading">The <var>Brik Market</var> is Launched</div>
-				<div id="slider-date">May 2nd, 2022 | GEA Annoucement</div>
+				<div id="slider-heading">The Brik Market is Launched</div>
+				<div id="slider-date">June, 2022 | GEA Annoucement</div>
 				<div id="slider-body">After three years of development, we're excited to launch a place to exchange, trade, barter and drop-off your ecobricks.</div>
-				<div id="slider-link">>> <a href="drop-off.php">The <var>Brik Market</var></a></div>
+				<div id="slider-link">>> <a href="drop-off.php">The Brik Market</a></div>
 
 				</div>
 			</div>
@@ -76,23 +83,24 @@ _END;?>
 				<div id="slider-image"><img src="webp/catalyst.webp" width="92%" height="100%" alt="Our eco brick enterprise program"></div>
 					<div id="slider-textbox">
 					<div id="slider-heading">Plastic Transition Platform for Enterprises</div>
+					<div id="slider-date">GEA Announcements</div>
 					<div id="slider-body">Our Catalyst program for keen green companies, organizations and projects is now live!  Track, disclose and accelerate your plastic transition.</div>
 					<div id="slider-link"><a href="catalyst.php">Learn more</a></div>
-					<div id="slider-date">Feb. 6, 2022 | GEA Announcements</div>
 				</div>
 			</div>
 
 			
 			<div id="slider4">
-				<div class="slider-textbox">
-					<div id="slider-heading">Learn how to build with Ecobricks</div>
-					<div id="slider-date">June 16th, 2022 | GEA Update Content</div>
+			<div id="slider-image"><img src="webp/build-blue-450px.webp" style="width: 85%;"></div>	
+				<div id="slider-textbox">
+					<div id="slider-heading">Build with Ecobricks</div>
+					<div id="slider-date">GEA Updated Content</div>
 					<div id="slider-body">Learn about the various ways to put ecobricks to good use in everything from module to earth construction.</div>
-					<div id="slider-link">>> <a href="build.php">Ecobrick Building</a></div>
+					<div id="slider-link"><a href="build.php">Ecobrick Building</a></div>
 				</div>
-				<div class="slider-image"><img src="webp/build-blue-450px.webp" style="width: 85%;"></div>	
-												
-			</div>		
+			</div>
+
+
 		</div>
 
 	<!--FIRST CONTENT SECTION-->
@@ -100,14 +108,13 @@ _END;?>
 	<div id="content-sect1">
 	 	<div id="main-content">
 			<div id="dolphin-graphic"><img src="svgs/dolphin-top-optimized.svg"width="100%" height="76%" alt="eco bricks help keep plastic out of the ocean" loading="lazy"></div>
-			<div class="big-header">Accelerating Plastic Transition</div>
+			<div class="big-header">Mempercepat Transisi Plastik</div>
 			<div class="sub-text">
-			<p>Together we can keep our plastic out of the oceans.  With ecobricks we can secure plastic to build our greenest visions and ignite regenerative transition.</p>
+			<p>Bersama-sama kita bisa menjaga plastik kita terhindar dari lautan. Dengan ecobrick, kita dapat mengamankan plastik untuk membangun visi kami yang paling hijau dan memicu transisi regeneratif.</p>
 			</div>
-			 <a href="faqs.php" button class="main-button">All About Ecobricks</a>
-			<h6><a href="what.php">What is an ecobrick?</a> | <a href="/how">How to Make?</a> | <a href="/why">Why Make?</a></h6>
+			 <a href="faqs.php" button class="main-button">Semua Tentang Ecobrick</a>
+			<h6><a href="what.php">Apa itu ecobrick?</a> | <a href="/how">Bagaimana membuat? </a> | <a href="/why">Mengapa Membuat?</a></h6>
 		</div>	
-	
 	</div>
 
 	<!--SECOND CONTENT SECTION-->
@@ -117,13 +124,13 @@ _END;?>
 		<div id="main-content">
 			<div class="lead-graphic"><img src="webp/earth-service-700px.webp" width="100%" height="96%" alt="eco bricks follow the Earth's example." max-width="700px">
 			</div>
-			<div class="big-header">Follow the Earth's Example</div>
-			<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
+			<div class="big-header">Mencontoh Bumi</div>
+			
 			<div class="sub-text">
-			<p>Ecobricking follows the Earth's example of concentrating and securing carbon out of the biosphere.  Following the science of plastic degradation and Earthen principles, ecobricking is a non-capital, net-zero means of plastic sequestration.</p>
+			<p>Ecobrick mengikuti contoh bumi dalam memusatkan dan mengamankan karbon dari biosfer. Mengikuti ilmu degradasi plastik dan prinsip-prinsip tanah liat, ecobrick adalah cara tanpa modal dan tanpa modal untuk penyerapan plastik.</p>
 			</div>
-			<a href="sequest.php" button class="main-button">Plastic Sequestration</a>
-			<h6><a href="/ayyew">Ayyew Inspired</a> | <a href="/circular">Circular by Design</a> | <a href="/plastic">Towards Transition</a> | <a href="/principles">Regenerative</a> <!--| <a href="/reports">Reports & Whitepapers</a>--></h6>
+			<a href="sequest.php" button class="main-button">Sekuestrasi Plastik</a>
+			<h6><a href="/ayyew">Terinspirasi dari Ayyew</a> | <a href="/circular">Desain Melingkar</a> | <a href="/plastic">Menuju Transisi </a> | <a href="/principles">Prinsip Regeneratif</a> <!--| <a href="/reports">Reports & Whitepapers</a>--></h6>
 		</div>
 	</div>	
 		
@@ -132,13 +139,13 @@ _END;?>
 	<div id="content-sect3">
 		<div id="main-content">
 			<div class="lead-graphic"><img src="webp/spiral-circular-800px.webp" width="100%" max-width="777px" height="90%" alt="eco brick plastic brick building"></div>
-			<div class="big-header">The Problem is the Solution.</div>
-			<div class="sub-text"><p>Ecobricks turn used plastic into reusable building blocks ideal for circular & spiral building applications.  Use them make modules, home furniture, play parks, and food-forest gardens.  Using the problem, we can build our greenest visions.</p>
+			<div class="big-header">Masalah adalah Solusi</div>
+			<div class="sub-text"><p>Ecobrick (batu bata ramah lingkungan) mengubah plastik bekas menjadi blok bangunan yang dapat digunakan kembali, ideal untuk aplikasi bangunan melingkar & spiral. Gunakan mereka untuk membuat modul, perabot rumah tangga, taman bermain, dan kebun hutan makanan. Dengan menggunakan solusi ini maka kita dapat membangun visi yang paling hijau.</p>
 			</div> 
 
-			<a href="build.php" button class="main-button">Building Applications</a>
+			<a href="build.php" button class="main-button">Membangun Aplikasi</a>
 			
-			<h6><a href="/earth">Earth & Ecobrick Building</a> | <a href="/earth-methods">E&B Methods</a> | <a href="/modules">Milstein Modules</a> | <a href="/circular">Spiral & Circular</a> | <a href="/openspaces">Open Spaces</a> | <a href="/fire">Fire Safety</a></h6>
+			<h6><a href="/earth">Bangunan Tanah & Ecobrick</a> | <a href="/earth-methods"> Metode Bangunan Tanah</a> | <a href="/modules">Modul Milstein</a> | <a href="/circular"> Spiral & Melingkar</a> | <a href="/openspaces">Open Spaces</a> | <a href="/fire">Keamanan Kebakaran</a></h6>
 		</div>
 	</div>
 
@@ -149,18 +156,18 @@ _END;?>
 			<div class="lead-graphic"><img src="webp/balancing-green.webp"  width="100%" height="100%"></div>
 			<div class="lead-graphic"><img src="svgs/aes-brk.svg" width="100%" height="40%" alt="authenticated eco brick sequetration"></div><!--width="300px" height="254px"-->
 			<!--<div class="lead-paragraph"><br>Introducing AES Plastic Offsetting</div>-->
-			<div class="sub-text"><p><br>Fast track your journey to zero-waste through plastic offsetting.  Plastic offsets are directly correlated to authenticated ecobricked plastic through the Brikcoin manual blockchain.</p></div>
-			<a href="https://gobrik.com/#offset" target="_blank" class="main-button">Plastic Offsetting</a>
-			<h6><a href="/brikcoins">Brikcoin Blockchain</a> | <a href="/aes">AES Plastic</a> | <a href="transition.php">Plastic Transition</a> | <a href="about.php">About the GEA</a></h6><br><br>
-			<p>The Brikcoin manual blockchain enables the authentication, valuation and vitalization of ecobricks.<br><a href="https://gobrik.com/#offset" target="_blank">Offset your household or company plastic generation with us.</a></p><br>
+			<div class="sub-text"><p><br>Lacak cepat perjalanan Anda menuju nol limbah melalui penggantian plastik. Offset plastik berkorelasi langsung dengan plastik ecobrick yang diautentikasi melalui blockchain manual Brikcoin.</p></div>
+			<a href="https://gobrik.com/#offset" target="_blank" class="main-button">Plastik Offset</a>
+			<h6><a href="/brikcoins">Brikcoin Blockchain</a> | <a href="/aes">AES Plastik</a> | <a href="transition.php">Transisi Plastik</a> | <a href="about.php">Tentang GEA</a></h6><br><br>
+			<p>Blockchain manual Brikcoin memungkinkan otentikasi, penilaian, dan vitalisasi ecobrick.<br><a href="https://gobrik.com/#offset" target="_blank">Seimbangkan (offset) generasi plastik rumah tangga atau perusahaan Anda bersama kami.</a></p><br>
 			<img src="svgs/3brikcoins.svg" width="30%">
 
 			<br><br>
 			<h4>Eco-brick, eco brick, or ecobrick?</h4>
 
-			<p>Back in the early days of putting plastic into a bottle we called it just that-- plastic bottle bricks!  Then when we realized it was helpful to the <b>eco</b>systems around us, the name changed to "eco bricks" or "eco-brick". Today 'ecobrick' is the <a href="https://en.wikipedia.org/wiki/Ecobricks" target="_blank">Wikipedia recongnized</a> term for the go-to, non-capital, zero-carbon solution for <a href="https://en.wikipedia.org/wiki/plastic_sequestration" target="_blank">plastic sequestration</a></a></p>
-				<p>Learn about the main focus of the ecobrick movement and technology:</p>
-				<a href="transition.php" button class="main-button">Plastic Transition</a>
+			<p>Kembali pada hari-hari awal memasukkan plastik ke dalam botol, kami menyebutnya begitu saja-- batu bata botol plastik! Kemudian ketika kami menyadari itu membantu sistem lingkungan di sekitar kami, namanya berubah menjadi "batu bata ramah lingkungan" atau "batu bata ramah lingkungan". Hari ini 'ecobrick' adalah istilah yang dikenal di <a href="https://en.wikipedia.org/wiki/Ecobricks" target="_blank">Wikipedia </a> untuk solusi tanpa modal, tanpa modal, tanpa karbon untuk <a href="https://en.wikipedia.org/wiki/plastic_sequestration" target="_blank">penyerapan plastik.</a></a></p>
+				<p>Pelajari tentang fokus utama gerakan dan teknologi ecobrick:</p>
+				<a href="transition.php" button class="main-button">Transisi Plastik</a>
 			
 		</div>
 	</div>
