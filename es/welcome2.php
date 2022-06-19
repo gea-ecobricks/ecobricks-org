aff6f83d8903d705d80da42b4a32c8b840c59ad0
russmaier <russmaier@gmail.com>
2022-06-19 22:35:06 +0800

    go spanish

--- es/welcome2.php
+++ es/welcome2.php
@@ -1,4 +1,4 @@
-<!--PAGE LANGUAGE:  ENGLISH--> 
+<!--PAGE LANGUAGE:  SPANISH--> 
 
 <!DOCTYPE html>
 <?php require_once ("lang.php");
@@ -109,12 +109,12 @@ _END;?>
 	<div id="content-sect1">
 	 	<div id="main-content">
 			<div id="dolphin-graphic"><img src="svgs/dolphin-top-optimized.svg"width="100%" height="76%" alt="eco bricks help keep plastic out of the ocean" loading="lazy"></div>
-			<div class="big-header">Accelerating Plastic Transition</div>
+			<div class="big-header">Acelerando la Transición Plástica</div>
 			<div class="sub-text">
-			<p>Together we can keep our plastic out of the oceans.  With ecobricks we can secure plastic to build our greenest visions and ignite regenerative transition.</p>
+			<p>Juntos podemos mantener nuestro plástico fuera de los océanos. Con los ladrillos ecológicos podemos asegurar el plástico para construir nuestras visiones más ecológicas y encender la transición regenerativa.</p>
 			</div>
-			 <a href="faqs.php" button class="main-button">All About Ecobricks</a>
-			<h6><a href="what.php">What is an ecobrick?</a> | <a href="/how">How to Make?</a> | <a href="/why">Why Make?</a></h6>
+			 <a href="faqs.php" button class="main-button">Los Ecobricks</a>
+			<h6><a href="what.php">Que es un ecobrick?</a> | <a href="/how">¿Cómo hacer? </a> | <a href="/why">¿Por qué hacer?</a></h6>
 		</div>	
 	
 	</div>
@@ -126,13 +126,13 @@ _END;?>
 		<div id="main-content">
 			<div class="lead-graphic"><img src="webp/earth-service-700px.webp" width="100%" height="96%" alt="eco bricks follow the Earth's example." max-width="700px">
 			</div>
-			<div class="big-header">Follow the Earth's Example</div>
+			<div class="big-header">Sigue el ejemplo de la Tierra</div>
 			<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
 			<div class="sub-text">
-			<p>Ecobricking follows the Earth's example of concentrating and securing carbon out of the biosphere.  Following the science of plastic degradation and Earthen principles, ecobricking is a non-capital, net-zero means of plastic sequestration.</p>
+			<p>Ecobricking sigue el ejemplo de la Tierra de concentrar y asegurar el carbono fuera de la biosfera. Siguiendo la ciencia de la degradación del plástico y los principios de Earthen, el ecobricking es un medio de secuestro de plástico sin capital y cero neto.</p>
 			</div>
-			<a href="sequest.php" button class="main-button">Plastic Sequestration</a>
-			<h6><a href="/ayyew">Ayyew Inspired</a> | <a href="/circular">Circular by Design</a> | <a href="/plastic">Towards Transition</a> | <a href="/principles">Regenerative</a> <!--| <a href="/reports">Reports & Whitepapers</a>--></h6>
+			<a href="sequest.php" button class="main-button">Secuestro de plástico</a>
+			<h6><a href="/ayyew">Ayyew inspirado</a> | <a href="/circular">Diseño Circular</a> | <a href="/plastic">Transición Plastico</a> | <a href="/principles">Principios Regenerador</a> <!--| <a href="/reports">Reports & Whitepapers</a>--></h6>
 		</div>
 	</div>	
 		
@@ -141,13 +141,13 @@ _END;?>
 	<div id="content-sect3">
 		<div id="main-content">
 			<div class="lead-graphic"><img src="webp/spiral-circular-800px.webp" width="100%" max-width="777px" height="90%" alt="eco brick plastic brick building"></div>
-			<div class="big-header">The Problem is the Solution.</div>
-			<div class="sub-text"><p>Ecobricks turn used plastic into reusable building blocks ideal for circular & spiral building applications.  Use them make modules, home furniture, play parks, and food-forest gardens.  Using the problem, we can build our greenest visions.</p>
+			<div class="big-header">El problema es la solución.</div>
+			<div class="sub-text"><p>Los ecoladrillos convierten el plástico usado en bloques de construcción reutilizables, ideales para aplicaciones de construcción circulares y en espiral. Úselos para hacer módulos, muebles para el hogar, parques de juegos y jardines forestales para alimentos. Utilizando el problema, podemos construir nuestras visiones más ecológicas.</p>
 			</div> 
 
-			<a href="build.php" button class="main-button">Building Applications</a>
+			<a href="build.php" button class="main-button">Aplicaciones de construcción</a>
 			
-			<h6><a href="/earth">Earth & Ecobrick Building</a> | <a href="/earth-methods">E&B Methods</a> | <a href="/modules">Milstein Modules</a> | <a href="/circular">Spiral & Circular</a> | <a href="/openspaces">Open Spaces</a> | <a href="/fire">Fire Safety</a></h6>
+			<h6><a href="/earth">Edificio Tierra & Ecobrick</a> | <a href="/earth-methods">Metodos Tierra & Ecobrick</a> | <a href="/modules">Modulos</a> | <a href="/circular">Diseño Circular</a> | <a href="/openspaces">Open Spaces</a> | <a href="/fire">Seguridad incendios</a></h6>
 		</div>
 	</div>
 
@@ -158,18 +158,18 @@ _END;?>
 			<div class="lead-graphic"><img src="webp/balancing-green.webp"  width="100%" height="100%"></div>
 			<div class="lead-graphic"><img src="svgs/aes-brk.svg" width="100%" height="40%" alt="authenticated eco brick sequetration"></div><!--width="300px" height="254px"-->
 			<!--<div class="lead-paragraph"><br>Introducing AES Plastic Offsetting</div>-->
-			<div class="sub-text"><p><br>Fast track your journey to zero-waste through plastic offsetting.  Plastic offsets are directly correlated to authenticated ecobricked plastic through the Brikcoin manual blockchain.</p></div>
-			<a href="https://gobrik.com/#offset" target="_blank" class="main-button">Plastic Offsetting</a>
-			<h6><a href="/brikcoins">Brikcoin Blockchain</a> | <a href="/aes">AES Plastic</a> | <a href="transition.php">Plastic Transition</a> | <a href="about.php">About the GEA</a></h6><br><br>
-			<p>The Brikcoin manual blockchain enables the authentication, valuation and vitalization of ecobricks.<br><a href="https://gobrik.com/#offset" target="_blank">Offset your household or company plastic generation with us.</a></p><br>
+			<div class="sub-text"><p><br>Acelere su viaje hacia el desperdicio cero a través de la compensación de plástico. Las compensaciones de plástico se correlacionan directamente con el plástico ecobrick autenticado a través de la cadena de bloques manual Brikcoin.</p></div>
+			<a href="https://gobrik.com/#offset" target="_blank" class="main-button">Compensación de plástico</a>
+			<h6><a href="/brikcoins">Brikcoin Blockchain</a> | <a href="/aes"> Plástico AES</a> | <a href="transition.php">Transición plástica </a> | <a href="about.php">Acerca de GEA</a></h6><br><br>
+			<p>La cadena de bloques manual de Brikcoin permite la autenticación, valoración y vitalización de ecoladrillos.<br><a href="https://gobrik.com/#offset" target="_blank">Compensa la generación plástica de tu hogar o empresa con nosotros.</a></p><br>
 			<img src="svgs/3brikcoins.svg" width="30%">
 
 			<br><br>
-			<h4>Eco-brick, eco brick, or ecobrick?</h4>
+			<h4> Ecoladrillo, Eco-brick, o ecobrick?</h4>
 
-			<p>Back in the early days of putting plastic into a bottle we called it just that-- plastic bottle bricks!  Then when we realized it was helpful to the <b>eco</b>systems around us, the name changed to "eco bricks" or "eco-brick". Today 'ecobrick' is the <a href="https://en.wikipedia.org/wiki/Ecobricks" target="_blank">Wikipedia recongnized</a> term for the go-to, non-capital, zero-carbon solution for <a href="https://en.wikipedia.org/wiki/plastic_sequestration" target="_blank">plastic sequestration</a></a></p>
-				<p>Learn about the main focus of the ecobrick movement and technology:</p>
-				<a href="transition.php" button class="main-button">Plastic Transition</a>
+			<p>En los primeros días de poner plástico en una botella, lo llamábamos así: ¡ladrillos de botellas de plástico! Luego, cuando nos dimos cuenta de que era útil para los ecosistemas que nos rodean, el nombre cambió a "ladrillos ecológicos" o "ladrillos ecológicos". Hoy en día, 'ecoladrillo' es el término reconocido por <a href="https://en.wikipedia.org/wiki/Ecobricks" target="_blank">Wikipedia</a> para la solución sin emisiones de carbono y sin capital para el <a href="https://en.wikipedia.org/wiki/plastic_sequestration" target="_blank">secuestro de plástico</a></a></p>
+				<p>Conozca el enfoque principal del movimiento y la tecnología del ecoladrillo:</p>
+				<a href="transition.php" button class="main-button">Transición plástica</a>
 			
 		</div>
 	</div>
--- styles-landing.php
+++ styles-landing.php
@@ -3,7 +3,7 @@
 @media screen and (min-width: 770px) { 
 
     #clouds {
-      background: url(svgs/top-clouds-animated-desktop) center top;
+      background: url(svgs/top-clouds-animated-desktop?v1) center top;
         background-repeat: no-repeat;
         background-size: cover;
         width: 100%;
@@ -20,7 +20,7 @@
 @media screen and (max-width: 769px) { 
 
     #clouds {
-      background: url(svgs/top-clouds-mobile.svg) center top;
+      background: url(svgs/top-clouds-mobile.svg?v1) center top;
         background-repeat: no-repeat;
         background-size: cover;
         width: 100%;
