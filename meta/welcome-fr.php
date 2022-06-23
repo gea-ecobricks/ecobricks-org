37e725a84414f593bc857b2263de6bb3869ca4e5
russmaier <russmaier@gmail.com>
2022-06-23 17:40:48 +0800

    all launched

--- meta/welcome-fr.php
+++ meta/welcome-fr.php
@@ -1,7 +1,7 @@
 
     <title>Transition Plastique  | Ecobricks.org</title>
 
-<meta name="keywords" content="ecibriques, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-briques, briques, eco brique, brik, briks, plastique, gestion du plastique, séquestration du carbone, éthique de la terre, plastique, alternatives de recyclage, incinération, compensations plastiques, solutions plastiques, upcycling, séquestration du plastique, alternative de recyclage, alternative d'incinération, prévention de la pollution plastique, pollution plastique, solution plastique régénérative, déchets, gestion des déchets , déchets plastiques, non capitaux, zéro déchet, net zéro, séquestration du plastique"> 
+<meta name="keywords" content="ecobriques, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-briques, briques, eco brique, brik, briks, plastique, gestion du plastique, séquestration du carbone, éthique de la terre, plastique, alternatives de recyclage, incinération, compensations plastiques, solutions plastiques, upcycling, séquestration du plastique, alternative de recyclage, alternative d'incinération, prévention de la pollution plastique, pollution plastique, solution plastique régénérative, déchets, gestion des déchets , déchets plastiques, non capitaux, zéro déchet, net zéro, séquestration du plastique"> 
 
 <meta name="description" content="Les écobricks empêchent le plastique de devenir un déchet, un déchet, un recyclage, une incinération ou une pollution. Les briques écologiques permettent une séquestration du plastique sans capital, zéro déchet, net zéro.">
 	
