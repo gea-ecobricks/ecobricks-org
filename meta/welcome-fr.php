9a654e8abc9b876bf83cefe04b9ec7c7f1dc36e7
russmaier <russmaier@gmail.com>
2022-06-23 16:58:57 +0800

    updateddd

---
+++ meta/welcome-fr.php
@@ -0,0 +1,19 @@
+
+    <title>Transition Plastique  | Ecobricks.org</title>
+
+<meta name="keywords" content="ecibriques, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-briques, briques, eco brique, brik, briks, plastique, gestion du plastique, séquestration du carbone, éthique de la terre, plastique, alternatives de recyclage, incinération, compensations plastiques, solutions plastiques, upcycling, séquestration du plastique, alternative de recyclage, alternative d'incinération, prévention de la pollution plastique, pollution plastique, solution plastique régénérative, déchets, gestion des déchets , déchets plastiques, non capitaux, zéro déchet, net zéro, séquestration du plastique"> 
+
+<meta name="description" content="Les écobricks empêchent le plastique de devenir un déchet, un déchet, un recyclage, une incinération ou une pollution. Les briques écologiques permettent une séquestration du plastique sans capital, zéro déchet, net zéro.">
+	
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+  <meta property="og:url"           content="https://www.ecobricks.org/fr/welcome.php">
+  <meta property="og:type"          content="website">
+ <meta property="og:title"         content="Transition Plastiques | Ecobricks.org">
+  <meta property="og:description"   content="Les écobricks empêchent le plastique de devenir un déchet, un déchet, un recyclage, une incinération ou une pollution. Les briques écologiques permettent une séquestration du plastique sans capital, zéro déchet, net zéro."/>
+  <meta property="og:image"         content="https://www.ecobricks.org/pngs/social-banner2.png"/>
+  <meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="638" />
+  <meta property="og:image:alt"     content="Une route métaphorique qui serpente au loin avec diverses constructions écologiques en brique et en terre à ses côtés."/>
+  <meta property="og:locale" content="fr_FR" />
