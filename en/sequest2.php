537c7530dd5433e3da9e1ec419fa46fe5ffd5f30
russmaier <russmaier@gmail.com>
2022-06-24 20:07:59 +0800

    sequest time

--- en/sequest2.php
+++ en/sequest2.php
@@ -1,200 +1,15 @@
-<!--PAGE LANGUAGE:  ENGLISH--> 
 
-<!-- Translators:   Look for untranslated text wherever it appears, but leave the HTML  between markers like these > <.  Don't worry about translating these comments.  Be sure NOT to translate english page names, file names, div names, div class names, or html syntax.-->
+<!--PAGE LANGUAGE:  ENGLISH
+Content Page template: v.1.0.1-->
+  
+<!-- TRANSLATORS:   Look for untranslated text inside HTML tags.  In other words <a tag>any content text between markers like these</a tag>.  Don't worry about translating these comments.  Be sure NOT to translate english page names, file names, div names, div class names, or html syntax.-->
+ 
+<?php require_once ("includes/seuest-inc.php");?>
 
+<!--Once translation is completed, you can remove the line below to take the translation notice off the page-->
+<?php require_once ("translate-please.php");?>
 
-<?php require_once ("lang.php");
-echo <<<_END
-
-<!DOCTYPE html>
-
-<!-- this grabs the language identifier for the page so that it can used in the meta and canonical url variables-->
-
-<html lang="$lang">
-
-_END;?>
-
-<HEAD>
-
-<!--Image files to preload that are unique to this page-->
-
-<link rel="preload" as="image" href="https://www.ecobricks.org/logos/gea-horizontal.svg">
-<link rel="preload" as="image" href="https://www.ecobricks.org/webp/earthen-400px.webp">
-<link rel="preload" as="image" href="https://www.ecobricks.org/webp/blue-ode-750px.webp">
-
-
-<!-- This loads the page's meta tags:  Be sure the page name is in place in English-->
-
-<?php require_once ("meta/sequest-$lang.php");?>
-
-<!--This loads the page's header-->
-
-<?php require_once ("header.php");?>
-
-<!--This loads CSS specific to this page-->
-
-<STYLE>
-
-@media screen and (max-width: 700px) { 
-	.splash-content-block {
-		text-align: left;
-		height: 77vh;
-		padding: 50px 5% 25px 5%;
-		margin-bottom: 0px;
-		z-index: 5;
-		
-		background: url(webp/blue-ode-750px.webp) bottom;
-		/*background-color: #8BBAF0;*/
-    	background-repeat: no-repeat;
-		background-size: cover;
-		margin-top: 45px;
-		width: 100%;
-		display: flex;
- 		box-sizing: border-box;
-		flex-direction: column;
-	position: relative;
-
-	}
-}
-
-
-@media screen and (min-width: 700px) { 
-	.splash-content-block {
-		text-align: left;
-		height: 70vh;
-		padding: 50px 7% 50px 7%;
-		z-index: 5;
-		position: relative;
-		
-		background: url(svgs/BlUE-ode-to-plastic-Sequestration.svg) bottom;
-		/*background-color: #fff;*/
-    	background-repeat: no-repeat;
-		margin: -3px 0 -20px 0;
-		display: flex;
- 		flex-wrap: wrap;
- 		box-sizing: border-box;
-	    flex-direction: row;
-		width: 100%;
-		background-size: cover;
-	    position: relative;
-}
-}
-
-
-@media screen and (min-width: 700px) { 
-.splash-image {
-        z-index: 5;
-        position: relative;
-        text-align: center;
-        flex: 35%;
-	    padding: 0px;
-	    box-sizing: border-box;
-	    margin-top: 0px;
-}
-}
-
-
-@media screen and (min-width: 700px) { 
-.splash-box {
-        z-index: 5;
-        position: relative;
-        flex: 65%;
-	    padding: 10px 30px 0px 0px;
-	    box-sizing: border-box;
-	    text-align: right;
-}
-}
-
-
-
-@media screen and (max-width: 700px) { 
-.splash-image {
-        z-index: 5;
-        position: relative;
-        text-align: left;
-        flex: 25%;
-        width: 250px;
-	    padding: 0px;
-	    box-sizing: border-box;
-	    margin: 0px 0px 0px 10px;
-}
-}
-
-
-
-@media screen and (max-width: 700px) { 
-.splash-box {
-	    position: relative;
-        flex: 100%;
-	    padding: 10px 10px 0px 0px;
-	    box-sizing: border-box;
-	    text-align: right;
-}
-}
-
-
-
-.splash-heading { 
-    /*font-family: 'Mulish', Arial, Helvetica, sans-serif;*/
-        font-family: Arvo, serif;
-        color: white;
-        font-weight: 500;
-        text-shadow: 0 0 8px black;
-
-}
-
-@media screen and (max-width: 700px) {
-	.splash-heading {
-        font-size: 2.0em;
-        line-height: 1.1;
-        margin: 10px 0;
-  }
-}
-
-@media screen and (min-width: 700px) {
-	.splash-heading {
-        font-size: 3.3em;
-        line-height: 1.3;
-        margin: 40px 0 10px;
-  }
-}
-
-
-.splash-sub {
-    font-family: 'Mulish', Arial, Helvetica, sans-serif;
-    color: #fff;
-    /*text-shadow: 0px 0px 10px #fff;*/
-    margin: 15px 0;
-    text-shadow: 0 0 7px black;
-}
-
-@media screen and (max-width: 700px) {
-	.splash-sub {
-		font-size: 1.2em;
-		line-height: 1.3;
-		font-weight: 400;
-  }
-}
-@media screen and (min-width: 700px) {
-	.splash-sub {
-		font-size: 2.1em;
-		line-height: 1.3;
-		font-weight: 400;
-  }
-}
-
-#splash-bar {
-	margin-top: -50px;
-	width: 100%;	
-	height:100px;	
-	position: relative;
-	z-index: 0;
-	margin-bottom: -50px;
-}
-
-</style>	
-
-</head>
+<!--TOP PAGE BANNER-->
 							  
 											  
 <BODY id="full-page">
@@ -212,7 +27,7 @@ _END;?>
                 <div class="splash-heading">Plastic Sequestration</div>
             <div class="splash-sub">By following the Earth’s example with our plastic we can make a green contribution to the biosphere.</div>
             </div>
-            <div class="splash-image"><img src="webp/earthen-400px.webp" style="width: 85%;"></div>	
+            <div class="splash-image"><img src="webp/earth-service-700px.webp" style="width: 85%;"></div>	
         </div>
         <div id="splash-bar"></div>
 
@@ -760,9 +575,6 @@ _END;?>
                 </div>
             </div>
         </div>
-
-
-	
 
 
 	<!--FOOTER STARTS HERE-->
