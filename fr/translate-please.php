4790128b214220138b42fecfbe48c5263d9131d1
russmaier <russmaier@gmail.com>
2022-06-24 14:27:25 +0800

    about

--- fr/translate-please.php
+++ fr/translate-please.php
@@ -1,3 +1,3 @@
 <div id="translation-required">
-🇫🇷 Pardon! Cette page n'a pas été entièrement traduite en indonésien. Vous connaissez HTML ? <a href="https://github.com/gea-ecobricks/ecobricks-org/blob/main/<?php echo ($lang); ;?>/<?php echo ($name); ;?>" target="_blank" rel="noopener">📝 Aide à la traduction sur Github.</a>
+🇫🇷 Pardon! Cette page n'a pas encore été entièrement traduite en français.. Vous connaissez HTML? <a href="https://github.com/gea-ecobricks/ecobricks-org/blob/main/<?php echo ($lang); ;?>/<?php echo ($name); ;?>" target="_blank" rel="noopener">📝 Aide à la traduction sur Github.</a>
 </div>
