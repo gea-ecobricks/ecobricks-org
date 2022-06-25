f9607a029cfce275c8531abe1df9be8250c5c799
russmaier <russmaier@gmail.com>
2022-06-25 16:12:58 +0800

    yup

---
+++ meta/faqs-fr.php
@@ -0,0 +1,21 @@
+
+
+<!-- Meta tags for page display and search engine listing-->
+
+<title>FAQ sur le plastique et les écobriques | Ecobricks.org</title>
+
+<meta name="keywords" content="éco, brique, brique écologique, ecobrick, eco-brick, eco, des briques, eco brick, ecobricks, eco-bricks, brik, briks, plastique, gestion du plastique, déchets plastiques, séquestration du carbone, éthique de la terre, gestion des déchets, recyclage, plastique poubelle, poubelle, plastique, alternatives de recyclage, incinération, compensations plastiques, solutions plastiques, recyclage, séquestration du plastique, questions sur les écobriques, questions fréquentes, questions fréquemment posées sur les écobriques, recyclage, incinération, pollution, pollution plastique, dépôt, FAQ">
+
+<meta name="description" content="Réponses aux préoccupations, critiques et questions d'ecobrick. Découvrez comment les briques écologiques sont liées au plastique, au recyclage, à l'incinération, à la séquestration, au carbone, à la compensation et à la régénération de l'économie du pétro-capital.">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://www.ecobricks.org/fr/faqs.php"/>
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="FAQ sur le plastique et les écobriques | Ecobricks.or">
+  	<meta property="og:description"   content="Réponses aux préoccupations, critiques et questions d'ecobrick. Découvrez comment les briques écologiques sont liées au plastique, au recyclage, à l'incinération, à la séquestration, au carbone, à la compensation et à la régénération de l'économie du pétro-capital."/>
+  	<meta property="og:image"         content="https://www.ecobricks.org/pngs/faq-banner2.png"/>
+  	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="686" />
+  	<meta property="og:image:alt"     content="Un point d'interrogation flottant réalisé avec des écobriques"/>
+	  <meta property="og:locale" content="fr_FR" />
