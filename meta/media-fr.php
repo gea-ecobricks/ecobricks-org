7173dc75803aba734f60b1f8a12f418d6d92d6a5
russmaier <russmaier@gmail.com>
2022-06-28 16:56:27 +0800

    up all media

---
+++ meta/media-fr.php
@@ -0,0 +1,21 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Dossier de presse | Ecobricks.org</title>
+
+<meta name="keywords" content="Dossier de presse, histoire des écobricks, directives pour les médias, kit média, écobricks, séquestration du plastique, recyclage, alternative, séquestration du plastique, compensation plastique, plastique aes, séquestration du carbone, histoire des écobricks.">
+
+<meta name="description" content="Un guide pour rendre compte des écobriques et du mouvement de transition plastique.">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://ecobricks.org/en/media.php"/>
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="Dossier de presse | Ecobricks.org">
+  	<meta property="og:description"   content="Un guide pour rendre compte des écobriques et du mouvement de transition plastique."/>
+  	<meta property="og:image"         content="pngs/earthcenter-banner-1000px.webp.png"/>
+  	<meta property="og:image:width" content="1000" />
+	<meta property="og:image:height" content="1048" />
+  	<meta property="og:image:alt"     content="Construction d'un centre communautaire en terre et écobriques"/>
+	  <meta property="og:locale" content="fr_FR" />
+
+	
