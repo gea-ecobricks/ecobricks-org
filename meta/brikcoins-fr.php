419483dcdb3364a2531a478174f04462c97ea916
russmaier <russmaier@gmail.com>
2022-06-28 17:35:47 +0800

    update fr

---
+++ meta/brikcoins-fr.php
@@ -0,0 +1,21 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Brikcoins | Ecobricks.org</title>
+
+<meta name="keywords" content="séquestration du carbone, séquestration du plastique, valeur écologique, chaîne de blocs, brikcoins, blockchain manuelle, blockchain, preuve de valeur, écobricks, plastique, séquestration du plastique, BRK, valeur écologique, marchandisation, service écologique, plastique aes, écobricks connectés, NFT écologiques">
+
+<meta name="description" content="Une blockchain de preuve de valeur manuelle basée sur la séquestration du plastique">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://www.ecobricks.org/fr/brikcoins.php"/>
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="Brikcoins | Ecobricks.org">
+  	<meta property="og:description"   content="Une blockchain de preuve de valeur manuelle basée sur la séquestration du plastique"/>
+  	<meta property="og:image"         content="https://ecobricks.org/pngs/brikcoin-banner-1000px.png"/>
+  	<meta property="og:image:width" content="1000px" />
+	<meta property="og:image:height" content="600px" />
+  	<meta property="og:image:alt"     content="Une blockchain de preuve de valeur manuelle basée sur la séquestration du plastique"/>
+	  <meta property="og:locale" content="fr_FR" />
+
+	
