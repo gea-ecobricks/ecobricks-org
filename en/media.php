061e89a06503e3a4a4fa54d4e432ddf076e63b01
russmaier <russmaier@gmail.com>
2022-06-28 16:59:55 +0800

    d

--- en/media.php
+++ en/media.php
@@ -6,7 +6,7 @@ Content Page template: v.1.0.1-->
 <?php require_once ("includes/media-inc.php");?>
 
 <!--Once translation is completed, you can remove the line below to take the translation notice off the page-->
-<?php require_once ("translate-please.php");?>
+<!--<?php require_once ("translate-please.php");?>-->
 
 <!--PAGE BANNER-->
 
