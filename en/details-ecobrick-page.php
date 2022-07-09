dc97dbfcf70ed47364d0a51e46e45029b5fc1b9d
russmaier <russmaier@gmail.com>
2022-07-09 15:46:11 +0800

    meta

--- en/details-ecobrick-page.php
+++ en/details-ecobrick-page.php
@@ -7,6 +7,40 @@ Special Ecobrick View Page: v.1.0.1-->
 
 require_once ("includes/details-ecobrick-page-inc.php");
 
+
+
+// Get the contents from the Ecobrick table as an ordered View, using the serial_no from the URL.
+$serialNo = $_GET['serial_no'];
+
+// Refered to  https://www.w3schools.com/php/php_mysql_select_where.asp1
+$sql = "SELECT * FROM tb_ecobricks WHERE serial_no = " . $serialNo;
+
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+	
+    //  echo "<h1> Use Serial Number from URL => " . $serialNo ."</h1>"; Output data of each row 
+    while($array = $result->fetch_assoc()) {
+
+		echo '<meta name="title" content="Ecobrick '. $array["serial_no"] .' | 0.62 Kg of plastic secured by Alnisah Basher in Philippines.">';
+		echo '<meta name="description" content="'. $array["vision"] .'">';
+		echo '<meta name="keywords" content="plastic sequestration, recycling, alternative, sequestration of plastic, plastic offsetting, aes plastic, carbon sequestration.">';
+		echo '<title>Regenerative Principles | Ecobricks.org</title>' ;
+        echo '<meta property="og:url"           content="https://ecobricks.org/en/sequest.php"/>' ;
+        echo '<meta property="og:title"         content="Regenerative Principles | Ecobricks.org">';
+        echo '<meta property="og:description"   content="The principles that guide the ecobrick plastic transition movement."/>';
+        echo '<meta property="og:image"         content="'. $array["ecobrick_full_photo_url"] .'"/>';
+        echo '<meta property="og:image:alt"     content="A metaphorical road heading far out into the distance of time"/>';
+        echo '<meta property="og:locale" content="'  . $lang .'_GB" />';
+        echo '<meta property="og:type"          content="website">';
+
+    }
+
+} else {
+    echo '<META NAME="robots" CONTENT="noindex">';
+}
+$conn->close();
+
+
 echo '
 
 </head>
@@ -20,9 +54,7 @@ require_once ("menus/menu-$lang.php");
 
 require_once ("translate-please.php");
 
-echo '
-
-	<div class="splash-content-block">
+echo '<div class="splash-content-block">
 		<div class="splash-box">
 			<div class="splash-heading">';
 				
