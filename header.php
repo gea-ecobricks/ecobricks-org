4030667b2db0426795fd0881e26efdab07af639b
russmaier <russmaier@gmail.com>
2022-07-11 12:56:47 +0800

    t

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 
 <!-- This calls our Github hosted stylesheet - if it gets hopelessly cached and doesn't load, up the version number! -->
 		
-<link rel="stylesheet" type="text/css" href="http://ecobricks.org/stylesheet-general.css?v1.6">
+<link rel="stylesheet" type="text/css" href="http://ecobricks.org/stylesheet-general.css?v1.7">
 
 	
 <!-- This calls GEA typography -->
