999db72d0fd82a69e1f00db938dcbaf942094711
russmaier <russmaier@gmail.com>
2022-07-20 15:51:11 +0800

    upload principles

--- meta/principles-en.php
+++ meta/principles-en.php
@@ -2,17 +2,17 @@
 
 <title>Regenerative Principles | Ecobricks.org</title>
 
-<meta name="keywords" content="plastic sequestration, recycling, alternative, sequestration of plastic, plastic offsetting, aes plastic, carbon sequestration.">
+<meta name="keywords" content="regenerative principles, biosphere, ecological ethics, transcaste, for-earth, ecological accounting, circular design, spiral design, cyclical design, earthen ethics, vitalizing biodiversity, plastic sequestration, recycling, alternative, sequestration of plastic, plastic offsetting, aes plastic, carbon sequestration.">
 
-<meta name="description" content="The principles that guide the ecobrick plastic transition movement.">
+<meta name="description" content="Ecobricking is guided by principles that point us towards ever increasing harmony with the ecological cycles of which we are a part.">
 	
 	<!-- Facebook Open Graph Tags for social sharing-->
 
 	<meta property="og:url"           content="https://ecobricks.org/en/sequest.php"/>
 	<meta property="og:type"          content="website">
  	<meta property="og:title"         content="Regenerative Principles | Ecobricks.org">
-  	<meta property="og:description"   content="The principles that guide the ecobrick plastic transition movement."/>
-  	<meta property="og:image"         content="https://ecobricks.org/pngs/sequest-1000px.png"/>
+  	<meta property="og:description"   content="Ecobricking is guided by principles that point us towards ever increasing harmony with the ecological cycles of which we are a part."/>
+  	<meta property="og:image"         content="https://ecobricks.org/pngs/principles-earth-home-1000px.png"/>
   	<meta property="og:image:width" content="1000" />
 	<meta property="og:image:height" content="1000" />
   	<meta property="og:image:alt"     content="A metaphorical road heading far out into the distance of time"/>
