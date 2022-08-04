7e6bd071442558a7c8700ae59537d3882461beac
russmaier <russmaier@gmail.com>
2022-08-04 13:09:38 +0800

    brickable

---
+++ brickable.php
@@ -0,0 +1,306 @@
+<!--PAGE LANGUAGE:  ENGLISH
+Content Page template: v.1.0.1-->
+  
+<!-- TRANSLATORS:   Look for untranslated text inside HTML tags.  In other words <a tag>any content text between markers like these</a tag>.  Don't worry about translating these comments.  Be sure NOT to translate english page names, file names, div names, div class names, or html syntax.-->
+ 
+<?php require_once ("includes/catalyst-inc.php");?>
+
+<!--Once translation is completed, you can remove the line below to take the translation notice off the page-->
+<?php require_once ("under-construction.php");?>-->
+
+<!--PAGE BANNER-->
+
+	
+<div class="splash-content-block">
+	<div class="splash-box">
+		<div class="splash-heading">Brickable Design</div>
+		<div class="splash-sub">Make your plastic product circular today with the Brikcable Designator Icon.</div>
+	</div>
+	<div class="splash-image"><img src="https://ecobricks.org/svgs/ecobrickable-icon-from-ecobricks-org.svg" style="width: 70%;"></div>	
+</div>
+<div id="splash-bar"></div>
+
+
+<!-- PAGE CONTENT-->
+
+<a name="top"></a>
+
+<div id="main-content">
+<!-- The flexible grid (content) -->
+	<div class="row">
+		<div class="main">
+
+			<div class="lead-page-paragraph">
+				
+			<p>The Brickable Designator is a simple symbol that can be placed on single-use plastic to designate that it should be ecobricked.  Our Kit contains all the graphic files necessary to print stickers on insert into your digital packaging designs. Ecobrickable Designation enables companies to start their transition from linear to circular design, today.</p>
+			</div>
+
+			<div class="page-paragraph">
+				<p>The majority of today’s products are manufactured without thought to where they will go once consumed. Many products are even designed to fail within a certain period to promote further consumption. This design ethic (or lack of it) is one of the root causes of plastic pollution.  Learning from the ecologies around us, the first step is to shift from linear consumption design (and dead-end plastic products) to cradle-to-cradle, circular design.  Afterall, plastic isn’t a ‘bad’ material in and of itself– its how we’ve been putting it to use that has been causing problems.  Designing a product as Ecobrickable, and clearly designating it as so, can be a giant leap for a company into the brave new world of circular design and regenerative business.  The GEA has developed guidelines and a designator symbol to assist companies in their plastic transition.</i><p> 
+
+				<p>For many companies making the switch to circular design and extended producer responsibility (EPR) appears to require a massive investment in manufacturing, material sourcing as well as implementing new systems to reabsorb their dead products. Furthermore it may seem that the required organic materials are still on the horizon.  The transition can appear daunting!</p>
+				
+				<p>Making a product ecobrickable is a first step in a business’s plastic transition.</p>
+					
+				<p>With a little forethought and advanced planning, a product can be made easily and effectively ecobrickable.  In this way, ecobrickable designs enables companies and designers to make their first step to cradle-to-cradle and ayyew vision.  Designating a product or package as ecobrickable gives it a next life and purpose.  When ecobricked, the plastic is saved from a fate of industrial recycling, incineration, and/or dumping.  The ecobricked plastic is transformed into a reusable building block. Ecobricks and their applications are themselves cradle-to-cradle and follow regenerative principles, enabling an indefinite, localized cycle of reuse of the ecobricked plastic.</p>
+
+                <h3>Ecobrickable Design Guidelines</h3>
+
+<p>The GEA has established simple criteria to meet when designing your product or packaging.  It is obligatory that your product meet these stipulation in order to use Ecobrickable Designator Symbol your product or packaging must…</p>
+
+<ul>
+
+    <li>… not contain substances that will corrode PET plastic over time.</li>
+    <li>… not contain any primary or secondary biodegradable material.</li>
+    <li>… can be easily disassembled, broken or cut into parts that can fit within the 22mm diameter neck of a standard PET bottle.</li>
+    <li>.. maximizes the ease of disassembly for this purpose.</li>
+    <li>… not have sharp protrusions that could puncture an ecobrick from within while it is being packed down.</li>
+    <li>… not contain reactive chemicals, or if so, these components are clearly marked as un-ecobrickable.</li>
+</ul>
+					
+				<p>The Ecobrickable Designator & Stickers are licensed under a Creative Commons Attribution-NoDerivatives 4.0 International License. This is to ensure the standardized and resonant usage of the designators by committed and resonant companies and individuals.   See copyright notice and terms below.</p>
+				<br><br>
+				<a class="action-btn" href="https://gobrik.com/#catalyze" target="_blank">🚀 Launch Catalyst Setup</a>
+			<p style="font-size: 0.85em; margin-top:20px;">We'll send you over to GoBrik in a new window.</a></p>
+
+			</div>	
+
+			<div class="reg-content-block" id="block1">
+				
+				<div class="opener-header">
+					
+					<div class="opener-header-text">
+						<h4>Plastic Transition Tracking</h4>
+						<h5>Monitor your subtractive and additive plastic impacts over the year.</h5>
+					</div>
+					
+
+					<button onclick="preclosed1()" class="block-toggle" id="block-toggle-show1">+</button>
+					
+				</div>
+
+				<div id="preclosed1">
+					<br>
+					<div class="row">
+						<div class="main2">
+							<p>Today its clear that the vast majority of the plastic we use eventually makes its way to the biosphere.  When your enterprise consumes or produces plastic, we call this a "grey plastic impact".  Tracking your company's production and consumption of plastic is the first step in reducing one's grey impact on the environment.</p>
+						</div>
+						<div class="side2"><img src="webp/balancing-green.webp" width="60%" alt="net green impact" loading="lazy">
+						</div>
+					</div>
+
+					<div class="row2">
+						<div class="main3">
+						<br><p><b>Get started in 15 minutes</b></p>
+					
+						</div>
+						<div class="side3"><img src="webp/balancing-green.webp" width="125px" loading="lazy">
+						</div>
+					</div>
+					<p>The Catalyst program gets you going in no time using a process of estimation refinement. As an owner, manager of president of your enterprise you can estimate better than anyone else how much plastic you are currently using.</p>
+					<p>
+					Month by month, you and your team will receive Plastic Transition reports.  These reports will remind you and your team to step up their reduction and subtraction initiatives.  As you put attention on your plastic you can refine your estimates.</p>
+					<br><p><b>Detailed Tracking</b></p>
+				<p>Once your enterprise has established systems for detailed plastic tracking and weighing, you can enabled month by month data entry.  Managers can observe the enterprise's plastic transition charted over the year. </p>
+				<br><p><b>Reaching out to communities</b></p>
+				<p>By catalyzing ecobrick workshops in communities, you can gain credit for the ecobricks made by participants.  Over the course of the year, our system can track in real time the social increase of your green impact.</p>
+						
+				</div>	
+			</div>
+
+			<div class="reg-content-block" id="block2">
+				<div class="opener-header">
+					<div class="opener-header-text">
+						<h4>Plastic Transition Disclosure</h4>
+						<h5>Certification & Hosting of your Report</h5><br>
+					</div>
+					<button onclick="preclosed2()" class="block-toggle" id="block-toggle-show2">+</button>
+				</div>
+
+				<div id="preclosed2">
+					<div class="row">
+						<div class="main2">
+						<p>You're passionate about the planet and so is your enterprise.  So let the world know through open accounting and transparency!  Plastic transition disclosure allows you to showcase your efforts, lead by example and inspire others.</p>
+						</div>
+						<div class="side2"><img src="webp/floating-dial-500px.webp" width="175px" alt="ecological accounting score" loading="lazy">
+						</div>
+					</div>
+
+					
+
+					<div class="row">
+						
+						<p><b>Automated Reporting</b></p>
+						<p>The Catalyst program allows you to create yearly plastic impact reports that summarize your grey and green impacts.  As a third-party host, we certify your AES plastic impacts so that you can confidently share them with your customers, clients and in government sustainability reviews.</p>
+						
+					</div>
+
+
+
+					
+
+					<div class="row2">
+						<div class="main3">
+						<br><p><b>Public Disclosure</b></p>
+						
+						</div>
+						<div class="side3"><img src="pngs/disclosedial.png" width="150px" alt="ecological accounting score" loading="lazy">
+						</div>
+					</div>
+					
+					<p>Showcase your plastic transition with our GEA certified banners on your website, products and marketing.  Link to your reports and showcase your plastic transition.  Like this...</p><br>
+					<p>
+					<img src="https://www.ecobricks.org/banners/gea-disclose-300px.png" width="250px"></p>
+				</div>
+			</div>
+
+			<div class="reg-content-block" id="block3">
+
+				<div class="opener-header">
+					
+					<div class="opener-header-text">
+					<h4>Plastic Transition Acceleration</h4>
+					<h5>Ecobricking, Offseting & Workshop Catalyzation.</h5>
+					<br>
+					</div>
+
+					<button onclick="preclosed3()" class="block-toggle" id="block-toggle-show3">+</button>
+				</div>
+
+				<div id="preclosed3">
+					<br>
+					<div class="row">
+						<div class="main2">
+						<p>But best of all, the Catalyst program allows you to take decisive and quantifiable action to green your enterprise's impact.  You can use our system to log your ecobricks, buy AES plastic offsets and mobilize communities.  The green impact of all these goes to the green credit of your enterprise.  This credit shows up in your year end ecological accounting to balance off your grey impacts.</p>
+						</div>
+						<div class="side2"><img src="svgs/aes-no-shadow.svg" width="80%" alt="Authenticated Ecobrick Sequestered Plastic">
+						</div>
+					</div>
+
+					<div class="row2">
+						<div class="main3">
+						<p>There are three types of green impacts that can be certified as AES credit.</p>
+						</div>
+						<div class="side3"><img src="svgs/aes-no-shadow.svg" width="120px" alt="Authenticated Ecobrick Sequestered Plastic">
+						</div>
+					</div>
+			
+
+					<p><b>Ecobricking</b></p>
+						<p>Ecobricks logged by the staff in your company, then which are successfully authenticated on the Brikcoin blockchain, qualify as AES plastic.</p><br>
+					<p><b>Plastic Offsetting</b></p>
+					<p>Your enterprise can purchase plastic offset credit on the GoBrik platform using Brikcoins or cash, to credit you year end reporting.</p><br>
+					<p><b>Community Catalyzation</b></p>
+					<p>By sponsoring workshops in your community, the ecobricks of new ecobrickers are credited to your enterprise.  By sponsoring GEA training of trainers workshops, the future trainers in these events, their ecobrickers and the communities they start, generate AES credits for your enterprise.
+					</p>
+					<h6><a href="/trainings">Learn more about GEA Trainings</a></h6><br>
+				</div>
+			</div>
+
+
+
+			<div class="reg-content-block" id="block4">
+
+				<div class="opener-header">
+					
+					<div class="opener-header-text">
+					<h4>See How We do It.</h4>
+					<h5>How the GEA and other Enterprises Use the Program</h5>
+					<br>
+					</div>
+
+					<button onclick="preclosed4()" class="block-toggle" id="block-toggle-show4">+</button>
+				</div>
+
+				<div id="preclosed4">
+					
+					<p><b>The Global Ecobrick Alliance</b> </p>
+					<p>We're an enterprise too!  And despite all our best efforts, we consume and produce plastic in our operations.  We use the catalyst program to track, disclose and accelerate our plastic impacts.  In the footer of our site and apps, we link to our public, past year reporting-- just click on this banner to see how we're doing:</p><br><p>
+					
+						<a href="https://www.gobrik.com/#my-catalyst/2021-report/5e1f513586a7fe0015e77628/" target="_blank"><img src="banners/762-disclose-light-350px.png"></a>
+					<p style="font-size: 0.85em; margin-top:8px;">This will open our report in a new window.</a></p>
+					<br>
+					<p><b>Ubud Raw Chocoloate</b> </p>
+					<p>These guys make amazing chocoloate in Bali, Indonesia.  They are also super keen to transition from plastic! They use the Catalyst program to monitor their monthly consumption of plastic from their suppliers, to the plastic they use for their product packaging.  In 2021 they attained 105% zero-waste!</p><br><p>
+					
+						<a href="https://www.gobrik.com/#my-catalyst/2021-report/5b8c2b0922ddfb2591bf67be/" target="_blank"><img src="banners/100-disclose-light-350px.png"></a>
+					<p style="font-size: 0.85em; margin-top:8px;">This will open our report in a new window.</a></p>
+					<br>
+			
+				</div>
+			</div>
+
+			
+	
+
+
+		<div class="page-paragraph">
+			<br><br>
+			<h2>Get Started!</h2>
+			<p>Setup your Catalyst account today.  Use your GoBrik account to get started.  If you don't have a GoBrik account yet, be sure to register first for a free personal account.</p>
+			<br><br>
+			<a class="action-btn" href="https://gobrik.com/#catalyze" target="_blank">🚀 Launch Catalyst Setup</a>
+			<p style="font-size: 0.85em; margin-top:20px;">We'll send you over to GoBrik in a new window!</a></p>
+		</div>
+		
+		</div>
+
+
+
+
+		<div class="side">
+
+
+			
+		<div id="side-module-desktop-only">
+			
+			
+				<h4>Get Started</h4>
+				<h5>Use a free Catalyst Account to ignite your plastic transition.</h5><br>
+				<a class="action-btn" href="https://gobrik.com/#catalyze" target="_blank">🚀 Launch</a><br><br>
+			</div>
+
+	
+			<div id="side-module-desktop-mobile">
+				<img src="webp/gea-logo-400px.webp" width="90%">
+				<h4>Global Ecobrick Alliance</h4>
+				<h5>The GEA is dedicated to accelerating plastic transition.  We preside over the GoBrik app and the Brikcoin blockchain.</h5><br>
+				<a class="module-btn" href="about.php">About Us</a>
+			</div>
+
+			<div id="side-module-desktop-mobile">
+				<img src="webp/for-earth500px.webp" width="80%">
+				<h4>For-Earth Enterprise</h4>
+				<h5>By discolosing our carbon, plastic and biodiversity impacts and by ensuring that they are net-green, the GEA commits to for-Earth Enterprise</h5><br>
+				<a class="module-btn" href="2021-regen-report.php">Learn More</a>
+			</div>
+
+			<div id="side-module-desktop-mobile">
+				<img src="webp/balancing-green.webp" width="80%">
+				<h4>Plastic Offsetting</h4>
+				<h5>Our Catalyst Program allows you to offset your plastic generation and support the global ecobrick movement.</h5><br>
+				<a class="module-btn" href="/aes">Learn More</a>
+			</div>
+
+		</div>
+
+	</div>
+</div>
+
+
+
+	<!--FOOTER STARTS HERE-->
+
+	<?php require_once ("footers/footer-$lang.php");?>
+
+
+<!-- CUSTOM PAGE SCRIPTS-->
+
+<!-- This script is for pages that use the accordion content system-->
+<script src="accordion-scripts.js" defer></script>
+
+
+</div>
+</body>
+</html>
+
