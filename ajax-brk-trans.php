735c1b9bfc72c3d29e27b61826e4c4afd2a8ce84
russmaier <russmaier@gmail.com>
2022-08-10 10:50:18 +0800

    go again

--- ajax-brk-trans.php
+++ ajax-brk-trans.php
@@ -83,7 +83,13 @@ array(
 );
 
  
-
+// SQL server connection information    
+$sql_details = array(
+    'user' => 'ecobricks_russs',
+    'pass' => '1ecobricks!',
+    'db'   => 'ecobricks_gobrik_msql_db',
+    'host' => 'localhost'
+);
 
 
 
