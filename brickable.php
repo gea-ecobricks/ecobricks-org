8d1f9af543f247119c9a04906d82df349b280f85
russmaier <russmaier@gmail.com>
2022-09-01 18:18:26 +0800

    update

--- brickable.php
+++ brickable.php
@@ -201,10 +201,10 @@ Content Page template: v.1.0.1-->
 
 		<div class="page-paragraph">
 			<br><br>
-			<h2>Get Started!</h2>
+			<p><h2>Get Started!</h2></p>
 			<p>Download the Brickable Designator Kit today to use on your legacy plastic products and packaging.</p>
 			<br><br>
-			<a class="action-btn" href="https://files.ecobricks.org/s/oq9xPkNksf2ZPNX" target="_blank">🚀 Download Kit</a>
+			<p><a class="action-btn" href="https://files.ecobricks.org/s/oq9xPkNksf2ZPNX" target="_blank">🚀 Download Kit</a></p>
 			<p style="font-size: 0.85em; margin-top:20px;">ZIP file | 3.2MB </a></p>
 		</div>
 	</div>
