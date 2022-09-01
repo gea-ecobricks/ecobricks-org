d623fd03a5da5bd0c58eb278e5edb5bf8a6f9558
russmaier <russmaier@gmail.com>
2022-09-01 18:26:15 +0800

    meta

---
+++ meta/brickable-es.php
@@ -0,0 +1,31 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Diseño Brickable</title>
+
+<meta name="keywords" content="brikable, diseño brickable, ecobrickable, icono de designador, producto ecobrickable, embalaje ecobrickable, icono de embalaje, brik, ladrillo, seguimiento de plástico de la empresa, contabilidad ecológica, contabilidad verde, informes regenerativos, contabilidad de carbono, seguimiento de la biodiversidad, divulgación, seguimiento, carbono, plástico, seguimiento, divulgación, ecoladrillos, ecoladrillos, ecoladrillos, ecobriks, briks, plástico, gestión de plásticos, secuestro de carbono, ética de la tierra, plástico, alternativas de reciclaje, incineración, compensaciones de plástico, soluciones plásticas, upcycling, secuestro de plástico, alternativa de reciclaje, alternativa a la incineración, prevención de la contaminación plástica, contaminación plástica, solución plástica regenerativa, residuos, gestión de residuos, residuos plásticos">
+
+<meta name="description" content="Haga que su producto o embalaje de plástico sea circular con Brickable Designator Icon">
+
+<meta name="author" content="Ecobricks.org">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://www.ecobricks.org/es/brickable.php"/>
+	<meta property="og:locale" content="es_ES" />
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="Diseño Brickable">
+  	<meta property="og:description"   content="Haga que su producto o embalaje de plástico sea circular con Brickable Designator Ico"/>
+  	<meta property="og:image"         content="https://www.ecobricks.org/pngs/brickable-banner-1000px.png"/>
+  	<meta property="og:image:width" content="1000px" />
+	<meta property="og:image:height" content="1000px" />
+  	<meta property="og:image:alt"     content="Designador Brickable Ícono de un envoltorio usado que entra en una botella."/>
+
+	<meta property="og:type" content="article" />
+	<meta property="og:site_name" content="Ecobricks.org" />
+	<meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" />
+	<meta property="article:modified_time" content="2022-08-31T09:15:13+00:00" />
+	<meta property="og:image:type" content="image/png" />
+	<meta name="author" content="Global Ecobrick Alliance" />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:label1" content="Est. reading time" />
+	<meta name="twitter:data1" content="4 minutes" /> 
