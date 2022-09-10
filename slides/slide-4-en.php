7b9be2ccaf37113e1776b6044c5a655b6e4589cf
russmaier <russmaier@gmail.com>
2022-09-10 18:39:32 +0800

    slide4

---
+++ slides/slide-4-en.php
@@ -0,0 +1,18 @@
+
+	<!-- ENGLISH SLIDE - template v.1.0 -->
+
+    <div id="slider3">
+
+    <div id="slider-image"><img src="webp/brickable-kit.webp" style="width: 85%;" alt="Brickable Designator Icon">
+        </div>
+		
+        <div id="slider-textbox">
+            <div id="slider-heading">New Brikable Designator Kit!</div>
+            <div id="slider-date">Sept 9th, 2022 Updated | Core Content</div>
+            <div id="slider-body">Still using plastic in your products and packaging?  We're excited to launch our fully revamp brickable designator icon.  Go circular today and jump start your plastic transition.</div>
+            <a class="module-btn" href="brikable.php">Brickable Kit 2.0</a>	
+		</div>
+	
+    </div>
+
+ 
