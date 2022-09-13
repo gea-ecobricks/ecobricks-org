086de9f057aa27c81154c2d8f5f0e397826a5c55
russmaier <russmaier@gmail.com>
2022-09-13 14:57:41 +0800

    go

--- side-modules/ecobrick-of-the-month.php
+++ side-modules/ecobrick-of-the-month.php
@@ -1,29 +1,11 @@
 
-<?php
 
-$sql = "SELECT * FROM vw_sum_brk_total ;";
 
-$result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-
-            echo'<div id="side-module-desktop-mobile">';
-
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            
-
-            echo '<img src="svgs/aes-brk.svg?v=2" style="width:70%; margin-top:25px;"><p style="font-size: 1.2em; margin-top:5px;">Current value of 1 Brikcoin:</p>
-            <p><span class="courier" style="font-size: 1.5em;"><span class="blink">⬤<span style="font-size: 0.4em;"> </span></span>'.$row["plastic_value_g_per_brk"].'<span style="font-size: 0.1em;"> </span>g<span style="font-size: 0.3em;"> </span>AES</span></p>';
-            echo '<p style="font-size: 1.2em; margin-top:10px;">Total Circulation:<br><b><span class="courier">'.$row["net_brk_in_circulation"].'&#8202;BRK</b></span></p>' ; 
-            echo '<p style="font-size: 0.9em; margin-top:10px;">Unlike other currencies brikcoins <a href="/brikcoins.php">(BRK / ß)</a> are based in ecological value as measured in kilograms of authenticated ecobrick sequestered (<a href="sequestered.php">AES</a>)plastic.</p>
-            <p style="color: white; font-size: 0.8em; margin-top:10px;"> Updated every 15 minutes</p></div>' ; 
-        
-            } 
-            
-        } else {
-            echo "Failed to connect to Brikchain</div>";
-        }
-        ?>
-
-      
+ <div id="side-module-desktop-mobile">
+    <img src="https://ecobricks.org/briks/ecobrick-190124-file.jpeg" width="100%" alt="Ecobrick basic pic"/>
+    <h4>Ecobrick of the Month!</h4>
+    <p><b>564g of plastic sequestered by John Malkovitch, East Village, NY, USA.</b></p>
+    <h5>Of all the thousands of ecobricks logged each month we keep a running tally of those that receive the highest authentication scores.</h5>
+    <a class="module-btn" href="top-tens.php">Top Ten Briks</a>
+</div>
+  
