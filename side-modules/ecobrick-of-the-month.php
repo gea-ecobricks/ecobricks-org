4b9dfc9ce23d95f86f3847513d7f5547a5463bad
russmaier <russmaier@gmail.com>
2022-09-14 12:29:54 +0800

    f

--- side-modules/ecobrick-of-the-month.php
+++ side-modules/ecobrick-of-the-month.php
@@ -13,15 +13,15 @@ $sql = "SELECT * FROM vw_top_10_last_month ;";
          echo '<div id="side-module-desktop-mobile">';
 
         // output data of each row
-        //while($row = $result->fetch_assoc()) {
+        while($row = $result->fetch_assoc()) {
 
             $row = $result->fetch_assoc();
             
             echo '<a href="https://ecobricks.org/en/details-ecobrick-page.php?serial_no='.$row["ecobrick_unique_id"].'"><img src="https://ecobricks.org/briks/ecobrick-'.$row["ecobrick_unique_id"].'-file.jpeg" width="80%" alt="Ecobrick '.$row["ecobrick_unique_id"].' basic pic"/></a>';
             echo '<h4>Ecobrick of the Month!</h4>
-            <h5><a href="https://ecobricks.org/en/details-ecobrick-page.php?serial_no='.$row["ecobrick_unique_id"].'">Ecobrick '.$row["ecobrick_unique_id"].' made the top tens this month with a validation score of '.$row["final_validation_score"].' Al Ikhlas Putri has ecobricked 269 g of community plastic in Jombang, Indonesia using a 600 bottle to make a Regular Ecobrick</a></h5>
+            <h5><a href="https://ecobricks.org/en/details-ecobrick-page.php?serial_no='.$row["ecobrick_unique_id"].'">Ecobrick '.$row["ecobrick_unique_id"].'</a> made the top tens this month with a validation score of '.$row["final_validation_score"].'</h5><br>
             
-            <a class="module-btn" href="top-tens.php">Top Ten Briks</a>
+            <a class="module-btn" href="top-tens.php">Top Ten Briks</a><br>
         </div>';
             
         
