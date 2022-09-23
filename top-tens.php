3e7075a68f499f4a9167f1e371808f6d23b535db
russmaier <russmaier@gmail.com>
2022-09-23 18:56:07 +0800

    hh

--- top-tens.php
+++ top-tens.php
@@ -38,6 +38,7 @@ Content Page template: v.1.0.0-->
 	
 			<div class="page-paragraph">
                 <p>And here they are...</p>
+                <hr>
             </div>	
 
             <?php include 'ecobricks_env.php';?> 
@@ -63,7 +64,7 @@ $sql = "SELECT * FROM vw_top_10_last_month ;";
             
             echo '<p><a href="https://ecobricks.org/en/details-ecobrick-page.php?serial_no='.$row["ecobrick_unique_id"].'"><img src="'.$row["ecobrick_full_photo_url"].'" style="max-width:100%; width:600px;" alt="Top Ten Ecobrick '.$row["ecobrick_unique_id"].' by '.$row["ecobrick_owner"].' in '.$row["location"].'" loading="lazy"/></a></p>';
             echo '<h4><p><b>'.$row["location"].'</b></p></h4>';
-            echo '<p>'.$row["ecobrick_owner"].' logged <a href="https://ecobricks.org/en/details-ecobrick-page.php?serial_no='.$row["ecobrick_unique_id"].'">Ecobrick '.$row["ecobrick_unique_id"].'</a> with a weigh of '.$row["weight_in_g"].'&#8202;g and a density of '.$row["density"].'&#8202;g/ml.   It made this months top ten ecobricks with a validation score of '.$row["final_validation_score"].'.</p>';
+            echo '<p>'.$row["ecobrick_owner"].' logged <a href="https://ecobricks.org/en/details-ecobrick-page.php?serial_no='.$row["ecobrick_unique_id"].'">Ecobrick '.$row["ecobrick_unique_id"].'</a> with a weight of '.$row["weight_in_g"].'&#8202;g and a density of '.$row["density"].'&#8202;g/ml.   It made this months top ten ecobricks with a validation score of '.$row["final_validation_score"].'.</p>';
             
             echo '<p><a class="module-btn" href="https://ecobricks.org/en/details-ecobrick-page.php?serial_no='.$row["ecobrick_unique_id"].'">🔎 Check it out</a>';
             echo '</p><hr><br>';
@@ -80,7 +81,7 @@ $sql = "SELECT * FROM vw_top_10_last_month ;";
             
       
         
-
+<hr>
             <br>
                  
             <div class="row">
