2e1f7132927e305916aa690711f301eae336a731
RakshaEcobricker <72146851+RakshaEcobricker@users.noreply.github.com>
2022-10-02 16:10:13 +0100

    Update under-construction.php
    Add Close link-button-image to fake under-construction-popup

--- en/under-construction.php
+++ en/under-construction.php
@@ -1,3 +1,4 @@
 <div id="translation-required">
+      <a href="#" style="text-decoration: none; float: right; " onclick="this.parentNode.style.display = 'none'" > ❎ </a><br><br>
 🚧 This page is still under development.  It is not yet ready for translation and may contain incomplete data or content.  Know PHP, HTML or CSS? <br><a href="https://github.com/gea-ecobricks/ecobricks-org/blob/main/<?php echo ($lang); ;?>/<?php echo ($name); ;?>" target="_blank" rel="noopener">📝 Help us on Github.</a>
 </div>
