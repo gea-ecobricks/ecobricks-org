32838f8d2e15fc5da4d44076a3f8c1323b6b9242
russmaier <russmaier@gmail.com>
2022-10-02 15:37:04 +0800

    formated

--- includes/gallery-inc.php
+++ includes/gallery-inc.php
@@ -37,7 +37,7 @@ _END;?>
 }
 
 .flex-container > .gal-photo {
-  background-color: white;
+  background-color: #DFDFDF;
   margin: 2px;
   padding: 10px;
  /*text-align: center;*/
@@ -45,7 +45,9 @@ _END;?>
 
 .gal-photo {
     padding: 10px;
-	width: 200px;
+	width: 225px;
+	max-width:225px;
+	overflow: hidden;
 }
 
 @media screen and (min-width: 700px) { 
@@ -79,7 +81,7 @@ _END;?>
 	.splash-content-block {
 		text-align: left;
 		min-height: 73vh;
-		padding: 50px 5% 25px 5%;
+		/*padding: 50px 5% 25px 5%;*/
 		margin-bottom: 0px;
 		z-index: 5;
        
@@ -103,7 +105,7 @@ _END;?>
 	.splash-content-block {
 		text-align: left;
 		min-height: 67vh;
-		padding: 50px 7% 50px 7%;
+		/*padding: 50px 7% 50px 7%;*/
 		z-index: 5;
 		position: relative;
         
@@ -126,110 +128,6 @@ _END;?>
 } 
 
 
-@media screen and (min-width: 700px) { 
-.splash-image {
-  z-index: 5;
-  position: relative;
-  text-align: center;
-  flex: 35%;
-	padding: 0px;
-	box-sizing: border-box;
-	margin: auto;
-}
-}
-
-
-@media screen and (min-width: 700px) { 
-.splash-box {
-  z-index: 5;
-  position: relative;
-  flex: 65%;
-	padding: 10px 30px 0px 0px;
-	box-sizing: border-box;
-	text-align: right;
-    margin: auto;
-}
-}
-
-
-
-@media screen and (max-width: 700px) { 
-.splash-image {
-  z-index: 5;
-  position: relative;
-  text-align: left;
-  flex: 25%;
-  width: 250px;
-	padding: 0px;
-	box-sizing: border-box;
-	margin: 0px 0px 0px 10px;
-}
-}
-
-
-
-@media screen and (max-width: 700px) { 
-.splash-box {
-	position: relative;
-  flex: 100%;
-	padding: 10px 10px 0px 0px;
-	box-sizing: border-box;
-
-}
-}
-
-
-.splash-heading { 
-
-font-family: 'Arvo', Georgia, serif;
-
-color: white;
-font-weight: 300;
-text-shadow: 0px 0px 8px #666;
-
-}
-
-@media screen and (max-width: 700px) {
-.splash-heading {
-  font-size: 2.6em;
-  line-height: 1.3;
-  margin: 0px 0;
-}
-}
-
-@media screen and (min-width: 700px) {
-.splash-heading {
-  font-size: 6em;
-  line-height: 1.3;
-  margin: auto;
-}
-}
-
-.splash-sub {
-font-family: 'Mulish', Arial, Helvetica, sans-serif;
-color: #fff;
-margin: 15px 0;
-text-shadow: 0px 0px 6px #666;
-/*text-shadow: 0px 0px 10px #fff;*/
-}
-
-@media screen and (max-width: 700px) {
-.splash-sub {
-	font-size: 1.45em;
-	line-height: 1.3;
-	font-weight: 400;
-}
-}
-@media screen and (min-width: 700px) {
-.splash-sub {
-	font-size: 2.2em;
-	line-height: 1.3;
-	font-weight: 400;
-	padding: 0px 30px 0px 0px;
-}
-} 
-
-
 #splash-bar {
 	margin-top: -50px;
 	width: 100%;
@@ -240,7 +138,7 @@ text-shadow: 0px 0px 6px #666;
 	z-index: 0;
 
 box-shadow: 0 8px 7px rgba(85, 84, 84, 0.4);
-    background-color: #F98D03;
+    background-color: grey;
 
 	-webkit-transform: skewY(-3deg);
   -moz-transform: skewY(-3deg);
