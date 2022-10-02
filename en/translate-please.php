36ea108457c218dd8d8961304115c58fb7e9ec1c
RakshaEcobricker <72146851+RakshaEcobricker@users.noreply.github.com>
2022-10-02 16:13:13 +0100

    Update translate-please.php
    Add the Close button-link-image same as the underconstruct page

--- en/translate-please.php
+++ en/translate-please.php
@@ -1,3 +1,4 @@
 <div id="translation-required">
+      <a href="#" style="text-decoration: none; float: right; " onclick="this.parentNode.style.display = 'none'" > ❎ </a><br><br>
 Our apologies!  This page is still under development.  Do you know HTML or PHP? <br><a href="https://github.com/gea-ecobricks/ecobricks-org/blob/main/<?php echo ($lang); ;?>/<?php echo ($name); ;?>" target="_blank" rel="noopener">📝 Help us on Github.</a>
 </div>
