a7c294fcbd5ba62d2f92adb6c980acf925b9ccad
russmaier <russmaier@gmail.com>
2022-10-02 17:41:51 +0800

    go gogggg

--- meta/featured-en.php
+++ meta/featured-en.php
@@ -2,20 +2,20 @@
 
 <title>Featured Latest Ecobricks</title>
 
-<meta name="keywords" content="top ten ecobricks, monthly, featured, great ecobrick, example ecobrick, peer review, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-bricks, brik, briks, plastic, building, earth, cob, adobe, mud, building, construction, earth & ecobricks, earth and ecobrick buidling, plastic sequestration, sequestration building, sponge homes, green building, green construction, net-green building, net-zero building, techniques, regenerative, principles">
+<meta name="keywords" content="featured ecobricks, monthly, featured, great ecobrick, example ecobrick, peer review, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-bricks, brik, briks, plastic, building, earth, cob, adobe, mud, building, construction, earth & ecobricks, earth and ecobrick buidling, plastic sequestration, sequestration building, sponge homes, green building, green construction, net-green building, net-zero building, techniques, regenerative, principles">
 
-<meta name="description" content="Every month we feature the ecobricks that received the highest peer review authentication scores from the global ecobrick community.">
+<meta name="description" content="A live feed of the 50 latest and greatest ecobrick from around the world.">
 	
 	<!-- Facebook Open Graph Tags for social sharing-->
 
-	<meta property="og:url"           content="https://ecobricks.org/en/top-tens.php"/>
+	<meta property="og:url"           content="https://ecobricks.org/en/gallery.php"/>
 	<meta property="og:type"          content="website">
  	<meta property="og:title"         content="Featured Latest Ecobricks">
-  	<meta property="og:description"   content="Every month we feature the ecobricks that received the highest peer review authentication scores from the global ecobrick community."/>
-  	<meta property="og:image"         content="https://ecobricks.org/pngs/gobriktrophy.png"/>
-  	<meta property="og:image:width" content="600" />
-	<meta property="og:image:height" content="600" />
-  	<meta property="og:image:alt"     content="Ecobrick of the month trophy"/>
+  	<meta property="og:description"   content="A live feed of the 50 latest and greatest ecobrick from around the world."/>
+  	<meta property="og:image"         content="https://ecobricks.org/photos/featured-flow.jpg"/>
+  	<meta property="og:image:width" content="899" />
+	<meta property="og:image:height" content="989" />
+  	<meta property="og:image:alt"     content="an ecobricker from the philippines with her ecobrick"/>
 	  <meta property="og:locale" content="en_GB" />
 
       <meta name="author" content="Ecobricks.org">
@@ -23,9 +23,6 @@
   <meta property="og:type" content="article" />
  <meta property="og:site_name" content="Ecobricks.org" />
  <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" />
- <meta property="article:modified_time" content="2022-09-13T09:14:13+00:00" />
- <meta property="og:image:type" content="image/png" />
+ <meta property="og:image:type" content="image/jpg" />
  <meta name="author" content="Global Ecobrick Alliance" />
- <meta name="twitter:card" content="summary" />
- <meta name="twitter:label1" content="Est. reading time" />
- <meta name="twitter:data1" content="7 minutes" /> 
+ 
