4077ee04e83eb00b85ec9e7a596eaf5434d2ae4a
RakshaEcobricker <72146851+RakshaEcobricker@users.noreply.github.com>
2022-10-05 12:04:46 +0100

    Update details-ecobrick-page.php
    Make Vision quotes consistant with our own Quoting.

--- details-ecobrick-page.php
+++ details-ecobrick-page.php
@@ -56,7 +56,8 @@ echo '
 			<div class="row-details">';
 
 	if ( isset($array["vision"]) && $array["vision"] != '' ) {
-				echo '<p><div class="vision-quote">'. $array["vision"] .'</div></p>';
+			//	echo '<p><div class="vision-quote">'. $array["vision"] .'</div></p>';
+		                echo '<p><div class="vision-quote"> "'. str_replace('"', "", $array["vision"]) . '"  </div></p>';
 			}
 			
 			echo '<div class="lead-page-paragraph">
