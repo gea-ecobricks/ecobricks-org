1942cd8b3e6a1e16c504750764a600b4827eb046
russmaier <russmaier@gmail.com>
2022-10-11 14:44:11 +0800

    back

--- en/welcome.php
+++ en/welcome.php
@@ -88,7 +88,7 @@
 					echo '
 					<div class="gal-photo"><div class="photo-box">
 					<a href="https://ecobricks.org/details-ecobrick-page.php?serial_no='.$row["ecobrick_unique_id"].'"><img src="'.$row["thumb_url"].'?v=1"  alt="Ecobrick '.$row["ecobrick_unique_id"].' by '.$row["ecobrick_owner"].' in '.$row["location"].'" title="Ecobrick '.$row["ecobrick_unique_id"].' by '.$row["ecobrick_owner"].' in '.$row["location"].'" loading="lazy"/></a></div>';
-					echo '<div class="gal10-photo-text">'.$row["ecobrick_unique_id"].'</div>';
+				
 					echo '</div>';
 					}
 
@@ -155,7 +155,7 @@
 				echo '
 				<div class="gal-photo10">
 				<a href="https://ecobricks.org/details-ecobrick-page.php?serial_no='.$row["ecobrick_unique_id"].'"><img src="'.$row["ecobrick_full_photo_url"].'?v=1"  alt="Ecobrick '.$row["ecobrick_unique_id"].' by '.$row["ecobrick_owner"].' in '.$row["location"].'" title="Ecobrick '.$row["ecobrick_unique_id"].' by '.$row["ecobrick_owner"].' in '.$row["location"].'" loading="lazy"/></a>';
-				echo '<div class="gal10-photo-text">By '.$row["ecobrick_owner"].' in '.$row["location"].'"</div>';
+			
 
 				}
 
