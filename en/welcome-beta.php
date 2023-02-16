f13ee33b1864790c053f7aa4fb5f8f1cf0e74f2e
Russell Maier <russ@earthen.io>
2023-02-16 10:42:25 +0800

    top ten

--- en/welcome-beta.php
+++ en/welcome-beta.php
@@ -24,7 +24,37 @@
 	
 		<div id="sliders">
 
+			<!-- TOP TEN ECOBRICKS-->
+
+	<div class="gallery-background">
+
+<div class="gallery10-content-block">
 			
+	<div class="flex-container10">
+			
+		<?php
+		$sql = "SELECT * FROM vw_top_10_last_month ;";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+
+		echo '
+		<div class="gal-photo10">
+		<a href="https://ecobricks.org/details-ecobrick-page.php?serial_no='.$row["ecobrick_unique_id"].'"><img src="'.$row["ecobrick_full_photo_url"].'?v=1"  alt="Ecobrick '.$row["ecobrick_unique_id"].' by '.$row["ecobrick_owner"].' in '.$row["location"].'" title="Ecobrick '.$row["ecobrick_unique_id"].' by '.$row["ecobrick_owner"].' in '.$row["location"].'" loading="lazy"/></a>';
+		echo '<div class="gal10-photo-text"><b>Ecobrick '.$row["ecobrick_unique_id"].'</b><br>By '.$row["ecobrick_owner"].'<br>'.$row["location"].'</div></div>';
+
+		}
+
+		} else {
+		echo "Failed to connect to the Brikchain database";
+		}
+
+		?>
+				
+	</div>
+	
+</div>
 
 
 </div>
