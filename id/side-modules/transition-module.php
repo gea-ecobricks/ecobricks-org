803e5b87aeb2485f6e6eca5f60c3e991be7b38fa
Russell Maier <russ@earthen.io>
2023-02-21 14:35:15 +0800

    uild i

---
+++ id/side-modules/transition-module.php
@@ -0,0 +1,9 @@
+
+			<div id="side-module-desktop-mobile">
+				<img src="webp/earthhome-400px.webp" width="100%" loading="lazy" alt="eco brik and earth building can make regenerative structures">
+				<h4>Transisi Plastik</h4>
+				<h5>Membangun Ecobrick adalah teknologi transisi. Alih-alih melihat plastik sebagai 'limbah', kami melihatnya sebagai blok bangunan untuk visi hijau kami.</h5><br>
+			
+                <button type=button class="module-btn" onclick="location.href='transition.php'" aria-label="Learn more" title="Pelajari lebih lanjut tentang transisi plastik">Pelajari Lebih Lanjut</button>
+                <br>
+			</div>
