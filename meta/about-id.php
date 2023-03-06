ba6a4fb7097381ca0b6ee8478b2992215e449f6f
Russell Maier <russ@earthen.io>
2023-03-06 17:24:39 +0800

    richard up

--- meta/about-id.php
+++ meta/about-id.php
@@ -2,20 +2,20 @@
 
 <title>Tentang Kami | Ecobricks.org</title>
 
-<meta name="keywords" content="global ecobrick alliance, GEA, gea, ecobricks.org, earth enterprise, about us, ecobrick company, ecobrick organization, ecobrick NGO, earthen, principled.">
+<meta name="keywords" content="aliansi ecobrick global, GEA, gea, ecobricks.org, perusahaan bumi, tentang kami, perusahaan ecobrick, organisasi ecobrick, LSM ecobrick, tanah, berprinsip.">
 
-<meta name="description" content="The Global Ecobrick Alliance is an Earth Enterprise accelerating local and global plastic transition through earthen principled ecobricking">
+<meta name="description" content="Aliansi Ecobrick Global adalah Perusahaan Bumi yang mempercepat transisi plastik lokal dan global melalui ecobricking berprinsip tanah">
 	
 	<!-- Facebook Open Graph Tags for social sharing-->
 
 	<meta property="og:url"           content="https://www.ecobricks.org/id/about.php"/>
 	<meta property="og:type"          content="website">
  	<meta property="og:title"         content="Tentang Kami | Ecobricks.org">
-  	<meta property="og:description"   content="The Global Ecobrick Alliance is an Earth Enterprise accelerating local and global plastic transition through earthen principled ecobricking."/>
+  	<meta property="og:description"   content="Aliansi Ecobrick Global adalah Perusahaan Bumi yang mempercepat transisi plastik lokal dan global melalui ecobricking berprinsip tanah."/>
   	<meta property="og:image"         content="pngs/gea-perspective-1000px.png"/>
   	<meta property="og:image:width" content="1000px" />
 	<meta property="og:image:height" content="1039px" />
-  	<meta property="og:image:alt"     content="Our GEA seven circle, mandalic, hexagonal logo that resembles a milstein ecobrick module with each color of the spectrum represented and progressing."/>
+  	<meta property="og:image:alt"     content="Logo tujuh lingkaran, mandalik, heksagonal GEA kami yang menyerupai modul ecobrick milstein dengan setiap warna spektrum terwakili dan berkembang."/>
 	  <meta property="og:locale" content="id_ID" />
 
 	
