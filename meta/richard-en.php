eb8decf982d2efadaa1decb5e6d141fb7e07216f
Russell Maier <russ@earthen.io>
2023-03-06 17:42:49 +0800

    richard3

---
+++ meta/richard-en.php
@@ -0,0 +1,21 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Richard Goldsmith's Passing | Ecobricks.org</title>
+
+<meta name="keywords" content="Richard Goldsmith, obituary, passing, trainer, developer, ecobricker, father, friend, colleague">
+
+<meta name="description" content="It is with a heavy heart that we share the news of our friend and colleague Richard Goldsmith's passing...">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://www.ecobricks.org/en/richard.php"/>
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="Richard Goldsmith's Passing | Ecobricks.org">
+  	<meta property="og:description"   content="It is with a heavy heart that we share the news of our friend and colleague Richard Goldsmith's passing..."/>
+  	<meta property="og:image"         content="Richard Goldsmith's Passing"/>
+  <!--	<meta property="og:image:width" content="1000px" />
+	<meta property="og:image:height" content="1039px" />-->
+  	<meta property="og:image:alt"     content="Richard looking fondly at an ecobrick"/>
+	  <meta property="og:locale" content="en_GB" />
+
+	
