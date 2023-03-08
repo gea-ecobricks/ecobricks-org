b84621ebc5e9490734f46fc7a37c42e15dc8a19b
Russell Maier <russ@earthen.io>
2023-03-08 09:51:54 +0800

    go baby

--- en/richard.php
+++ en/richard.php
@@ -12,8 +12,9 @@ Content Page template: v.1.0.1-->
 							  
 <div class="splash-content-block">
 	<div class="splash-text-box">
-		<div class="splash-heading">Richard Goldsmith.</div>
+		<div class="splash-heading">Richard Goldsmith</div>
 		<div class="splash-sub">— GEA Trainer</div>
+		<div class="splash-sub"><i>in memorandum</i></div>
 	</div>
 	<div class="splash-image"><img src="photos/richard-goldsmith.webp" style="width: 80%; width: 70%;
 border: 10px solid #666;
@@ -32,29 +33,37 @@ border-radius: 10px;"></div>
 
 			<div class="lead-page-paragraph">
 				
-				<p>It is with a heavy heart that we share the news of our friend and colleague Richard Goldsmith's passing.  Richard, one of our UK trainers, was known to many of you through his work on the GEA's development team. Personally, myself and the other team members, met with with him almost every Tuesday for the last two or three years. </p>
+				<p>It is with a heavy heart that we share the news of our friend and colleague Richard Goldsmith's passing.  Richard, one of our UK trainers, was known to many of you through his work on the GEA's development circle where, without fail for the last three years, he had met with the members of the team.</p>
 				</div>
 
 			<div class="page-paragraph">
 				  
 
-				<p>After battling with Covid in January in the hospital (and overcoming it!), just as he was recovering, he got an infection that the doctors weren't able to treat.</p>
+				<p>After battling Covid in January at the hospital (and overcoming it!), just as he was recovering, he got an infection that the doctors weren't able to treat.</p>
 
-<p>His digital dedication has touched all of us trainers-- and the global ecobrick community at large. His contribution developing GoBrik and Ecobricks.org has touched hundred of thousands of ecobrickers around the world who have been inspired by the platforms, or who put the tools to use.</p>
+<p>His dedication has touched all of us trainers— and the global ecobrick community at large. His contribution helping to develop GoBrik and Ecobricks.org has touched hundred of thousands of ecobrickers around the world who have been inspired by the platforms, or who put the tools to use.</p>
 
-<p>For those of you who didn't know him personally, most likely you knew of him-- tirelessly tweaking and fixing our authentication system, helping with validations and rejections-- all to ensure that quality ecobricks were being made around the world, and that plastic would be kept out of the biosphere.</p>
+<p>For those ecobrickers who didn't know him personally, most likely you knew of him, or were touched by his work: tirelessly tweaking and fixing our authentication system, helping with validations and rejections— all to ensure that quality ecobricks were being made around the world, and that plastic would be kept out of the biosphere.</p>
 
-<p>In a culture where we typically honor social status and accomplishments, it is worth noting the great ecological legacy Richard has left-- and his journey to get there.</p>
+<p>In a culture where we typically honor social status and accomplishments, it is worth noting the great ecological legacy Richard has left— and his journey to get there.</p>
 
-<p>Richard spent the bulk of his career working for some of the largest international banks as a database designer. After retiring, whereas some would choose a quiet life minding their own business, Richard went the other direction. He started to feel more and more concern about the traditional financial system that he helped build-- and the plastic he was consuming. He started his green transition by buying a Tesla-- which he was immensely proud. He bought it not so much for the ride (which he did love)-- but to help energize and encourage humanity's green transition.</p>
+<p>Richard spent the bulk of his career working for some of the largest international blue chip financial companies as a database designer. After retiring, whereas some would choose a quiet retirement minding their own business, Richard chose the other direction.</p>
 
-<p>It was around then that he stumbled upon ecobricks and brikcoins. He joined our UK training of trainer program, where Ani, Lucie and I had the great pleasure to meet with him numerous times in person.</p>
+<p>As he began to feel more and more concern about the traditional financial system that he helped build, he also began to feel more and more concern for the plastic he was consuming. Determined to do something about both, he started his green transition by buying a Tesla— of which he was immensely proud! However, he bought it not so much for the ride (which he did love!) but, as he put it "to help energize and encourage humanity's green transition".</p>
 
-<p>Not so much inclined to lead workshops, he was nonetheless determined to help out with the movement's IT needs. He put his extensive database skills to use helping us to manage the thousands of ecobrickers keen to log and record their hundreds of thousands of ecobricks. Whereas he could have used these skills to continue contracting on the side, he instead spent hundreds of hours helping us establish a nothing short of miraculous online platform for the global plastic transition movement.</p>
+<p>It was around then that he stumbled upon ecobricks and brikcoins.</p> 
 
-<p>Whereas other platforms around the world that cater to ~100,000 users cost millions of dollars to develop and maintain, with Richard's help our small development team of 3-5 volunteers, managed to do it with only several thousands dollars over the last few years-- money that came directly from the Brikchain system of plastic offsetting that he was instrumental in helping setup.</p>
+<p>Richard joined our UK training of trainer program in Newforest UK where Ani, Lucie and I had the great pleasure to meet with him numerous times in person.</p>
 
-<p>All this of course had an immense real world impact (and continues to do so). The database and serial number system that he up set honors, energizes and encourages plastic from being kept from polluting the biosphere. Thanks to his system every single ecobrick that has been logged is recorded with its own serial number inscribed digitally and physical onto the ecobrick-- two hundred thousand tiny mark's that Richard has left-- and countless tons of plastic that are secured from polluting rivers, forest and fields around the world.</p>
+<p>Not so much inclined to lead community workshops, he was nonetheless determined to help out with the community's IT needs. He put his extensive database skills to use helping us to manage the thousands of ecobrickers keen to log and record their hundreds of thousands of ecobricks. Whereas he could have used these skills to continue contracting on the side, he instead spent hundreds of hours helping us establish a nothing-short-of-miraculous online platform for the global plastic transition movement.</p>
+
+<p>Whereas other platforms that serve ~100,000 users cost millions of dollars to develop and maintain, with Richard's help our small development team of 3-5 volunteers, managed to do it with only a few thousands dollars over the last few years— funds that came directly from the Brikchain system of plastic offsetting that he was instrumental in helping setup.</p>
+
+<div style="width:100%"><img src="../webp/richard-system.webp" style="width:100%"><h6>A dynamic flow chart of the API system that Richard setup that processes every ecobrick that is authenticated</h6></div>
+
+<p>All this of course had an immense real world impact (and continues to do so). The database and serial number system that he up set honors, energizes and encourages ecobricks to be made.  Or in other words: keeping plastic out of the biosphere and out of carbon-intensive industrial facilities. Thanks to Richard's API system every single ecobrick that has been logged is recorded with its own serial number inscribed digitally and physical onto the ecobrick— now over two hundred thousand tiny mark's that Richard can take credit for—  and the corresponding tons of plastic that are secured from polluting rivers, forest and fields around the world.</p>
+
+
 
 <p>All this is to say that Richard's passing will leave a large hole that we'll struggle to fill over the next months and years.</p>
 
