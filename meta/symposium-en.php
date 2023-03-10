d3bae7c2ad72098d42d4369be7d5160a0484860e
Russell Maier <russ@earthen.io>
2023-03-10 18:27:29 +0800

    go baby

---
+++ meta/symposium-en.php
@@ -0,0 +1,31 @@
+
+<title>Earthen Ethics Symposium  |  April 23-30 2023</title>
+
+<meta name="keywords" content="eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-bricks, brik, briks, plastic, plastic management, carbon sequestration, earthen ethics, plastic, recycling alternatives, incineration, plastic offsets, plastic solutions, upcycling, plastic sequestration, recycling alternative, incineration alternative, preventing plastic pollution, plastic pollution, regenerative plastic solution, waste, waste management, plastic waste, non-capital, zero-waste, net-zero, plastic sequestration"> 
+
+<meta name="description" content="A series of lectures, workshops and talks on what it means to follow Earth's example in our modern world.">
+	
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+  <meta property="og:url"           content="https://ecobricks.org/symposium">
+  <meta property="og:type"          content="website">
+ <meta property="og:title"         content="Earthen Ethics Symposium  |  April 23-30 2023">
+  <meta property="og:description"   content="A series of lectures, workshops and talks on what it means to follow Earth's example in our modern world."/>
+  <meta property="og:image"         content="https://www.ecobricks.org/pngs/social-banner2.png"/>
+  <meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="638" />
+  <meta property="og:image:alt"     content="A metaphorical road winding into the distance with various eco brick and earth constructions along side it."/>
+  <meta property="og:locale" content="en_GB" />
+
+  <meta name="author" content="Ecobricks.org">
+  
+  <meta property="og:type" content="article" />
+ <meta property="og:site_name" content="Ecobricks.org" />
+ <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" />
+ <meta property="article:modified_time" content="2022-10-17T09:14:13+00:00" />
+ <meta property="og:image:type" content="image/png" />
+ <meta name="author" content="Global Ecobrick Alliance" />
+ <meta name="twitter:card" content="summary" />
+ <meta name="twitter:label1" content="Est. reading time" />
+ <meta name="twitter:data1" content="3 minutes" /> 
