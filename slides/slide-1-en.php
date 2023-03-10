d0c6fda3dc943f1891f9981e579d368c056e5c10
Russell Maier <russ@earthen.io>
2023-03-10 13:05:37 +0800

    symp

--- slides/slide-1-en.php
+++ slides/slide-1-en.php
@@ -7,10 +7,26 @@
         </div>
 		
         <div id="slider-textbox">
+            <div id="slider-heading">Earthen Ethics Symposium</div>
+            <div id="slider-date">GEA News</div>
+            <div id="slider-body">23-30 April 2023</div>
+            <a class="slider-btn" href="register.php">Register</a>	
+		    </div>
+	
+    </div>
+
+
+    <!--
+    <div id="slider1">
+
+		<div id="slider-image"><img src="svgs/earth-home-animated-v4.svg?v=2" alt="Regenerative principles" width="100%" height="102%" style="min-width:175px;">
+        </div>
+		
+        <div id="slider-textbox">
             <div id="slider-heading">Our Principles</div>
             <div id="slider-date">Sept 9th, 2022 Updated | Core Content</div>
             <div id="slider-body">Ecobricking is guided by Earthen principles.  By following the Earth's example of carbon care we can build the green world that we all long to live in.  One ecobrick at a time.</div>
             <a class="slider-btn" href="principles.php">Learn More</a>	
 		    </div>
 	
-    </div>
+    </div>-->
--- slides/slide-2-en.php
+++ slides/slide-2-en.php
@@ -7,12 +7,26 @@
         </div>
 		
         <div id="slider-textbox">
+            <div id="slider-heading">The Power of Us</div>
+            <!--<div id="slider-date">Sept 9th, 2022 Updated | Core Content</div>-->
+            <div id="slider-body">Planet wide individuals are working daily to cocreate a more hospitable home and greener planet for all living beings.</div>
+            <a class="slider-btn" href="build.php">Learn More</a>	
+		</div>
+	
+    </div>
+
+    <!--    <div id="slider2">
+
+    <div id="slider-image"><img src="svgs/building-methods.svg" style="width: 85%;" alt="Regenerative ecobrick building methods">
+        </div>
+		
+        <div id="slider-textbox">
             <div id="slider-heading">Build with Ecobricks</div>
             <div id="slider-date">Sept 9th, 2022 Updated | Core Content</div>
             <div id="slider-body">Learn about the various ways to put ecobricks to good use in everything from module to earth construction.</div>
             <a class="slider-btn" href="build.php">Learn More</a>	
 		</div>
 	
-    </div>
+    </div>-->
 
 
