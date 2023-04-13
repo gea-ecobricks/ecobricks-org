a1f658a38d78a1201c2801cac32b3d500c6dcaf0
Russell Maier <russ@earthen.io>
2023-04-13 15:35:00 +0800

    5

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 
 <!-- This calls our Github hosted stylesheet - if it gets hopelessly cached and doesn't load, up the version number! -->
 		
-<link rel="stylesheet" type="text/css" href="http://ecobricks.org/stylesheet-general.css?v2.9">
+<link rel="stylesheet" type="text/css" href="http://ecobricks.org/stylesheet-general.css?v=3">
 
 <meta property="og:site_name" content="Ecobricks.org">
 <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org">
