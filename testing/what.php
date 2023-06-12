0543dd6f055e3110df18aa9ba3648cae95c2032b
JosephOFlanagan <joseph.oflanagan@yahoo.co.uk>
2023-06-12 14:11:49 +0100

    Update what.php
    Attempt to fix to the formatting issues that this page has to be more in line with the rest of the page.

--- testing/what.php
+++ testing/what.php
@@ -7,109 +7,122 @@
 <!--Once translation is completed, you can remove the line below to take the translation notice off the page-->
 <!--<?php require_once ("translate-please.php");?>-->
 
-
+<div class="splash-content-block">
+	<div class="splash-box">
+		<div class="splash-heading">Ecobricks are a simple, low-tech, non-capital, plastic transition technology that follows the Earth’s example of carbon care.</div>
+		
+	</div>
+	<div class="splash-image"><img src="https://ecobricks.org/svgs/eb-blue-no-clouds.svg" style="width:85%;"></div>	
+</div>
+<div id="splash-bar"></div>
 
 <!-- PAGE CONTENT-->
-	<h4>
-	In every moment we have the power to contribute with our choices to make the change we want to see in the world.</h4>
-	<p>Every day Counts.  Small changes make a big impact together.</p>
+<a name="top"></a>
 
-	<div class="big-header">Composting</div>
-	<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
-	<div class="sub-text">
-	<p>Composting is mother nature's waste management system.  All organic things compost naturally.  The human race can emulate this practice.  Anything from piling leaves to wormeries are considered composting.</p>
+<div id="main-content">
+<!-- The flexible grid (content) -->
+	<div class="row">
+		<div class="main">
+			<div class="lead-page-paragraph">
+			<h4>In every moment we have the power to contribute with our choices to make the change we want to see in the world.</h4>
+			<p>Every day Counts.  Small changes make a big impact together.</p>
+		</div>
 
-	<p>Watch Lucie Mann, New Forest Aquaponics CIC and Global Ecobrick Alliance Core Team explain how she composts all sorts of food waste from the food waste programme being run in the New Forest, UK.</p>
+		<div class="reg-content-block" id="block1">
+			
+			<div class="big-header">Composting</div>
+			<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
+			<div class="sub-text">
+				<p>Composting is mother nature's waste management system.  All organic things compost naturally.  The human race can emulate this practice.  Anything from piling leaves to wormeries are considered composting.</p>	
 
-	<p>As Lucie and her family run an aquaponics farm, the composting she undertakes is part of a fully circular permaculture ecosystem.</p>
-	</div>
+				<p>Watch Lucie Mann, New Forest Aquaponics CIC and Global Ecobrick Alliance Core Team explain how she composts all sorts of food waste from the food waste programme being run in the New Forest, UK.</p>
 
-	<div class="big-header">Permaculture</div>
-	<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
-	<div class="sub-text">
-	<p>Permaculture is the development of sustainable and self-efficient agricultural ecosystems. We have [speaker name] presenting our permaculture offering.  Like Mother Nature permaculture is a mandalic act of mindful recycling
-	</p>
-	</div>
-	<div class="big-header">Mandalic Collaboration</div>
-	<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
-	<div class="sub-text">
-		<p>To improve our connection with the earth’s natural cycles we practise Mandala design principles.<p>
+				<p>As Lucie and her family run an aquaponics farm, the composting she undertakes is part of a fully circular permaculture ecosystem.</p>
+			</div>
 
-		Join Ani Himawati in a mindful mandala meditation.  Training our minds to think circularly through artistic impressions using natural resources.</p>
-	</div>
+			<div class="big-header">Permaculture</div>
+			<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
+				<div class="sub-text">
+					<p>Permaculture is the development of sustainable and self-efficient agricultural ecosystems. We have [speaker name] presenting our permaculture offering.  Like Mother Nature permaculture is a mandalic act of mindful recycling
+				</p>
+			</div>
+			<div class="big-header">Mandalic Collaboration</div>
+			<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
+			<div class="sub-text">
+				<p>To improve our connection with the earth’s natural cycles we practise Mandala design principles.<p>
 
-	<div class="big-header">Food Poverty/Food Banks</div>
-	<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
-	<div class="sub-text">
-		<p>Despite there being more than enough food in the world to feed everyone one on the planet, many are still facing food poverty.  Some people are fortunate enough to be able to access food banks (UK) to reduce the impact.</p>
+				Join Ani Himawati in a mindful mandala meditation.  Training our minds to think circularly through artistic impressions using natural resources.</p>
+			</div>
 
-		<p>Join Waterside Food Waste project whilst they share how they set up and operate in their local community and the invaluable impact it has had on the people who live in perceived affluent area but has deep pockets of people living with food poverty due to the cost of living.</p>
+			<div class="big-header">Food Poverty/Food Banks</div>
+			<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
+			<div class="sub-text">
+				<p>Despite there being more than enough food in the world to feed everyone one on the planet, many are still facing food poverty.  Some people are fortunate enough to be able to access food banks (UK) to reduce the impact.</p>
 
-	</div>
+				<p>Join Waterside Food Waste project whilst they share how they set up and operate in their local community and the invaluable impact it has had on the people who live in perceived affluent area but has deep pockets of people living with food poverty due to the cost of living.</p>
 
-	<div class="big-header">Zero Waste Shops</div>
-	<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
-	<div class="sub-text">
-		<p>There are more and more zero waste shops opening in the UK
-		</p>
+			</div>
 
-	</div>
+			<div class="big-header">Zero Waste Shops</div>
+			<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
+			<div class="sub-text">
+				<p>There are more and more zero waste shops opening in the UK
+				</p>
 
-	<div class="big-header">Waste Streams around the World</div>
-	<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
-	<div class="sub-text">
-		<p>Waste management infrastructure differs greatly around the world.  Often the countries with the least infrastructure are the ones that are taking others waste to fund the country.  </p>
-		<p>Join us in this review of how waste management differs around the world and the impact your waste could be having when we entrust it to waste management companies.</p>
+			</div>
 
-	</div>
+			<div class="big-header">Waste Streams around the World</div>
+			<!--<div class="lead-paragraph">Join the Regenerative Movement</div>-->
+			<div class="sub-text">
+				<p>Waste management infrastructure differs greatly around the world.  Often the countries with the least infrastructure are the ones that are taking others waste to fund the country.  </p>
+				<p>Join us in this review of how waste management differs around the world and the impact your waste could be having when we entrust it to waste management companies.</p>
 
-			 <a href="faqs.php" button class="main-button">Register</a>
+			</div>
+
+			<a href="faqs.php" button class="main-button">Register</a>
 			<h6><a href="what.php">Find out more</a> | <a href="how.php">Schedule</a> | <a href="why.php">Speakers</a></h6>
 		</div>
 	</div>
 	
 <!-- PAGE SIDE BAR STARTS HERE-->
 
-		<div class="side">
+	<div class="side">
 
-			<div id="side-module-desktop-mobile">
-			<img src="webp/earthen-400px.webp" width="90%">
-				<h4>Plastic Sequestration</h4>
-				<h5>Ecobricking follows the Earth's example of carbon care to sequester plastics out of the biosphere.</h5><br>
-				<a href="sequest.php" target="_blank" class="module-btn">Learn More</a>
-			</div>
+		<div id="side-module-desktop-mobile">
+		<img src="webp/earthen-400px.webp" width="90%">
+			<h4>Plastic Sequestration</h4>
+			<h5>Ecobricking follows the Earth's example of carbon care to sequester plastics out of the biosphere.</h5><br>
+			<a href="sequest.php" target="_blank" class="module-btn">Learn More</a>
+		</div>
 				
-			<div id="side-module-desktop-only">
-			<img src="webp/earth-home.webp" width="85%">
-				<h4>Regenerative</h4>
-				<h5>Ecobricking is based on following the Earth's example and is guided by seven green principles.</h5><br>
-				<a class="module-btn" href="/principles">Principles</a>
-			</div>
-
-			<div id="side-module-desktop-mobile">
-				<img src="webp/spiral-circular-400px.webp" width="90%">
-				<h4>Circular Design</h4>
-				<h5>Ecobricks and their building applications are circular by design.</h5><br>
-				<a class="module-btn" href="/circular">Learn More</a>
-			</div>
-
-	
-			<div id="side-module-desktop-mobile">
-				<img src="webp/gea-logo-400px.webp" width="90%">
-				<h4>Global Ecobrick Alliance</h4>
-				<h5>The GEA is dedicated to accelerating plastic transition.  We preside over the GoBrik app and the Brikcoin blockchain.</h5><br>
-				<a class="module-btn" href="about.php">About Us</a>
-			</div>
-
-			<div id="side-module-desktop-mobile">
-				<img src="webp/for-earth500px.webp" width="80%">
-				<h4>For-Earth Enterprise</h4>
-				<h5>By discolosing our carbon, plastic and biodiversity impacts and by ensuring that they are net-green, the GEA commits to for-Earth Enterprise</h5><br>
-				<a class="module-btn" href="2021-regenreport.php">Learn More</a>
-			</div>
-
+		<div id="side-module-desktop-only">
+		<img src="webp/earth-home.webp" width="85%">
+			<h4>Regenerative</h4>
+			<h5>Ecobricking is based on following the Earth's example and is guided by seven green principles.</h5><br>
+			<a class="module-btn" href="/principles">Principles</a>
 		</div>
 
+		<div id="side-module-desktop-mobile">
+			<img src="webp/spiral-circular-400px.webp" width="90%">
+			<h4>Circular Design</h4>
+			<h5>Ecobricks and their building applications are circular by design.</h5><br>
+			<a class="module-btn" href="/circular">Learn More</a>
+		</div>
+
+	
+		<div id="side-module-desktop-mobile">
+			<img src="webp/gea-logo-400px.webp" width="90%">
+			<h4>Global Ecobrick Alliance</h4>
+			<h5>The GEA is dedicated to accelerating plastic transition.  We preside over the GoBrik app and the Brikcoin blockchain.</h5><br>
+			<a class="module-btn" href="about.php">About Us</a>
+		</div>
+
+		<div id="side-module-desktop-mobile">
+			<img src="webp/for-earth500px.webp" width="80%">
+			<h4>For-Earth Enterprise</h4>
+			<h5>By discolosing our carbon, plastic and biodiversity impacts and by ensuring that they are net-green, the GEA commits to for-Earth Enterprise</h5><br>
+			<a class="module-btn" href="2021-regenreport.php">Learn More</a>
+		</div>
 	</div>
 </div>
 
