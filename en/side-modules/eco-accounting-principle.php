d8a0a7aa9ae896e9e7a07ecca4ad31d6851c7bd4
Russell Maier <russ@earthen.io>
2023-06-14 12:42:53 +0800

    mdoules2

---
+++ en/side-modules/eco-accounting-principle.php
@@ -0,0 +1,8 @@
+
+
+			<div class="side-module-desktop-mobile">
+				<img src="icons/eco-accounting-icon-from-ecobricks-org.svg?v=5" width="70%" alt="Ecological Accounting of grey and green impacts">
+				<h4>Ecological Accounting</h4>
+				<h5>Accounting for our grey and green ecological impacts is a key component of regenerative enterprise.</h5><br>
+				<a class="module-btn" href="regenreports.php" target="_blank">See Ours</a>
+			</div>
