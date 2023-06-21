1f9758b54b3237f617d94e9fa26d999b6b442f84
Russell Maier <russ@earthen.io>
2023-06-21 16:55:39 +0800

    up menu2

--- meta/principles-id.php
+++ meta/principles-id.php
@@ -1,8 +1,8 @@
 <!-- Meta tags for page display and search engine listing-->
 
-<title>Prinsip Regeneratif</title>
+<title>Prinsip Bumi</title>
 
-<meta name="keywords" content="prinsip regeneratif, biosfer, etika ekologi, transcaste, for-earth, perhitungan ekologis, desain melingkar, desain spiral, akuntansi keanekaragaman hayati, desain siklus, etika tanah, vitalisasi keanekaragaman hayati, penyerapan plastik, daur ulang, alternatif, penyerapan plastik, penggantian plastik, aes plastik, penyerapan karbon, etika bumi">
+<meta name="keywords" content="prinsip regeneratif, silat bumi, princip bumi, biosfer, etika ekologi, transcaste, for-earth, perhitungan ekologis, desain melingkar, desain spiral, akuntansi keanekaragaman hayati, desain siklus, etika tanah, vitalisasi keanekaragaman hayati, penyerapan plastik, daur ulang, alternatif, penyerapan plastik, penggantian plastik, aes plastik, penyerapan karbon, etika bumi">
 
 <meta name="description" content="Ecobrick dipandu oleh prinsip-prinsip yang mengarahkan kita menuju keselarasan yang semakin meningkat dengan siklus ekologi di mana kita menjadi bagiannya.">
 	
@@ -23,7 +23,7 @@
   <meta property="og:type" content="article" />
  <meta property="og:site_name" content="Ecobricks.org" />
  <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" />
- <meta property="article:modified_time" content="2022-08-14T09:14:13+00:00" />
+ <meta property="article:modified_time" content="2023-06-21T09:14:13+00:00" />
  <meta property="og:image:type" content="image/png" />
  <meta name="author" content="Global Ecobrick Alliance" />
  <meta name="twitter:card" content="summary" />
