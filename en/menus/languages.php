5e47469a351ee724f75a5efd4580403faae81957
Russell Maier <russ@earthen.io>
2023-06-22 16:55:44 +0800

    languages main

---
+++ en/menus/languages.php
@@ -0,0 +1,7 @@
+         
+<a href='https://ecobricks.org/en/<?php echo ($name); ;?>' >🇬🇧 EN</a>
+<a href='https://ecobricks.org/id/<?php echo ($name); ;?>'>🇮🇩 IN</a>
+<a href='https://ecobricks.org/es/<?php echo ($name); ;?>'>🇪🇸 ES</a>
+<a href='https://ecobricks.org/fr/<?php echo ($name); ;?>'>🇫🇷 FR</a>
+                
+<a href="javascript:void(0);" style="font-size:15px;" class="icon-lang" onclick="languageMenu()"></a>
