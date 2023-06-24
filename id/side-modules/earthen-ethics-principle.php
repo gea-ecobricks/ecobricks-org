be9ced5b3edae9671234c92a2ae3f03d50f65b41
Russell Maier <russ@earthen.io>
2023-06-24 13:40:31 +0800

    silat

---
+++ id/side-modules/earthen-ethics-principle.php
@@ -0,0 +1,7 @@
+<div class="side-module-desktop-mobile">
+				<img src="../icons/earthen-ethics-icon-ecobricks-org.svg" width="80%" alt="For-Earth Enterprise through eco bricking">
+				<h4>Silat Bumi</h4>
+				<h5>Ecobrick didasarkan pada lima prinsip contoh Bumi.</h5><br>
+				<a class="module-btn" href="https://book.earthen.io/summary" target="_blank">Belajarlah lagi</a>
+				<h6 style="font-size:smaller">Summary of Earthen Ethics ⇗</h6>
+			</div>
