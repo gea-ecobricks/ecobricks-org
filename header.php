aa7db24632b8e2404d1360e5dd5ad12d867b2bcc
Russell Maier <russ@earthen.io>
2023-07-07 10:46:07 +0800

    up how

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 
 <!-- This calls our Github hosted stylesheet - if it gets hopelessly cached and doesn't load, up the version number! -->
 		
-<link rel="stylesheet" type="text/css" href="../stylesheet-general.css?v=4.2">
+<link rel="stylesheet" type="text/css" href="../stylesheet-general.css?v=4.3">
 
 <meta property="og:site_name" content="Ecobricks.org">
 <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org">
