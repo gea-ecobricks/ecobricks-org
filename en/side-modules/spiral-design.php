f7ea5444dd9ace2cfd032f2176df52befc5d39cc
Russell Maier <russ@earthen.io>
2023-10-30 18:15:14 +0000

    upsy

--- en/side-modules/spiral-design.php
+++ en/side-modules/spiral-design.php
@@ -1,6 +1,7 @@
 <div class="side-module-desktop-mobile">
-				<img src="icons/cyclical-design-icon-ecobricks-org.svg" width="80%" loading="lazy" alt="eco brik applications are circular and spiral in design">
-				<h4>Cyclical Design</h4>
-				<h5>Ecobrick applications follows the Earthen principle of crafting indefinite cycles that ever enriching, both socially and ecologically.</h5><br>
-				<a class="module-btn" href="/circular">Learn More</a>
+				<img src="icons/spiral-design-icon-ecobricks-org.svg" width="80%" loading="lazy" alt="eco brik applications are circular and spiral in design">
+				<h4>Spiral Design</h4>
+				<h5>Ecobrick applications follow the Earthen principle of crafting enriching cycles-- both socially and ecologically.</h5><br>
+				<a class="module-btn" href="circular">Learn More</a>
+				<h6 style="font-size:smaller">↗️ Learn about this Earthen principle</h6>
 			</div>
