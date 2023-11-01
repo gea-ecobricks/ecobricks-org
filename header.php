69345dc9815da15cb24aefad97e2e890a1b729f2
Russell Maier <russ@earthen.io>
2023-11-01 16:42:25 +0000

    font weight"

--- header.php
+++ header.php
@@ -98,13 +98,15 @@
   font-family: "Mulish";
   src: url("../fonts/Mulish-Light.ttf");
   font-display: swap;
+  font-weight: 300;
  }
 
  @font-face {
   font-family: "Mulish";
   src: url("../fonts/Mulish-Regular.ttf");
   font-display: swap;
- }
+  font-weight: 500;
+ } 
 
  @font-face {
   font-family: "Arvo";
