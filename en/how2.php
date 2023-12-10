81abc0341c8d15478eb3a291c0d5ab5212fb7d15
Russell Maier <russ@earthen.io>
2023-12-10 15:36:45 +0800

    go now all

--- en/how2.php
+++ en/how2.php
@@ -444,7 +444,7 @@ Once translation is completed, you can remove the line below to take the transla
 	<br>
 	<div class="page-paragraph-reg">
 
-		<div id="video-placeholder" style="width:600px; height:400px; background:url('webp/how-to-preview.webp') center center no-repeat; cursor:pointer; position: relative;">
+		<div id="video-placeholder" style="width:600px; height:400px; background:url('../webp/how-to-preview.webp') center center no-repeat; cursor:pointer; position: relative;">
 			<div id="play-button"></div>
 		</div>
 
@@ -505,7 +505,7 @@ Once translation is completed, you can remove the line below to take the transla
 <!-- CUSTOM PAGE SCRIPTS-->
 
 <!-- This script is for pages that use the accordion content system-->
-<script src="../accordion-scripts.js" defer></script>
+<script src="../scripts/accordion-scripts.js" defer></script>
 
 
 <script>
