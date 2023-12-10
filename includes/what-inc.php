89139bf2d06aa4c761d6d7d6a37b2aa1ebf7a37c
Russell Maier <russ@earthen.io>
2023-12-10 10:00:04 +0800

    what on

--- includes/what-inc.php
+++ includes/what-inc.php
@@ -180,11 +180,11 @@ _END;?>
 
 <!-- This loads the page's meta tags:  Be sure the page name is in place in English-->
 
-<?php require_once ("meta/what-$lang.php");?>
+<?php require_once ("../meta/what-$lang.php");?>
 
 <!--This loads the page's header-->
 
-<?php require_once ("header.php");?>
+<?php require_once ("../header.php");?>
 
 </head>
 							  				  
