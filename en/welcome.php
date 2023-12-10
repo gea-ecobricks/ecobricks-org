6800ae05c45015360f7f0cc3d7a34f3498105b4c
Russell Maier <russ@earthen.io>
2023-12-10 13:45:20 +0800

    d

--- en/welcome.php
+++ en/welcome.php
@@ -5,11 +5,7 @@ echo <<<_END
 _END;?>
 <HEAD>
 <meta charset="utf-8">
-
-
-	<!--PAGE LANGUAGE:  ENGLISH-->  
-
-
+<!--PAGE LANGUAGE:  ENGLISH-->  
 <!-- Translators:   Look for untranslated text inside HTML tags.  In other words <a tag>any content text between markers like these</a tag>.  Don't worry about translating these comments.  Be sure NOT to translate english page names, file names, div names, div class names, or html syntax.-->
  
 <?php require_once ("../includes/welcome-inc.php");?>
