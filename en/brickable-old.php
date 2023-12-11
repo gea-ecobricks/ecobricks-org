5f2dfc1d0110323acaf9e02f720ac72cd79e7709
Russell Maier <russ@earthen.io>
2023-12-11 14:32:57 +0800

    new pages

---
+++ en/brickable-old.php
@@ -0,0 +1,127 @@
+<!--PAGE LANGUAGE:  ENGLISH
+Content Page template: v.1.0.1-->
+  
+<!-- TRANSLATORS:   Look for untranslated text inside HTML tags.  In other words <a tag>any content text between markers like these</a tag>.  Don't worry about translating these comments.  Be sure NOT to translate english page names, file names, div names, div class names, or html syntax.-->
+ 
+<?php require_once ("../includes/brickable-inc.php");?>
+
+<!--Once translation is completed, you can remove the line below to take the translation notice off the page-->
+<?php require_once ("under-construction.php");?>
+
+<!--PAGE BANNER-->
+
+	
+<div class="splash-content-block">
+	<div class="splash-box">
+		<div class="splash-heading">Ecobrickable Design</div>
+		<div class="splash-sub">Apply Spiral Design Principles to Your Product</div>
+	</div>
+	<div class="splash-image"><img src="https://ecobricks.org/svgs/brickable-icon-from-ecobricks-org.svg" style="width: 70%;"></div>	
+</div>
+<div id="splash-bar"></div>
+
+
+<!-- PAGE CONTENT-->
+
+<a name="top"></a>
+
+<div id="main-content">
+<!-- The flexible grid (content) -->
+	<div class="row">
+		<div class="main">
+
+			<div class="lead-page-paragraph">
+				
+			<p>The Ecobrickable Designator is a simple symbol that can be placed on single-use plastic to designate that it should be ecobricked.  Our Kit contains all the graphic files necessary to print stickers on insert into your digital packaging designs. Ecobrickable Designation enables companies to start their transition from linear to circular design, today.</p>
+			</div>
+
+			<div class="page-paragraph">
+				<p>The majority of today’s products are manufactured without thought to where they will go once consumed. Many products are even designed to fail within a certain period to promote further consumption. This design ethic (or lack of it) is one of the root causes of plastic pollution.  Learning from the ecologies around us, the first step is to shift from linear consumption design (and dead-end plastic products) to cradle-to-cradle, circular design.  Afterall, plastic isn’t a ‘bad’ material in and of itself– its how we’ve been putting it to use that has been causing problems.  Designing a product as Ecobrickable, and clearly designating it as so, can be a giant leap for a company into the brave new world of circular design and regenerative business.  The GEA has developed guidelines and a designator symbol to assist companies in their plastic transition.</i><p> 
+
+				<p>For many companies making the switch to circular design and extended producer responsibility (EPR) appears to require a massive investment in manufacturing, material sourcing as well as implementing new systems to reabsorb their dead products. Furthermore it may seem that the required organic materials are still on the horizon.  The transition can appear daunting!</p>
+				
+				<p>Making a product ecobrickable is a first step in a business’s plastic transition.</p>
+					
+				<p>With a little forethought and advanced planning, a product can be made easily and effectively ecobrickable.  In this way, ecobrickable designs enables companies and designers to make their first step to cradle-to-cradle and ayyew vision.  Designating a product or package as ecobrickable gives it a next life and purpose.  When ecobricked, the plastic is saved from a fate of industrial recycling, incineration, and/or dumping.  The ecobricked plastic is transformed into a reusable building block. Ecobricks and their applications are themselves cradle-to-cradle and follow regenerative principles, enabling an indefinite, localized cycle of reuse of the ecobricked plastic.</p>
+
+                <h3>Ecobrickable Design Guidelines</h3>
+
+<p>The GEA has established simple criteria to meet when designing your product or packaging.  It is obligatory that your product meet these stipulation in order to use Ecobrickable Designator Symbol your product or packaging must…</p>
+
+<ul>
+
+    <li>… not contain substances that will corrode PET plastic over time.</li>
+    <li>… not contain any primary or secondary biodegradable material.</li>
+    <li>… can be easily disassembled, broken or cut into parts that can fit within the 22mm diameter neck of a standard PET bottle.</li>
+    <li>...maximizes the ease of disassembly for this purpose.</li>
+    <li>… not have sharp protrusions that could puncture an ecobrick from within while it is being packed down.</li>
+    <li>… not contain reactive chemicals, or if so, these components are clearly marked as un-ecobrickable.</li>
+</ul>
+					
+				<p>The Ecobrickable Designator & Stickers are licensed under a Creative Commons Attribution-NoDerivatives 4.0 International License. This is to ensure the standardized and resonant usage of the designators by committed and resonant companies and individuals.   See copyright notice and terms below.</p>
+				<br><br>
+				<a class="action-btn" href="https://nextcloud.ecobricks.org/index.php/s/rL3yFwETbcLF9dR" target="_blank">🚀 Download Kit</a>
+			<p style="font-size: 0.85em; margin-top:20px;">Opens the dowloand location in a new window.</a></p>
+
+			</div>	
+
+
+
+
+
+		<div class="side">
+
+
+			
+		<div id="side-module-desktop-only">
+			
+			
+				<h4>Get Started</h4>
+				<h5>Use a free Catalyst Account to ignite your plastic transition.</h5><br>
+				<a class="action-btn" href="https://gobrik.com/#catalyze" target="_blank">🚀 Launch</a><br><br>
+			</div>
+
+	
+			<div id="side-module-desktop-mobile">
+				<img src="../webp/gea-logo-400px.webp" width="90%">
+				<h4>Global Ecobrick Alliance</h4>
+				<h5>The GEA is dedicated to accelerating plastic transition.  We preside over the GoBrik app and the Brikcoin blockchain.</h5><br>
+				<a class="module-btn" href="about.php">About Us</a>
+			</div>
+
+			<div id="side-module-desktop-mobile">
+				<img src="../webp/for-earth500px.webp" width="80%">
+				<h4>For-Earth Enterprise</h4>
+				<h5>By discolosing our carbon, plastic and biodiversity impacts and by ensuring that they are net-green, the GEA commits to for-Earth Enterprise</h5><br>
+				<a class="module-btn" href="2021-regen-report.php">Learn More</a>
+			</div>
+
+			<div id="side-module-desktop-mobile">
+				<img src="../webp/balancing-green.webp" width="80%">
+				<h4>Plastic Offsetting</h4>
+				<h5>Our Catalyst Program allows you to offset your plastic generation and support the global ecobrick movement.</h5><br>
+				<a class="module-btn" href="/aes">Learn More</a>
+			</div>
+
+		</div>
+
+	</div>
+</div>
+
+
+
+	<!--FOOTER STARTS HERE-->
+
+	<?php require_once ("../footers/footer-$lang.php");?>
+
+
+<!-- CUSTOM PAGE SCRIPTS-->
+
+<!-- This script is for pages that use the accordion content system-->
+<script src="../scripts/accordion-scripts.js" defer></script>
+
+
+</div>
+</body>
+</html>
+
