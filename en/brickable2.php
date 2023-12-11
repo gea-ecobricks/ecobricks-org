65d75eafe26b762050d201bbfa685d2eb1387a74
Russell Maier <russ@earthen.io>
2023-12-11 14:25:26 +0800

    arc and brikable

---
+++ en/brickable2.php
@@ -0,0 +1,285 @@
+<!--PAGE LANGUAGE:  ENGLISH
+Content Page template: v.1.0.1-->
+  
+<!-- TRANSLATORS:   Look for untranslated text inside HTML tags.  In other words <a tag>any content text between markers like these</a tag>.  Don't worry about translating these comments.  Be sure NOT to translate english page names, file names, div names, div class names, or html syntax.-->
+ 
+<?php require_once ("../includes/brickable-inc.php");?>
+
+<!--Once translation is completed, you can remove the line below to take the translation notice off the page-->
+<?php require_once ("under-construction.php");?>
+
+<!--PAGE BANNER-->
+
+	
+<div class="splash-content-block">
+	<div class="splash-text-box">
+		<div class="splash-heading">Brickable Design</div>
+		<div class="splash-sub">Make your plastic product or packaging circular using the Brickable Designator Icon.</div>
+	</div>	
+	<div class="splash-image"><img src="https://ecobricks.org/icons/ecobrickable-icon-from-ecobricks-org-white.svg?v=1" style="width: 70%;"></div>	
+</div>
+<div id="splash-bar"></div>
+
+<!-- PAGE CONTENT-->
+
+<a name="top"></a>
+
+<div id="main-content">
+<!-- The flexible grid (content) -->
+	<div class="row">
+		<div class="main">
+
+			<div class="lead-page-paragraph">
+
+			
+			<p>The Brickable Designator Icon can be placed on single-use plastics to designate its next life as an ecobrick.  Our free Kit contains everything you need to make your packaging and products circular and spiral.</p>
+				
+			
+			</div>
+
+			<div class="page-paragraph">
+				
+
+			<p>Transitioning from plastic is not easy.  Nor are today's industrial solutions for plastic processing satisfactory!  However, plastic in-itself is not the cause of its pollution.  Rather, it is our poor design and planning.  In particular: <i>the failure to designate a next-life for our plastic creations.  And the our failure tend our the plans for plastic towards  sequestration.</i></p>
+				
+				
+				
+			<p>Designing a product as circular is the first and essential step in a business’s plastic transition.  Planning our plastic toward's sequestration is the second.  Phasing it out entirely is the third.  Ecobrickable Designation enables companies to start their <a href="transition.php">plastic transition today</a>.</p>
+
+			</div>	
+
+
+            <div class="reg-content-block" id="block3">
+
+				<div class="opener-header">
+					
+					<div class="opener-header-text">
+					<h4>Philosophy</h4>
+					<h5>The context and call for cyclical design.</h5>
+					<br>
+					</div>
+
+					<button onclick="preclosed3()" class="block-toggle" id="block-toggle-show3">+</button>
+				</div>
+
+				<div id="preclosed3">
+					<br>
+					<div class="row">
+						<div class="main2">
+                        <p>For many companies making the transition to regenerative products is daunting.  It appears to require large investments in manufacturing, materials and systems. Furthermore it may seem that alternative organic materials do not yet meet health and safety standards and are not yet cost effective.</p>
+                    
+                   
+						</div>
+						<div class="side2"><img src="../svgs/vsrecycling.svg" width="80%" alt="cyclical design icon">
+						</div>
+					</div>
+
+					<div class="row2">
+						<div class="main3">
+						<p>As a transition-step, plastic products and packaging can be made brickable!</p> 
+						</div>
+						<div class="side3"><img src="../icons/cyclical-design-icon-ecobricks-org.svg" width="120px" alt="Cyclical Circular Design">
+						</div>
+					</div>
+					 
+					
+					<p>Designing a product as Ecobrickable, and clearly designating it as so, is a first step into circular and spiral design.  By designating a product or package as ecobrickable we can keep plastic out of industrial recycling and incineration.  Using the plastic to make it an ecobricks gives it a next life and purpose as a reusable building block.</p>
+					
+					<p>Ecobricks and their applications are themselves fully circular and based on <a href="principles.php">regenerative principles</a>, enabling the plastic's indefinite and localized reuse towards long-term <a href="sequestration.php">sequestration.</a>  Both the Brickable Designator Icon and ecobricking are based on following the Earth's example of carbon care and the ethics of ecologial contribution.</p>
+
+						<h6>🟢  <a href="https://earthen.io/spirals">Learn more about the Earthen Ethic of Spiralling Cycles</a></h6>
+
+					<h6>🟢  <a href="/circular">Learn more about Circular & Spiral Design</a></h6><br>
+				</div>
+			</div>
+            
+
+			<div class="reg-content-block" id="block1">
+				
+				<div class="opener-header">
+					
+					<div class="opener-header-text">
+						<h4>Brickable Icon Useage Criteria</h4>
+						<h5>The material criteria for products to use the designator icon.</h5>
+					</div>
+					
+
+					<button onclick="preclosed1()" class="block-toggle" id="block-toggle-show1">+</button>
+					
+				</div>
+
+				<div id="preclosed1">
+					<br>
+					<p>The GEA has established simple criteria to meet when designing your product or packaging.  It is obligatory that your product meet these stipulation in order to use Ecobrickable Designator Symbol your product or packaging must…</p>
+
+			<ol>
+
+			<li><p>Must not contain substances that will corrode PET plastic over time.</p></li>
+				<li><p>Must not contain any primary or secondary biodegradable material.</p></li>
+				<li><p>Must can be easily disassembled, broken or cut into parts that can fit within the 22mm diameter neck of a standard PET bottle.</p></li>
+				<li><p>Must maximizes the ease of disassembly for this purpose.</p></li>
+				<li><p>Must not have sharp protrusions that could puncture an ecobrick from within while it is being packed down.</p></li>
+				<li><p>Must not contain reactive chemicals, or if so, these components are clearly marked as un-brickable.</p></li>
+			</ol>
+						
+				</div>	
+			</div>
+
+			<div class="reg-content-block" id="block2">
+				<div class="opener-header">
+					<div class="opener-header-text">
+						<h4>Creative Commons License</h4>
+						<h5>Conditions for graphic use</h5><br>
+					</div>
+					<button onclick="preclosed2()" class="block-toggle" id="block-toggle-show2">+</button>
+				</div>
+
+				<div id="preclosed2">
+                <p>The Ecobrickable Designator & Stickers are licensed under a Creative Commons Attribution-NoDerivatives 4.0 International License. This is to ensure the standardized and resonant usage of the designators by committed and resonant companies and individuals.   See copyright notice and terms below.</p><br><br>
+				<h6><a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nd/4.0/88x31.png" /></a><br /><a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/">Creative Commons Attribution-NoDerivatives 4.0 International License</a>.</h6>
+				</div>
+			</div>
+
+<!--
+			<div class="reg-content-block" id="block4">
+
+				<div class="opener-header">
+					
+					<div class="opener-header-text">
+					<h4>See How We Use It.</h4>
+					<h5>When we use plastic, we use the icon.</h5>
+					<br>
+					</div>
+
+					<button onclick="preclosed4()" class="block-toggle" id="block-toggle-show4">+</button>
+				</div>
+
+				<div id="preclosed4">
+					
+					<p><b>The Global Ecobrick Alliance</b> </p>
+					<p>We're an enterprise too!  And despite all our best efforts, we consume and produce plastic in our operations.  When we and our trainers make use of plastic we make sure to label it with Brickable sticker</p><br>
+					<p>Here's some examples of our useage:</p>
+						
+			
+				</div>
+			</div>
+
+-->
+
+			<div class="reg-content-block" id="block4">
+
+				<div class="opener-header">
+					
+					<div class="opener-header-text">
+					<h4>See How Its Used.</h4>
+					<h5>Companies and corporations around the world use the designator icon for their non-recyclable packaging.</h5>
+					<br>
+					</div>
+
+					<button onclick="preclosed4()" class="block-toggle" id="block-toggle-show4">+</button>
+				</div>
+
+				<div id="preclosed4">
+					
+					<p><b></b> </p>
+					<p>The designator icon can be used directly in packaging designs or added as sticker to pre-existing packaging and products.</p><br>
+					<p>Here's some examples of our useage:</p>
+
+					<div id="flex-gallery" width="100%">
+						<a href="photos/brickable-1-716px.jpeg"><img src="../photos/brickable-1-716px.jpeg" width="30%" alt="A black sticker used on a chocolate powder plastic bag"></a>
+						<a href="photos/brickable-2-716px.jpeg"><img src="../photos/brickable-2-716px.jpeg" width="30%" alt="Integrated into the packaging of a Marimas juice product"></a>
+						<a href="photos/brickable-3-716px.jpeg"><img src="../photos/brickable-3-716px.jpeg" width="30%" alt="A stick on a home based business health drink product"></a>
+					</div>
+						
+			
+				</div>
+			</div>
+
+			
+	
+	<div id="main">
+
+		<div class="page-paragraph">
+			<br><br>
+			<h2><p>Get Started!</p></h2>
+			<p>Download the Brickable Designator Kit today to use on your legacy plastic products and packaging.  The ZIP package includes:</p>
+			<ul>
+				<li>Icon & Sticker Design Elements (Vector / PDF)</li>
+				<li>Recommended colors (Vector / PDF)</li>
+				<li>Example of A3 sticker printing (JPEG)</li>
+				<li>High, medium and low resolution icons (PNG)</li>
+				<li>Terms and Conditions of use (TXT)</li>
+				<li>Creative Commons Attribution-NoDerivatives 4.0 International License (TXT/PNG)</li>
+			<ul> 
+
+
+			<br>
+			<p><a class="action-btn" href="https://files.ecobricks.org/s/oq9xPkNksf2ZPNX" target="_blank">🚀 Download Kit</a></p>
+			<p style="font-size: 0.85em; margin-top:20px;">ZIP file | 3.2MB </a></p>
+		</div>
+	</div>
+		
+	</div>
+
+
+
+
+		<div class="side">
+
+
+			
+		<div id="side-module-desktop-only">
+			
+		
+		<img src="../webp/brickable-kit.webp?v=1" width="90%">
+				<h4>Get Started</h4>
+				<h5>Download the Brickable Designator Kit today.</h5><br>
+				<a class="action-btn" href="https://files.ecobricks.org/s/oq9xPkNksf2ZPNX" target="_blank">🚀 Download Kit</a>
+			<p style="font-size: 0.85em; margin-top:20px;">ZIP file | 3.2 MB </a></p>
+			</div>
+
+	
+			<div id="side-module-desktop-mobile">
+				<img src="../webp/gea-logo-400px.webp" width="90%">
+				<h4>Global Ecobrick Alliance</h4>
+				<h5>The GEA is dedicated to accelerating plastic transition.  We preside over the GoBrik app and the Brikcoin blockchain.</h5><br>
+				<a class="module-btn" href="about.php">About Us</a>
+			</div>
+
+			<div id="side-module-desktop-mobile">
+				<img src="../icons/cyclical-design-icon-ecobricks-org.svg" width="64%" alt="Following the Earth's example through eco bricking">
+				<br><h4>Circular & Spiral Design</h4>
+				<h5>Ecobricks and all their applications are designed for cycles of indefinite reuse.</h5><br>
+				<a class="module-btn" href="/circular">Circular Design</a>
+                <br>
+			</div>   
+
+			<div id="side-module-desktop-mobile">
+				<img src="../webp/balancing-green.webp" width="80%">
+				<h4>Plastic Offsetting</h4>
+				<h5>Our Catalyst Program allows you to offset your plastic generation and support the global ecobrick movement.</h5><br>
+				<a class="module-btn" href="/aes">Learn More</a>
+			</div>
+
+		</div>
+
+	</div>
+</div>
+
+
+
+	<!--FOOTER STARTS HERE-->
+
+	<?php require_once ("../footers/footer-$lang.php");?>
+
+
+<!-- CUSTOM PAGE SCRIPTS-->
+
+<!-- This script is for pages that use the accordion content system-->
+<script src="../scripts/accordion-scripts.js" defer></script>
+
+
+</div>
+</body>
+</html>
+
---
+++ en/brikchain.php
@@ -0,0 +1,312 @@
+<!--PAGE LANGUAGE:  ENGLISH-->  
+
+<!-- Translators:   Look for untranslated text inside HTML tags.  In other words <a tag>any content text between markers like these</a tag>.  Don't worry about translating these comments.  Be sure NOT to translate english page names, file names, div names, div class names, or html syntax.-->
+
+
+<?php require_once ("../includes/brikchain-inc.php");?>
+
+<!--Once translation is completed, you can remove the line below to take the translation notice off the page-->
+<?php require_once ("under-construction.php");?>
+
+<!--TOP PAGE BANNER-->
+
+<div class="splash-content-block">
+	<div class="splash-text-box">
+		<div class="splash-heading"><br>Brikchain Explorer</div>
+		<div class="splash-sub">All the Briks, Blocks & Transactions.</div>
+	</div>
+	<div class="splash-image"><img src="../webp/brikchain-450px.webp" style="width: 80%;"></div>	
+</div>
+<div id="splash-bar"></div>
+
+
+
+<!-- PAGE CONTENT-->
+
+<?php include 'ecobricks_env.php';?> 
+
+<a name="top"></a>
+<div id="main-content">
+<!-- The flexible grid (content) -->
+	<div class="row">
+		<div class="main">
+
+			<div class="lead-page-paragraph">
+				
+				<p>Search and explore the full chain of authenticated ecobricks, blocks and transactions that make up the brikcoin manual blockchain.</p>
+			</div>
+
+			<div class="page-paragraph">
+				  
+				<p>The <a href="brikchain.php">Brikchain is based on the ecological value of plastic <a href="sequest">sequestered</a> out of the biosphere and out of industry.  Every ecobrick that is authenticated on the <a href="/gobrik">GoBrik platform</a> is permanently recorded on the <a href="brikcoins">Brikcoin Manual Blockchain</a>.  With each authentication, the corresponding value of sequestered plastic (<a href="aes">AES plastic</a>) is issued in brikcoins</a>.  Each issuance is made through a block of transactions that are recorded sequentially.  This chain of blocks, transaction and ecobricks is fully searchable here.  
+				
+				<p>Use the tools below to browse the Brikchain.  You can also view our <a href="open-books.php">Open Books</a> financial accounting.</p>
+			</div>
+
+
+
+
+		</div>
+		
+
+		<div class="side">
+
+		
+		<?php require_once ("side-modules/brikcoin-live-values.php");?>
+
+         
+		</div>
+
+	</div>
+
+
+
+	<div class="reg-content-block" id="block2">
+		<div class="opener-header">
+			<div class="opener-header-text">
+				<h4>Authenticated Ecobricks</h4>
+				<h6>The archival chain of authenticated ecobricks.  Updated every six hours with the latest confirmed authentications.</h6>
+				<div class="ecobrick-data"><p><span class="blink">⬤  </span> Data live</p></div><div class="ecobrick-data"><p>🟠 Historical Data pending transfer</p></div>
+			</div>
+			
+			<button onclick="preclosed2()" class="block-toggle" id="block-toggle-show2">+</button>
+
+		</div>
+
+		<div id="preclosed2">
+
+			<div class="overflow">
+
+				<table id="ecobricks" class="display" style="width:100%">
+					<thead>
+						<tr>
+							<th style="max-width: 150px !important;">Brik</th>
+							<th>Authenticated</th>
+							<th>AES Plastic</th>
+							<th>Maker</th>
+							<th>Value</th>
+							<th>CO2e</th>
+							<th>🔎 Serial</th>
+						</tr>
+					</thead>
+					<!--<tfoot>
+						<tr>
+							<<th style="max-width: 150px !important;">Brik</th>
+							<th>Logged</th>
+							<th>AES Plastic</th>
+							<th>Value</th>
+							<th>CO2e</th>
+							<th>Serial</th>
+						</tr>
+					</tfoot>-->
+				</table>
+			</div>
+		</div>
+	</div>
+
+
+
+	<div class="reg-content-block" id="block1">
+		<div class="opener-header">
+			<div class="opener-header-text">
+				<h4>Blocks & Transactions</h4>
+				<h6>The full chain transactions chronicling the generation, exchange and destruction of brikcoins.</h6>
+				<div class="ecobrick-data"><p><span class="blink">⬤  </span> Data live & current</p></div>
+			</div>
+		
+			<button onclick="preclosed1()" class="block-toggle" id="block-toggle-show1">+</button>
+
+		</div>
+
+		<div id="preclosed1">
+
+			<div class="overflow">
+				<table id="brikchain" class="display" style="width:100%">
+					<thead>
+						<tr>
+							
+						<th>🔎 Transaction</th>
+						<th>Issued</th>
+							<th>Sender</th>	
+							<th>Recipient</th>	
+							<th>Type</th>
+							<th>Block</th>
+							<th>Shard</th>
+							<th>Ecobrick</th>
+
+						</tr>
+					</thead>
+					<!--<tfoot>
+						<tr>
+						<th>Transaction</th>
+						<th>Issued</th>
+							<th>Sender</th>	
+							<th>Type</th>
+							<th>Block</th>
+							<th>Shard</th>
+							<th>Ecobrick</th>
+						</tr>
+					</tfoot>-->
+				</table>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="reg-content-block" id="block3">
+				
+		<div class="opener-header">
+			
+			<div class="opener-header-text">
+			<h4>AES Plastic Valuations</h4>
+	
+			<h6>Each year the value of 1 Kg of AES plastic is determined by the ecobricks authenticated in that year.  The net weight of the authenticated plastic is divided by the GEA's expenses maintaining the blockchain (see the GEA's yearly <a href="open-books.php">Open Books</a> financial accounting)</h6>
+			<div class="ecobrick-data"><p><span class="blink">⬤  </span> Brikcoin Data live</p></div>
+			<div class="ecobrick-data"><p><span class="blink">⬤  </span> OpenBooks Accounting live</p></div><div class="ecobrick-data"><p>🟠 Ecobrick weight data estimated (pending archival transfer)</p></div>
+			</div>
+			<button onclick="preclosed3()" class="block-toggle" id="block-toggle-show3">+</button>
+
+		</div>
+
+		<div id="preclosed3">
+
+			<div class="overflow">
+
+			<?php
+
+			$sql = "SELECT * FROM vw_detail_sums_by_year Order by `year` DESC;";
+
+			$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) {
+			
+				echo'<table id="brikchain" class="display"><tr>
+				<th>Year</th>
+				<th>BRK Generated</th>
+				<!--<th>Authenticated</th>-->
+				<th>Calculated AES plastic</th>
+				<!--<th>Tallied AES Plastic</th>-->
+				<th>GEA Year Expenses</th>
+				<th>1kg AES Value</th></tr>';
+			
+			// output data of each row
+			while($row = $result->fetch_assoc()) {
+				
+				echo "<tr>
+				<td>".$row["year"]."</td>
+				<td>".$row["total_brk"]."&#8202;ß</td>
+				<!--<td>".$row["brick_count"]." ecobricks</td>-->
+				<td>".$row["calculated_weight"]."&#8202;Kg</td>
+				<!--<td>".$row["weight"]."&#8202;Kg</td>-->
+				<td>".$row["tot_usd_exp_amt"]."&#8202;$ USD</td>
+				<td>".$row["final_aes_plastic_cost"]."&#8202;$ USD</td>
+				</tr>"; 
+				}
+				echo "</table>";
+			} else {
+				echo "0 results";
+			}
+			?>
+			</div>
+		</div>
+	</div>
+
+	<div class="reg-content-block" id="block4">
+				
+		<div class="opener-header">
+			<div class="opener-header-text">
+				<h4>Total Brikcoin Pool</h4>
+				<h6>Running and yearly totals of brikcoins generated, destroyed and in circulation. </h6>
+				<div class="ecobrick-data"><p><span class="blink">⬤  </span> Data live & current</p></div>
+			</div>
+		
+			<button onclick="preclosed4()" class="block-toggle" id="block-toggle-show4">+</button>
+
+		</div>
+
+		<div id="preclosed4">
+
+			<div class="overflow">
+	
+
+				<?php
+
+				$sql = "SELECT * FROM vw_sum_brk_total ;";
+
+				$result = $conn->query($sql);
+
+				if ($result->num_rows > 0) {
+
+					echo'<table id="brikchain" class="display"><tr><th>From</th><th>To</th><th>Total BRK Generated</th><th>Total BRK Destroyed</th><th>Total Brikcoins</th></tr>';
+
+				// output data of each row
+				//until($row = $result->fetch_assoc()) {
+					$row = $result->fetch_assoc();
+					echo "<tr><td>".$row["from_date"]."</td><td>".$row["to_date"]."</td><td>".$row["total_brk"]."&#8202;ß</td><td>".$row["aes_purchased"]."&#8202;kg</td><td>".$row["net_brk_in_circulation"]."&#8202;ß</td></tr>";
+				//	}
+					echo "</table>";
+				} else {
+					echo "Failed to connect to database";
+				}
+
+			?>
+			</div>
+		</div>
+	</div>
+
+
+	<?php 	$conn->close();?>
+
+
+	<br><br> 
+
+	<div class="page-paragraph-reg">
+                 
+                 <div class="row">
+                
+                      <div class="main2">
+                         <h3>More Accounting!</h3>
+                        
+                         <p>Who said accounting is boring?  We believe that accounting done right is far more than just financials.  Every year we not only count our coins, but also the very species we host in our spaces We also account for our give and take of plastic and CO2.</p>
+
+						 <p>   🟢 All our financial transaction are maintained in our <a href="open-books.php">Open Books</a> accounting system.</p> 
+
+						<p>  🟢 Our ecological accounting is disclosed separately in our <a href="regenreports">annual regenerative reports</a>.</p>
+
+						<p>Our accounting is grounded in the principles of Earthen Ethics.  Learn more about our principles of <a href="https://earthen.io/energy/" target="_blank">financial and ecological energy management, ecological accounting and disclosure.</p><br><br>
+                    
+                        <a class="action-btn" href="principles.php">🚀 Our Principles</a>
+                    <p style="font-size: 0.85em; margin-top:20px;">Our Accounting is guided by our regenerative principles</a></p>
+                        
+                 </div>
+
+                    <div class="side2">
+                        <br><img src=" https://ecobricks.org/en/icons/eco-accounting-icon-from-ecobricks-org.svg?v=5" width="77%" padding="15%" alt="ecological accounting" loading="lazy" alt="financial and ecological accounting.">
+                    </div>
+                </div>
+             </div>
+
+
+
+
+
+
+</DIV>
+
+
+
+<!-- This script is for pages that use the accordion content system-->
+<script src="../scripts/accordion-scripts.js" defer></script>
+
+
+
+	<!--FOOTER STARTS HERE-->
+
+	<?php require_once ("../footers/footer-$lang.php");?>
+
+	<!--FOOTER ENDS HERE-->
+
+</div>
+</body>
+</html>
+
