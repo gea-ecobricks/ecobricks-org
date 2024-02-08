6d7ced5e1e9dc294d223ebecfdad776298e36175
Russell Maier <russ@earthen.io>
2024-02-08 20:13:06 +0800

    no stop

--- meta/welcome-fr.php
+++ meta/welcome-fr.php
@@ -9,22 +9,22 @@
   <meta property="og:url"           content="https://ecobricks.org/fr/">
   <meta property="og:type"          content="website">
  <meta property="og:title"         content="Transition de la Plastique avec les Ecobriques">
-  <meta property="og:description"   content="Les écobricks empêchent le plastique de devenir un déchet, un déchet, un recyclage, une incinération ou une pollution. Les briques écologiques permettent une séquestration du plastique sans capital, zéro déchet, net zéro."/>
-  <meta property="og:image"         content="https://ecobricks.org/pngs/social-banner2.png"/>
-  <meta property="og:image:width" content="1200" />
-<meta property="og:image:height" content="638" />
+  <meta property="og:description"   content="Les écobricks empêchent le plastique de devenir un déchet, un déchet, un recyclage, une incinération ou une pollution. Les briques écologiques permettent une séquestration du plastique sans capital, zéro déchet, net zéro.">
+  <meta property="og:image"         content="https://ecobricks.org/pngs/social-banner2.png">
+  <meta property="og:image:width" content="1200" >
+<meta property="og:image:height" content="638" >
   <meta property="og:image:alt"     content="Une route métaphorique qui serpente au loin avec diverses constructions écologiques en brique et en terre à ses côtés."/>
-  <meta property="og:locale" content="fr_FR" />
+  <meta property="og:locale" content="fr_FR" >
 
   <meta name="author" content="Ecobricks.org">
   
-  <meta property="og:type" content="article" />
- <meta property="og:site_name" content="Ecobricks.org" />
- <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" />
- <meta property="article:modified_time" content="2022-08-14T09:14:13+00:00" />
- <meta property="og:image:type" content="image/png" />
- <meta name="author" content="Global Ecobrick Alliance" />
- <meta name="twitter:card" content="summary" />
- <meta name="twitter:label1" content="Est. reading time" />
- <meta name="twitter:data1" content="3 minutes" /> 
+  <meta property="og:type" content="article" >
+ <meta property="og:site_name" content="Ecobricks.org" >
+ <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" >
+ <meta property="article:modified_time" content="2022-08-14T09:14:13+00:00" >
+ <meta property="og:image:type" content="image/png" >
+ <meta name="author" content="Global Ecobrick Alliance" >
+ <meta name="twitter:card" content="summary" >
+ <meta name="twitter:label1" content="Est. reading time" >
+ <meta name="twitter:data1" content="3 minutes" > 
 
--- meta/what-fr.php
+++ meta/what-fr.php
@@ -8,24 +8,24 @@
 	
 	<!-- Facebook Open Graph Tags for social sharing-->
 
-	<meta property="og:url"           content="https://ecobricks.org/en/what.php"/>
+	<meta property="og:url"           content="https://ecobricks.org/en/what.php">
 	<meta property="og:type"          content="website">
  	<meta property="og:title"         content="Qu'est-ce qu'une Ecobrick? | Ecobricks.org">
   	<meta property="og:description"   content="Les écobricks sont une solution de transition plastique pour fabriquer des blocs de construction réutilisables en plastique séquestré."/>
   	<meta property="og:image"         content="https://ecobricks.org/pngs/what-page-ecobrick-banner-1024px.png"/>
-  	<meta property="og:image:width" content="1024" />
-	<meta property="og:image:height" content="1017" />
+  	<meta property="og:image:width" content="1024" >
+	<meta property="og:image:height" content="1017" >
   	<meta property="og:image:alt"     content="Une écobrique emballée avec du plastique à l'aide d'un bâton en bois."/>
-	  <meta property="og:locale" content="fr_FR" />
+	  <meta property="og:locale" content="fr_FR" >
 
-	  <meta property="og:type" content="article" />
- <meta property="og:site_name" content="Ecobricks.org" />
- <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" />
- <meta property="article:modified_time" content="2022-08-14T09:14:13+00:00" />
- <meta property="og:image:type" content="image/png" />
- <meta name="author" content="Global Ecobrick Alliance" />
- <meta name="twitter:card" content="summary" />
- <meta name="twitter:label1" content="Est. reading time" />
- <meta name="twitter:data1" content="3 minutes" /> 
+	  <meta property="og:type" content="article" >
+ <meta property="og:site_name" content="Ecobricks.org" >
+ <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" >
+ <meta property="article:modified_time" content="2022-08-14T09:14:13+00:00" >
+ <meta property="og:image:type" content="image/png" >
+ <meta name="author" content="Global Ecobrick Alliance" >
+ <meta name="twitter:card" content="summary" >
+ <meta name="twitter:label1" content="Est. reading time" >
+ <meta name="twitter:data1" content="3 minutes" > 
 
 	
