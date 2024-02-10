f1cc9441cf7731b71f1419a072c539cc254ad2e7
Russell Maier <russ@earthen.io>
2024-02-10 11:54:56 +0800

    vetical

--- en/side-modules/brikcoin-live-values.php
+++ en/side-modules/brikcoin-live-values.php
@@ -13,18 +13,17 @@ $result = $conn->query($sql);
         while($row = $result->fetch_assoc()) {
             
 
-            echo '<img src="../svgs/aes-brk.svg?v=2" style="width:65%; margin-top:20px;"><p style="font-size: 1.0em; margin-top:5px;">Live Brikcoin value:</p>
+            echo '<img src="../svgs/aes-brk-vertical.svg?v=2" style="width:65%; margin-top:20px;"><p style="font-size: 1.0em; margin-top:5px;">Live Brikcoin value:</p>
 
             <p>
             <span class="courier">
-                <span class="blink" style="font-size: 1.0em; padding-bottom: 5px">⬤</span>
-                <span style="font-size: 1.4em;">'.$row["plastic_value_g_per_brk"].'g</span>
-                <span style="font-size: 0.3em;">AES</span>
+                <span class="blink" style="font-size: 1.0em; padding-bottom: 10px">⬤</span>
+                <span style="font-size: 1.4em;">'.$row["plastic_value_g_per_brk"].'g AES</span>
                 </span>
             </p>';
-            echo '<p style="font-size: 1.0em; margin-top:10px;">2024 Price per Kg AES:<br><span class="courier">2.41 USD</span></p>' ; 
+            echo '<p style="font-size: 1.0em; margin-top:10px;">Current 2024 Price per 1 Kg AES plastic:<br><span class="courier">2.41 USD</span></p>' ; 
             echo '<p style="font-size: 1.0em; margin-top:10px;">Total Circulation:<br><b><span class="courier">'.$row["net_brk_in_circulation"].'&#8202;BRK</b></span></p>' ; 
-            echo '<p style="font-size: 0.9em; margin-top:10px;">Brikcoins <a href="/brikcoins.php">(BRK / ß)</a> are commodified in ecological value measured in kilograms of authenticated ecobrick sequestered plastic (<a href="sequestered.php">AES</a>).</p>
+            echo '<p style="font-size: 0.75em; margin-top:10px;">Brikcoins <a href="/brikcoins.php">(BRK / ß)</a> are commodified in ecological value measured in kilograms of authenticated ecobrick sequestered plastic (<a href="sequestered.php">AES</a>).</p>
             <p><a class="module-btn" href="aes/">ℹ️ AES Plastic</a></p>
             </div>' ; 
         
