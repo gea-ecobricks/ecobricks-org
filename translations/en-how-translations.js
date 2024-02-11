02752531e643dce9475e4111ac4c698745377dba
Russell Maier <russ@earthen.io>
2024-02-11 18:29:20 +0800

    yoollla

--- translations/en-how-translations.js
+++ translations/en-how-translations.js
@@ -71,6 +71,62 @@ const en_Page_Translations = {
 '048-block-4-advanced-paragraph': 'What about combinations of plastic and paper? What about a sticker on a paper envelope? Putting a little paper into your ecobrick isn\'t the end of the world. The contained environment will keep it from degrading. The main reason for not putting dry paper (or other organic materials) into an ecobrick is to maximize the space for containing plastic. There are many lively discussions in our social media communities about what should and should not go into an ecobrick. Feel free to join a group near you and ask a question.',
 
 
+'049-block-5-opener-header': '5. Start by adding a bottom color',
+'050-block-5-opener-subheader': 'It is important to start your ecobrick by adding a bottom color.',
+'051-block-5-main-image-alt': 'Add a bottom color to your eco brick',
+'052-block-5-paragraph-1': 'This is part of the circular design principle of ecobricking: we\'re thinking about the next use of the ecobrick. When your ecobrick is put to use in a module or an earth construction, the bottom color will help make patterns and designs.',
+'053-block-5-paragraph-2': 'To create your ecobrick\'s bottom color, choose a soft plastic with a solid color, then push it down to the bottom of your ecobrick. You\'ll want to fill the bottle loosely about halfway with soft plastic of your chosen color. Then, pack it in with your stick. If your bottle has "legs" or dimples at the bottom, be sure to pack these solid and full. Once all your soft plastic is compressed, you want it to fill the first 1-2cms of the bottle.',
+'054-block-5-paragraph-3': 'Often schools or companies will ask their ecobrickers to make ecobricks with their community colors. By giving your ecobrick bottom a color, you open up colorful design possibilities for making modules or earth and ecobrick walls.',
+'055-block-6-opener-header': '6. Pack the bottle tight, mixing plastics as you go',
+'056-block-6-opener-subheader': 'Now it\'s time to pack away! Cut or rip up large plastics into smaller pieces.',
+'057-block-6-main-image-alt': 'Pack your eco-brick right with recycled plastics',
+'058-block-6-paragraph-1': 'The smaller the pieces, the denser you\'ll get! Fill the bottle up halfway with loose plastic, then use your stick to push down around the sides of the bottle. Keep pushing down as you move around the circumference of the bottle. Once compressed, add more loose plastics. To maximize the density, it\'s good to mix soft, then hard plastic. If it\'s your first ecobrick, weigh occasionally to make sure you are on track for your target minimum weight.',
+'059-block-6-paragraph-2': 'Did you push too hard? In rare cases, when using a sharp stick, a thin bottle or too much lateral force, an ecobrick will rupture. If so, start again. Ruptured bottles will not last and can leak plastics. Cut the bottle open, remove the plastic and start again. Cut or damaged PET plastic is readily recycled.',
+'060-block-6-advanced-title': 'Advanced',
+'061-block-6-advanced-paragraph': '💡 Once you get to the middle of your ecobrick, pack 2-3cms of lighter colored plastic. By forming a center white ring of plastic, you will later have a clear location to label your Ecobrick with a permanent black marker.',
+
+
+'062-block-7-opener-header': '7. Weigh your Ecobrick to ensure quality',
+'063-block-7-opener-subheader': 'Now it\'s time to weigh! Cut or rip up large plastics into smaller pieces.',
+'064-block-7-main-image-alt': 'Weigh the eco brick and its recycled plastic waste',
+'065-block-7-paragraph-1': 'Ecobricks that are solid and strong are densely packed. The full volume is used for plastics, and there is no air or spaces inside. The GEA has determined that an ecobrick\'s density must be higher than <strong>0.33 g/ml</strong>. This means that a 600ml bottle must be above 200 grams and a 1500ml bottle must be above a weight of 500 grams. Experienced ecobrickers tend to regard 0.37 g/ml and above as the range of good ecobricks.',
+'066-block-7-paragraph-2': 'We\'ve discovered that obligating minimum density in your community\'s ecobricking is essential to ensure quality ecobricks. Quality ecobricks ensure solid and safe constructions and maximize the bottle\'s volume for securing plastic out of the environment.',
+'067-block-7-paragraph-3': 'Ecobricks that are below 0.33 g/ml are too squishy to be used for modules and are not ideal for earth building. Soft ecobricks can compromise structural constructions and will dent easily, which reduces their lifespan. Low-density ecobricks are also <a href="/fire">fire hazards</a> because of the air pockets remaining inside them.',
+'068-block-7-paragraph-4': 'That said, a good solid ecobrick that meets all GEA guidelines is something you can be proud of and which will inspire others! An ecobrick\'s heft and quality set an inspiring example for others and energize the social spread of ecobricks. When others see and hold your ecobrick, they will understand immediately that something important has happened: "waste" has been transformed into something fundamentally useful.',
+'069-block-7-advanced-title': 'Advanced',
+'070-block-7-advanced-paragraph': 'Some advanced ecobrickers are so industrious at chopping up their plastic and packing their bottles that their ecobricks are too dense! When these ecobricks are used to make modules, the weight of the ecobricks can be too much for the silicone bonds to hold. Also, such modules are too heavy to be safely moved by children or older people. For these reasons, the GEA recommends keeping your density below 0.7 g/ml.',
+'071-block-8-opener-header': '8. Cap and Log',
+'072-block-8-opener-subheader': 'Avoid packing your ecobrick too full.',
+'073-block-8-main-image-alt': 'Add a cap to your plastic waste packed ecoBrick',
+'074-block-8-paragraph-1': 'So your ecobrick is packed as solid as possible - now it\'s time to cap it! First, be sure your ecobrick is <strong>not</strong> packed to overflowing. There should be about 1-2cm between the cap and the plastic inside. The cap (HDPE) is made of a different type of plastic than the bottle (PET) and is the weakest part of the ecobrick. If plastic is even slightly pressing up against the lid, the pressure will inevitably crack it over the years. Also, be sure to avoid flip-tops or sports caps when sealing your ecobrick as they are fragile and will break easily over time.',
+'075-block-8-paragraph-2': 'Once capped, it is time to record your ecobrick! Recording your ecobrick\'s key data is an important part of ecobricking whether you\'re doing it on your own or as part of a community. This is particularly important for community leaders. You can use a log sheet, a spreadsheet, or the <a href="https://www.gobrik.com" target="_blank" rel="noopener noreferrer">GoBrik app</a> to record data such as the ecobricker\'s id, the final weight, date and year, the intended project, and serial number (generated by the GoBrik App).',
+'076-block-8-paragraph-3': 'Recording ecobricks is key to calculating your overall impact, monitoring quality, and preparing for building projects.',
+'077-block-8-paragraph-4': 'By recording the weight, you can tally up all your ecobricks and calculate the total plastic you have kept out of the environment. By recording id and weight you enable quality accountability. By maintaining a record of how many ecobricks you have completed, you\'ll know when you\'re ready to move to the building stage of your project. By recording the date you let people tomorrow know that we cared today about their world.',
+'078-block-8-paragraph-5': 'We\'ve worked hard to craft a universal web app for you to use to log and connect your ecobricks with those in your community. Simply visit <a href="https://www.gobrik.com" target="_blank" rel="noopener noreferrer">www.GoBrik.com</a> with the web browser on your phone, sign in with Facebook or email, create an account, then begin to log your ecobricks. GoBrik will do the rest - compiling stats for you and your community.',
+'079-block-8-paragraph-6': 'We\'ve also made a point-chart and sample form in our <a href="https://www.ecobricks.org/download/" target="_blank" rel="noopener noreferrer">Vision Ecobrick Guide</a> that can be photocopied for logging ecobricks in schools.',
+
+
+'080': '9. Inscribe',
+'081': 'As you log and record your ecobrick, be sure to record the key data on the ecobrick itself too.',
+'082': 'Log your eco brick on our gobrik app',
+'083': 'Inscribing the weight, data, serial, etc. onto the ecobrick will indicate that the ecobrick has been logged and registered. Be sure to use a permanent means of recording your ecobrick. Your ecobrick will endure for decades and centuries. The more information we can leave future generations, the richer our gift. After much experimentation, we have found that enamel/oil paint and nail polish work best.',
+'084': '<ul><li>Avoid using paper labels, tape, or stickers to record the data. These methods will last for several years at best then fall off.</li><li>Avoid using permanent markers. Not only does their writing not last long on PET, they are made of non-recyclable and non-ecobrickable materials.</li><li>Enamel paint usually comes in a metal can or glass jar. These containers are effectively recyclable.</li><li>Use a paint brush to carefully and clearly inscribe the ecobrick\'s data. The paint bonds to the plastic and is almost impossible to remove.</li><li>Use contrasting colors like white and black to make your inscription stand out.</li><li>Nail polish is a form of oil paint and also permanently inscribes the ecobrick. However, often the nail polish brush makes it hard to write legibly. Also, the smell of the polish drying is strong.</li></ul>',
+'085': 'Some ecobrickers are innovating with other methods; such as etching onto the ecobrick or inserting a written plastic tag into the bottle itself.',
+'086': 'Extra',
+'087': 'Many ecobrickers write a prayer or a vision on the bottle in a permanent marker. Your ecobrick will last a long time. Why not let your grandkids know that you were thinking of them? For example: "I envision a world where plastic is used seldom and wisely and where we live in harmony with the cycles of nature"',
+'088': '10. Store your ecobrick',
+'089': 'Once complete, you will need to store your ecobricks until you have enough for your project.',
+'090': 'how to store your ecobricks',
+'091': 'Store ecobricks indoors, out of the sun. If possible, protect with a cloth or tarp from accumulating dust and dirt (PET attracts dust and chemicals and is hard to remove). Stack horizontally, with the ends pointed outward. This enables you to organize your ecobricks by colour and brand– which later facilitates project planning and making. It\'s good to have the ecobricks raised slightly above the floor — rats have been known to chew away at bottles! Ideally, ecobricks are stored off of the ground (on a pallet or raised on wood) and fully protected from the sun and the elements. Ecobricks are best stacked horizontally with their bottom pointed outwards.',
+
+
+'092': 'And Build!',
+'093': 'Once you have enough Ecobricks you’re ready to build. There are five main techniques for building with bottles and ecobricks. The easiest, fastest, and perhaps the most fun Ecobrick application, is to make Milstein modules for horizontal building and Dieleman Lego modules for vertical building.',
+'094': 'eco brick building methods.',
+'095': 'With modules you can create benches, tables, chairs, structures and more. Perhaps the best use for Ecobricks is building community green spaces. Using local earth and ecobricks you and your community can build beautiful spaces that enrich the local environment.',
+'096': '<a class="action-btn" href="build">🚀 Building Applications</a>',
+'097': 'An overview of ecobrick building methods',
+
 
 
 
