716f15a9d549486cf9e78087554a3c629dc263f3
Russell Maier <russ@earthen.io>
2024-02-11 20:08:27 +0800

    dd

--- fr/how.php
+++ fr/how.php
@@ -34,7 +34,7 @@
 		<div class="main">
 
 			<div class="lead-page-paragraph">
-				<h3 data-lang-id="004-lead-page-paragraph">Ecobricks can be made by the young, the old and everyone in between — no machines, special skills or experts required!  However, there are important guidelines to follow to ensure the quality and usability of your ecobricks.</h3>
+				<h5 data-lang-id="004-lead-page-paragraph">Ecobricks can be made by the young, the old and everyone in between — no machines, special skills or experts required!  However, there are important guidelines to follow to ensure the quality and usability of your ecobricks.</h5>
     
 			</div>
 			
@@ -67,9 +67,9 @@
 
 
 						<div class="advanced-box" aria-expanded="false" role="region" aria-labelledby="advancedBoxLabel-1"> 
-							<div class="advanced-box-header" aria-label="Advanced Section Toggle" id="advancedBoxLabel-1">
+							<div class="advanced-box-header"  id="advancedBoxLabel-1">
 								<div class="advanced-title" data-lang-id="012-block-1-advanced">Advanced</div>
-								<div class="advanced-open-icon">+
+								<div class="advanced-open-icon" aria-label="Advanced Section Toggle">+
 								</div>
 							</div>
 							<div class="advanced-box-content">
@@ -126,9 +126,9 @@
 					</li>
 				</ol>
 				<div class="advanced-box" aria-expanded="false" role="region" aria-labelledby="advancedBoxLabel-2"> 
-							<div class="advanced-box-header" aria-label="Advanced Section Toggle" id="advancedBoxLabel-2">
+							<div class="advanced-box-header" id="advancedBoxLabel-2">
 						<div class="advanced-title" data-lang-id="027-block-2-advanced-title">Advanced</div>
-						<div class="advanced-open-icon">+</div>
+						<div class="advanced-open-icon" aria-label="Advanced Section Toggle" >+</div>
 					</div>
 					<div class="advanced-box-content">
 						<p data-lang-id="028-block-2-advanced-paragraph">💡 If you’re thinking of making <a href="../dms">Dieleman Lego Modules</a> with your ecobricks, you must also consider the top tapper of your bottle. Dieleman modules work best when the distance between the top of the bottle and the start of the tapper is small. This allows more surface area for the inverted peg of the Dieleman module to be attached.</p>
@@ -205,9 +205,9 @@
 						</ol>
 
 						<div class="advanced-box" aria-expanded="false" role="region" aria-labelledby="advancedBoxLabel-4"> 
-									<div class="advanced-box-header" aria-label="Advanced Section Toggle" id="advancedBoxLabel-4">
+									<div class="advanced-box-header"  id="advancedBoxLabel-4">
 								<div class="advanced-title" data-lang-id="047-block-4-advanced-title">Advanced</div>
-								<div class="advanced-open-icon">+</div>
+								<div class="advanced-open-icon" aria-label="Advanced Section Toggle">+</div>
 							</div>
 
 							<div class="advanced-box-content">
@@ -261,9 +261,10 @@
 						<p data-lang-id="059-block-6-paragraph-2">Did you push too hard? In rare cases, when using a sharp stick, a thin bottle or too much lateral force, an ecobrick will rupture. If so, start again. Ruptured bottles will not last and can leak plastics. Cut the bottle open, remove the plastic and start again. Cut or damaged PET plastic is readily recycled.</p>
 
 						<div class="advanced-box" aria-expanded="false" role="region" aria-labelledby="advancedBoxLabel-5"> 
-									<div class="advanced-box-header" aria-label="Advanced Section Toggle" id="advancedBoxLabel-5">
+									<div class="advanced-box-header"  id="advancedBoxLabel-5">
 								<div class="advanced-title" data-lang-id="060-block-6-advanced-title">Advanced</div>
-								<div class="advanced-open-icon">+</div>
+									<div class="advanced-box-header"  id="advancedBoxLabel-5">
+								<div class="advanced-open-icon" >+</div>
 							</div>
 							<div class="advanced-box-content">                    
 								<p data-lang-id="061-block-6-advanced-paragraph">💡 Once you get to the middle of your ecobrick, pack 2-3cms of lighter colored plastic. By forming a center white ring of plastic, you will later have a clear location to label your Ecobrick with a permanent black marker.</p>
@@ -297,9 +298,9 @@
 						<p data-lang-id="068-block-7-paragraph-4">That said, a good solid ecobrick that meets all GEA guidelines is something you can be proud of and which will inspire others! An ecobrick's heft and quality set an inspiring example for others and energize the social spread of ecobricks. When others see and hold your ecobrick, they will understand immediately that something important has happened: "waste" has been transformed into something fundamentally useful.</p>
 
 						<div class="advanced-box" aria-expanded="false" role="region" aria-labelledby="advancedBoxLabel-6"> 
-									<div class="advanced-box-header" aria-label="Advanced Section Toggle" id="advancedBoxLabel-6">
+									<div class="advanced-box-header"  id="advancedBoxLabel-6">
 								<div class="advanced-title" data-lang-id="069-block-7-advanced-title">Advanced</div>
-								<div class="advanced-open-icon">+</div>
+								<div class="advanced-open-icon" aria-label="Advanced Section Toggle">+</div>
 							</div>
 							<div class="advanced-box-content">
 								<p data-lang-id="070-block-7-advanced-paragraph">Some advanced ecobrickers are so industrious at chopping up their plastic and packing their bottles that their ecobricks are too dense! When these ecobricks are used to make modules, the weight of the ecobricks can be too much for the silicone bonds to hold. Also, such modules are too heavy to be safely moved by children or older people. For these reasons, the GEA recommends keeping your density below 0.7 g/ml.</p>
@@ -338,11 +339,6 @@
 				</div>  
 			</section>
 
-		
-
-
-		
-
 			<section id="INSCRIBE">
 				<div class="reg-content-block" id="block9">
 					<div class="opener-header">
@@ -367,9 +363,9 @@
 						</ul>
 						<p data-lang-id="085">Some ecobrickers are innovating with other methods; such as etching onto the ecobrick or inserting a written plastic tag into the bottle itself.</p>
 						<div class="advanced-box" aria-expanded="false" role="region" aria-labelledby="advancedBoxLabel-7"> 
-									<div class="advanced-box-header" aria-label="Advanced Section Toggle" id="advancedBoxLabel-7">
+									<div class="advanced-box-header"  id="advancedBoxLabel-7">
 								<div class="advanced-title" data-lang-id="086">Extra</div>
-								<div class="advanced-open-icon">+</div>
+								<div class="advanced-open-icon" aria-label="Advanced Section Toggle">+</div>
 							</div>
 							<div class="advanced-box-content">                    
 								<p data-lang-id="087">Many ecobrickers write a prayer or a vision on the bottle in a permanent marker. Your ecobrick will last a long time. Why not let your grandkids know that you were thinking of them? For example: "I envision a world where plastic is used seldom and wisely and where we live in harmony with the cycles of nature"</p>
