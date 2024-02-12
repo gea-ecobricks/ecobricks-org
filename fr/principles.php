98a1e32c30daa3a0b579b68c3dea8854f082ec19
Russell Maier <russ@earthen.io>
2024-02-12 11:36:25 +0800

    dfs

--- fr/principles.php
+++ fr/principles.php
@@ -4,7 +4,7 @@
 <HEAD>
 <META charset="UTF-8">
 <?php $lang='fr';?>
-<?php $version='1.71';?>
+<?php $version='1.72';?>
 <?php $page='how';?>
 
 <?php require_once ("../includes/principles-inc2.php");?>
