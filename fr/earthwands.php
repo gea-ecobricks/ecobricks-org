7ef801c33f7559feea29968227336f5dadfad42c
Russell Maier <russ@earthen.io>
2024-02-16 10:04:22 +0800

    go up

---
+++ fr/earthwands.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<HTML lang="fr"> 
+<HEAD>
+<META charset="UTF-8">
+<?php $lang='fr';?>
+<?php $version='1.53';?>
+<?php $page='sequest';?>
+
+<?php require_once ("../includes/sequest2-inc.php");?>
+
+<article id="sequest-with-ecobricks">
+
+
+<!--PAGE BANNER-->
+
+
+ <div class="splash-content-block">
+	<div class="splash-box">
+		<div class="splash-heading" data-lang-id="001-splash-title">The Earthwand</div>
+	    <div class="splash-sub" data-lang-id="002-splash-subtitle">Make your plastic disapear</div>
+	</div>
+	<div class="splash-image" data-lang-id="003-splash-image-alt"><img src="../svgs/building-methods.svg" style="width: 85%" alt="Séquestration du plastique avec les ecobricks">
+    </div>	
+</div>
+
+<!-- PAGE CONTENT-->
+
+
+        <div id="main-content">
+
+        <!-- The flexible grid (content) -->
+            <div class="row">
+                <div class="main">
+
+                    <div class="lead-page-paragraph">
+                         <p data-lang-id="005-first-page-paragraph">Why not you pack your ecobrick with the best possible tool available? </p>
+                     </div>
+                
+                    <div class="page-paragraph">
+                         <p data-lang-id="006-second-page-paragraph">The EarthWand is a regenerative product designed from the ground up to be the perfect plastic packing, ecobrick making tool.  Its simple, yet super practical, ergonomic, sturdy and strong.  Best of all, its made without plastic-- fully hand crafted from bamboo, rattan and coconut fiber.</p>
+
+                         <p>Developed over three years of ecobricking and prototyping by one of the co-founders of the Global Ecobrick Alliance, the Earthwand it is designed to not only pack plastic, but to support the local and global ecobrick movement financially. EarthWands are produced by GEA Trainer and sold by GEA Trainers as a way to fund their ecological enterprises– such as conducting as many ecobrick workshops as possible in their communities!  Trainers also contribute a minimum of 25% of the retail price back to the GEA to support its global work.  Sale transactions, and the usage of these funds are fully trackable on the GEA Open Books accounting system.</p>
+
+                         <img src="../pngs/earthwand-virtue-icons.png" style="width:100%;">
+
+
+                        <p>Best of all, we’ve carefully designed the Earthwand’s production, consumption and disposal so that after all the impacts of its life-cycle are added up, it has in the end made the world a greener place.   To see what we mean, see the EarthWand’s Product Impact report.  In this way the EarthWand is a symbol of the ayyew and regenerative principles which inspire the global plastic transition movement.</p>
+
+                        <h4>More than a product</h4>
+
+                        <p>The core design of the EarthWand were developed by GEA principal and inventor Russell Maier as an embodiment of the regenerative principles behind the ecobrick movement.  The EarthWand thus is much more than a simple product…</p>
+
+                        <p>It is a way for ecobrickers to support their Trainer, the GEA, regenerative makers and the Ecobricking movement.</p>
+                        
+                        <ul>
+                            <li>It is a highly effective tool for packing plastic.</li>
+
+                            <li>It represents an intentional positive impact to the biosphere.</li>
+
+                           <li>Purchase the EarthWand on the GoBrik Regenerative Store…</li>
+
+                        </ul>
+                        
+                           
+                        <button type="button" class="module-btn" onclick="window.open('https://gobrik.com/en/gobrik.com/#shop')" aria-label="En savoir plus" title="En savoir plus sur la transition plastique et petrocapital.">En savoir plus</button>
+
+                </div>
+
+
+
+
+                
+            
+
+                <div class="side">
+            
+
+
+
+
+    
+                <?php require_once ("side-modules/transition-module.php");?>
+
+                <?php require_once ("side-modules/good-use.php");?>
+
+                <?php require_once ("side-modules/spiral-design.php");?>
+
+                <?php require_once ("side-modules/earthen-ethics.php");?>
+
+
+
+
+
+                </div>
+            </div>
+       
+
+            
+            </div>  <!--closes main-->
+
+
+	<!--FOOTER STARTS HERE-->
+
+	<?php require_once ("../footer-2024.php");?>
+
+
+<!-- CUSTOM PAGE SCRIPTS-->
+
+
+<!-- This script is for pages that use the accordion content system-->
+<script src="../accordion-scripts.js?v=2" defer></script>
+
+</div>
+</article>
+</body>
+</html>
+
+
--- fr/side-modules/transition-module.php
+++ fr/side-modules/transition-module.php
@@ -1,8 +1,8 @@
 
 			<div class="side-module-desktop-mobile">
-				<img src="../webp/earthhome-400px.webp" width="100%" loading="lazy" alt="eco brik and earth building can make regenerative structures">
+				<img src="../webp/earthhome-400px.webp" width="100%" loading="lazy" alt="une construction avec les ecobriques et la terre.">
 				<h4>Transition plastique</h4>
 <h5>La construction Ecobrick est une technologie de transition. Plutôt que de considérer le plastique comme un "déchet", nous le considérons comme un élément constitutif de nos visions écologiques.</h5><br>
-                <button type=button class="module-btn" onclick="location.href='transition.php'" aria-label="Learn more" title="Learn more about plastic transition">Learn More</button>
+                <button type=button class="module-btn" onclick="location.href='transition.php'" aria-label="En savoir plus" title="En savoir plus sur la transition plastique et petrocapital.">En savoir plus</button>
                 <br>
 			</div>
