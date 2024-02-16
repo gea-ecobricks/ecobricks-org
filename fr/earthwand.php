8b03d5a778d7e4cca9f854ce13477995c49246a1
Russell Maier <russ@earthen.io>
2024-02-16 13:32:25 +0800

    sdf

--- fr/earthwand.php
+++ fr/earthwand.php
@@ -3,7 +3,7 @@
 <HEAD>
 <META charset="UTF-8">
 <?php $lang='fr';?>
-<?php $version='1.6';?>
+<?php $version='1.71';?>
 <?php $page='earthwand';?>
 
 <?php require_once ("../includes/earthwand-inc.php");?>
