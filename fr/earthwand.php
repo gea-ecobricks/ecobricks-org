682c40fef954d21182e3ea59a08cddfdf0381f45
Russell Maier <russ@earthen.io>
2024-02-16 13:16:40 +0800

    8

--- fr/earthwand.php
+++ fr/earthwand.php
@@ -3,7 +3,7 @@
 <HEAD>
 <META charset="UTF-8">
 <?php $lang='fr';?>
-<?php $version='1.57';?>
+<?php $version='1.58';?>
 <?php $page='earthwand';?>
 
 <?php require_once ("../includes/earthwand-inc.php");?>
@@ -49,7 +49,7 @@
                             <img src="../pngs/earthwand-virtue-icons.png" style="width:100%;padding:20px;margin-top:20px;margin-bottom:10px" alt="Earthwand design principles">
                         </div>
 
-                         <h4>Earthwand Vision</h4>
+                        
 
                          <p data-lang-id="006-second-page-paragraph">Developed over three years of ecobricking and prototyping by one of the co-founders of the Global Ecobrick Alliance, the Earthwand it is designed to not only pack plastic, but to support the local and global ecobrick movement financially. EarthWands are produced by GEA Trainer and sold by GEA Trainers as a way to fund their ecological enterprises– such as conducting as many ecobrick workshops as possible in their communities!  Trainers also contribute a minimum of 25% of the retail price back to the GEA to support its global work.  Sale transactions, and the usage of these funds are fully trackable on the GEA Open Books accounting system.</p>
 
