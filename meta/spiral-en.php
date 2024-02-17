635b1d699784b1823db0d8bb222b9a8f679be137
Russell Maier <russ@earthen.io>
2024-02-17 14:26:21 +0800

    sdf up

--- meta/spiral-en.php
+++ meta/spiral-en.php
@@ -2,31 +2,31 @@
 
 <title>Spiral Design</title>
 
-<meta name="keywords" content="regenerative principles, biosphere, ecological ethics, transcaste, for-earth, ecological accounting, circular design, spiral design, biodiversity accounting, spiral, cyclical design, earthen ethics, vitalizing biodiversity, plastic sequestration, recycling, alternative, sequestration of plastic, plastic offsetting, aes plastic, carbon sequestration.">
+<meta name="keywords" content="regenerative principles,ecological ethics,  circular design, spiral design, biodiversity accounting, spiral, cyclical design, earthen ethics, sequestration of plastic, plastic offsetting, aes plastic, carbon sequestration.">
 
-<meta name="description" content="Ecobricking is guided by earthen and regenerative principles that point us towards ever increasing harmony with Earth's cycles.">
+<meta name="description" content="Let's make sure that the cycles of all our products and processes are steadily enriching-- socially and ecologically.">
 	
 	<!-- Facebook Open Graph Tags for social sharing-->
 
-	<meta property="og:url"           content="https://ecobricks.org/en/spiral.php"/>
+	<meta property="og:url"           content="https://ecobricks.org/en/spiral.php">
 	<meta property="og:type"          content="website">
- 	<meta property="og:title"         content="Spiral Design | Ecobricks.org">
-  	<meta property="og:description"   content="Ecobricking is guided by regenerative principles that point us towards ever increasing harmony with Earth's cycles."/>
-  	<meta property="og:image"         content="https://ecobricks.org/pngs/principles-earth-home-1000px.png"/>
-  	<meta property="og:image:width" content="1000" />
-	<meta property="og:image:height" content="1000" />
-  	<meta property="og:image:alt"     content="An earth and ecobrick home with a tree growing from inside, a wind turbine turning and a happy bird."/>
-	  <meta property="og:locale" content="en_GB" />
+ 	<meta property="og:title"         content="Spiral Design">
+  	<meta property="og:description"   content="Let's make sure that the cycles of all our products and processes are steadily enriching-- socially and ecologically.">
+  	<meta property="og:image"         content="https://ecobricks.org/webp/spiral-design-banner-695px.webp">
+  	<meta property="og:image:width" content="695" >
+	<meta property="og:image:height" content="695" >
+  	<meta property="og:image:alt"     content="The iconic symbol of spiral design">
+	  <meta property="og:locale" content="en_GB" >
 
 	  <meta name="author" content="Ecobricks.org">
   
-  <meta property="og:type" content="article" />
- <meta property="og:site_name" content="Ecobricks.org" />
- <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" />
- <meta property="article:modified_time" content="2023-10-30T09:14:13+00:00" />
- <meta property="og:image:type" content="image/png" />
- <meta name="author" content="Global Ecobrick Alliance" />
- <meta name="twitter:card" content="summary" />
- <meta name="twitter:label1" content="Est. reading time" />
- <meta name="twitter:data1" content="5 minutes" /> 
+  <meta property="og:type" content="article" >
+ <meta property="og:site_name" content="Ecobricks.org" >
+ <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" >
+ <meta property="article:modified_time" content="2024-02-16T09:14:13+00:00" >
+ <meta property="og:image:type" content="image/png" >
+ <meta name="author" content="Global Ecobrick Alliance" >
+ <meta name="twitter:card" content="summary" >
+ <meta name="twitter:label1" content="Est. reading time" >
+ <meta name="twitter:data1" content="5 minutes" > 
 	 
---
+++ meta/spiral-fr.php
@@ -0,0 +1,29 @@
+<title>Conception en spirale</title>
+
+<meta name="keywords" content="principes régénératifs, éthique écologique, conception circulaire, conception en spirale, comptabilité de la biodiversité, spirale, conception cyclique, éthique de la terre, séquestration du plastique, compensation plastique, plastique aes, séquestration du carbone.">
+
+<meta name="description" content="Assurons-nous que les cycles de tous nos produits et processus s'enrichissent constamment - socialement et écologiquement.">
+
+<!-- Balises Facebook Open Graph pour le partage social-->
+
+<meta property="og:url" content="https://ecobricks.org/en/spiral.php">
+<meta property="og:type" content="website">
+  <meta property="og:title" content="Conception en spirale">
+   <meta property="og:description" content="Assurons-nous que les cycles de tous nos produits et processus s'enrichissent constamment - socialement et écologiquement.">
+   <meta property="og:image" content="https://ecobricks.org/webp/spiral-design-banner-695px.webp">
+   <meta property="og:image:width" content="695" >
+<meta property="og:image:height" content="695" >
+   <meta property="og:image:alt" content="Le symbole emblématique du design en spirale">
+<meta property="og:locale" content="en_GB" >
+
+<meta name="auteur" content="Ecobricks.org">
+  
+   <meta property="og:type" content="article" >
+  <meta property="og:site_name" content="Ecobricks.org" >
+  <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" >
+  <meta property="article:modified_time" content="2024-02-16T09:14:13+00:00" >
+  <meta property="og:image:type" content="image/webp" >
+  <meta name="author" content="Alliance mondiale Ecobrick" >
+  <meta name="twitter:card" content="summary" >
+  <meta name="twitter:label1" content="Temps de lecture estimé" >
+  <meta name="twitter:data1" content="5 minutes" >
