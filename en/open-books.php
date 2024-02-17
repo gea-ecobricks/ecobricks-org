faca7221b533fd7d66007a839132886ef4acc1cc
Russell Maier <russ@earthen.io>
2024-02-17 19:26:56 +0800

    sdfsdf

--- en/open-books.php
+++ en/open-books.php
@@ -3,7 +3,7 @@
 <HEAD>
 <META charset="UTF-8">
 <?php $lang='fr';?>
-<?php $version='1.58';?>
+<?php $version='1.59';?>
 <?php $page='principles';?>
 
 <?php require_once ("../includes/openbooks2-inc.php");?>
