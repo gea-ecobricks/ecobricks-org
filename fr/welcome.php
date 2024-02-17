e194abe8d2f8281a919492b321445af67da93861
Russell Maier <russ@earthen.io>
2024-02-17 14:57:34 +0800

    reset

--- fr/welcome.php
+++ fr/welcome.php
@@ -38,36 +38,7 @@ https://github/globalecobrickalliance/ecobricks.org
 
 
 
-<!-- FULL GALLERY-->
-<?php include 'ecobricks_env.php';?> 
 
-			<div class="gallery10-content-block">
-								
-				<div class="flex-container10">
-						
-					<?php
-					$sql = "SELECT * FROM vw_top_10_last_month ;";
-					$result = $conn->query($sql);
-					if ($result->num_rows > 0) {
-					// output data of each row
-					while($row = $result->fetch_assoc()) {
-
-					echo '
-					<div class="gal-photo10">
-					<a href="https://ecobricks.org/details-ecobrick-page.php?serial_no='.$row["ecobrick_unique_id"].'"><img src="'.$row["ecobrick_full_photo_url"].'?v=1"  alt="Ecobrick '.$row["ecobrick_unique_id"].' by '.$row["ecobrick_owner"].' in '.$row["location"].'" title="Ecobrick '.$row["ecobrick_unique_id"].' by '.$row["ecobrick_owner"].' in '.$row["location"].'"></a>';
-					echo '<div class="gal10-photo-text"><b>Ecobrick '.$row["ecobrick_unique_id"].'</b><br>By '.$row["ecobrick_owner"].'<br>'.$row["location"].'</div></div>';
-
-					}
-
-					} else {
-					echo "Failed to connect to the Brikchain database";
-					}
-
-					?>
-							
-				</div>
-		
-			</div>
 
 
     <div class="featured-content-2"  >
