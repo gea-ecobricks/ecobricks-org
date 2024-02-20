c1772a118abff03f56a67e6024336a4a14dea6a3
Russell Maier <russ@earthen.io>
2024-02-20 10:40:05 +0800

    upsy

--- testing/side-modules/plastic-from-oil.php
+++ testing/side-modules/plastic-from-oil.php
@@ -3,5 +3,5 @@
 				<h4>Plastic's Origins</h4>
 				<h5>Plastic comes from ancient carbon that has transformed into oil and that we've extracted and refined.</h5><br>
 				<a class="module-btn" href="https://book.earthen.io/en/stellar.html" target="_blank">Plastic's Stellar Story</a>
-                <h6>- From 'Plastic's Stellar Story'</h6>
+                <h6 style="font-size:smaller;">- From 'Plastic's Stellar Story' ⇗</h6>
 			</div>
