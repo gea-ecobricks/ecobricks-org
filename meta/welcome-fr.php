e27a307854c654e5a351136136c939055825c84c
Russell Maier <russ@earthen.io>
2024-02-21 18:39:18 +0800

    banner

--- meta/welcome-fr.php
+++ meta/welcome-fr.php
@@ -10,9 +10,9 @@
   <meta property="og:type"          content="website">
  <meta property="og:title"         content="Transition de la Plastique avec les Ecobriques">
   <meta property="og:description"   content="Les écobricks empêchent le plastique de devenir un déchet, un déchet, un recyclage, une incinération ou une pollution. Les briques écologiques permettent une séquestration du plastique sans capital, zéro déchet, net zéro.">
-  <meta property="og:image"         content="https://ecobricks.org/pngs/social-banner2.png">
-  <meta property="og:image:width" content="1200" >
-<meta property="og:image:height" content="638" >
+  <meta property="og:image"         content="https://ecobricks.org/pngs/ecobricks-org-2024-banner.png">
+  <meta property="og:image:width" content="1000" >
+<meta property="og:image:height" content="905" >
   <meta property="og:image:alt"     content="Une route métaphorique qui serpente au loin avec diverses constructions écologiques en brique et en terre à ses côtés.">
   <meta property="og:locale" content="fr_FR" >
 
@@ -21,7 +21,7 @@
   <meta property="og:type" content="article" >
  <meta property="og:site_name" content="Ecobricks.org" >
  <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" >
- <meta property="article:modified_time" content="2024-02-08T09:14:13+00:00" >
+ <meta property="article:modified_time" content="2024-02-18T09:14:13+00:00" >
  <meta property="og:image:type" content="image/png" >
  <meta name="author" content="Global Ecobrick Alliance" >
  <meta name="twitter:card" content="summary" >
