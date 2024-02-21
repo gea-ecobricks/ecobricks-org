bf80b2f439de395fabfa4096c080caf9b0ff1775
Russell Maier <russ@earthen.io>
2024-02-21 18:52:06 +0800

    faa

--- meta/welcome-fr.php
+++ meta/welcome-fr.php
@@ -1,15 +1,15 @@
 
 <meta name="keywords" content="ecobriques, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-briques, briques, eco brique, brik, briks, plastique, gestion du plastique, séquestration du carbone, éthique de la terre, plastique, alternatives de recyclage, incinération, compensations plastiques, solutions plastiques, upcycling, séquestration du plastique, alternative de recyclage, alternative d'incinération, prévention de la pollution plastique, pollution plastique, solution plastique régénérative, déchets, gestion des déchets , déchets plastiques, non capitaux, zéro déchet, net zéro, séquestration du plastique"> 
 
-<meta name="description" content="Les écobricks empêchent le plastique de devenir un déchet, un déchet, un recyclage, une incinération ou une pollution. Les briques écologiques permettent une séquestration du plastique sans capital, zéro déchet, net zéro.">
+<meta name="description" content="Du plastique problématique aux solutions régénératives">
 	
 	
 	<!-- Facebook Open Graph Tags for social sharing-->
 
-  <meta property="og:url"           content="https://ecobricks.org/fr/welcome.php">
+  <meta property="og:url"           content="https://ecobricks.org/fr/">
   <meta property="og:type"          content="website">
- <meta property="og:title"         content="Transition de la Plastique avec les Ecobriques">
-  <meta property="og:description"   content="Les écobricks empêchent le plastique de devenir un déchet, un déchet, un recyclage, une incinération ou une pollution. Les briques écologiques permettent une séquestration du plastique sans capital, zéro déchet, net zéro.">
+ <meta property="og:title"         content="Du polution plastique a solution regenerative">
+  <meta property="og:description"   content="Du plastique problématique aux solutions régénératives">
   <meta property="og:image"         content="https://ecobricks.org/pngs/ecobricks-org-2024-banner.png">
   <meta property="og:image:width" content="1000" >
 <meta property="og:image:height" content="905" >
