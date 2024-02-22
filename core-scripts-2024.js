9b7359e6afe53e99122d8803ef7b24de4cb925c1
Russell Maier <russ@earthen.io>
2024-02-22 14:35:50 +0800

    arc

--- core-scripts-2024.js
+++ core-scripts-2024.js
@@ -178,16 +178,16 @@ document.addEventListener('keydown', modalCloseCurtains);
 
 
 /*Updates certain colors to the Dark or Light theme*/
-function updateLogoColor() {
-  const svg = document.querySelector("html");
-  const elementsWithColor = svg.querySelectorAll("[fill='#646464']");
+// function updateLogoColor() {
+//   const svg = document.querySelector("html");
+//   const elementsWithColor = svg.querySelectorAll("[fill='#646464']");
 
-  for (let element of elementsWithColor) {
-    if (element.getAttribute("fill") === "#646464") {
-      element.setAttribute("fill", "var(--logo-color)");
-    }
-  }
-}
+//   for (let element of elementsWithColor) {
+//     if (element.getAttribute("fill") === "#646464") {
+//       element.setAttribute("fill", "var(--logo-color)");
+//     }
+//   }
+// }
 
 
 
