fcd412f826c9593b05905534ae20187448b38393
Russell Maier <russ@earthen.io>
2024-02-24 12:31:59 +0800

    retr

--- includes/what2-inc.php
+++ includes/what2-inc.php
@@ -3,7 +3,7 @@
 <!--Image files to preload that are unique to this page-->
 
 <link rel="preload" as="image" href="../svgs/eb-blue-no-clouds.svg?v=2">
-<link rel="preload" as="image" href="../svgs/top-clouds-mobile.svg?v=2">
+<link rel="preload" as="image" href="../svgs/top-clouds-mobile2.svg?v=2">
 <link rel="preload" as="image" href="../svgs/top-clouds-animated-desktop.svg?v=2">
 <!--This loads CSS specific to this page-->
 
@@ -12,7 +12,7 @@
 @media screen and (max-width: 700px) { 
 	.splash-content-block {
 		background-color: #00E6FF;
-		background: url(../svgs/top-clouds-mobile.svg?v=2) top;
+		background: url(../svgs/top-clouds-mobile2.svg?v=2) top;
 	
 	}
 }
