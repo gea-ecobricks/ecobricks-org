4d876aeb2519cd1227bf1aef2668d5423d9aeb21
Russell Maier <russ@earthen.io>
2024-02-24 17:19:16 +0800

    what all

--- fr/what.php
+++ fr/what.php
@@ -136,144 +136,131 @@
         </ul>
 
         <p data-lang-id="038-block-2-p3">Seules les écobriques qui répondent à ces normes peuvent être authentifiées et comptées comme du plastique séquestré.</p>
+		<br><br>
         <a class="action-btn" href="how.php" data-lang-id="039-block-2-btn">ℹ️ Comment faire</a>
         <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="040-block-2-p4">Consultez notre guide en 10 étapes pour fabriquer une écobrique</p>
     </div>
 </div>
 
 
-			<div class="reg-content-block" id="block3">
+<div class="reg-content-block" id="block3">
+    <div class="opener-header">
+        <div class="opener-header-text">
+            <h4 data-lang-id="041-block-3-header">Cigbricks</h4>
+            <h5 data-lang-id="042-block-3-subheader">Plastic sequestration for cigarette filers.</h5>
+        </div>
+        <button onclick="preclosed3()" class="block-toggle" id="block-toggle-show3" aria-label="Plus"></button>
+    </div>
 
-				<div class="opener-header">
-					
-					<div class="opener-header-text">
-					<h4>Cigbricks</h4>
-					<h5>Plastic sequestration for cigarette filers.</h5>
-					<br>
-					</div>
+    <div id="preclosed3">
+        <div class="row">
+            <div class="main2">
+                <p data-lang-id="043-block-3-main-p1">Cigbricks are a class of ecobrick to secure the potentially toxic plastic/acetate of the cigarette filters-- one of the most prevalent and damaging of all plastic pollutants. Cigbricks are made exclusively from the packed acetate filters of cigarette butts once the paper and ash has been fully removed.</p>
+            </div>
+            <div class="side2">
+                <img src="../svgs/Cigbrick.svg" width="300" style="width:100%;" alt="Cigbricks are for cigarettes" loading="lazy" data-lang-id="044-block-3-img1-alt">
+            </div>
+        </div>
 
-					<button onclick="preclosed3()" class="block-toggle" id="block-toggle-show3">+</button>
-				</div>
+        <div class="row2">
+            <div class="main3">
+                <p data-lang-id="045-block-3-main-p2">To be a Cigbrick the following standards must be met:</p>
+            </div>
+            <div class="side3">
+                <img src="../svgs/Cigbrick.svg" width="120" style="width:100%;" alt="Cigbricks are for cigarettes" loading="lazy" data-lang-id="046-block-3-img2-alt">
+            </div>
+        </div>
 
-				<div id="preclosed3">
-					<br>
-					<div class="row">
-						<div class="main2">
-						<p>Cigbricks are a class of ecobrick to secure the potentially toxic plastic/acetate of the cigarette filters-- one of the most prevalent and damaging of all plastic pollutants. Cigbricks are made exclusively from the packed acetate filters of cigarette butts once the paper and ash has been fully removed.</p>
-						</div>
-						<div class="side2"><img src="../svgs/Cigbrick.svg" width="300" style="width:100%;" alt="Cigbricks are for cigarettes" loading="lazy">
-						</div>
-					</div>
+        <ul>
+            <li data-lang-id="047-block-3-li-1">A density higher than 0.29 g/ml</li>
+            <li data-lang-id="048-block-3-li-2">A maximum density of 0.70g/ml</li>
+            <li data-lang-id="049-block-3-li-3">Only packed with acetate cigarette butts – the paper and ash have been removed.</li>
+            <li data-lang-id="050-block-3-li-4">The cigbrick is sealed tight with a screw down lid.</li>
+            <li data-lang-id="051-block-3-li-5">Care has been taken to prepare the cigbrick as a reusable building block:
+                <ul>
+                    <li data-lang-id="052-block-3-li-5-1">The label has been removed and ecobricked.</li>
+                    <li data-lang-id="053-block-3-li-5-2">A bottom color has been added</li>
+                    <li data-lang-id="054-block-3-li-5-3">The weight has been recorded on the outside.</li>
+                </ul>
+            </li>
+        </ul>
 
-					<div class="row2">
-						<div class="main3">
-						<p>To be a Cigbrick the following standards must be met:</p>
-						</div>
-						<div class="side3"><img src="../svgs/Cigbrick.svg" width="120" style="width:100%;" alt="Cigbricks are for cigarettes" loading="lazy">
-						</div>
-					</div>
-			
+        <p data-lang-id="055-block-3-p1">Only cigbricks that meet these standards can be authenticated and count as sequestered plastic.</p>
 
-					<ul>
-						<li>A density higher than 0.29 g/ml</li>
-						<li>A maximum density of 0.70g/ml</li>
-						<li>Only packed with acetate cigarette butts – the paper and ash have been removed.</li>
-						<li>The cigbrick is sealed tight with a screw down lid.</li>
-						<li>Care has been taken to prepare the cigbrick as a reusable building block:
-							<ul>
-							<li>The label has been removed and ecobricked.</li>
-							<li>A bottom color has been added</li>
-							<li>The weight has been recorded on the outside.</li>
-							</ul>
-						</li>
-						</ul>
-
-
-						<p>Only cigbricks that meet these standards can be authenticated and count as sequestered plastic.</p>
-						<br><br>
-
-						<a class="action-btn" href="/cigbricks">ℹ️ Make a Cigbrick</a>
-                    <p style="font-size: 0.85em; margin-top:20px;">Learn more about Cibricks.</p>
-
-
-				</div>
-			</div>
-
-			
-
-			<div class="reg-content-block" id="block4">
-				<div class="opener-header">
-					<div class="opener-header-text">
-					<h4>Ocean Ecobricks</h4>
-					<h5>Sequestration for plastic collected from beaches, ocean and rivers.</h5><br>
-					</div>
-
-					<button onclick="preclosed4()" class="block-toggle" id="block-toggle-show4">+</button>
-				</div>
-
-				<div id="preclosed4">
-					<br>	
-					<div class="row">
-						<div class="main2">
-						<p>Ocean Ecobricks are a class of ecobrick designed for plastics found on beaches, rivers and in the ocean. These plastics tend to be large, chunky, dirty, and wet and are not suited to make a regular ecobrick. The Ocean Ecobrick technique enables these plastics to be easily transformed into a practical, useful, and reusable building block.</p>
-						</div>
-						<div class="side2">
-						<img src="../webp/oeb-400px.webp" width="300" style="width:100%;" alt="Ocean Ecobricks are for dirty plastic" loading="lazy">
-						</div>
-					</div>
-
-					<div class="row2">
-						<div class="main3">
-						<p>To be an Ocean Ecobrick the following standards must be met:</p>
-						</div>
-						<div class="side3"><img src="../webp/oeb-400px.webp" width="120" style="width:100%;" alt="Ocean Ecobricks are for dirty plastic" loading="lazy">
-						</div>
-					</div>
-
-					<ul>
-						<li>A density higher than 0.25 g/ml</li>
-						<li>A maximum density of 0.70 g/ml</li>
-						<li>Are packed with as much plastic as possible</li>
-						<li>Sealed tight (but not air tight).</li>
-						<li>Care has been taken taken to prepare the ocean ecobrick as a reuseable building block:
-						<li>The bottle's label has been removed and ecobricked.</li>
-						<li>A bottom color has been added</li>
-						<li>The weight has been recorded on the outside.</li>
-					</ul>
-				
-					<p>Only ocean ecobricks that meet these standards can be authenticated and count as <a href="sequest.php">sequestered plastic</a>.</p>
-					<br><br>
-
-					<a class="action-btn" href="/ocean">ℹ️ Make an Ocean Ecobrick</a>
-                    <p style="font-size: 0.85em; margin-top:20px;">Check our page all about Ocean Ecobricks.</p>
-
-				</div>
-			</div>
-
-		
-		<div class="page-paragraph">
-			<br><br>
-		
-			<h4>Autonomisation personnelle et communautaire</h4>
-<p>L'écobrication est une entreprise à la fois individuelle et collaborative. Il unit les classes, les continents et les cultures autour de l'intention commune de prendre soin de la biosphère locale et mondiale. Le processus d'écobrique personnelle sensibilise aux conséquences de la consommation et aux dangers du plastique pour accélérer la <a href="transition.php">transition plastique</a>. Le processus collaboratif donne aux individus et aux communautés une expérience pratique de la <a href="/principles">mode de vie régénérateur</a> et oriente vers d'autres technologies régénératives, telles que la <a href="/earth">construction en terre </a> et la permaculture.</p>
-<br><br>
-
-<h4>En savoir plus</h4>
-
-<p>La Global Ecobrick Alliance a développé des ressources complètes, gratuites et open source (comme cette page !) pour vous aider à bien démarrer l'écobrick. Notre objectif est de vous donner, à vous et à votre communauté, le savoir-faire nécessaire pour fabriquer des écobriques et construire avec elles. Les pages de ce site sont le résultat d'années de recherche, d'expérimentation et d'essais et d'erreurs par des leaders de la transition plastique à travers le monde.
-</p>
-<ul>
-<li><a href="/why">Pourquoi fabriquer des écobricks ?</a></li>
-<li><a href="how.php">Comment Ecobrick ?</a></li>
-<li><a href="spiral.php">Conception spirale</a></li>
-<li><a href="build.php">Construire avec des Ecobricks</a></li>
-<li><a href="/movement">Le Mouvement</a></li>
-<li><a href="/dropoff">Vous déposez des écobriques ?</a></li>
-<li><a href="/trainings">Formations Ecobrick</a></li>
-</ul>
+        <a class="action-btn" href="/cigbricks" data-lang-id="056-block-3-btn">ℹ️ Make a Cigbrick</a>
+        <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="057-block-3-p2">Learn more about Cibricks.</p>
+    </div>
 </div>
 
-	</div>
+
+<div class="reg-content-block" id="block4">
+    <div class="opener-header">
+        <div class="opener-header-text">
+            <h4 data-lang-id="058-block-4-header">Ocean Ecobricks</h4>
+            <h5 data-lang-id="059-block-4-subheader">Sequestration for plastic collected from beaches, ocean and rivers.</h5>
+        </div>
+        <button onclick="preclosed4()" class="block-toggle" id="block-toggle-show4" aria-label="Plus"></button>
+    </div>
+
+    <div id="preclosed4">
+        <div class="row">
+            <div class="main2">
+                <p data-lang-id="060-block-4-main-p1">Ocean Ecobricks are a class of ecobrick designed for plastics found on beaches, rivers and in the ocean. These plastics tend to be large, chunky, dirty, and wet and are not suited to make a regular ecobrick. The Ocean Ecobrick technique enables these plastics to be easily transformed into a practical, useful, and reusable building block.</p>
+            </div>
+            <div class="side2">
+                <img src="../webp/oeb-400px.webp" width="300" style="width:100%;" alt="Ocean Ecobricks are for dirty plastic" loading="lazy" data-lang-id="061-block-4-img1-alt">
+            </div>
+        </div>
+
+        <div class="row2">
+            <div class="main3">
+                <p data-lang-id="062-block-4-main-p2">To be an Ocean Ecobrick the following standards must be met:</p>
+            </div>
+            <div class="side3">
+                <img src="../webp/oeb-400px.webp" width="120" style="width:100%;" alt="Ocean Ecobricks are for dirty plastic" loading="lazy" data-lang-id="063-block-4-img2-alt">
+            </div>
+        </div>
+
+        <ul>
+            <li data-lang-id="064-block-4-li-1">A density higher than 0.25 g/ml</li>
+            <li data-lang-id="065-block-4-li-2">A maximum density of 0.70 g/ml</li>
+            <li data-lang-id="066-block-4-li-3">Are packed with as much plastic as possible</li>
+            <li data-lang-id="067-block-4-li-4">Sealed tight (but not air tight).</li>
+            <li data-lang-id="068-block-4-li-5">Care has been taken to prepare the ocean ecobrick as a reusable building block:
+                <ul>
+                    <li data-lang-id="069-block-4-li-5-1">The bottle's label has been removed and ecobricked.</li>
+                    <li data-lang-id="070-block-4-li-5-2">A bottom color has been added</li>
+                    <li data-lang-id="071-block-4-li-5-3">The weight has been recorded on the outside.</li>
+                </ul>
+            </li>
+        </ul>
+
+        <p data-lang-id="072-block-4-p1">Only ocean ecobricks that meet these standards can be authenticated and count as <a href="sequest.php">sequestered plastic</a>.</p>
+
+        <a class="action-btn" href="/ocean" data-lang-id="073-block-4-btn">ℹ️ Make an Ocean Ecobrick</a>
+        <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="074-block-4-p2">Check our page all about Ocean Ecobricks.</p>
+    </div>
+</div>
+
+
+<div class="page-paragraph">
+    <h4 data-lang-id="075-final-header">Autonomisation personnelle et communautaire</h4>
+    <p data-lang-id="076-final-p1">L'écobrication est une entreprise à la fois individuelle et collaborative. Il unit les classes, les continents et les cultures autour de l'intention commune de prendre soin de la biosphère locale et mondiale. Le processus d'écobrique personnelle sensibilise aux conséquences de la consommation et aux dangers du plastique pour accélérer la <a href="transition.php">transition plastique</a>. Le processus collaboratif donne aux individus et aux communautés une expérience pratique de la <a href="/principles">mode de vie régénérateur</a> et oriente vers d'autres technologies régénératives, telles que la <a href="/earth">construction en terre </a> et la permaculture.</p>
+
+    <h4 data-lang-id="077-final-header-more">En savoir plus</h4>
+    <p data-lang-id="078-final-p2">La Global Ecobrick Alliance a développé des ressources complètes, gratuites et open source (comme cette page !) pour vous aider à bien démarrer l'écobrick. Notre objectif est de vous donner, à vous et à votre communauté, le savoir-faire nécessaire pour fabriquer des écobriques et construire avec elles. Les pages de ce site sont le résultat d'années de recherche, d'expérimentation et d'essais et d'erreurs par des leaders de la transition plastique à travers le monde.</p>
+
+    <ul>
+        <li data-lang-id="079-list-item-1"><a href="/why">Pourquoi fabriquer des écobricks ?</a></li>
+        <li data-lang-id="080-list-item-2"><a href="how.php">Comment Ecobrick ?</a></li>
+        <li data-lang-id="081-list-item-3"><a href="spiral.php">Conception spirale</a></li>
+        <li data-lang-id="082-list-item-4"><a href="build.php">Construire avec des Ecobricks</a></li>
+        <li data-lang-id="083-list-item-5"><a href="/movement">Le Mouvement</a></li>
+        <li data-lang-id="084-list-item-6"><a href="/dropoff">Vous déposez des écobriques ?</a></li>
+        <li data-lang-id="085-list-item-7"><a href="/trainings">Formations Ecobrick</a></li>
+    </ul>
+</div>
+
 
 	
 <!-- PAGE SIDE BAR STARTS HERE-->
