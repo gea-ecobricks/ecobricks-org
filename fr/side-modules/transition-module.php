9638e6fe5ccc8bc949d5b40ad363192222396c55
Russell Maier <russ@earthen.io>
2024-02-24 13:16:14 +0800

    6

--- fr/side-modules/transition-module.php
+++ fr/side-modules/transition-module.php
@@ -1,8 +1,8 @@
 
 			<div class="side-module-desktop-mobile">
-				<img src="../webp/earthhome-400px.webp" width="100%" loading="lazy" alt="une construction avec les ecobriques et la terre.">
+				<img src="../webp/earthhome-400px.webp" width="300" style="width:100%;" loading="lazy" alt="une construction avec les ecobriques et la terre.">
 				<h4>Transition plastique</h4>
-<h5>La construction Ecobrick est une technologie de transition. Plutôt que de considérer le plastique comme un "déchet", nous le considérons comme un élément constitutif de nos visions écologiques.</h5><br>
+<h5>L'Ecobriquage est une technologie de transition. Plutôt que de considérer le plastique comme un "déchet", nous le considérons comme un élément constitutif de nos visions les plus vertes!</h5><br>
                 <button type=button class="module-btn" onclick="location.href='transition.php'" aria-label="En savoir plus" title="En savoir plus sur la transition plastique et petrocapital.">En savoir plus</button>
                 <br>
 			</div>
