3934e05032c2706526738c60e4ad762dbf799d95
Russell Maier <russ@earthen.io>
2024-02-24 12:24:47 +0800

    d

--- fr/what.php
+++ fr/what.php
@@ -4,7 +4,7 @@
 <HEAD>
 <META charset="UTF-8">
 <?php $lang='fr';?>
-<?php $version='1.772';?>
+<?php $version='1.773';?>
 <?php $page='what';?>
 <title>Qu'est-ce qu'une Ecobrick? | Ecobricks.org</title>
 <?php require_once ("../includes/what2-inc.php");?>
