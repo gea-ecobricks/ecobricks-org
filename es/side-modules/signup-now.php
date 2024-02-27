b4dea4ee1944becd3d25ba63e4a2ce15f8a4d306
Russell Maier <russ@earthen.io>
2024-02-27 15:34:28 +0800

    s

--- es/side-modules/signup-now.php
+++ es/side-modules/signup-now.php
@@ -2,7 +2,7 @@
     <img src="../logos/earthen-newsletter-logo.svg?v=2" width="300" style="width:80%" loading="lazy" alt="ecoladrillo y construcción con tierra pueden hacer bancos circulares con árboles plantados en el medio">
     <br><h4>Recibe Earthen</h4>
     <h5>Recibe nuestro boletín bimensual Earthen y sigue los últimos desarrollos en el movimiento de transición del plástico.</h5><br>
-    <button class="module-btn" href="build.php" onclick="sendUpRegistration()" aria-label="Registro al Boletín">Regístrate</button>
+    <button class="module-btn" onclick="sendUpRegistration()" aria-label="Registro al Boletín">Regístrate</button>
     <h6 style="font-size:smaller;">Gratis y fantástico.</h6>
     <br>
 </div>
--- id/side-modules/signup-now.php
+++ id/side-modules/signup-now.php
@@ -2,7 +2,7 @@
     <img src="../logos/earthen-newsletter-logo.svg?v=2" width="300" style="width:80%" loading="lazy" alt="l'ecobrique et la construction en terre peuvent créer des bancs circulaires avec des arbres plantés au milieu">
     <br><h4>Recevez Earthen</h4>
     <h5>Recevez notre newsletter bimensuelle Earthen et suivez les derniers développements du mouvement de transition plastique.</h5><br>
-    <button class="module-btn" href="build.php" onclick="sendUpRegistration()" aria-label="Inscription à la Newsletter">Inscrivez-vous</button>
+    <button class="module-btn" onclick="sendUpRegistration()" aria-label="Inscription à la Newsletter">Inscrivez-vous</button>
     <h6 style="font-size:smaller;">Gratuit et génial.</h6>
     <br>
 </div>
