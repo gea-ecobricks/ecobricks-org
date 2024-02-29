96d363d965fd9c12170c4bcc6ba6cacec5266b3b
Russell Maier <russ@earthen.io>
2024-02-29 11:11:30 +0800

    3

--- fr/about.php
+++ fr/about.php
@@ -3,7 +3,7 @@
 <HEAD>
 <META charset="UTF-8">
 <?php $lang='fr';?>
-<?php $version='1.62';?>
+<?php $version='1.63';?>
 <?php $page='about';?>
 
 <?php require_once ("../includes/about2-inc.php");?>
