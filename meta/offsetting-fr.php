ddca7bad331a52d3309271a2e9f45d332a8094ec
Russell Maier <russ@earthen.io>
2024-03-01 16:48:57 +0800

    123

--- meta/offsetting-fr.php
+++ meta/offsetting-fr.php
@@ -8,18 +8,19 @@
 
 	<meta property="og:url"           content="https://ecobricks.org/fr/offsetting.php">
 	<meta property="og:type"          content="website">
- 	<meta property="og:title"         content="Plastic Offsetting">
-  	<meta property="og:description"   content="By taking responsibility for our personal plastic we can energize local and global plastic transition."/>
-  	<meta property="og:image"         content="https://ecobricks.org/pngs/plastic-offsetting-banner.png"/>
-  	<meta property="og:image:width" content="600" >
-	<meta property="og:image:height" content="600" >
-  	<meta property="og:image:alt"     content="Balancing our green and grey impacts"/>
+	<meta property="og:title"         content="Compensation du Plastique">
+  	<meta property="og:description"   content="En prenant la responsabilité de notre plastique personnel, nous pouvons dynamiser la transition plastique locale et mondiale.
+Comptabilité d'impact écologique pour assurer notre verdure.">
+  	<meta property="og:image"         content="https://ecobricks.org/png/plastic-offsetting-banner.png">
+  	<meta property="og:image:width" content="600">
+	<meta property="og:image:height" content="600" >	
+  	<meta property="og:image:alt"     content="La compensation est tout au sujet de s'assurer que nos impacts verts l'emportent sur nos gris"/>
 	  <meta property="og:locale" content="fr_FR" >
 
 	  <meta property="og:type" content="article" >
  <meta property="og:site_name" content="Ecobricks.org" >
  <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" >
- <meta property="article:modified_time" content="2024-02-14T09:14:13+00:00" >
+ <meta property="article:modified_time" content="2024-03-01T09:14:13+00:00" >
  <meta property="og:image:type" content="image/png" >
  <meta name="author" content="Global Ecobrick Alliance" >
  <meta name="twitter:card" content="summary" >
