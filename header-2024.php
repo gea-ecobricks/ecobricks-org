8f1ff45eb1975f0f301f60b4f41712aedb271800
Russell Maier <russ@earthen.io>
2024-03-03 12:36:41 +0800

    a

--- header-2024.php
+++ header-2024.php
@@ -169,7 +169,7 @@ window.onload = function() {
 
 
 
-.accordion-title button:hover {
+.accordion-title a:hover {
   background: var(--side-overlays) !important;
   color: var(--h1);
     font-weight: 600;
