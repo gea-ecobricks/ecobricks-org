3269fe6cb0946e4331c6f366bdd70c291598f918
Russell Maier <russ@earthen.io>
2024-03-04 14:11:14 +0800

    moved

--- en/side-modules/offset-presentation.php
+++ en/side-modules/offset-presentation.php
@@ -4,7 +4,6 @@
 					<h5>Download our PDF slide deck that provides a comprehensive overview of offsetting plastic through the GEA.</h5><br>
 
 					<a class="module-btn" target="_blank" href="https://nextcloud.ecobricks.org/index.php/s/XFc7KM3tSyWqzZ9">📄 AES Presentation</a>
-
 					<h6 style="font-size:smaller">3.8 MB PDF download</h6>
 
 					
