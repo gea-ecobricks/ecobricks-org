045abf9be820ba071314f29a37fc3ea3bd3d023a
Russell Maier <russ@earthen.io>
2024-03-06 12:09:12 +0800

    df

--- translations/core-fr-translation.js
+++ translations/core-fr-translation.js
@@ -69,7 +69,7 @@ const fr_Translations = {
     "025-menu-3-sub-item-2": '<a href="earthwands.php">Earthwands (Bâtons de Terre)</a><span class="circle" title="Cette page a été traduite à 100% en anglais" style="color:green;  ">●</span>',
     "026-menu-3-trans-text-2": "🏴 earthwands.php a été traduit à 100% | <a href=\"https://github.com/gea-ecobricks/ecobricks-org/blob/main/translations/earthwands-fr-translation.js\" class=\"translate-link\">traduction ⇗</a> | <a href=\"https://github.com/gea-ecobricks/ecobricks-org/blob/main/en/earthwands.php\" class=\"translate-link\">code ⇗</a>",
     "027-menu-3-sub-item-3": '<a href="transition.php">Transition Plastique</a><span class="circle" title="Cette page a été traduite à 100% en anglais" style="color:green;  ">●</span>',
-    "028-menu-3-trans-text-3": "🏴 transition.php a été traduit à 100% | <a href=\"https://github.com/gea-ecobricks/ecobricks-org/blob/main/en/transition.php\" class=\"translate-link\">code ⇗</a>"
+    "028-menu-3-trans-text-3": "🏴 transition.php a été traduit à 100% | <a href=\"https://github.com/gea-ecobricks/ecobricks-org/blob/main/en/transition.php\" class=\"translate-link\">code ⇗</a>",
 
 
 
