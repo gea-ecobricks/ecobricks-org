9a245af5506c4f9947cb4d57165c9d50cc612ef7
Russell Maier <russ@earthen.io>
2024-03-10 14:41:55 +0800

    8

--- fr/transition.php
+++ fr/transition.php
@@ -4,7 +4,7 @@
 <HEAD>
 <META charset="UTF-8">
 <?php $lang='fr';?>
-<?php $version='1.7777';?>
+<?php $version='1.8';?>
 <?php $page='transition';?>
 
 <?php require_once ("../includes/transition2-inc.php");?>
