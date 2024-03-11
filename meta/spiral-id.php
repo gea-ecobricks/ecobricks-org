56be2139e0e345cad8137dc560d27f02dfe98905
Russell Maier <russ@earthen.io>
2024-03-11 12:11:41 +0800

    des

---
+++ meta/spiral-id.php
@@ -0,0 +1,32 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Spiral Design</title>
+
+<meta name="keywords" content="regenerative principles,ecological ethics,  circular design, spiral design, biodiversity accounting, spiral, cyclical design, earthen ethics, sequestration of plastic, plastic offsetting, aes plastic, carbon sequestration.">
+
+<meta name="description" content="Let's make sure that the cycles of all our products and processes are steadily enriching-- socially and ecologically.">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://ecobricks.org/en/spiral.php">
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="Spiral Design">
+  	<meta property="og:description"   content="Let's make sure that the cycles of all our products and processes are steadily enriching-- socially and ecologically.">
+  	<meta property="og:image"         content="https://ecobricks.org/webp/spiral-design-banner-695px.webp">
+  	<meta property="og:image:width" content="695" >
+	<meta property="og:image:height" content="695" >
+  	<meta property="og:image:alt"     content="The iconic symbol of spiral design">
+	  <meta property="og:locale" content="en_GB" >
+
+	  <meta name="author" content="Ecobricks.org">
+  
+  <meta property="og:type" content="article" >
+ <meta property="og:site_name" content="Ecobricks.org" >
+ <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" >
+ <meta property="article:modified_time" content="2024-02-16T09:14:13+00:00" >
+ <meta property="og:image:type" content="image/png" >
+ <meta name="author" content="Global Ecobrick Alliance" >
+ <meta name="twitter:card" content="summary" >
+ <meta name="twitter:label1" content="Est. reading time" >
+ <meta name="twitter:data1" content="5 minutes" > 
+	 
