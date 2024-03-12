1022500c7656740f0c8758e4492262861a543046
Russell Maier <russ@earthen.io>
2024-03-12 15:46:28 +0800

    dsf

---
+++ id/side-modules/concentration-principle.php
@@ -0,0 +1,10 @@
+<div class="side-module-desktop-mobile">
+     <img src="../svgs/towards-concentration-shell.svg"width="250" style="width:70%;" alt="Mari kita membuat siklus penggunaan kembali semakin memperkaya." memuat="malas">
+     <br><h4>Konsentrasi Ver Le</h4>
+     <h5>Aplikasi Ecobrick mengikuti prinsip desain Tanah untuk mengintegrasikan plastik dalam siklus spiral menuju konsentrasi.</h5><br>
+     <a class="module-btn" href="https://book.earthen.io/en/matter.html" target="_blank">Pelajari lebih lanjut</a>
+     <h6 style="font-size:smaller">Prinsip konsentrasi</h6>
+
+</div>
+
+
