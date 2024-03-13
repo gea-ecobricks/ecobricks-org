862aaa586e72737177162997eaffe7636c342e42
Russell Maier <russ@earthen.io>
2024-03-13 18:43:02 +0800

    spiralf

--- translations/spiral-en-translation.js
+++ translations/spiral-en-translation.js
@@ -5,11 +5,63 @@
 
 -----------------------------------*/
 
-
 const en_Page_Translations = {
-    
-    
-    
+    "001-splash-heading": "Spiral Design",
+    "002-splash-sub": "Ecobricking and all ecobrick applications are guided by the Earthen principle of enriching cycles.",
+    "003-splash-image-alt": '<img src="../icons/spiral-design-white3.svg" style="width: 100%;" alt="Spiral Design">',
+    "004-lead-page-paragraph": "When we build with ecobricks we plan for the end of the construction, for the next application of every ecobrick, and the next after that.  This way we can be sure the cycles that result, enrich socially and ecologically.",
+    "005-page-paragraph": 'One of the several core<a href="principles.php"> ecobricking principles</a> is that of <i>spiral design</i>.  Unlike linear and circular design, rather than plan in lines and circles, we plan in spirals.  This way, we can embed an enriching spin of short and long-term reuse into the way we put our ecobricks to use.',
+    "006-context-heading": "Context",
+    "007-context-subheading": "Everything comes to its end.",
+    "008-lead-panel-paragraph": "Today, the vast majority of the products we use go from their birth in a factory, into our hands, to their grave. Pollution is the result.  Designs that fail to anticipate and plan for the end of a product are known as \"cradle-to-grave\".",
+    "009-first-detail-paragraph": "Unfortunately, products and materials that are designed to be 'circular' also lead to pollution and ecological depletion.  Although, \"cradle-to-cradle\" designs that can be used over are a step in the right direction, if the external impacts of each cycle aren't considered carefully (for example the making and fueling of the factory that makes a product), a spiral of depletion will result.  Alas, we see this all too clearly with traditional industrial recycling:  each time a piece of plastic is recycled, large amounts of emission-intensive transportation, sorting, smelting are required.",
+    "010-second-detail-paragraph": "With ecobricks, we do things differently!",
+    "011-earthen-principles-paragraph": 'Guided by the principles of <a href="https://book.earthen.io/en/summaries.html" data-lang-id="012-earthen-ethics-link">Earthen ethics</a>, the cycles of ecobricks and their applications are designed to be ecologically enriching: <i>greening</i>.  Not only do we plan for the next-life of our application, we also plan for its subsequent cycles.',
+    "012-earthen-ethics-link": "https://book.earthen.io/en/summaries.html",
+    "013-final-detail-paragraph": "To do so, we leverage the resilient properties of plastic to create reusable building blocks that have a procession of short and long-term applications.   This means that we ensure that ecobricks are both indefinitely reusable and that each cycle of building and dissembly contributes socially and ecologically.",
+
+    "014-earth-example-heading": "Following Earth's Example",
+    "015-earth-example-subheading": "Spiral design embodies the first principle of Earthen Ethics.",
+    "016-lead-panel-quote": "\"Observe the way Earth tends its processes towards cycles that spiral.\"",
+    "017-earthen-ethic-citation": "— <a href=\"https://book.earthen.io/en/spirals.html\" title=\"Banayan Angway, Russell Maier, 'Tractatus Ayyew: An Earthen Ethics' (Earthen.io, Philippines, Indonesia, 2022), https://book.earthen.io/en/spirals.html\">Earthen Ethic No. 2</a>",
+    "018-earth-greening-paragraph": "For the last billion years, Earth has been slow and steady greening the surface of our common home.",
+    "019-earth-cycle-paragraph": "The way that Earth has cycled and stored carbon has led to the blossoming of the biosphere– while providing us an example to follow to ensure their own processes are green!  With our plastic– which happens to come from Earth's ancient carbon stores– we can emulate Earth’s example of ensuring that our processes and outputs are not only spiral, but that lead to ever enriching cycles.",
+    "020-side-image-eco-enrichment": "<img src=\"../svgs/eco-enrichment.svg\" width=\"100%\">",
+    "021-earthen-ethics-basis": "Following Earth's example of carbon care is the basis of <a href=\"https://book.earthen.io\" target=\"_blank\">Earthen ethics</a>– five principles of green based on our planet's cosmological character.",
+    "022-earthen-ethics-link": "https://book.earthen.io",
+    "023-side-image-eco-enrichment-sm": "<img src=\"../svgs/eco-enrichment.svg\" width=\"150px\">",
+    "024-plastic-reuse-paragraph": "In the same way that Earth tends its carbon towards cycles of indefinite reuse, so too can we with our plastic.  By packing our plastic into ecobricks and using our ecobricks properly, we can make sure our plastic can be used over and over indefinitely.",
+    "025-learn-plastic-story": "ℹ️ Learn about: <a href=\"/plastic\">The Long Story of Plastic</a>",
+    "026-learn-plastic-link": "/plastic",
+    "027-learn-earthen-ethics": "ℹ️ Learn about: <a href=\"https://book.earthen.io/en/summaries.html\" target=\"_blank\">Earthen Ethics</a>",
+    "028-learn-earthen-ethics-link": "https://book.earthen.io/en/summaries.html",
+
+    "029-making-ecobricks-heading": "Making Ecobricks",
+    "030-making-ecobricks-subheading": "Ecobricking and its application embody spiral design",
+    "031-lead-panel-ecobricking": "Spiral design underlies every ecobrick method that the GEA endorses. This principle also helps guide the way that we make ecobricks and all the GEA's recommended best practices.",
+    "032-ecobricking-practice": "Some people wonder why, when we start an ecobrick we put a colored plastic on the bottom.  Spiral design is why: we’re thinking of the ecobrick's next phase when we’ll build with it. This is also why we use cob or silicone when we attach ecobricks to make modules and not glue or cement.  The first materials form a non-permanent bond, while the second are permanent.  This way, when our creation comes to its end, we can take it apart and use the ecobricks again.",
+    "033-side-image-build-blue": "<img src=\"../webp/build-blue-450px.webp\" width=\"100%\">",
+    "034-inspiration-management": "By leading by example in following Earth's example, our management of plastic can be a powerful inspiration for all our other materials and technologies.",
+    "035-side-image-community": "<img src=\"../pngs/community.png\" width=\"150px\">",
 
 
-    };
+  "036-ecobrick-designator-heading": "Brickable Designator Icon",
+    "037-ecobrick-designator-subheading": "A clear expression of spiral intention and design",
+    "038-lead-panel-designator": "A symbol to set products on a spiral spin.",
+    "039-designator-kit-description": 'A practical extension of spiral design is the GEA’s work developing the <a href="../ecobrickable" data-lang-id="040-ecobrickable-link">Brickable Designator Kit</a>. As ecobricks are essentially a catch-all for dead-end-plastic, they present an exciting and easy transitional solution for product designs.',
+    "040-ecobrickable-link": "../ecobrickable",
+    "041-side-image-ecobrickable-icon": "<img src=\"../icons/ecobrickable-icon-from-ecobricks-org.svg\" width=\"100%\">",
+    "042-ecobrickability-design": 'Brickability allows designers to take their first step to spiral design. With a little forethought and advance planning, their product can be made easily and effectively Ecobrickable. This design direction allows the transition of a company to full regenerative principles (see our <a href="catalyst.php" data-lang-id="043-catalyst-program-link">Catalyst company program</a>).',
+    "043-catalyst-program-link": "catalyst.php",
+    "044-side-image-ecobrickable-icon-small": "<img src=\"../icons/ecobrickable-icon-from-ecobricks-org.svg\" width=\"150px\">",
+    "045-spiral-vs-circular": "<b>Spiral vs Circular Design</b>",
+    "046-spiral-design-explanation": "It can be helpful to better understand what we mean by spiral design by clarifying what it is not.  Ecobricking, for example, is often mistaken as a ‘sustainable’ or 'circular' technology.  However, these terms refer to enabling our current human systems to spin indefinitely by minimizing their environmental harm.  This is not the goal of ecobricking!",
+    "047-goal-of-ecobricking": "Rather than 'minimize harm' (and remain harmful), instead, with our ecobricks we aim at net ecological contribution.  Instead of a static circle that spins indefinitely (no such thing exists!), we instead intend the way we use ecobricks toward cycles that enable richer societies and greener ecosystems.",
+    "048-spiral-design-inspiration": 'Spiral design is inspired by the concept of <a href="/ayyew" data-lang-id="049-ayyew-link">Ayyew</a> in the culture of the Igorot people of Northern Luzon.  This Igorot concept inspired and guided the <a href="/story" data-lang-id="050-early-ecobrick-movement-link">early Asian ecobrick movement</a>, the work of the <a href="about.php" data-lang-id="051-gea-link">GEA</a> and the development of our <a href="principles.php" data-lang-id="052-earthen-principles-link">Earthen principles</a>.',
+    "049-ayyew-link": "/ayyew",
+    "050-early-ecobrick-movement-link": "/story",
+    "051-gea-link": "about.php",
+    "052-earthen-principles-link": "principles.php",
+    "053-about-ayyew-btn": "<a class=\"action-btn\" href=\"/ayyew\">🍃 About Ayyew</a>",
+    "054-learn-about-indigenous-wisdom": "Learn about the indigenous wisdom tradition that underlies the Earthen ecobrick movement."
+}
--- translations/spiral-fr-translation.js
+++ translations/spiral-fr-translation.js
@@ -5,11 +5,66 @@
 
 -----------------------------------*/
 
-
 const fr_Page_Translations = {
+    "001-splash-heading": "Conception en Spirale",
+    "002-splash-sub": "L'écobriquage et toutes les applications des écobriques sont guidés par le principe terrestre d'enrichissement des cycles.",
+    "003-splash-image-alt": '<img src="../icons/spiral-design-white3.svg" style="width: 100%;" alt="Conception en Spirale">',
+    "004-lead-page-paragraph": "Lorsque nous construisons avec des écobriques, nous planifions la fin de la construction, pour la prochaine application de chaque écobrique, et la suivante après cela. De cette façon, nous pouvons être sûrs que les cycles qui en résultent, enrichissent socialement et écologiquement.",
+    "005-page-paragraph": 'Un des principes fondamentaux de l\'écobriquage est celui de la <i>conception en spirale</i>. Contrairement à la conception linéaire et circulaire, plutôt que de planifier en lignes et en cercles, nous planifions en spirales. De cette manière, nous pouvons intégrer une dynamique enrichissante de réutilisation à court et long terme dans la manière dont nous utilisons nos écobriques.',
+    "006-context-heading": "Contexte",
+    "007-context-subheading": "Tout a une fin.",
+    "008-lead-panel-paragraph": "Aujourd'hui, la grande majorité des produits que nous utilisons passent de leur naissance en usine, entre nos mains, à leur tombe. La pollution en est le résultat. Les conceptions qui échouent à anticiper et planifier la fin d'un produit sont connues sous le nom de \"du berceau à la tombe\".",
+    "009-first-detail-paragraph": "Malheureusement, les produits et matériaux conçus pour être 'circulaires' mènent également à la pollution et à l'épuisement écologique. Bien que les conceptions \"du berceau au berceau\" qui peuvent être réutilisées soient un pas dans la bonne direction, si les impacts externes de chaque cycle ne sont pas soigneusement considérés (par exemple la fabrication et l'alimentation de l'usine qui fabrique un produit), une spirale de déplétion en résultera. Hélas, cela est trop clair avec le recyclage industriel traditionnel : chaque fois qu'un morceau de plastique est recyclé, de grandes quantités de transport, de tri et de fusion intensifs en émissions sont nécessaires.",
+    "010-second-detail-paragraph": "Avec les écobriques, nous faisons les choses différemment !",
+    "011-earthen-principles-paragraph": 'Guidés par les principes de <a href="https://book.earthen.io/en/summaries.html" data-lang-id="012-earthen-ethics-link">l\'éthique terrestre</a>, les cycles des écobriques et de leurs applications sont conçus pour être écologiquement enrichissants : <i>verdissement</i>. Nous ne planifions pas seulement la prochaine vie de notre application, mais aussi ses cycles subséquents.',
+
+
+        "012-earthen-ethics-link": "https://book.earthen.io/en/summaries.html",
+        "013-final-detail-paragraph": "Pour ce faire, nous exploitons les propriétés résilientes du plastique pour créer des blocs de construction réutilisables qui ont une procession d'applications à court et long terme. Cela signifie que nous assurons que les écobriques sont indéfiniment réutilisables et que chaque cycle de construction et de démontage contribue socialement et écologiquement.",
+        "014-earth-example-heading": "Suivre l'exemple de la Terre",
+        "015-earth-example-subheading": "La conception en spirale incarne le premier principe de l'Éthique Terrestre.",
+        "016-lead-panel-quote": "\"Observez la manière dont la Terre oriente ses processus vers des cycles qui spiralent.\"",
+        "017-earthen-ethic-citation": "— <a href=\"https://book.earthen.io/en/spirals.html\" title=\"Banayan Angway, Russell Maier, 'Tractatus Ayyew: Une Éthique Terrestre' (Earthen.io, Philippines, Indonésie, 2022), https://book.earthen.io/en/spirals.html\">Éthique Terrestre N°2</a>",
+        "018-earth-greening-paragraph": "Depuis le dernier milliard d'années, la Terre verdit lentement et régulièrement la surface de notre maison commune.",
+        "019-earth-cycle-paragraph": "La manière dont la Terre a cyclé et stocké le carbone a conduit à l'épanouissement de la biosphère – tout en nous fournissant un exemple à suivre pour assurer que nos propres processus soient verts ! Avec notre plastique – qui provient des réserves de carbone anciennes de la Terre – nous pouvons imiter l'exemple de la Terre en assurant que nos processus et nos résultats soient non seulement en spirale, mais qu'ils conduisent à des cycles toujours plus enrichissants.",
+        "020-side-image-eco-enrichment": "<img src=\"../svgs/eco-enrichment.svg\" width=\"100%\">",
+        "021-earthen-ethics-basis": "Suivre l'exemple de la Terre en matière de soin du carbone est la base de <a href=\"https://book.earthen.io\" target=\"_blank\">l'éthique terrestre</a> – cinq principes de vert basés sur le caractère cosmologique de notre planète.",
+        "022-earthen-ethics-link": "https://book.earthen.io",
+        "023-side-image-eco-enrichment-sm": "<img src=\"../svgs/eco-enrichment.svg\" width=\"150px\">",
+        "024-plastic-reuse-paragraph": "De la même manière que la Terre oriente son carbone vers des cycles de réutilisation indéfinie, nous pouvons aussi le faire avec notre plastique. En emballant notre plastique dans des écobriques et en utilisant correctement nos écobriques, nous pouvons nous assurer que notre plastique peut être utilisé encore et encore indéfiniment.",
+        "025-learn-plastic-story": "ℹ️ Découvrez : <a href=\"/plastic\">La longue histoire du plastique</a>",
+        "026-learn-plastic-link": "/plastic",
+        "027-learn-earthen-ethics": "ℹ️ Découvrez : <a href=\"https://book.earthen.io/en/summaries.html\" target=\"_blank\">L'Éthique Terrestre</a>",
+        "028-learn-earthen-ethics-link": "https://book.earthen.io/en/summaries.html",
+
+        "029-making-ecobricks-heading": "Fabrication d'Écobriques",
+        "030-making-ecobricks-subheading": "L'écobriquage et son application incarnent la conception en spirale",
+        "031-lead-panel-ecobricking": "La conception en spirale est à la base de chaque méthode d'écobriquage que la GEA approuve. Ce principe aide également à guider la manière dont nous fabriquons les écobriques et toutes les meilleures pratiques recommandées par la GEA.",
+        "032-ecobricking-practice": "Certaines personnes se demandent pourquoi, lorsque nous commençons une écobrique, nous mettons du plastique coloré au fond. La conception en spirale en est la raison : nous pensons à la prochaine phase de l'écobrique lorsque nous construirons avec. C'est également pourquoi nous utilisons du cob ou du silicone lorsque nous attachons des écobriques pour faire des modules et non de la colle ou du ciment. Les premiers matériaux forment une liaison non permanente, tandis que les seconds sont permanents. Ainsi, lorsque notre création arrive à son terme, nous pouvons la démonter et réutiliser les écobriques.",
+        "033-side-image-build-blue": "<img src=\"../webp/build-blue-450px.webp\" width=\"100%\">",
+        "034-inspiration-management": "En donnant l'exemple en suivant l'exemple de la Terre, notre gestion du plastique peut être une puissante inspiration pour tous nos autres matériaux et technologies.",
+        "035-side-image-community": "<img src=\"../pngs/community.png\" width=\"150px\">",
     
-    
-    
+        "036-ecobrick-designator-heading": "Icône du Designateur d'Écobriquable",
+        "037-ecobrick-designator-subheading": "Une expression claire de l'intention et de la conception en spirale",
+        "038-lead-panel-designator": "Un symbole pour orienter les produits vers une rotation en spirale.",
+        "039-designator-kit-description": 'Une extension pratique de la conception en spirale est le travail de la GEA pour développer le <a href="../ecobrickable" data-lang-id="040-ecobrickable-link">Kit de Designateur Écobriquable</a>. Comme les écobriques sont essentiellement un attrape-tout pour le plastique sans issue, elles présentent une solution transitoire excitante et facile pour les conceptions de produits.',
+        "040-ecobrickable-link": "../ecobrickable",
+        "041-side-image-ecobrickable-icon": "<img src=\"../icons/ecobrickable-icon-from-ecobricks-org.svg\" width=\"100%\">",
+        "042-ecobrickability-design": "L'Écobriquabilité permet aux concepteurs de faire leur premier pas vers la conception en spirale. Avec un peu de réflexion et de planification à l'avance, leur produit peut être facilement et efficacement Écobriquable. Cette direction de conception permet la transition d'une entreprise vers des principes entièrement régénératifs (voir notre <a href=\"catalyst.php\" data-lang-id=\"043-catalyst-program-link\">programme d'entreprise Catalyst</a>).",
+        "043-catalyst-program-link": "catalyst.php",
+        "044-side-image-ecobrickable-icon-small": "<img src=\"../icons/ecobrickable-icon-from-ecobricks-org.svg\" width=\"150px\">",
+        "045-spiral-vs-circular": "<b>Conception en Spirale vs Circulaire</b>",
+        "046-spiral-design-explanation": "Il peut être utile de mieux comprendre ce que nous entendons par conception en spirale en clarifiant ce qu'elle n'est pas. L'écobriquage, par exemple, est souvent considéré à tort comme une technologie 'durable' ou 'circulaire'. Cependant, ces termes font référence à permettre à nos systèmes humains actuels de tourner indéfiniment en minimisant leur impact environnemental. Ce n'est pas le but de l'écobriquage !",
+        "047-goal-of-ecobricking": "Plutôt que de 'minimiser les dégâts' (et de rester nocif), au contraire, avec nos écobriques, nous visons une contribution écologique nette. Au lieu d'un cercle statique qui tourne indéfiniment (ce qui n'existe pas !), nous avons l'intention d'utiliser nos écobriques vers des cycles qui permettent des sociétés plus riches et des écosystèmes plus verts.",
+        "048-spiral-design-inspiration": 'La conception en spirale est inspirée par le concept d\'<a href="/ayyew" data-lang-id="049-ayyew-link">Ayyew</a> dans la culture du peuple Igorot du nord de Luzon. Ce concept Igorot a inspiré et guidé <a href="/story" data-lang-id="050-early-ecobrick-movement-link">le mouvement écologique asiatique précoce</a>, le travail de la <a href="about.php" data-lang-id="051-gea-link">GEA</a> et le développement de nos <a href="principles.php" data-lang-id="052-earthen-principles-link">principes Terrestres</a>.',
+        "049-ayyew-link": "/ayyew",
+        "050-early-ecobrick-movement-link": "/story",
+        "051-gea-link": "about.php",
+        "052-earthen-principles-link": "principles.php",
+        "053-about-ayyew-btn": "<a class=\"action-btn\" href=\"/ayyew\">🍃 À propos d'Ayyew</a>",
+        "054-learn-about-indigenous-wisdom": "Découvrez la tradition de sagesse indigène qui sous-tend le mouvement écologique Terrestre."
 
 
     };
+    
