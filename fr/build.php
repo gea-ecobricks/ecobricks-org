4cab30a1c8c7491db008e3bba39af9d4cec43d2c
Russell Maier <russ@earthen.io>
2024-03-15 10:08:01 +0800

    go

--- fr/build.php
+++ fr/build.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<HTML lang="fr"> 
+<HTML lang="id"> 
 <HEAD>
 <META charset="UTF-8">
-<?php $lang='fr';?>
-<?php $version='1.6';?>
+<?php $lang='id';?>
+<?php $version='1.647';?>
 <?php $page='build';?>
 
 
@@ -16,11 +16,11 @@
 
 <div class="splash-content-block">
 	<div class="splash-box">
-		<div class="splash-heading" data-lang-id="001-splash-title">Ecobrick Applications</div>
-	    <div class="splash-sub" data-lang-id="002-splash-subtitle">Practical & Principled Ecobrick Building</div>
+		<div class="splash-heading" data-lang-id="001-splash-title">Applications écobrique</div>
+	    <div class="splash-sub" data-lang-id="002-splash-subtitle">Construction pratique et principée</div>
 	</div>
 	<div class="splash-image" data-lang-id="003-splash-image-alt"><img src="../svgs/building-methods.svg" style="width: 85%" alt="There are many ways to build with ecobricks">
-    </div>	
+    </div>	    
 </div>
 <div id="splash-bar"></div>
 <!--<div id="header-bar2"></div>-->
@@ -199,43 +199,43 @@
       <!-- OPEN SPACES -->
 <div class="reg-content-block" id="block5">
     <div class="opener-header">
-        <div class="opener-header-text" data-lang-id="033-opener-header-text">
+        <div class="opener-header-text" data-lang-id="1033-opener-header-text">
             <h4>4. Open Spaces</h4>
             <h5>Ecobrick Open Spaces are for community that have completed thousands of high quality ecobricks with consistent bottle sizes and brands. </h5><br>
         </div>
         <button onclick="preclosed5()" class="block-toggle" id="block-toggle-show5">+</button>
     </div>
     <div id="preclosed5">
-        <img src="../photos/openspace-960px.jpg" width="100%" alt="Eco-brick open spaces for use with eco brick modules of all kinds" loading="lazy" data-lang-id="034-openspace-img-alt">
-        <p data-lang-id="035-openspace-description">Ecobrick Open Spaces are for community that have completed thousands of high quality ecobricks with consistent bottle sizes and brands. These ecobricks can then be combined into an assortment of Milstein and Dieleman modules to create a diverse set of several hundred modules.</p>
+        <img src="../photos/openspace-960px.jpg" width="100%" alt="Eco-brick open spaces for use with eco brick modules of all kinds" loading="lazy" data-lang-id="1034-openspace-img-alt">
+        <p data-lang-id="1035-openspace-description">Ecobrick Open Spaces are for community that have completed thousands of high quality ecobricks with consistent bottle sizes and brands. These ecobricks can then be combined into an assortment of Milstein and Dieleman modules to create a diverse set of several hundred modules.</p>
         <div class="row">
-            <div class="main2" data-lang-id="036-main2-description">
+            <div class="main2" data-lang-id="1036-main2-description">
                 <p>These modules can then be deployed for conferences, exhibitions, fairs, and other special events to create customized interactive spaces. These spaces can either be set up in advance or invite participants to interact, play, and create their own space.</p>
             </div>
-            <div class="side2"><img src="../webp/openspace.webp" width="100%" loading="lazy" alt="Examples of an eco brick open space" data-lang-id="037-side2-img-alt">
+            <div class="side2"><img src="../webp/openspace.webp" width="100%" loading="lazy" alt="Examples of an eco brick open space" data-lang-id="1037-side2-img-alt">
             </div>
         </div>
         <div class="row2">
-            <div class="main3" data-lang-id="038-main3-description">
+            <div class="main3" data-lang-id="1038-main3-description">
                 <p>Ecobrick communities that have amassed Open Space sets often rent out to events and venues to showcase ecobrick modular technology and regenerative principles.</p>
             </div>
-            <div class="side3"><br><img src="../webp/openspace.webp" width="120px" loading="lazy" alt="Example eco brick open space" data-lang-id="039-side3-img-alt">
+            <div class="side3"><br><img src="../webp/openspace.webp" width="120px" loading="lazy" alt="Example eco brick open space" data-lang-id="1039-side3-img-alt">
             </div>
         </div>
         <br>
         <hr>
-        <h4 data-lang-id="040-maintenance-header">🟡 Maintenance</h4>
-        <p data-lang-id="041-maintenance-info">Open space module sets should be cleaned once a year. Broken modules can be repaired with silicone.</p>
+        <h4 data-lang-id="1040-maintenance-header">🟡 Maintenance</h4>
+        <p data-lang-id="1041-maintenance-info">Open space module sets should be cleaned once a year. Broken modules can be repaired with silicone.</p>
         <hr>
-        <h4 data-lang-id="042-next-life-header">🟢 Next Life</h4>
-        <p data-lang-id="043-next-life-info">Well-used ecobrick modules will last 2-3 years. After this time, the silicone joints began to weaken and to fail. Ecobricks also begin to lose their lustre as the colors of the contained plastic begin to fade. This is a good opportunity to transition them to long-term building applications where they are fully protected with earth.</p>
+        <h4 data-lang-id="1042-next-life-header">🟢 Next Life</h4>
+        <p data-lang-id="1043-next-life-info">Well-used ecobrick modules will last 2-3 years. After this time, the silicone joints began to weaken and to fail. Ecobricks also begin to lose their lustre as the colors of the contained plastic begin to fade. This is a good opportunity to transition them to long-term building applications where they are fully protected with earth.</p>
         <hr>
-        <h4 data-lang-id="044-caution-header">🔴 Caution</h4>
-        <p data-lang-id="045-caution-info">Open spaces should be indoor or in covered spaces only. The UV rays in sunlight will degrade and fade your ecobricks and weaken the bottle’s plastic.</p>
+        <h4 data-lang-id="1044-caution-header">🔴 Caution</h4>
+        <p data-lang-id="1045-caution-info">Open spaces should be indoor or in covered spaces only. The UV rays in sunlight will degrade and fade your ecobricks and weaken the bottle’s plastic.</p>
         <hr>
         <br>
-        <a class="action-btn" href="/openspace" data-lang-id="046-action-btn-openspace">🚀 Ecobrick Open Spaces</a>
-        <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="047-openspace-info">Check our page all about Ecobrick Open Spaces.</p>
+        <a class="action-btn" href="/openspace" data-lang-id="1046-action-btn-openspace">🚀 Ecobrick Open Spaces</a>
+        <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="1047-openspace-info">Check our page all about Ecobrick Open Spaces.</p>
     </div>
 </div>
 
@@ -243,7 +243,7 @@
 <!-- EARTH BUILDING -->
 <div class="reg-content-block" id="block6">
     <div class="opener-header">    
-        <div class="opener-header-text" data-lang-id="048-opener-header-text">
+        <div class="opener-header-text" data-lang-id="1048-opener-header-text">
             <h4>5. Earth & Ecobrick Building</h4>
             <h5>The most enduring and greenest way to build with ecobricks is using Earth Building techniques.</h5><br>
         </div>
@@ -251,8 +251,8 @@
     </div>
     <div id="preclosed6">
         <img src="https://www.ecobricks.org/wp-content/uploads/2017/05/green-space.jpg" width="100%" alt="eco brik green spaces" loading="lazy" data-lang-id="049-eco-brik-green-spaces-img-alt">
-        <p data-lang-id="050-earth-building-description">Our number one choice for structural building! This method is strong, earthquake resilient, allows curved walls and designs, allows ecobricks to be recycled at the end of the construction, and relies only on 100% organic, locally-sourced materials that cost next to nothing.</p>
-        <p data-lang-id="051-earth-building-advantages">Earth building was used by our ancestors to build homes and structures that have endured for centuries. This general method is known by different names in different languages and continents– cob, adobe, wattle-and-daub, etc. By using local earth, straw and sand to make the mortar for the ecobricks we can build in ways that are resilient, strong, beautiful, and thermal-retentive.</p>
+        <p data-lang-id="1050-earth-building-description">Our number one choice for structural building! This method is strong, earthquake resilient, allows curved walls and designs, allows ecobricks to be recycled at the end of the construction, and relies only on 100% organic, locally-sourced materials that cost next to nothing.</p>
+        <p data-lang-id="1051-earth-building-advantages">Earth building was used by our ancestors to build homes and structures that have endured for centuries. This general method is known by different names in different languages and continents– cob, adobe, wattle-and-daub, etc. By using local earth, straw and sand to make the mortar for the ecobricks we can build in ways that are resilient, strong, beautiful, and thermal-retentive.</p>
         <div class="row">
             <div class="main2" data-lang-id="052-main2-earth-principles">
                 <p>Earth building is in full resonance with our principles — it is simple and replicable, non-capital, non-petroleum, collaboration powered, cradle-to-cradle and potently regenerative. Earth building techniques are gender and age transcendent: involving men and women, young and old, and everyone in between in the building process.</p>
@@ -267,8 +267,8 @@
             <div class="side3"><br><img src="https://www.ecobricks.org/wp-content/uploads/2020/04/cricle-bench-and-mango-tree-simple.svg" width="120px" loading="lazy" alt="eco brik and earth building can make tree benches" data-lang-id="055-side3-earth-bench-img-alt">
             </div>
         </div>
-        <p data-lang-id="056-ecobrick-wall-technique">The Ecobrick and Earth Building techniques can then be extrapolated for wall and structure construction.</p>
-        <p data-lang-id="057-ecobrick-coverage">The Earth Building technique can completely cover ecobricks, effectively securing them indefinitely in the construction. This keeps them safe from UV light and other forms of degradation. After 10, 20 or 100 years, when your structure comes to its end (as everything eventually does) the ecobricks can be extricated undamaged and fully repurposed.</p>
+        <p data-lang-id="1056-ecobrick-wall-technique">The Ecobrick and Earth Building techniques can then be extrapolated for wall and structure construction.</p>
+        <p data-lang-id="1057-ecobrick-coverage">The Earth Building technique can completely cover ecobricks, effectively securing them indefinitely in the construction. This keeps them safe from UV light and other forms of degradation. After 10, 20 or 100 years, when your structure comes to its end (as everything eventually does) the ecobricks can be extricated undamaged and fully repurposed.</p>
         <br>
         <hr>
         <h4 data-lang-id="058-maintenance-header">🟡 Maintenance</h4>
@@ -290,14 +290,21 @@
 <!-- ECOJOINERS -->
 
 
-<div class="reg-content-block" id="block10">
-    <div class="live-data" style="margin-top:30px">
+<div class="reg-content-block" id="block9">
         <div class="opener-header">     
             <div class="opener-header-text" >
                 <h4 data-lang-id="082-ecojoiner-title-text">6. Ecojoiners</h4>
-                <h5 data-lang-id="083-ecojoiner-subtitle-text">A revolutionary new way to connect bottles and build. Coming soon!</h5><br>
+                <h5 data-lang-id="083-ecojoiner-subtitle-text">A revolutionary new way to connect bottles and build. Coming 2024.</h5><br>
             </div>
-        </div>
+            <button onclick="preclosed9()" class="block-toggle" id="block-toggle-show9">+</button>
+    </div>
+    <div id="preclosed9">
+        <img src="../photos/ecojoiner-preview-700px.png" width="100%" loading="lazy" alt="Ecojoiner preview image" data-lang-id="1000-ecojoiner-img-alt">
+
+        <p data-lang-id="1001-ecojoiner-lead-text">A revolutionary low-tech system to enable the construction of versatile, practical and stunning geometric matrixes with minimal skills or capital using nothing but reclaimed plastic. </p>
+        <p data-lang-id="1002-ecojoiner-paragraph">Remolded from locally-sourced, reclaimed-plastic, ecoJoiners enable ecobricks to be interconnected in a wide variety of ways.  Simple ecoJoiners (green) enable  vertices of any length.  Hex and penta hubs (red and pink)  enable lattices with the help of pupJoiners (orange).  All are held fast with sliders (yellow) that takes advantage of an ecobrick's slight compressibility to lock constructions rigidly in place-- all the while ensuring any construction can be easily taken apart and each piece can be used again and again.</p>
+        <p>Coming in mid-2024.</p>
+        <br><br>
     </div>
 </div>
 
@@ -355,7 +362,7 @@
             <p>Go deeper into putting ecobricks to good use. Learn about the core concepts of building with ecobricks and earth.</p>
             <br><p>
             <a class="action-btn" href="earth.php" data-lang-id="079-earth-building-btn">🚀 Earth & Ecobrick Building</a>
-            <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="080-earth-building-overview">An overview of earth building principles and methods.</p>            
+            <p style="font-size: 0.85em; margin-top:20px;">An overview of earth building principles and methods.</p>            
         </div>
         <div class="side2">
             <br><img src="../webp/road-500px.webp" width="100%" alt="eco brick road into the distance" loading="lazy" data-lang-id="081-eco-brick-road-img-alt">
