2ca6d0cece387a254fb5d1f7be231f6bd20d8dfb
Russell Maier <russ@earthen.io>
2024-03-16 10:01:03 +0800

    sdfsdf

--- en/catalyst.php
+++ en/catalyst.php
@@ -17,7 +17,7 @@
 		<div class="splash-heading">A Program for Keen Green Enterprises to Track, Disclose & Accelerate their Plastic Transition.</div>
 		<!--<div class="splash-sub">A Solution For Plastic transition</div>-->
 	</div>
-	<div class="splash-image"><img src="https://ecobricks.org/pngs/catalyst-logo-500px.png" style="width: 70%;"></div>	
+	<div class="splash-image"><img src="../pngs/catalyst-logo-500px.png" style="width: 70%;"></div>	
 </div>
 <div id="splash-bar"></div>
 
@@ -238,8 +238,9 @@
 		<div class="side">
 
 
-			
-		<div id="side-module-desktop-only">
+		
+
+		<div class="side-module-desktop-only">
 			
 			
 				<h4>Get Started</h4>
@@ -247,22 +248,23 @@
 				<a class="action-btn" href="https://gobrik.com/#catalyze" target="_blank">🚀 Launch</a><br><br>
 			</div>
 
-	
-			<div id="side-module-desktop-mobile">
+			<?php require_once ("side-modules/about-gea.php");?>
+
+			<div class="side-module-desktop-mobile">
 				<img src="../webp/gea-logo-400px.webp" width="90%">
 				<h4>Global Ecobrick Alliance</h4>
 				<h5>The GEA is dedicated to accelerating plastic transition.  We preside over the GoBrik app and the Brikcoin blockchain.</h5><br>
 				<a class="module-btn" href="about.php">About Us</a>
 			</div>
 
-			<div id="side-module-desktop-mobile">
+			<div class="side-module-desktop-mobile">
 				<img src="../webp/for-earth500px.webp" width="80%">
 				<h4>For-Earth Enterprise</h4>
 				<h5>By discolosing our carbon, plastic and biodiversity impacts and by ensuring that they are net-green, the GEA commits to for-Earth Enterprise</h5><br>
 				<a class="module-btn" href="2021-regen-report.php">Learn More</a>
 			</div>
 
-			<div id="side-module-desktop-mobile">
+			<div class="side-module-desktop-mobile">
 				<img src="../webp/balancing-green.webp" width="80%">
 				<h4>Plastic Offsetting</h4>
 				<h5>Our Catalyst Program allows you to offset your plastic generation and support the global ecobrick movement.</h5><br>
