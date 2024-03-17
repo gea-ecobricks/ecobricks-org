8be3cb078fd6ad036c938db0e77598e827c76db9
Russell Maier <russ@earthen.io>
2024-03-17 19:34:35 +0800

    en

--- translations/madia-en-translation.js
+++ translations/madia-en-translation.js
@@ -14,7 +14,21 @@ const en_Page_Translations = {
     "004-lead-page-paragraph": "Covering ecobricks?  We’ve assembled this guide to avoiding ecobrick misconceptions, using generally accepted terminology and attributing our content to help you with your story.",
     "005-first-page-paragraph": 'The Global Ecobrick Alliance uses terminology that has been collectively determined by the global ecobrick movement and established on the <a href="https://en.wikipedia.org/wiki/Plastic_sequestration" target="_blank">Wikipedia Ecobricks article</a> throughout our site and resources.',
     "006-second-page-paragraph": "We encourage the same usage of terms and spellings by journalist, researchers and ecobrickers to add consistency and momentum to local and global grass roots plastic transition movements.",
-    "007-third-page-paragraph": "Below we provide an overview of ecobrick terms, misconceptions, our creative commons licenses and our requisite attribution tags for using or texts, photos and videos."
+    "007-third-page-paragraph": "Below we provide an overview of ecobrick terms, misconceptions, our creative commons licenses and our requisite attribution tags for using or texts, photos and videos.",
+
+    "008-terms-title": "Terms & Spellings",
+    "009-terms-subtitle": "The generally accepted spelling for key terms.",
+    "010-ecobrick-definition": "<b>Ecobrick</b>",
+    "011-ecobrick-spelling": '<b>"ecobrick"</b> — no space, hyphen or added capitalization is the recognized spelling.  This is the recognized spelling of the ecobrick’s Wikipedia entry and URL: <a href="https://en.wikipedia.org/wiki/ecobricks" target="_blank">wikipedia.org/wiki/ecobricks.</a>',
+    "012-ecobrick-history": "In the past, terms such as \"bottle brick\", \"eco brick\" and \"eco-brick\" have been used to refer to a PET bottle packed with plastic.  In 2014, plastic transition leaders around the world conferred and agreed that our concept deserved its own full-fledged and normalized word.  The ecobrick Wikipedia page was updated accordingly in 2016.",
+    "013-ecobrick-note": 'NOTE:  Other brick-making technologies borrow the term ecobrick by using a variety of spellings.  Corporations such as Nestle have attempted to appropriate \'ecobrick\' for their cement block corporate social responsibility programs. However, plastic sequestration in a PET bottle is the <a href="https://en.wikipedia.org/wiki/Eco-Brick" target="_blank">recognized Wikipedia disambiguation for the non-hyphenated spelling.</a>',
+    "014-plastic-sequestration-definition": "<b>Plastic Sequestration</b>",
+    "015-plastic-sequestration-goal": 'The goal of ecobricking is the <a href="https://en.wikipedia.org/wiki/Plastic_sequestration">sequestration of plastic</a>.  This is the official term as defined and recognized on Wikipedia.',
+    "016-ecobricker-definition": "<b>Ecobricker</b>",
+    "017-ecobricker-description": "Someone who makes an ecobrick or who has committed to ecobricking all their plastic",
+    "018-ecobricking-definition": "<b>Ecobricking</b>",
+    "019-ecobricking-description": "The act of sequestering plastic inside an ecobrick."
+
 };
 
       
