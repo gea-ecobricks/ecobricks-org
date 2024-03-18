4d42b5231449ba088d80649a43df1805f94ccb97
Russell Maier <russ@earthen.io>
2024-03-18 14:12:57 +0800

    id

--- translations/modules-id-translation.js
+++ translations/modules-id-translation.js
@@ -15,18 +15,20 @@ const id_Page_Translations = {
     "005-first-page-paragraph": "Modul Milstein dapat dibuat oleh siapa saja, di mana saja menggunakan bahan dan metode sederhana. Menggunakan metode silikon atau pengikat tabung, ecobricks dapat langsung digunakan dalam aplikasi yang mewujudkan prinsip regeneratif dan siklus.",
     "006-second-page-paragraph": "Modul Milstein dapat digunakan sendiri sebagai tempat duduk atau digabungkan seperti LEGO untuk membuat permukaan horizontal. Orang-orang menggunakannya untuk meja, tempat tidur, bangku dan bahkan panggung! Milsteins sering dikombinasikan dengan <a href=\"/dms\">Modul Dieleman</a> untuk menciptakan <a href=\"/openspace\">Ruang Terbuka Ecobrick.</a>",
 
-    "007-eco-brik-benches-img": '<img src="../webp/milstein.webp" width="100%" loading="lazy" alt="12 ecobricks dapat membuat modul ecobrick segitiga milstein">',
-    "008-start-with-briks": "Mulai hanya dengan 12 Ecobricks",
-    "009-first-milstein-module": "Untuk membuat modul milstein segitiga pertama Anda, yang Anda butuhkan hanyalah dua belas ecobricks. Setelah selesai, Anda dapat terus menambahkannya untuk membuat heksagon.",
-    "010-module-table-img": '<img src="../webp/module-table.webp" width="100%" loading="lazy" alt="16 ecobricks membuat heksagon, banyak modul heksagon dapat membuat instalasi furnitur yang keren">',
-    "011-combine-modules": "Gabungkan Modul",
-    "012-module-combination": "Modul Milstein cocok bersama! Gabungkan modul secara vertikal atau horizontal untuk membuat lebih banyak furnitur.",
-    "013-module-table-img": '<img src="../webp/earth-tree-400px.webp" width="100%" loading="lazy" alt="setelah ecobrick telah digunakan dalam sebuah modul, itu dapat dispiralkan ke dalam konstruksi tanah">',
-    "014-then-earth": "Lalu Bangun dengan Tanah!",
-    "015-module-spiral": "Setelah ecobricks Anda telah digunakan dalam satu atau dua modul, maka Anda dapat memasukkannya ke dalam konstruksi tanah jangka panjang.",
-    "module-btn": '<a class="module-btn" href="earth.php">Pembangunan dengan Tanah</a>'
-  
-  
+    "008-milstein-showcase-gal-img": "<img src=\"../photos/modules/Danielle-Wilkinson-rainbow-Milstein-Module.jpg\" alt=\"Modul milstein pelangi\" loading=\"lazy\"><p>Modul milstein pelangi | Inggris, UK</p>",
+    "009-milstein-showcase-gal-img": "<img src=\"../photos/modules/first-modules.jpeg\" alt=\"Beberapa modul pertama yang pernah dibuat | Tadjian, Filipina\" loading=\"lazy\"><p>Beberapa modul pertama yang pernah dibuat | Tadjian, Filipina</p>",
+    "010-milstein-showcase-gal-img": "<img src=\"../photos/modules/indonesia-module.jpg\" alt=\"Modul yang dibuat dari botol persegi | Indonesia\" loading=\"lazy\"><p>Modul yang dibuat dari botol persegi | Indonesia</p>",
+    "011-milstein-showcase-gal-img": "<img src=\"../photos/modules/kid-sitting-module.jpg\" alt=\"Sederhana dan praktis | Jogja, Indonesia.\" loading=\"lazy\"><p>Sederhana dan praktis | Jogja, Indonesia.</p>",
+    "012-milstein-showcase-gal-img": "<img src=\"../photos/modules/lots-of-modules.jpg\" alt=\"Konstruksi cob dan ecobrick lingkaran.\" loading=\"lazy\"><p>Konstruksi cob dan ecobrick lingkaran.</p>",
+    "013-milstein-showcase-gal-img": "<img src=\"../photos/modules/modules-storage.jpg\" alt=\"Modul juga dapat dengan mudah disimpan | Bali, Indonesia.\" loading=\"lazy\"><p>Modul juga dapat dengan mudah disimpan | Bali, Indonesia.</p>",
+
+    "016-opener-header-text": "<h4>Modul Milstein</h4><h5>Mudah untuk membuat furnitur dalam ruangan yang serbaguna, tahan lama dan sangat praktis yang dapat digunakan dalam berbagai cara.</h5>",
+    "023-modules-start": "Modul Milstein memerlukan jumlah ecobrick minimal untuk memulai. Yang Anda butuhkan hanyalah sekitar selusin ecobrick dengan ukuran yang sama persis untuk memulai. Anda dapat menggunakan silikon atau ban dalam tua untuk menggabungkannya. Modul adalah aplikasi sirkular jangka pendek yang dengan perawatan yang tepat akan bertahan bertahun-tahun sebelum memerlukan perbaikan.",
+    "024-gea-methods": "Metode yang disetujui GEA di sini tidak merusak ecobrick selama siklus hidupnya. Ini berarti bahwa modul dapat dengan mudah diperbaiki atau dapat dibongkar dan ecobrick digunakan dalam aplikasi baru.",
+    "025-individual-usage": "Modul Milstein dapat digunakan secara individual sebagai tempat duduk yang nyaman yang dapat digeser dan ditumpuk. Modul milstein heksagon dapat seperti sarang lebah untuk menciptakan permukaan horizontal seperti meja, tempat tidur, bangku dan lainnya. Modul segitiga dapat dikunci bersama heksagon untuk membuat tingkat kedua. Untuk pembangunan vertikal lengkap lihat modul Dieleman.",
+    "026-scalable-applications": "Aplikasi modul ecobrick dapat diperluas secara tak terbatas. Semakin banyak Anda memiliki semakin banyak yang dapat Anda bangun. Akhirnya dengan ratusan milstein dan komunitas dielman menciptakan Ruang Terbuka Ecobrick— ruang komunitas interaktif yang dapat dibangun, dibongkar, dan dibangun lagi selama konferensi dan pertemuan untuk melayani kebutuhan saat itu.",
+    "027-indoor-use": "Penting untuk menggunakan modul di dalam ruangan. Ecobrick tidak boleh terpapar sinar matahari. Sinar ultra violet (UV) akan secara bertahap memudarkan warna ecobrick dan mendegradasi PET botol plastik. Setelah hanya dua atau tiga tahun, botol yang rapuh berisiko retak dan pecah, melepaskan semua plastik padat kita. Menjaga modul sepenuhnya di dalam ruangan dan jauh dari sinar matahari adalah esensial untuk mencegah degradasi."
+
     
 
 
