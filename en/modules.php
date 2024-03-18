79ba44584d62ff1d76e5050541f4697830b32859
Russell Maier <russ@earthen.io>
2024-03-18 13:13:13 +0800

    2.6

--- en/modules.php
+++ en/modules.php
@@ -3,7 +3,7 @@
 <HEAD>
 <META charset="UTF-8">
 <?php $lang='en';?>
-<?php $version='2.5';?>
+<?php $version='2.6';?>
 <?php $page='modules';?>
 
 <?php require_once ("../includes/modules2-inc.php");?>
