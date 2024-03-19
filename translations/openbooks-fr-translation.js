2b0ce2cd5d062ab8cc0bedd8fb7eded02bd94f4f
Russell Maier <russ@earthen.io>
2024-03-19 12:35:11 +0800

    91

--- translations/openbooks-fr-translation.js
+++ translations/openbooks-fr-translation.js
@@ -15,9 +15,46 @@ const fr_Page_Translations = {
     "003-splash-image-alt": "<img src=\"../pngs/openbooks.png\" style=\"width: 80%;\" alt=\"Notre système de divulgation de la comptabilité financière Nos Livres Ouverts\">",
     "004-lead-page-paragraph": "En tant qu'entreprise pour la Terre, nous rendons compte et divulguons toutes nos finances pour répondre à nos mandats à but non lucratif et net-vert.",
     "005-first-page-paragraph": "L'<a href=\"about.php\">Alliance Globale d'Écobriques</a> est une Entreprise Terrestre. Cela signifie que nous suivons les principes de l'éthique Terrestre (<a href=\"principles.php\">Earthen ethics</a>) dans la gestion de nos finances et de nos impacts écologiques pour garantir que les deux sont bénéfiques nettement. Cela signifie également que pour la conscience de tous, notre comptabilité financière et écologique sont toutes deux divulguées publiquement.",
-    "006-second-page-paragraph": "Sur cette page, vous pouvez consulter la divulgation de nos finances. Nous appelons cela notre système de comptabilité Livres Ouverts. Pour la divulgation de notre comptabilité écologique, consultez notre <a href=\"regen-reports.php\">Rapport de Régénération</a>. Pour apprendre comment la combinaison des deux comptabilités génère le prix par Kg pour nos ventes de compensations plastiques, consultez notre <a href=\"offsets.php\">page de compensation</a>."
+    "006-second-page-paragraph": "Sur cette page, vous pouvez consulter la divulgation de nos finances. Nous appelons cela notre système de comptabilité Livres Ouverts. Pour la divulgation de notre comptabilité écologique, consultez notre <a href=\"regen-reports.php\">Rapport de Régénération</a>. Pour apprendre comment la combinaison des deux comptabilités génère le prix par Kg pour nos ventes de compensations plastiques, consultez notre <a href=\"offsets.php\">page de compensation</a>.",
   
-  
+    "007-live-aes-value": "Ceci est le prix actuel pour la <a href=\"offsets.php\" target=\"_blank\">vente des compensations plastiques AES</a>.",
+    "009-live-revenues-title": "Revenus en Temps Réel",
+    "010-live-revenues-subtitle": "Une liste complète des transactions de revenus en cours de la GEA.",
+    "010b-live-data": "<span class=\"blink\">⬤  </span> Données en direct & actuelles",
+    "011-gea-revenue-transactions-header": "Toutes les transactions de revenus de la GEA",
+    "012-id-column": "ID",
+    "013-date-column": "Date",
+    "014-sender-column": "Expéditeur",
+    "014b-reciver-column": "Destinataire",
+    "015-category-column": "Catégorie",
+    "016-tran-name-column": "Nom de la Trans",
+    "017-amount-usd-column": "Montant USD",
+    "018-final-amt-column": "Montant Final",
+    "019-type-column": "Type",
+
+    "021-live-expenses-title": "Dépenses en Temps Réel",
+    "022-live-expenses-subtitle": "Une liste complète des transactions de dépenses en cours de la GEA.",
+    "023-gea-expense-transactions-header": "Toutes les transactions de dépenses de la GEA",
+
+    "033-current-year-expenses-title": "Dépenses de l'Année en Cours",
+    "034-current-year-expenses-summary": "Un résumé des revenus de l'année en cours de la GEA.",
+    "036-current-year-revenue-title": "Revenus de l'Année en Cours",
+    "037-current-year-revenue-summary": "Un résumé des revenus de l'année en cours de la GEA.",
+    "039-year-summaries-title": "Résumés Annuels",
+    "040-year-summaries-description": "Totaux OpenBooks de 2019 à l'année en cours.",
+    "042-historical-aes-title": "Valeurs Plastiques AES Historiques",
+    "043-historical-aes-subtitle": "Totaux et valeurs de 2019 à 2024.",
+    "044-aes-data-explanation": "Nous utilisons nos données de l'année dernière pour calculer le coût par Kg de compensations plastiques pour cette année.",
+    "045-aes-example-explanation": "Par exemple, cela fonctionne ainsi : En 2021, l'Alliance Globale d'Écobriques a engagé 24 483,24 $ de dépenses pour authentifier 10 176,38 Kg de plastique écobriqué AES. Cela revient à un coût de 2,41 $ USD par Kg pour maintenir la blockchain et le système d'authentification. Cela a établi notre évaluation du prix par Kg de plastique AES pour l'année 2022.",
+    "047-more-accounting-title": "Plus de Comptabilité !",
+    "048-accounting-intro": "Qui a dit que la comptabilité est ennuyeuse ? Nous croyons que bien faite, la comptabilité est bien plus que de simples finances. Chaque année, nous comptons non seulement nos pièces, mais aussi les espèces que nous accueillons dans notre espace, ainsi que notre apport et prise de plastique et de CO2. Nous hébergeons également la <a href=\"brikcoins.php\">blockchain manuelle Brikcoin</a>, pour laquelle nous fournissons un grand livre digital complet et en direct.",
+    "050-brikcoin-transactions": "<span class=\"blink\">◉ </span>Les transactions Brikcoin sont enregistrées séparément sur l'<a href=\"brikchain.php\">Explorateur Brikchain.</a>",
+    "052-ecological-accounting-disclosure": "<span class=\"blink\">◉ </span>Notre comptabilité écologique est divulguée séparément dans nos <a href=\"regenreports\">rapports régénératifs annuels</a>.",
+    "054-earthen-ethics-principles": "Notre comptabilité est ancrée dans les principes de l'Éthique Terrestre. En savoir plus sur nos principes de <a href=\"https://earthen.io/energy/\" target=\"_blank\">gestion de l'énergie financière et écologique, comptabilité écologique et divulgation.</a>",
+    "056-our-principles-action": "🚀 Nos Principes",
+    "057-guided-by-regenerative-principles": "Notre comptabilité est guidée par nos principes régénératifs",
+    "058-eco-accounting-icon": '<br><img src="../icons/eco-accounting-icon-from-ecobricks-org.svg?v=5" width="77%" padding="15%" alt="comptabilité écologique, l\'équilibre des impacts gris et verts" loading="lazy">'
+
     
 }
         
