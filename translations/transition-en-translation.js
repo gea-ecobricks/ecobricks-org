c94fbcc77b9edd174c11bc461515ed67fd4994f6
Russell Maier <russ@earthen.io>
2024-03-20 10:00:52 +0800

    ec

--- translations/transition-en-translation.js
+++ translations/transition-en-translation.js
@@ -42,7 +42,54 @@ const en_Page_Translations = {
     "017-side-module-2-alt": '<img src="../webp/tens-thousands.webp" width="80%">',
     "018-side-2-module-title": "Regenerative",
     "019-side-2-module-subtitle": "Plastic transition is a regenerative concept. Rather than see plastic as ‘waste’ we can see it as an ayyew opportunity to move towards ecological enrichment.",
-    "020-side-module-2-button": '<a class="module-btn" href="/ayyew">About Ayyew</a>'
+    "020-side-module-2-button": '<a class="module-btn" href="/ayyew">About Ayyew</a>',
+
+    "021-quote": "“When you sit in council for the welfare of the people, you must not think of yourself or of your family, not even of your generation. Make your decisions on behalf of the seven generations coming, so that they may enjoy what you have today.",
+"022-quote-author": "— Oren Lyons (Seneca), Onondaga Nation",
+"023-the-green-impact-of-ecobricking": "The Green Impact of Ecobricking",
+"024-ecobricking-follows-earth-example": "Ecobricking follows the Earth’s example in subtracting and sequestering carbon.",
+"025-plastic-by-product-of-fossil-fuel": "Plastic is a by-product of fossil fuel production. As the global petro-capital economy steadily spins down, there is, alas, much plastic still to come. Ecobricks are a way that everyone can help secure this plastic and speed up the spin down. When we pack our plastic into ecobricks properly, we are providing the valuable ecological service of keeping plastic out of the biosphere for the long term. We call this <a href=\"sequest.php\">plastic sequestration</a>.",
+"026-plastic-sequestration-link": "plastic sequestration",
+"027-eco-bricks-to-serve-earth-image": "<img src=\"../webp/eb-sky-400px.webp\" width=\"300\" style=\"width:87%\" loading=\"lazy\" alt=\"We make eco bricks to serve the Earth\">",
+"028-but-theres-more": "But there’s more!",
+"029-eco-bricks-to-serve-earth-more-image": "<img src=\"../webp/eb-sky-400px.webp\" width=\"120\" style=\"width:120px\" loading=\"lazy\" alt=\"We make eco bricks to serve the Earth\">",
+"030-ecobricking-keeps-plastic-out-of-industry": "Ecobricking also keeps plastic out of industry. This is important too: when our used plastic is recycled, dumped, or incinerated, it must be transported and factories must spin. These processes are energy, capital, and carbon intense. By keeping plastic out of industry we prevent more fossil fuels from being extracted and burned.",
+"031-ecobricking-raises-consciousness": "Ecobricking also raises our consciousness about the dangers of plastic and the impact of the petro-capital economy. As we become more aware of these dangers and impacts, the more we move on to greener ways of living. This in turn prevents future factories from burning fuels and creating more plastics.",
+"032-ecobricks-as-building-blocks": "Finally, ecobricks are building blocks. But not for conventional building! Ecobrick applications are guided by regenerative and earthen principles. With our ecobricks, we can set about building the very green new world we are transitioning to.",
+
+"033-plastic-transition-title": "Plastic Transition",
+"034-plastic-transition-subtitle": "The process of reducing our plastic generation and increasing our green impacts is what we call ‘plastic transition’.",
+"035-living-with-plastic": "While living modern lives it is all but impossible to avoid consuming plastic and CO2 emissions. As the petro-capital economy spins down, we can do our part by ensuring our green impacts are greater than our grey.",
+"036-co2-subtraction": "While CO2 subtraction is crucial, it can also be abstract. After all, it can't be touched! Ecobricking, as a form of plastic and carbon sequestration, is clear, tangible, and immediate. You can touch it!. Because of plastic’s direct connection to the petro-capital economy, our plastic transition is an important indicator of our larger shift to regenerative living.",
+"037-ecobricking-sequestration": "When we pack our plastic into ecobricks properly, we are effectively sequestering it– providing the ecological service of keeping it out of the biosphere. By tracking our plastic generation versus our ecobricking we can track our plastic transition. This is what we call the plastic transition ratio. It is an important indicator of where we are in our regenerative journeys.",
+"038-beyond-reducing-harm": "Before we were simply focused on reducing our grey harm (i.e., “reducing footprints”, “zero waste” and “carbon neutral”) by focusing on our green impacts, we can way beyond. In the <a href=\"/ayyew\">Ayyew spirit</a>, our green contributions can go many times beyond our grey-- putting us on a journey of cultivating ever greener harmony with the cycles of life around us.",
+"039-ayyew-spirit-link": "Ayyew spirit",
+"040-plastic-transition-score": "The Global Ecobrick Alliance has developed formulas, standards, and indicators to generate a Plastic Transition Score on the GoBrik.com app. By comparing our subtraction to additive plastic impacts we measure our transition.",
+"041-determine-transition-score": "🟢 Determine your transition score: <a href=\"https://gobrik.com\" target=\"_blank\">Sign up for a GoBrik account</a>",
+
+"043-plastic-transition-score-title": "The Plastic Transition Score",
+"044-plastic-transition-score-subtitle": "By comparing our year’s green and grey impacts we get a simple score to show our plastic transition.",
+"045-gobrik-score-explanation": "The Global Ecobrick Alliance has developed a Plastic Transition Score to track plastic transition on its GoBrik.com platform. All user accounts on our GoBrik generate our user’s score based on responses to registration questions. Scores are calculated by household– families or groups of people living under the same roof.",
+"046-score-dial-image": "<img src=\"../webp/floating-dial-500px.webp\" width=\"300\" style=\"width:90%\" loading=\"lazy\" alt=\"Plastic transition score regen dial\">",
+"047-assessing-plastic-generation": "The first step is simple yet crucial: assessing how much plastic one’s household generates!",
+"048-dial-image": "<img src=\"../webp/floating-dial-500px.webp\" width=\"120\" style=\"width:120px\" loading=\"lazy\" alt=\"plastic transition score dial\">",
+"049-importance-of-knowing-plastic-usage": "Afterall, how can we tell if we’re transitioning from plastic if we don’t know how much we’re generating in the first place?",
+"050-reducing-plastic-consumption": "Only once we know how much plastic we’re consuming each month and year and we meaningful work to reduce it.",
+"051-focus-on-green-impacts": "Once we’ve assessed our grey impacts, we can focus on increasing our green. By tracking our plastic sequestration activities we can mindfully increase our subtractive plastic impact. This is the basis of the Plastic Transition Score.",
+"052-score-calculation-factors": "A household score is calculated with several factors: Your plastic monthly generation and your household ecobricking and household offsetting. These are measured month by month over the course of a year.",
+"053-gobrik-comparison-algorithm": "GoBrik uses a per capita algorithm to compare your household to your country’s per capita average. Estimate how you compare to others in your country and GoBrik does the rest using peer-reviewed scientific data.",
+"054-manual-tracking-option": "Alternatively, you can manually track and disclose your plastic generation yourself. Many ecobrickers do this with a two basket system. Over the course of the month, the plastic that they consume they clean, dry and put in a Weighing basket. At the end of the month, they weigh this plastic, and move it to their Ecobricking Basket. From this basket they grab the plastic for their ecobricks. Meanwhile, they begin the month anew filling the Weighing Basket with their consumption. As the month goes by, they also pay attention to the plastic they give away to others or use outside their home (produced plastic).",
+"055-monthly-net-plastic-consumption": "Each month, GoBrik then calculates the net amount of plastic you’ve consumed so far this year.",
+"056-gobrik-ecobrick-comparison": "GoBrik then compares this to the net weight of a user’s authenticated ecobricks made in that year as well as any offsetting that the user has done. A similar process is used for companies in the GEA Catalyst program. Your green impacts are divided by your grey impacts for the year so far.",
+"057-plastic-transition-score-percentage": "The resulting Plastic Transition Score is then calculated as a percentage of zero waste.",
+"058-score-explanation": "In other words, if you’re generating and ecobricking exactly the same amounts of plastic, then your score is 100 (i.e., 100% of zero waste). A PTR below 100 shows your household is still putting plastic into the biosphere. A score above 100 means you are removing plastic from the biosphere.",
+"059-transition-score-examples-image": "<img src=\"../webp/ptr-examples-900px.webp\" width=\"400\" style=\"width:100%\" loading=\"lazy\" alt=\"Examples of different plastic transition scores\">",
+
+
+
+
+
+
   
   
     
