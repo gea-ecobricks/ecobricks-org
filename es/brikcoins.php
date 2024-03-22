22bc280119f5f6c18f32888c4bc40c1bedd89c16
Russell Maier <russ@earthen.io>
2024-03-22 10:21:27 +0800

    build

--- es/brikcoins.php
+++ es/brikcoins.php
@@ -3,7 +3,7 @@
 <HEAD>
 <META charset="UTF-8">
 <?php $lang='es';?>
-<?php $version='1.7';?>
+<?php $version='2.0';?>
 <?php $page='brikcoins';?>
 
 <?php require_once ("../includes/brikcoins2-inc.php");?>
