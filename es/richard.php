750aa05dbeaa144fe695f80118f6c1d9d4050abd
Russell Maier <russ@earthen.io>
2024-03-22 17:08:43 +0800

    fix

--- es/richard.php
+++ es/richard.php
@@ -1,1 +1,138 @@
-nothing
+<!DOCTYPE html>
+<HTML lang="es"> 
+<HEAD>
+<META charset="UTF-8">
+<?php $lang='es';?>
+<?php $version='2.0';?>
+<?php $page='richard';?>
+
+<?php require_once ("../includes/richard-inc.php");?>
+
+
+<!--TOP PAGE BANNER-->
+							  
+<div class="splash-content-block" id="top">
+	<div class="splash-box">
+		<div class="splash-heading">Richard Goldsmith</div>
+		<div class="splash-sub">— GEA Trainer</div>
+		<div class="splash-sub"><i>in memorandum</i></div>
+	</div>
+	<div class="splash-image"><img src="../photos/richard-goldsmith.webp" style="width: 80%; width: 70%;
+border: 10px solid #666;
+border-radius: 10px;"></div>	
+</div>
+<div id="splash-bar"></div>
+
+<!-- PAGE CONTENT-->
+
+
+<div id="main-content">
+<!-- The flexible grid (content) -->
+	<div class="row">
+		<div class="main">
+		<div class="lead-page-paragraph">
+    <p data-lang-id="001-richard-passing-notice">It is with a heavy heart that we share the news of our friend and colleague Richard Goldsmith's passing. Richard, one of our UK trainers, was known to many of you through his work on the GEA's development circle where, without fail for the last three years, he had met with the members of the team.</p>
+</div>
+
+<div class="page-paragraph">
+    <p data-lang-id="002-richard-covid-battle">After battling Covid in January at the hospital (and overcoming it!), just as he was recovering, he got an infection that the doctors weren't able to treat.</p>
+
+    <p data-lang-id="003-richard-dedication">His dedication has touched all of us trainers— and the global ecobrick community at large. His contribution helping to develop GoBrik and Ecobricks.org has touched hundred of thousands of ecobrickers around the world who have been inspired by the platforms, or who put the tools to use.</p>
+
+    <p data-lang-id="004-richard-work-impact">For those ecobrickers who didn't know him personally, most likely you knew of him, or were touched by his work: tirelessly tweaking and fixing our authentication system, helping with validations and rejections— all to ensure that quality ecobricks were being made around the world, and that plastic would be kept out of the biosphere.</p>
+
+    <p data-lang-id="005-richard-legacy">In a culture where we typically honor social status and accomplishments, it is worth noting the great ecological legacy Richard has left— and his journey to get there.</p>
+
+    <p data-lang-id="006-richard-career-background">Richard spent the bulk of his career working for some of the largest international blue chip financial companies as a database designer. After retiring, whereas some would choose a quiet retirement minding their own business, Richard chose the other direction.</p>
+
+    <p data-lang-id="007-richard-green-transition">As he began to feel more and more concern about the traditional financial system that he helped build, he also began to feel more and more concern for the plastic he was consuming. Determined to do something about both, he started his green transition by buying a Tesla— of which he was immensely proud! However, he bought it not so much for the ride (which he did love!) but, as he put it "to help energize and encourage humanity's green transition".</p>
+
+    <p data-lang-id="008-richard-ecobricking-discovery">It was around then that he stumbled upon ecobricks and brikcoins.</p>
+
+    <p data-lang-id="009-richard-uk-training">Richard joined our UK training of trainer program in Newforest UK where Ani, Lucie and I had the great pleasure to meet with him numerous times in person.</p>
+
+    <p data-lang-id="010-richard-it-contributions">Not so much inclined to lead community workshops, he was nonetheless determined to help out with the community's IT needs. He put his extensive database skills to use helping us to manage the thousands of ecobrickers keen to log and record their hundreds of thousands of ecobricks. Whereas he could have used these skills to continue contracting on the side, he instead spent hundreds of hours helping us establish a nothing-short-of-miraculous online platform for the global plastic transition movement.</p>
+
+    <p data-lang-id="011-richard-development-costs">Whereas other platforms that serve ~100,000 users cost millions of dollars to develop and maintain, with Richard's help our small development team of 3-5 volunteers, managed to do it with only a few thousands dollars over the last few years— funds that came directly from the Brikchain system of plastic offsetting that he was instrumental in helping setup.</p>
+
+    <div style="width:100%">
+        <img src="../webp/richard-system.webp" style="width:100%" data-lang-id="012-richard-api-system-image">
+        <p style="font-size:0.9em; padding-left: 12px;" data-lang-id="013-richard-api-system-description">A dynamic flow chart of the API system that Richard setup that processes every ecobrick that is authenticated</p>
+    </div>
+
+    <p data-lang-id="014-richard-real-world-impact">All this of course had an immense real world impact (and continues to do so). The database and serial number system that he up set honors, energizes and encourages ecobricks to be made. Or in other words: keeping plastic out of the biosphere and out of carbon-intensive industrial facilities. Thanks to Richard's API system every single ecobrick that has been logged is recorded with its own serial number inscribed digitally and physical onto the ecobrick— now over two hundred thousand tiny mark's that Richard can take credit for— and the corresponding tons of plastic that are secured from polluting rivers, forest and fields around the world.</p>
+
+    <p data-lang-id="015-richard-passing-impact">All this is to say that Richard's passing will leave a large hole that we'll struggle to fill over the next months and years.</p>
+
+    <p data-lang-id="016-we-will-miss-richard">We will miss him dearly.</p>
+
+    <img src="../photos/richard-circle.jpg" width="100%" alt="Richard in our ToT Talking circle" data-lang-id="017-richard-circle-image">
+
+    <p data-lang-id="018-inspired-by-richard">But as we do so, we can be inspired by the great contributions, social and ecological, that are possible with quiet determination, focus and commitment.</p>
+
+    <p data-lang-id="019-richard-commemoration">To commemorate Richard's passing we've updated Ecobricks.org to feature his last work: An algorithm that selects the ten ecobricks of the past month with the highest authentication scores to make a gallery.</p>
+
+    <p data-lang-id="020-ecobricks-org-update">The gallery is now at the top of Ecobricks.org and has its own page at <a href="https://ecobricks.org/en/top-tens.php">https://ecobricks.org/en/top-tens.php</a> (now in English and Indonesian).</p>
+
+    <p data-lang-id="021-gobrik-upgrade">Meanwhile, we're also working on upgrading GoBrik to also feature the last of his work-- another algorithm that shows the latest authenticated ecobricks.</p>
+
+    <p data-lang-id="022-ecobrick-in-honor">In his honor, we'll be making an ecobrick in his honor this week. I will log mine with a prayer/vision for his passing on the system he helped develop using the plastic insertion technique he pioneered. I'll be tagging and posting this ecobrick on social media with #thanksRichard.</p>
+
+    <p data-lang-id="023-sign-off">Russell, Lucie, Ani</p>
+</div>
+
+		
+		</div>
+
+		<div class="side">
+
+		<div class="side-module-desktop-mobile">
+    <img src="../photos/richard-talking.jpg" width="80%" alt="richard talking about plastic" data-lang-id="024-richard-talking-photo">
+    <h4 data-lang-id="025-richard-goldsmith-name">Richard Goldsmith</h4>
+    <h5 data-lang-id="026-richard-practicum-description">Richard during his practicum at our 2019 Newforest UK Training of Trainers.</h5>
+</div>
+
+<div class="side-module-desktop-mobile">
+    <img src="../photos/richard-ecobrick.jpg" width="80%" alt="For-Earth Enterprise through eco bricking" data-lang-id="027-richard-ecobrick-photo">
+    <h4 data-lang-id="028-richard-ecobricks-title">Richard's Ecobricks</h4>
+    <h5 data-lang-id="029-richard-ecobricks-description">Richard's ecobricks were famous for their pioneering use of white plastic inserts with the serial number written on.  This technique ensured the serial number was protected for... pretty much ever!  Meanwhile, as part of the tech team, Richard worked hard to ensure that recorded ecobrick data would last just as long.</h5>
+    <a class="module-btn" href="https://ecobricks.org/en/details-ecobrick-page.php?serial_no=191547" data-lang-id="030-view-ecobrick">View</a>
+</div>
+
+<div class="side-module-desktop-mobile" data-lang-id="031-third-module">
+    <img src="https://ecobricks.org/briks/ecobrick-186125-file.jpeg" width="80%" alt="For-Earth Enterprise through eco bricking" data-lang-id="032-richard-top-notch-photo">
+    <h4 data-lang-id="033-always-top-notch">Always Top Notch</h4>
+    <h5 data-lang-id="034-top-notch-description">Richard's ecobricks, as well as his code, were also well known for being top notch-- every detail considered.</h5>
+    <a class="module-btn" href="https://ecobricks.org/en/details-ecobrick-page.php?serial_no=186125" data-lang-id="035-view-top-notch">View</a>
+</div>
+
+<div class="side-module-desktop-mobile">
+    <img src="https://ecobricks.org/briks/ecobrick-182514-file.jpeg" width="80%" alt="For-Earth Enterprise through eco bricking" data-lang-id="036-richard-colorful-photo">
+    <h4 data-lang-id="037-always-colorful">Always Colorful</h4>
+    <a class="module-btn" href="https://ecobricks.org/en/details-ecobrick-page.php?serial_no=182514" data-lang-id="038-view-colorful">View</a>
+</div>
+
+
+		</div>
+
+</div>
+
+</div>
+
+
+
+<!--FOOTER STARTS HERE-->
+
+<?php require_once ("../footer-2024.php");?>
+
+
+<!-- CUSTOM PAGE SCRIPTS-->
+
+
+<!-- This script is for pages that use the accordion content system-->
+
+</div>
+</body>
+</html>
+
+
