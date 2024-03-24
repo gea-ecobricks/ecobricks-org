d9bf7dcf011dd19c6accdcd87e369b9170db949c
Russell Maier <russ@earthen.io>
2024-03-24 13:34:54 +0800

    sdfsdf

--- meta/top-tens-es.php
+++ meta/top-tens-es.php
@@ -1,6 +1,6 @@
 <!-- Meta tags for page display and search engine listing-->
 
-<title>Top Ten Ecobricks of the Month</title>
+<title>Top Diez Ecoladrillos Mensual</title>
 
 <meta name="keywords" content="top ten ecobricks, monthly, featured, great ecobrick, example ecobrick, peer review, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-bricks, brik, briks, plastic, building, earth, cob, adobe, mud, building, construction, earth & ecobricks, earth and ecobrick buidling, plastic sequestration, sequestration building, sponge homes, green building, green construction, net-green building, net-zero building, techniques, regenerative, principles">
 
@@ -8,22 +8,22 @@
 	
 	<!-- Facebook Open Graph Tags for social sharing-->
 
-	<meta property="og:url"           content="https://ecobricks.org/en/top-tens.php"/>
+	<meta property="og:url"           content="https://ecobricks.org/es/top-tens.php"/>
 	<meta property="og:type"          content="website">
- 	<meta property="og:title"         content="Top Ten Ecobricks of the Month">
+ 	<meta property="og:title"         content="Top Diez Ecoladrillos Mensual">
   	<meta property="og:description"   content="Every month we feature the ecobricks that received the highest peer review authentication scores from the global ecobrick community."/>
   	<meta property="og:image"         content="https://ecobricks.org/pngs/gobriktrophy.png"/>
   	<meta property="og:image:width" content="600" />
 	<meta property="og:image:height" content="600" />
   	<meta property="og:image:alt"     content="Ecobrick of the month trophy"/>
-	  <meta property="og:locale" content="en_GB" />
+	  <meta property="og:locale" content="es_ES" />
 
       <meta name="author" content="Ecobricks.org">
   
   <meta property="og:type" content="article" />
  <meta property="og:site_name" content="Ecobricks.org" />
  <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" />
- <meta property="article:modified_time" content="2022-09-13T09:14:13+00:00" />
+ <meta property="article:modified_time" content="2024-03-21T09:14:13+00:00" />
  <meta property="og:image:type" content="image/png" />
  <meta name="author" content="Global Ecobrick Alliance" />
  <meta name="twitter:card" content="summary" />
