f5a721719521c112ae49fd673e3c0c4366d6a97c
Russell Maier <russ@earthen.io>
2024-03-24 13:54:19 +0800

    ypo

---
+++ en/gobrik-store.php
@@ -0,0 +1,6 @@
+<div class="side-module-desktop-mobile">
+<img src="../webp/gobrik-store-get.webp" width="80%" alt="Dapatkan di Toko GoBrik">
+<h4>The GoBrik Store</h4>
+<h5>Get green regenerative products at the GoBrik store.</h5><br>
+<a class="module-btn" href="https://gobrik.com/en/go.php#shop" target="_blank">Shop</a>
+</div>
---
+++ es/side-modules/gobrik-store.php
@@ -0,0 +1,6 @@
+<div class="side-module-desktop-mobile">
+<img src="../webp/gobrik-store-get.webp" width="80%" alt="Dapatkan di Toko GoBrik">
+<h4>GoBrik Store</h4>
+<h5>Consigue este y otros estupendos productos regenerativos ecológicos en la tienda GoBrik</h5><br>
+<a class="module-btn" href="https://gobrik.com/en/go.php#shop" target="_blank">Shop</a>
+</div>
