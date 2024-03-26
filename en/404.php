16431e9dd7c72e1cc29cd75c5fe0cc3a1d7331a8
Russell Maier <russ@earthen.io>
2024-03-26 21:23:24 +0800

    404b

--- en/404.php
+++ en/404.php
@@ -30,41 +30,35 @@
                 <div class="main">
 
                     <div class="lead-page-paragraph">
-                         <p data-lang-id="005-first-page-paragraph">Your seeing this page because the url you entered doesn't correspond to a page on our server.</p>
+                         <p data-lang-id="004-first-page-paragraph">You're seeing this page because the url you entered doesn't correspond to a page on our server.</p>
                     </div>
                 
                     <div class="page-paragraph">
-                         <p data-lang-id="006-second-page-paragraph">Look carefully at the URL to see if there is a mispelling.  We've just launched a new version of our site, and alas, there could be some bugs!</p>
-
-                         
+                         <p data-lang-id="006-second-page-paragraph">Look carefully at the URL to see if there is a mispelling.  We've just launched a new version of our site, and alas, there could be some bugs!</p> 
                     </div>
 
 
                     <div class="page-paragraph-reg">
 
-                         <p data-lang-id="006-second-page-paragraph">If you think there's an error on our site, please let us know at support@ecobricks.org.</p>
+                         <p data-lang-id="007-page-paragraph">If you think there's an error on our site, please let us know at support@ecobricks.org.</p>
 
 
                         <p data-lang-id="008-page-paragraph">Meanwhile, use the new search feature on our site (top right menu bar) to look for the content you're trying to find.</p>
-
-                        
                     
-                        <button type="button" class="module-btn" style="margin-top:20px;" onclick="openSearch()">🔎 Search Site</button>
+                        <button type="button" class="module-btn" style="margin-top:20px;" onclick="openSearch()" data-lang-id="008-search-button">🔎 Search Site</button>
                         </div>
+
+                        <button type="button" class="module-btn" style="margin-top:20px;" onclick="faqs.php" data-lang-id="009-faqs-button">ℹ️ Questions & Answers</button>
+                    </div>
                 </div>
 
                 <div class="side">
             
                 
-                <?php require_once ("side-modules/about-gea.php");?>
-
+hello
                 </div>
             </div>
        
-
-            
-            </div>  <!--closes main-->
-                        </div>
 
 
 	<!--FOOTER STARTS HERE-->
