0d71f6090a908deb2d940deb94b3684dace7ed2e
Russell Maier <russ@earthen.io>
2024-03-27 10:27:53 +0800

    retry

---
+++ 404-handler.php
@@ -0,0 +1,12 @@
+<?php
+// Get the original request URI
+$request_uri = $_SERVER['REQUEST_URI'];
+
+// Extract the language directory from the request URI
+preg_match('/^\/(en|fr|id|es)\//', $request_uri, $matches);
+$language_dir = isset($matches[1]) ? $matches[1] : 'en'; // Default to English if no language directory found
+
+// Redirect the user to the appropriate language directory's 404 page
+header("Location: /$language_dir/404.php");
+exit;
+?>
