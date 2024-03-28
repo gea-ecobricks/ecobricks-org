1e5b22971e90d707b709c690418558290b92d6ee
Russell Maier <russ@earthen.io>
2024-03-28 20:20:34 +0800

    sdf

--- en/upload-images.php
+++ en/upload-images.php
@@ -39,26 +39,31 @@
 
 
 <script>
-// Function to show the modal
-function showFormModal(message) {
-    var modal = document.getElementById('form-modal-message');
-    var modalMessage = modal.querySelector('.modal-message');
-    modalMessage.innerHTML = message;
-    modal.style.display = 'flex';
-
-    // Add blur effect and hide overflow on page-content and footer-full
-    document.getElementById('page-content').classList.add('blurred');
-    document.getElementById('footer-full').classList.add('blurred');
-    document.body.classList.add('modal-open');
-
-    // Close modal when clicking outside
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            closeInfoModal();
-        }
-    }
+    
+    
+// Function to handle form submission response
+function handleFormResponse(response) {
+    showFormModal(response);
 }
 
+function showFormModal(message) {
+var modal = document.getElementById('form-modal-message');
+var modalMessage = modal.querySelector('.modal-message');
+modalMessage.innerHTML = message;
+modal.style.display = 'flex';
+
+// Add blur effect and hide overflow on page-content and footer-full
+document.getElementById('page-content').classList.add('blurred');
+document.getElementById('footer-full').classList.add('blurred');
+document.body.classList.add('modal-open');
+
+// Close modal when clicking outside
+window.onclick = function(event) {
+if (event.target == modal) {
+    closeInfoModal();
+}
+}
+}
 
 // Add event listener to form submission
 document.getElementById('submit-form').addEventListener('submit', function(event) {
@@ -81,16 +86,7 @@ xhr.open(form.method, form.action, true);
 xhr.send(formData);
 });
 
-// Function to close the modal
-function closeInfoModal() {
-    var modal = document.getElementById('form-modal-message');
-    modal.style.display = 'none';
 
-    // Remove blur effect and show overflow on page-content and footer-full
-    document.getElementById('page-content').classList.remove('blurred');
-    document.getElementById('footer-full').classList.remove('blurred');
-    document.body.classList.remove('modal-open');
-}
 </script>
                     <!-- <div class="form-item">
                 <label for="featured_img" data-lang-id="011-featured-image">Featured Image:</label><br>
