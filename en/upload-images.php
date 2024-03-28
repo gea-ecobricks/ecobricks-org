856557581e96342c436c05c1d556bbabf3756b43
Russell Maier <russ@earthen.io>
2024-03-28 22:47:18 +0800

    q

--- en/upload-images.php
+++ en/upload-images.php
@@ -37,41 +37,48 @@
 
 
 
-
-<script>
-    
-    
+    <script>
 // Function to handle form submission response
 function handleFormResponse(response) {
     showFormModal(response);
 }
 
 function showFormModal(message) {
-var modal = document.getElementById('form-modal-message');
-var modalMessage = modal.querySelector('.modal-message');
-modalMessage.innerHTML = message;
-modal.style.display = 'flex';
+    var modal = document.getElementById('form-modal-message');
+    var modalMessage = modal.querySelector('.modal-message');
+    modalMessage.innerHTML = message;
+    modal.style.display = 'flex';
 
-// Add blur effect and hide overflow on page-content and footer-full
-document.getElementById('page-content').classList.add('blurred');
-document.getElementById('footer-full').classList.add('blurred');
-document.body.classList.add('modal-open');
+    // Add blur effect and hide overflow on page-content and footer-full
+    document.getElementById('page-content').classList.add('blurred');
+    document.getElementById('footer-full').classList.add('blurred');
+    document.body.classList.add('modal-open');
 
-// Close modal when clicking outside
-window.onclick = function(event) {
-if (event.target == modal) {
-    closeInfoModal();
-}
-}
+    // Close modal when clicking outside
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            closeInfoModal();
+        }
+    }
 }
 
+// Function to close the modal
+function closeInfoModal() {
+    var modal = document.getElementById('form-modal-message');
+    modal.style.display = 'none';
+
+    // Remove blur effect and show overflow on page-content and footer-full
+    document.getElementById('page-content').classList.remove('blurred');
+    document.getElementById('footer-full').classList.remove('blurred');
+    document.body.classList.remove('modal-open');
+}
 
 // Add event listener to form submission
-document.querySelector('photoform').addEventListener('submit', function(event) {
+document.querySelector('#photoform').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent default form submission
     var form = event.target;
     var formData = new FormData(form);
-    
+
     // Send form data using AJAX
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
@@ -86,9 +93,8 @@ document.querySelector('photoform').addEventListener('submit', function(event) {
     xhr.open(form.method, form.action, true);
     xhr.send(formData);
 });
-
-
 </script>
+
                   
         </div>
     </div>
