bee4996bbdcd45c5d64f055cd373c1f3d1473531
Russell Maier <russ@earthen.io>
2024-03-28 19:10:02 +0800

    test

--- en/submit_project.php
+++ en/submit_project.php
@@ -20,37 +20,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $description = $_POST['description'];
     $start = $_POST['start'];
     $briks_used = $_POST['briks_used'];
-    $featured_img = $_POST['featured_img'];
     $location_full = $_POST['location_full'];
+
+    // Check if 'featured_img' file has been uploaded
+    if (isset($_FILES['featured_img']) && $_FILES['featured_img']['error'] === UPLOAD_ERR_OK) {
+        $file_name = $_FILES['featured_img']['name'];
+        $file_tmp = $_FILES['featured_img']['tmp_name'];
+        
+        // Read the file contents
+        $file_content = file_get_contents($file_tmp);
+        
+        // Set the featured_img parameter
+        $featured_img = $file_content;
+    } else {
+        // No file uploaded or error occurred
+        $featured_img = null; // Or handle the error accordingly
+    }
 
     // Execute the SQL statement
     if ($stmt->execute()) {
         // Get the last inserted project_id
         $project_id = $conn->insert_id;
+        $response_message = "The photos for Project $name are now being uploaded. This is project number $project_id";
         
-        // File upload directory
-        $upload_dir = '../projects/featured/';
-
-        // Check if file is uploaded successfully
-        if (isset($_FILES['featured_img']) && $_FILES['featured_img']['error'] === UPLOAD_ERR_OK) {
-            $file_name = $_FILES['featured_img']['name'];
-            $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
-            
-            // Construct new file name
-            $new_file_name = 'featured-img-project-' . $project_id . '.' . $file_ext;
-            
-            // Rename the uploaded file
-            if (rename($_FILES['featured_img']['tmp_name'], $upload_dir . $new_file_name)) {
-                // File renamed successfully
-                $response_message = "The photos for Project $name are now being uploaded. This is project number $project_id";
-            } else {
-                // Error occurred while renaming the file
-                $response_message = "Error renaming the file.";
-            }
-        } else {
-            // No file uploaded or error occurred
-            $response_message = "Error uploading file: " . $_FILES['featured_img']['error'];
-        }
+        // Redirect to upload_images.php with project_id as a parameter
+        header("Location: upload_images.php?project_id=$project_id");
+        exit();
     } else {
         $response_message = "Error: " . $sql . "<br>" . $conn->error;
     }
