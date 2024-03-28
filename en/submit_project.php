05ebea4c1ac4c4f429de386ad69e2d20cc59aee8
Russell Maier <russ@earthen.io>
2024-03-28 19:01:46 +0800

    now

--- en/submit_project.php
+++ en/submit_project.php
@@ -8,7 +8,14 @@ include '../ecobricks_env.php';
 // Check if the form has been submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Set parameters from the form data
+    // Prepare SQL statement
+    $sql = "INSERT INTO tb_projects (name, description, start, briks_used, tmb_featured_img, location_full) VALUES (?, ?, ?, ?, ?, ?)";
+
+    // Bind parameters
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssssss", $name, $description, $start, $briks_used, $tmb_featured_img, $location_full);
+
+    // Set parameters
     $name = $_POST['name'];
     $description = $_POST['description'];
     $start = $_POST['start'];
@@ -16,42 +23,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $tmb_featured_img = $_POST['tmb_featured_img'];
     $location_full = $_POST['location_full'];
 
-    // Check if file is uploaded successfully
-    if (isset($_FILES['featured_img']) && $_FILES['featured_img']['error'] === UPLOAD_ERR_OK) {
-        // Move uploaded file to the specified directory
-        $upload_dir = '../projects/featured/';
-        $file_tmp = $_FILES['featured_img']['tmp_name'];
-        $file_ext = strtolower(pathinfo($_FILES['featured_img']['name'], PATHINFO_EXTENSION));
-        $new_file_name = 'featured-img-project-' . uniqid() . '.' . $file_ext; // Generate unique file name
-        if (move_uploaded_file($file_tmp, $upload_dir . $new_file_name)) {
-            // File uploaded successfully
-            $featured_img = $new_file_name;
-        } else {
-            // Error occurred while moving the file
-            $response_message = "Error moving the file.";
-            echo $response_message;
-            exit();
-        }
-    } else {
-        // No file uploaded or error occurred
-        $response_message = "Error uploading file: " . $_FILES['featured_img']['error'];
-        echo $response_message;
-        exit();
-    }
-
-    // Prepare SQL statement
-    $sql = "INSERT INTO tb_projects (name, description, start, briks_used, featured_img, tmb_featured_img, location_full) VALUES (?, ?, ?, ?, ?, ?, ?)";
-
-    // Bind parameters
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sssiiss", $name, $description, $start, $briks_used, $featured_img, $tmb_featured_img, $location_full);
-
     // Execute the SQL statement
     if ($stmt->execute()) {
-        // Project submitted successfully
-        $response_message = "The project was submitted successfully.";
+        // Get the last inserted project_id
+        $project_id = $conn->insert_id;
+        
+        // File upload directory
+        $upload_dir = '../projects/featured/';
+
+        // Check if file is uploaded successfully
+        if (isset($_FILES['featured_img']) && $_FILES['featured_img']['error'] === UPLOAD_ERR_OK) {
+            $file_name = $_FILES['featured_img']['name'];
+            $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+            
+            // Construct new file name
+            $new_file_name = 'featured-img-project-' . $project_id . '.' . $file_ext;
+            
+            // Rename the uploaded file
+            if (rename($_FILES['featured_img']['tmp_name'], $upload_dir . $new_file_name)) {
+                // File renamed successfully
+                $response_message = "The photos for Project $name are now being uploaded. This is project number $project_id";
+            } else {
+                // Error occurred while renaming the file
+                $response_message = "Error renaming the file.";
+            }
+        } else {
+            // No file uploaded or error occurred
+            $response_message = "Error uploading file: " . $_FILES['featured_img']['error'];
+        }
     } else {
-        // Error occurred while executing SQL statement
         $response_message = "Error: " . $sql . "<br>" . $conn->error;
     }
 
