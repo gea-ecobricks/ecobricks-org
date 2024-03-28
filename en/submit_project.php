868e5dc02696eefb4ee2f1dd8d03e16da4eba1b7
Russell Maier <russ@earthen.io>
2024-03-28 20:03:18 +0800

    simple

--- en/submit_project.php
+++ en/submit_project.php
@@ -24,27 +24,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($stmt->execute()) {
         // Get the last inserted project_id
         $project_id = $conn->insert_id;
-        $response_message = "Project $name has been submitted successfully. This is project number $project_id";
-        
+
         // Close statement
         $stmt->close();
-        
+
         // Close connection
         $conn->close();
-        
-        // Return response message to be displayed in modal
-        echo "<script>
-                setTimeout(function() {
-                    document.getElementById('form-modal-message').style.display = 'block';
-                    document.querySelector('.modal-message').innerHTML = 'Your project record is now setup. Now let\'s upload some images...';
-                    setTimeout(function() {
-                        window.location.href = 'upload-images.php?project_id=$project_id';
-                    }, 2000); // 2 seconds delay
-                }, 0);
-              </script>";
+
+        // Redirect to the next page
+        echo "<script>window.location.href = 'upload-images.php?project_id=$project_id';</script>";
+        exit();
     } else {
         $response_message = "Error: " . $sql . "<br>" . $conn->error;
     }
 }
-
 ?>
