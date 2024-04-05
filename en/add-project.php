0c7245926bd6c66d0389708ae71cbe5ac994c140
Russell Maier <russ@earthen.io>
2024-04-05 15:57:08 +0800

    ddd

--- en/add-project.php
+++ en/add-project.php
@@ -13,12 +13,35 @@
  <!-- PAGE CONTENT-->
 
  <div id="form-submission-box" style="margin-top: 100px">
-    <div class="form-container">
-        <h1 data-lang-id="001-form-title">Post & Share Your project</h1>
-        <p data-lang-id="002-form-description">Share your ecobrick project with the world. Use this form to post your completed ecobricks project onto ecobricks.org. Projects will be featured on our main page and archived in our database."</p>
-        <form id="submit-form" method="post" action="submit_project.php" enctype="multipart/form-data">
+        <div class="form-container">
+            <h1 data-lang-id="001-form-title">Post & Share Your project</h1>
+            <p data-lang-id="002-form-description">Share your ecobrick project with the world. Use this form to post your completed ecobricks project onto ecobricks.org. Projects will be featured on our main page and archived in our database."</p>
+            <form id="submit-form" method="post" action="submit_project.php" enctype="multipart/form-data">
 
-            <!-- Existing form fields here -->
+                <div class="form-item" style="margin-top: 25px;">
+                    <label for="name" data-lang-id="003-project-name">Project Name:</label><br>
+                    <input type="text" id="name" name="name" title="Max 255 characters">
+                    <p class="form-caption" data-lang-id="005-project-name-caption">Give a name or title to your project post.</p>
+                </div>
+                
+                <div class="form-item">
+                    
+                    <label for="description" data-lang-id="004-project-desc">Project Description:</label><br>
+                    <textarea id="description" name="description" title="Max 150 words"></textarea>
+                    <p class="form-caption" data-lang-id="006-project-desc-caption">Provide a short description of this project. Max 150 words.</p>
+                </div>
+                
+                <div class="form-item">
+                    <label for="start" data-lang-id="007-start-date">Start Date:</label><br>
+                    <input type="date" id="start" name="start">
+                    <p class="form-caption" data-lang-id="008-start-date-caption">When did this project begin?</p>
+                </div>
+                
+                <div class="form-item">
+                    <label for="briks_used" data-lang-id="009-bricks-used">Bricks Used:</label><br>
+                    <input type="number" id="briks_used" name="briks_used" title="Maximum value allowed">
+                    <p class="form-caption" data-lang-id="010-bricks-used-caption">How many briks did the project use?</p>
+                </div>
 
             <div class="form-item">
                 <label for="location_full" data-lang-id="015-location">Project Location:</label><br>
