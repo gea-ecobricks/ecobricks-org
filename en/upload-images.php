dce4a09f157f0851bf2f7928d3e904e037df0e3c
Russell Maier <russ@earthen.io>
2024-04-07 17:56:29 +0800

    tweaked

--- en/upload-images.php
+++ en/upload-images.php
@@ -32,10 +32,10 @@
         <form id="photoform" action="upload_images.php" method="post" enctype="multipart/form-data">
             <input type="hidden" name="project_id" value="<?php echo $_GET['project_id']; ?>">
             
-            <div class="form-item" style="background-color:#0003;border-radius:5px;padding:10px;">
+            <div class="form-item" style="border-radius:5px;padding-left:10px;padding-right:10px;background-color:var(--main-background);">
                 <div>
-                    <label for="photo1_main" data-lang-id="003-feature-photo" style="padding:10px">Feature image:</label><br>
-                    <input type="file" id="photo1_main" name="photo1_main" style="background-color:var(--main-background); padding:10px;font-size:1.2em;color:var(--text-color);border-radius:5px;" required>
+                    <label for="photo1_main" data-lang-id="003-feature-photo" style="padding:10px;margin-bottom:10px;">Feature image:</label><br>
+                    <input type="file" id="photo1_main" name="photo1_main" style="background-color:#0003; padding:10px;font-size:1.2em;color:var(--text-color);border-radius:5px;" required>
                     <p class="form-caption" data-lang-id="004-feature-desc" style="padding:10px">Please choose a featured photo for this project.  Required.</p>
                 </div>
 
