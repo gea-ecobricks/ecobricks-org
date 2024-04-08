d73ffd979f2301ec01aa575eb44c62676b348bad
Russell Maier <russ@earthen.io>
2024-04-08 08:54:24 +0800

    check

--- en/details-ecobrick-page.php
+++ en/details-ecobrick-page.php
@@ -21,23 +21,24 @@
 	
     while($array = $result->fetch_assoc()) {
 
-	echo 
-	'<div class="splash-content-block">
-		<div class="splash-box">
-
-			<div class="splash-heading"><span data-lang-id="001-splash-title">Ecobrick</span> ' . $array["serial_no"] .'</div>
+		echo 
+		'<div class="splash-content-block">
+			<div class="splash-box">
+	
+				<div class="splash-heading"><span data-lang-id="001-splash-title">Ecobrick</span> ' . htmlspecialchars($array["serial_no"], ENT_QUOTES, 'UTF-8') . '</div>
+				
+				<div class="splash-sub">' . htmlspecialchars($array["weight_authenticated_kg"], ENT_QUOTES, 'UTF-8') . '&#8202;kg <span data-lang-id="002-splash-subtitle">of plastic has been secured out of the biosphere in</span> ' . htmlspecialchars($array["location_full"], ENT_QUOTES, 'UTF-8') . '</div>
+			</div>
 			
-			<div class="splash-sub">'. $array["weight_authenticated_kg"] .'&#8202;kg <span data-lang-id="002-splash-subtitle">of plastic has been secured out of the biosphere in</span> '. $array["location_full"] .'</div>
+			<div class="splash-image">
+				<a href="javascript:void(0);" onclick="viewGalleryImage(\'' . htmlspecialchars($array["ecobrick_full_photo_url"], ENT_QUOTES, 'UTF-8') . '\', \'Test\')"><img src="' . htmlspecialchars($array["ecobrick_full_photo_url"], ENT_QUOTES, 'UTF-8') . '" alt="Ecobrick ' . htmlspecialchars($array["serial_no"], ENT_QUOTES, 'UTF-8') . ' was made in ' . htmlspecialchars($array["location_full"], ENT_QUOTES, 'UTF-8') . ' and authenticated on ' . htmlspecialchars($array["last_validation_ts"], ENT_QUOTES, 'UTF-8') . '"
+			title="Ecobrick Serial ' . htmlspecialchars($array["serial_no"], ENT_QUOTES, 'UTF-8') . ' was made in ' . htmlspecialchars($array["location_full"], ENT_QUOTES, 'UTF-8') . ' and authenticated on ' . htmlspecialchars($array["last_validation_ts"], ENT_QUOTES, 'UTF-8') . '"></a>
+			</div>    
 		</div>
-		
-		<div class="splash-image">
-			<a href="ecobrick_full_photo_url" onclick="viewGalleryImage(\''. $array["ecobrick_full_photo_url"] .\',\'Test\')"><img src="'. $array["ecobrick_full_photo_url"] .'"  alt="Ecobrick '. $array["serial_no"] .' was made in '. $array["location_full"] .' and authenticated on ' . $array["last_validation_ts"] .'"
-		title="Ecobrick Serial '. $array["serial_no"] .' was made in '. $array["location_full"] .' and authenticated on ' . $array["last_validation_ts"] .'"></a>
-		</div>	
-	</div>
-
-	<div id="splash-bar"></div>
-	';
+	
+		<div id="splash-bar"></div>
+		';
+	
 
 echo '
 <div id="main-content">
