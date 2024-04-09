67ec1ddff9505320c40772437ca94508762279f2
Russell Maier <russ@earthen.io>
2024-04-09 17:48:08 +0800

    env

--- add_project_images.php
+++ add_project_images.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-include '../ecobricks_env.php';
+include 'ecobricks_env.php';
 
 $error_message = '';
 $full_urls = []; // Initialize array to store main image URLs
