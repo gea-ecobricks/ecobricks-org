8b89c48c11c417083a9eae0681d28c8f7e2c04b1
JosephOFlanagan <joseph.oflanagan@yahoo.co.uk>
2024-04-11 12:30:20 +0100

    Update plastic.php
    Renamed paragraph IDs so that they are all unique.

--- testing/plastic.php
+++ testing/plastic.php
@@ -49,71 +49,71 @@
                 
                 <p data-lang-id="007-page-paragraph">However, its just as important to understand that the story of plastic doesn’t begin in a factory.  Instead, plastic finds its origins millions of years ago on an ancient, barren Earth we would barely recognize.  In fact, the carbon molecules of our modern plastic played a key role in making our common home the thriving and diverse biosphere that we know today.</p>
 
-                <p data-lang-id="007-page-paragraph">As you will see, we have much to learn from the way that our once barren planet has slow and steady compacted carbon, stored it, and cultivated ever-richer ecological cycles.  From this long-view vantage point, we will see that plastic isn’t the problem.  Rather, it’s our starting point to follow the Earth’s ancient example in our modern lives…</p>
+                <p data-lang-id="008-page-paragraph">As you will see, we have much to learn from the way that our once barren planet has slow and steady compacted carbon, stored it, and cultivated ever-richer ecological cycles.  From this long-view vantage point, we will see that plastic isn’t the problem.  Rather, it’s our starting point to follow the Earth’s ancient example in our modern lives…</p>
 
                 <img src="../pngs/plastic-source-1000px.png" width="400px">
                 
 
-                <p data-lang-id="007-page-paragraph"><h4>Once upon a time…</h4></p>
+                <p data-lang-id="009-page-paragraph"><h4>Once upon a time…</h4></p>
 
-                <p data-lang-id="007-page-paragraph">About 3.2 billion years ago, multicellular organisms figured out how to turn the light of the sun into life-force energy.  Using photosynthesis organisms transformed sunlight by sucking in carbon dioxide and making sugars from the carbon to store energy.  It was a big breakthrough.  Plants grew bigger and spread across the planet.  Slow and steady they began to remove the carbon in the early Earth air.</p>
+                <p data-lang-id="010-page-paragraph">About 3.2 billion years ago, multicellular organisms figured out how to turn the light of the sun into life-force energy.  Using photosynthesis organisms transformed sunlight by sucking in carbon dioxide and making sugars from the carbon to store energy.  It was a big breakthrough.  Plants grew bigger and spread across the planet.  Slow and steady they began to remove the carbon in the early Earth air.</p>
 
-                <p data-lang-id="007-page-paragraph">Two hundred million years ago, when the dinosaurs roamed the Earth, plants had gotten really good at transforming carbon.  Immense forests covered the continents, and massive blooms of algae filled low lying seas.  As plants and creatures died they would fall to the ground or to the bottom of the sea.  As time passed, all this carbon-rich organic matter was covered with sediment.</p>
+                <p data-lang-id="011-page-paragraph">Two hundred million years ago, when the dinosaurs roamed the Earth, plants had gotten really good at transforming carbon.  Immense forests covered the continents, and massive blooms of algae filled low lying seas.  As plants and creatures died they would fall to the ground or to the bottom of the sea.  As time passed, all this carbon-rich organic matter was covered with sediment.</p>
 
-                <p data-lang-id="007-page-paragraph">With all this carbon out of the air, the Earth’s atmosphere calmed and cooled down.  A combination of pressure and time transformed the buried biomass into densely packed carbon storage deposits.</p>
+                <p data-lang-id="012-page-paragraph">With all this carbon out of the air, the Earth’s atmosphere calmed and cooled down.  A combination of pressure and time transformed the buried biomass into densely packed carbon storage deposits.</p>
 
                 <img src="../pngs/plastic-source-1000px.png" width="400px">
                 
 
-                <p data-lang-id="007-page-paragraph">Life-cycles on the planet continued to become richer and more diverse.  Thanks to a combination of plants, climate and geography, one particular primate emerged two million years ago.  It manged to do pretty well for itself!</p>
+                <p data-lang-id="013-page-paragraph">Life-cycles on the planet continued to become richer and more diverse.  Thanks to a combination of plants, climate and geography, one particular primate emerged two million years ago.  It manged to do pretty well for itself!</p>
 
-                <p data-lang-id="007-page-paragraph">Then, on a cold, dark Ice Age night, these early humans figured out fire.  By lighting some sticks they had ignited the process of recombining wood’s carbon molecules with the air’s oxygen in a reaction that warmed and lit their cave.</p>
+                <p data-lang-id="014-page-paragraph">Then, on a cold, dark Ice Age night, these early humans figured out fire.  By lighting some sticks they had ignited the process of recombining wood’s carbon molecules with the air’s oxygen in a reaction that warmed and lit their cave.</p>
 
-                <p data-lang-id="007-page-paragraph">Despite the pesky smoke, humans got better and better at burning organic stuff.  A few thousand years later, humans discovered the earth’s ancient carbon storage deposits.  By unearthing and burning black rocks (i.e. coal), they could get even more heat and light than by burning wood (but lots more pesky smoke).</p>
+                <p data-lang-id="015-page-paragraph">Despite the pesky smoke, humans got better and better at burning organic stuff.  A few thousand years later, humans discovered the earth’s ancient carbon storage deposits.  By unearthing and burning black rocks (i.e. coal), they could get even more heat and light than by burning wood (but lots more pesky smoke).</p>
 
-                <p data-lang-id="007-page-paragraph">Humans became better and better at extracting compacted carbon and turning it into energy.  They discovered other types too– like petroleum and natural gas. Humans gave the stuff a name: “fossil fuels”.</p>
+                <p data-lang-id="016-page-paragraph">Humans became better and better at extracting compacted carbon and turning it into energy.  They discovered other types too– like petroleum and natural gas. Humans gave the stuff a name: “fossil fuels”.</p>
 
-                <p data-lang-id="007-page-paragraph">As modern countries flourished their economies began to rely more on more on the easy energy gained from burning fossil fuels.  Factories could be built and powered and all sorts of new products made.  As more products were consumed, more factories needed to be made.  As more factories needed to be powered, more and more fossil fuels were extracted and refined. Petroleum and capital flowed while economies grew.</p>
+                <p data-lang-id="017-page-paragraph">As modern countries flourished their economies began to rely more on more on the easy energy gained from burning fossil fuels.  Factories could be built and powered and all sorts of new products made.  As more products were consumed, more factories needed to be made.  As more factories needed to be powered, more and more fossil fuels were extracted and refined. Petroleum and capital flowed while economies grew.</p>
 
                 <img src="../pngs/plastic-source3-1000px.png" width="400px">
 
 
-                <p data-lang-id="007-page-paragraph">However, organic compacted carbon deposits were never ideal for being ‘fossil fuels’.  In the process of refining petroleum, there was a left over residue (5-15% depending on the crude being used) that just couldn’t be turned into fuels.  This left over ‘naphtha’ began to accumulate as it had no where to go.</p>
+                <p data-lang-id="018-page-paragraph">However, organic compacted carbon deposits were never ideal for being ‘fossil fuels’.  In the process of refining petroleum, there was a left over residue (5-15% depending on the crude being used) that just couldn’t be turned into fuels.  This left over ‘naphtha’ began to accumulate as it had no where to go.</p>
 
-                <p data-lang-id="007-page-paragraph">It was soon realized, that naphtha could be used for making stuff.  With a little chemistry it could be turn into polymers, and with a little more chemistry, polymers could be turned into all sorts of new materials with an endless variety of properties.  Plastics were born.</p>
+                <p data-lang-id="019-page-paragraph">It was soon realized, that naphtha could be used for making stuff.  With a little chemistry it could be turn into polymers, and with a little more chemistry, polymers could be turned into all sorts of new materials with an endless variety of properties.  Plastics were born.</p>
 
-                <p data-lang-id="007-page-paragraph">Soon humans were solving all sorts of their problems by making all sorts of amazing things from plastic.  No longer did elephants need to be killed for their ivory to make billiard balls.  No longer did you need expensive silver plates to take a photograph.  No longer did food need to go bad in a few days.  Plastics had arrived.</p>
+                <p data-lang-id="020-page-paragraph">Soon humans were solving all sorts of their problems by making all sorts of amazing things from plastic.  No longer did elephants need to be killed for their ivory to make billiard balls.  No longer did you need expensive silver plates to take a photograph.  No longer did food need to go bad in a few days.  Plastics had arrived.</p>
 
-                <p data-lang-id="007-page-paragraph">
+                <p data-lang-id="021-page-paragraph">
                 <h4>But what About the Smoke?</h4>
                 </p>
 
-                <p data-lang-id="007-page-paragraph">Ever since the first human bond fire, the burning of compacted carbon had been releasing that pesky smoke–  mainly CO2.   As the easy energy of fossil fuels enabled more and more economy, more and more capital encouraged more and more extraction and industry and carbon decompaction– the reduction in carbon density of the original deposits.</p>
+                <p data-lang-id="022-page-paragraph">Ever since the first human bond fire, the burning of compacted carbon had been releasing that pesky smoke–  mainly CO2.   As the easy energy of fossil fuels enabled more and more economy, more and more capital encouraged more and more extraction and industry and carbon decompaction– the reduction in carbon density of the original deposits.</p>
 
                 <img src="../pngs/plastic-source4-chart.png" width="400px">
 
                     
-                <p data-lang-id="007-page-paragraph">All the hard work that the earth had been doing for the last few million years, was being undone faster and faster by the humans.  While burning some logs created a little smoke and some CO2, the Earth’s plants and oceans could manage to suck it back down.  However, as the humans began powering their economies with fossil fuels alot more CO2 was being released– more than the earth could manage.</p>
+                <p data-lang-id="023-page-paragraph">All the hard work that the earth had been doing for the last few million years, was being undone faster and faster by the humans.  While burning some logs created a little smoke and some CO2, the Earth’s plants and oceans could manage to suck it back down.  However, as the humans began powering their economies with fossil fuels alot more CO2 was being released– more than the earth could manage.</p>
                     
-                <p data-lang-id="007-page-paragraph">And since the capital in the economies was based on this burning, it created a self-perpetuating loop.  The more fuels were burned, the more were extracted, and the more were burned.  With so much carbon out of storage and in the air, it began to disrupt the harmony of the earth’s ecological cycles.</p>
+                <p data-lang-id="024-page-paragraph">And since the capital in the economies was based on this burning, it created a self-perpetuating loop.  The more fuels were burned, the more were extracted, and the more were burned.  With so much carbon out of storage and in the air, it began to disrupt the harmony of the earth’s ecological cycles.</p>
 
                 
                     
                 <img src="../pngs/plastic-source5-1000px.png" width="400px">
 
-                <p data-lang-id="007-page-paragraph">As humans extracted and refined more and more raw fossil fuels, there was always that little bit that couldn’t be processed.  This lead to  more and more cheap and abundant plastic.  And as petro-capital economies grew, so too did the use and application of plastic to cover and package products.</p>
+                <p data-lang-id="025-page-paragraph">As humans extracted and refined more and more raw fossil fuels, there was always that little bit that couldn’t be processed.  This lead to  more and more cheap and abundant plastic.  And as petro-capital economies grew, so too did the use and application of plastic to cover and package products.</p>
 
-                <p data-lang-id="007-page-paragraph">Soon plastic was everywhere!</p>
+                <p data-lang-id="026-page-paragraph">Soon plastic was everywhere!</p>
 
-                <p data-lang-id="007-page-paragraph">As plastic was bought, used and thrown away, the dynamic of carbon decompaction continued.  With surfaces areas often thousands of times expanded from the orginal compacted carbon, the plastics were easily degraded by the elements.  Soon microplastics could be found everywhere in the ocean, earth and air.</p>
+                <p data-lang-id="027-page-paragraph">As plastic was bought, used and thrown away, the dynamic of carbon decompaction continued.  With surfaces areas often thousands of times expanded from the orginal compacted carbon, the plastics were easily degraded by the elements.  Soon microplastics could be found everywhere in the ocean, earth and air.</p>
 
-                <p data-lang-id="007-page-paragraph">The more petro-capital economies grew, the more industry produced, the more it burned the more plastic was made. This made cheap plastic so abundant that it began to spill out into the biosphere. Humans tried their best to their plastic by putting it back into Industry. But the petro-economy already had so much cheap new plastic, that there was no-profit in recycling the old, and it was easier to burn it, dump it or send it somewhere else.</p>
+                <p data-lang-id="028-page-paragraph">The more petro-capital economies grew, the more industry produced, the more it burned the more plastic was made. This made cheap plastic so abundant that it began to spill out into the biosphere. Humans tried their best to their plastic by putting it back into Industry. But the petro-economy already had so much cheap new plastic, that there was no-profit in recycling the old, and it was easier to burn it, dump it or send it somewhere else.</p>
 
-                <p data-lang-id="007-page-paragraph">Soon humans came to see that industrial recycling wasn’t working. It became clear that dumpling, burning and exporting created more problems than they solved. In order for the petro-capital economy to continue growing, industry tried harder to convince itself and humans there was a solution just around the corner.</p>
+                <p data-lang-id="029-page-paragraph">Soon humans came to see that industrial recycling wasn’t working. It became clear that dumpling, burning and exporting created more problems than they solved. In order for the petro-capital economy to continue growing, industry tried harder to convince itself and humans there was a solution just around the corner.</p>
 
-                <p data-lang-id="007-page-paragraph">Yet, after turning many corner, there was still no solution. Meanwhile, there as so much burning, dumping, so much Co2 and so much plastic, that everyone knew that there was a problem with an economy and lifestyle that had to be powered by ‘fossil fuels’.</p>
+                <p data-lang-id="030-page-paragraph">Yet, after turning many corner, there was still no solution. Meanwhile, there as so much burning, dumping, so much Co2 and so much plastic, that everyone knew that there was a problem with an economy and lifestyle that had to be powered by ‘fossil fuels’.</p>
 
-                <p data-lang-id="007-page-paragraph">It became clear to the humans in these petro-capital economies that that they had much to learn from the planet’s way of relentlessly enriching the biosphere. Looking at indigenous peoples who had lived for centuries in ecological harmony with their surroundings, it was clear that they had much to learn from them also…</p>
+                <p data-lang-id="031-page-paragraph">It became clear to the humans in these petro-capital economies that that they had much to learn from the planet’s way of relentlessly enriching the biosphere. Looking at indigenous peoples who had lived for centuries in ecological harmony with their surroundings, it was clear that they had much to learn from them also…</p>
 				
             </div>
 
