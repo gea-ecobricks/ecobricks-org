449a90b57d483c083644f3b56fdad3eb9525baa0
Russell Maier <russ@earthen.io>
2024-04-20 13:13:52 +0800

    upgrade

--- includes/project-inc.php
+++ includes/project-inc.php
@@ -10,6 +10,178 @@
 
 <STYLE>
 
+
+
+
+.featured-content-gallery {
+    height: fit-content;
+    background-size: contain;
+    background-position: center;
+    position: relative;
+    overflow: hidden;
+    margin-top: 20px;
+    margin-bottom: 10px;
+    margin-right: 16px;
+    margin-left: 16px;
+    border-radius: 15px;
+    background-color: var(--gallery);
+    justify-content: center;
+  display: flex;
+  flex-flow: column;
+  }
+
+
+
+.feed-live {
+  text-align:center;
+  background: var(--darker);
+  border-radius: 15px 15px 0px 0px ;
+  font-size: 0.9em;
+  margin:15px auto -10px auto;
+}
+
+
+
+  @media screen and (min-width: 700px) { 
+
+.feed-live {
+width: 80%;
+padding: 10px;
+
+
+/* background-color: #DFDFDF; */
+}
+
+.feed-live p {
+font-size: 0.9em;
+font-family: courier new,monospace !important;
+color: var(--subdued-text);
+line-height: 1.1;
+font-weight: 300;
+
+}
+  }
+
+
+  @media screen and (max-width: 700px) { 
+.feed-live {
+width: 80%;
+padding: 10px;
+}
+
+.feed-live p {
+font-family: courier new,monospace !important;
+line-height: 1.1em;
+font-weight: 300;
+font-size: 0.8em;
+
+}
+  } 
+
+
+  .gallery-flex-container {
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+margin: 0px -15px 30px -15px}
+
+
+@media screen and (min-width: 700px) { 
+
+.gallery-flex-container > .gal-photo {
+
+  padding: 5px;
+  max-height: 100px;
+  max-width:100px;
+  width:100px;
+  overflow: hidden;
+}
+
+.gallery-flex-container > .gal-project-photo {
+
+padding: 5px;
+max-height: 160px;
+max-width:160px;
+width:160px;
+overflow: hidden;
+}
+/* 
+.gallery-flex-container::before {
+  content: '';
+  flex: auto;
+}
+
+.gal-photo:nth-child(2n + 1):last-child {
+  margin-left: auto;
+} */
+
+
+
+.gal-photo img {
+  width:100px;
+  height: 100px;
+  background: grey;
+  font-family: 'Mulish';
+  font-size: 0.8em;
+  cursor: pointer;
+  color: var(--text-color);
+}
+
+
+.gal-project-photo img {
+  width:160px;
+  height: 160px;
+  background: grey;
+  font-family: 'Mulish';
+  font-size: 0.8em;
+  cursor: pointer;
+  color: var(--text-color);
+}
+
+
+}
+
+
+@media screen and (max-width: 700px) { 
+
+.gallery-flex-container > .gal-photo {
+  max-height: 60px;
+  max-width:60px;
+  width:60px;
+  overflow: hidden;
+}
+
+.gallery-flex-container > .gal-project-photo {
+  max-height: 100px;
+  max-width:100px;
+  width:100px;
+  overflow: hidden;
+}
+
+
+.gal-photo img {
+  margin: 4px;
+  width:60px;
+  height: 60px;
+  background: grey;
+  font-family: 'Mulish';
+  font-size: 0.6em;
+  cursor: pointer;
+  color: var(--text-color);
+}
+
+.gal-project-photo img {
+  margin: 4px;
+  width:100px;
+  height: 100px;
+  background: grey;
+  font-family: 'Mulish';
+  font-size: 0.6em;
+  cursor: pointer;
+  color: var(--text-color);
+}
+}
+
 .modal-photo img{
     min-height: 400px;
     min-width: 400px;
