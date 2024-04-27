1559f965f3e8527773f98ddc088a06d7bfa34281
Russell Maier <russ@earthen.io>
2024-04-27 20:20:36 +0800

    edit enabled!

--- en/project.php
+++ en/project.php
@@ -174,7 +174,7 @@ if ($result->num_rows > 0) {
             echo ' <p><b data-lang-id="148">Plastic Sequestered:</b> ' . $array["est_total_weight"] . '&#8202;kg</p>';
             echo ' <p><b data-lang-id="149">Logged:</b> ' . $array["logged_ts"] . '</p>';
             echo ' <p><b data-lang-id="149b">Ready to Show:</b> ' . $array["ready_to_show"] . '</p>';
-            echo ' <p data-lang-id="150"> ||| END RECORD.</p>
+            echo ' <p data-lang-id="150"> <href="edit-project.php?project_id=' . $array["project_id"] . '">e |||</a> END RECORD.</p>
     
     </div>
 </div>
