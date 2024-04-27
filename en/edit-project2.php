b132cfed8da836f038cdc7624dd9ebd76994d741
Russell Maier <russ@earthen.io>
2024-04-27 12:24:11 +0800

    go

--- en/edit-project2.php
+++ en/edit-project2.php
@@ -81,7 +81,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
     
-    // Check if there were any new files processed and if there were no errors.
+    // 1 Check if there were any new files processed and if there were no errors.
     if (!empty($db_fields) && empty($error_messages)) {
         $fields_for_update = implode(', ', array_map(function($field) { return "{$field} = ?"; }, $db_fields));
         $sql = "UPDATE tb_projects SET $fields_for_update WHERE project_id=?";
@@ -114,7 +114,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $update_stmt = $conn->prepare($sql);
         $update_stmt->bind_param($db_types, ...$db_values);
         if ($update_stmt->execute()) {
-            echo "<script>alert('Project details and photos updated successfully.'); window.location.href='edit-project.php?project_id={$project_id}';</script>";
+            echo "<script>alert('Project details and photos updated successfully.'); window.location.href='project.php?project_id={$project_id}';</script>";
         } else {
             echo "Database update failed: " . $update_stmt->error;
         }
