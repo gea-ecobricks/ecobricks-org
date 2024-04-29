2f81ad60e1a2145daf0e7c3987c733f3b28eec4d
Russell Maier <russ@earthen.io>
2024-04-29 10:02:09 +0800

    php

--- scripts/newsletter-registration-proxy.php
+++ scripts/newsletter-registration-proxy.php
@@ -1,7 +1,12 @@
 <?php
 // Enable error reporting for development
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+header('Access-Control-Allow-Origin: https://book.earthen.io');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json');
 
 // Specify the URL of your Ghost Admin API
 $ghostApiUrl = 'https://earthen.io/ghost/api/v3/admin/members/';
