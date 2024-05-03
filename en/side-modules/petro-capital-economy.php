1ae9e14ccf0540343097c82a3d4b96da6110588e
Russell Maier <russ@earthen.io>
2024-05-03 10:07:38 +0800

    stellar

---
+++ en/side-modules/petro-capital-economy.php
@@ -0,0 +1,7 @@
+<div class="side-module-desktop-mobile">
+				<img src="../webp/oil-barrel-400px.webp" width="250" alt="Plastic comes from ancient carbon that has transformed into oil and that we've extracted and refined." class="dark-mode-invert">
+				<h4>Plastic & the Petro-Capital Economy</h4>
+				<h5>As petroleum is refined into the fuels that power our economies, the leftovers chemicals are used to produce plastics.</h5><br>
+				<!-- <a class="module-btn" href="https://book.earthen.io/en/stellar.html" target="_blank">Plastic's Stellar Story</a>
+                <h6 style="font-size:smaller;">- From 'Plastic's Stellar Story' ⇗</h6> -->
+			</div>
---
+++ en/side-modules/stellar-story.php
@@ -0,0 +1,7 @@
+<div class="side-module-desktop-mobile">
+				<img src="../webp/stellar-story-400px.webp" width="250" alt="Plastic comes from ancient carbon that has transformed into oil and that we've extracted and refined.">
+				<h4>Plastic's Stellar Story</h4>
+				<h5>In fact, plastic's story is even longer!  Indeed, plastic is unlike any matter in our solar system and its story is key to living in harmony on our planet.</h5><br>
+				<a class="module-btn" href="https://book.earthen.io/en/stellar.html" target="_blank">Read Full</a>
+                <h6 style="font-size:smaller;">- <i>Tractatus Ayyew</i>, 'Plastic's Stellar Story' ⇗</h6>
+			</div>
