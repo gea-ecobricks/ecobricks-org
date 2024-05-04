f42858b74e74a057dd3421d1e15fbad107d92f03
Russell Maier <russ@earthen.io>
2024-05-04 19:21:26 +0800

    dsf

--- en/ecojoiners.php
+++ en/ecojoiners.php
@@ -1,73 +1,4 @@
-<?php
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-include '../ecobricks_env.php';
-$conn->set_charset("utf8mb4");
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include '../project-photo-functions.php'; // Ensure this path is correct
-
-
-    $location_full = $_POST['location_address'] ?? 'Default Location';
-    $project_name = $_POST['project_name'];
-    $description_short = $_POST['description_short'];
-    $description_long = $_POST['description_long'];
-    $project_type = $_POST['project_type'];
-    $construction_type = $_POST['construction_type'];
-    $community = $_POST['community'] ?? '';
-    $project_admins = $_POST['project_admins'] ?? '';
-    $start_dt = $_POST['start_dt'];
-    $briks_used = $_POST['briks_used'];
-    $est_avg_brik_weight = $_POST['est_avg_brik_weight'];
-    $latitude = (double)$_POST['latitude'];
-    $longitude = (double)$_POST['longitude'];
-    $connected_ecobricks = $_POST['connected_ecobricks'] ?? '';
-
-
-    $sql = "INSERT INTO tb_projects (project_name, description_short, description_long, location_full, project_type, construction_type, community, project_admins, start_dt, briks_used, est_avg_brik_weight, location_lat, location_long, connected_ecobricks) 
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-if ($stmt = $conn->prepare($sql)) {
-$stmt->bind_param("sssssssssiidds", $project_name, $description_short, $description_long, $location_full, $project_type, $construction_type, $community, $project_admins, $start_dt, $briks_used, $est_avg_brik_weight, $latitude, $longitude, $connected_ecobricks);
-if ($stmt->execute()) {
-            $project_id = $conn->insert_id;
-
-        // Get the last inserted project_id
-        $project_id = $conn->insert_id;
-
-        // Calculate `est_total_weight`
-        $est_total_weight = ($briks_used * $est_avg_brik_weight) / 1000;
-
-        // Update `est_total_weight`
-        $update_weight_sql = "UPDATE tb_projects SET est_total_weight = ? WHERE project_id = ?";
-        $update_weight_stmt = $conn->prepare($update_weight_sql);
-        $update_weight_stmt->bind_param("di", $est_total_weight, $project_id);
-        $update_weight_stmt->execute();
-        $update_weight_stmt->close();
-
-        // Update `project_url`
-        $project_url = "https://ecobricks.org/en/project.php?id=" . $project_id;
-        $update_url_sql = "UPDATE tb_projects SET project_url = ? WHERE project_id = ?";
-        $update_url_stmt = $conn->prepare($update_url_sql);
-        $update_url_stmt->bind_param("si", $project_url, $project_id);
-        $update_url_stmt->execute();
-        $update_url_stmt->close();
-
-
-        $stmt->close();
-        $conn->close();
-            echo "<script>window.location.href = 'add-project-images.php?project_id=" . $project_id . "';</script>";
-        } else {
-            echo "Error: " . $stmt->error . "<br>";
-        }
-        $stmt->close();
-    } else {
-        echo "Prepare failed: " . $conn->error;
-    }
-    $conn->close();
-}
-?>
 
 
 
@@ -80,7 +11,7 @@ if ($stmt->execute()) {
 <?php $page='ecojoiners';?>
 
 
-<?php require_once ("../includes/add-project-inc.php");?>
+<?php require_once ("../includes/ecojoiners-inc.php");?>
 
 
 <div class="splash-content-block"></div>
@@ -98,7 +29,6 @@ if ($stmt->execute()) {
   background: #2d2e32;
   border-radius: 20px;
   margin-bottom: 15px;
-  width: 100%;
   height: 500px">
 <spline-viewer url="https://prod.spline.design/IHSMbi56bMqgyJxV/scene.splinecode"></spline-viewer></div>
 
