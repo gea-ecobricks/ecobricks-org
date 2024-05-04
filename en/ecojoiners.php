c928b54f8475050602d41094f673842256936c97
Russell Maier <russ@earthen.io>
2024-05-04 19:39:06 +0800

    yup

--- en/ecojoiners.php
+++ en/ecojoiners.php
@@ -64,12 +64,13 @@
             <img src="../webp/ecojoiner-sphere.webp" style="height:300px;width:300px">
         </div>
         
+        <p style="text-align: center;">A world of possibilities await.  Coming soon to ecobricks.org!</p>
         
         <div class="step-graphic" style="width:fit-content;margin:auto;margin-top:30px">
             <img src="../logos/ecojoiner-logo.svg" style="height:70px;">
         </div>
 
-        <p style="text-align: center;">Coming soon to ecobricks.org!</p>
+      
 
     </div>
 </div>
