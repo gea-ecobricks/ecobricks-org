4343852a33821fc5e410ed82b47aa0bbc601de74
Russell Maier <russ@earthen.io>
2024-05-06 18:56:20 +0800

    retry

--- en/ecojoiners.php
+++ en/ecojoiners.php
@@ -32,9 +32,11 @@
   height: 500px;
   display:flex; flex-flow:column;"><div>
 <spline-viewer url="https://prod.spline.design/IHSMbi56bMqgyJxV/scene.splinecode"></spline-viewer>
+
+</div>
 <!-- 
 <iframe src='https://my.spline.design/joinersjoining-652f02d873b295b9fdfe5388c4756aba/' frameborder='0' width='100%' height='100%' style="height:100%"></iframe>
-</div> -->
+ -->
 <!-- <div id="spline-instructions" style="font-size:smaller; color:grey;font-family:'Mulish',sans-serif;width: 100%; text-align:left;padding: 5px;margin-top: auto;
   margin-bottom: 5px;">Double touch or drag to interact</div> -->
 </div>
