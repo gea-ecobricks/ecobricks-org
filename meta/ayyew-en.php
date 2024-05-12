79ed4f31580dbf2f24c15b7c345c5cd9578203f7
Russell Maier <russ@earthen.io>
2024-05-12 09:32:51 +0800

    go ayyew

---
+++ meta/ayyew-en.php
@@ -0,0 +1,30 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Ayyew | An ancient ecological ethos for our modern moment</title>
+<meta name="keywords" content="Ecobricks, Ayyew, Sustainability, Plastic Sequestration, Regenerative Principles, Igorot, Northern Philippines, Ecological Harmony, Waste Management, Indigenous Wisdom, Plastic Pollution, Eco-friendly Building, Community Development, Green Technology, Cradle-to-Cradle, Circular Design, Environmental Conservation, Global Ecobrick Alliance, Regenerative Design, Cultural Heritage, Green Spaces, Biodegradable, Local Solutions, Recycle, Ecological Impact, Green Living, Sustainable Practices, Natural Cycles, Eco-Consciousness, Earth Care">
+
+<meta name="description" content="The Asian ecobrick movement emerged from land of the Igorot people and is deeply inspired by their ancient ancestral wisdom and Ayyew virtue.">
+
+<meta name="author" content="Ecobricks.org">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://www.ecobricks.org/en/ayyew.php">
+	<meta property="og:locale" content="en_GB" >
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="Ayyew | An ancient ecological ethos for our modern moment">
+  	<meta property="og:description"   content="The Asian ecobrick movement emerged from land of the Igorot people and is deeply inspired by their ayyew ecological ethos.">
+  	<meta property="og:image"         content="htttps://ecobricks.org/pngs/principles-earth-home-1000px.png"/>
+  	<meta property="og:image:width" content="1000" />
+	<meta property="og:image:height" content="1000" />
+  	<meta property="og:image:alt"     content="An earth and ecobrick home with a tree growing from inside, a wind turbine turning and a happy bird."/>
+
+	<meta property="og:type" content="article" >
+	<meta property="og:site_name" content="Ecobricks.org" >
+	<meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" >
+	<meta property="article:modified_time" content="2024-03-12T09:15:13+00:00" >
+	<meta property="og:image:type" content="image/png" >
+	<meta name="author" content="Global Ecobrick Alliance" >
+	<meta name="twitter:card" content="summary" >
+	<meta name="twitter:label1" content="Est. reading time" >
+	<meta name="twitter:data1" content="4 minutes" > 
