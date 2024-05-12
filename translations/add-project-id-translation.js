afb53fb2367955d521a8ddd3129307514749090a
Russell Maier <russ@earthen.io>
2024-05-12 20:59:33 +0800

    Indoensian

--- translations/add-project-id-translation.js
+++ translations/add-project-id-translation.js
@@ -7,7 +7,7 @@ Indonesian SNIPPETS FOR add-projectphp page on ECOBRICKS.ORG
 
 
 const id_Page_Translations = {
-  "000-language-code": "EN 🌐",
+  "000-language-code": "ID 🌐",
   "001-splash-title": "Posting Proyek",
   "002-form-description": "Bagikan proyek ecobrick Anda dengan dunia. Gunakan formulir ini untuk memposting proyek ecobricks yang telah selesai ke ecobricks.org. Proyek akan ditampilkan di halaman utama kami dan diarsipkan dalam database kami.",
   "003-splash-image-alt": '<img src="../svgs/building-methods.svg" style="width:65%" alt="Ada banyak cara untuk membangun dengan ecobricks">',
@@ -19,11 +19,49 @@ const id_Page_Translations = {
   "005-long-project-desc": "Deskripsi proyek lengkap:",
   "005-long-project-desc-caption": "Opsional. Ambil sebanyak yang Anda butuhkan untuk berbagi detail lengkap proyek Anda. Maksimal 1000 kata.",
   "000-field-required-error": "Bidang ini diperlukan.",
-  "000-name-field-too-long-error": "Nama proyek Anda terlalu panjang. Maksimal 50 karakter."
+  "000-name-field-too-long-error": "Nama proyek Anda terlalu panjang. Maksimal 50 karakter.",
 
+  "000-field-description-too-long-error": "Deskripsi proyek Anda terlalu panjang. Maksimal 255 karakter.",
+  "000-field-invalid-characters": "Entri Anda mengandung karakter yang tidak valid. Hindari tanda kutip, garis miring, dan tanda lebih besar.",
+  "000-long-field-too-long-error": "Deskripsi panjang proyek Anda terlalu panjang. Maksimal 2000 karakter.",
+  "009-bricks-used": "Berapa banyak ecobrick yang digunakan dalam proyek Anda?",
+  "009-bricks-used-caption": "Silakan masukkan jumlah ecobrick antara 1-5000.",
+  "010-est-avg-weight": "Tolong perkirakan berat rata-rata ecobrick yang digunakan dalam proyek Anda dalam gram?",
+  "010-est-avg-weight-range": "Hanya sebuah angka (antara 100 dan 2000).",
+  "011-project-type": "Apa jenis proyek ini?",
+  "011-select": "Pilih jenis proyek...",
+  "011-single-module": "Modul Tunggal",
+  "011-modular-furniture": "Perabotan",
+  "011-outdoor-garden": "Taman Luar",
+  "011-structure": "Struktur",
+  "011-art": "Seni",
+  "011-other": "Lainnya",
+  "012-construction-type": "Apa jenis konstruksi ini?",
+  "012-select": "Pilih jenis konstruksi...",
+  "012-construction-silicone": "Silikon",
+  "012-construction-tire-banding": "Banding Ban",
+  "012-construction-ecojoiner": "Ecojoiner",
+  "012-construction-earth": "Bumi/Cob",
+  "012-construction-installation": "Instalasi",
+  "012-other": "Lainnya",
 
-
-
+    "015-location": "Di mana lokasi proyek?",
+    "016-location-caption": "Demi privasi, tolong jangan gunakan alamat Anda secara tepat. Pilih area umum atau kota Anda. Lokasi proyek akan ditampilkan pada peta proyek kami.",
+    "012-block-1-advanced": "Opsi Lanjutan",
+    "012b-project-sort": "Proyek seperti apa ini?",
+    "012b-select": "Pilih jenis...",
+    "012b-community-project": "Proyek Komunitas",
+    "012b-personal-project": "Proyek Pribadi",
+    "013-community": "Komunitas mana yang bertanggung jawab atas proyek ini?",
+    "013b-optional": "Opsional",
+    "014-project-admins": "Proyek milik siapa ini?",
+    "014b-optional": "Opsional: Berikan nama prinsipal proyek. Jika Anda ingin menghubungkan ini ke akun GoBrik, pastikan ejaan nama sudah benar.",
+    "007-project-duration": "Durasi Proyek",
+    "007-start-date": "Tanggal Mulai:",
+    "008-start-date-caption": "Kapan proyek ini dimulai?",
+    "007b-end-date": "Tanggal Berakhir:",
+    "008b-end-date-caption": "Kapan proyek ini berakhir?",
+    "017-submit-button": "<input type='submit' value='Selanjutnya: Unggah Foto ➡️' aria-label='Kirim Formulir'>"
 
 
 };
