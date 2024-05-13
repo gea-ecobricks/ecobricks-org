75f04f2d0e6f73027c4322a1bfcbba0445a7d86e
Russell Maier <russ@earthen.io>
2024-05-13 09:00:17 +0800

    info

--- en/phpinfo.php
+++ en/phpinfo.php
@@ -5,5 +5,6 @@ phpinfo();
 <?php
 echo 'Maximum upload file size: ' . ini_get('upload_max_filesize') . "\n";
 echo 'Maximum POST size: ' . ini_get('post_max_size');
+echo gd_info()['WebP Support'] ? 'WebP is supported' : 'WebP is not supported';
 ?>
 
