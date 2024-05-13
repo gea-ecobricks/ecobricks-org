9f45a9c91d9fb31933bd2ac9bb00c7ee74a1b028
Russell Maier <russ@earthen.io>
2024-05-13 10:10:43 +0800

    arg

--- includes/add-project-inc.php
+++ includes/add-project-inc.php
@@ -1,6 +1,6 @@
 
 
-<?php require_once ("../meta/add-project-en.php");?>
+<?php require_once ("../meta/add-project-$lang.php");?>
 
   
 <STYLE>
--- meta/add-project-id.php
+++ meta/add-project-id.php
@@ -1,6 +1,5 @@
 <!-- Meta tags for page display and search engine listing-->
 
-
 <title>Pasang proyek ecobrick Anda</title>
 
 <meta name="keywords" content="post, share, submit, ecobrick, project">
@@ -11,7 +10,7 @@
 	
 	<!-- Facebook Open Graph Tags for social sharing-->
 
-	<meta property="og:url"           content="https://ecobricks.org/id/add-project.php">
+	<meta property="og:url"           content="https://ecobricks.org/<?php $lang;?>/add-project.php">
 	<meta property="og:type"          content="website">
  	<meta property="og:title"         content="Pasang proyek ecobrick Anda">
   	<meta property="og:description"   content="Posting dan bagikan proyek ecobrick mengagumkan Anda kepada dunia">
@@ -19,7 +18,7 @@
   	<meta property="og:image:width" content="450px" >
 	<meta property="og:image:height" content="450px" >
   	<meta property="og:image:alt"     content="Proyek Ecobrick">
-	  <meta property="og:locale" content="id_ID" >
+	  <meta property="og:locale" content="<?php include 'lang.php';?>_<?php include 'lang.php';?>" >
 
 	  <meta name="author" content="Ecobricks.org">
   
