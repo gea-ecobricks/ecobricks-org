1d72b8deae023dd920f3dbb6ea49e3d6424ec4e6
Russell Maier <russ@earthen.io>
2024-05-13 09:59:11 +0800

    go slo

---
+++ meta/add-project-fr.php
@@ -0,0 +1,24 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Postez votre projet écobrick</title>
+
+<meta name="keywords" content="post, share, submit, ecobrick, project">
+
+<meta name="description" content="Publiez et partagez votre superbe projet d'écobrick avec le monde">	
+
+<meta name="robots" content="noindex">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://ecobricks.org/<?php include 'lang.php';?>/add-project.php">
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="Postez votre projet écobrick">
+  	<meta property="og:description"   content="Publiez et partagez votre superbe projet d'écobrick avec le monde!">
+  	<meta property="og:image"         content="https://ecobricks.org/webp/build-blue-450px.webp">
+  	<meta property="og:image:width" content="450px" >
+	<meta property="og:image:height" content="450px" >
+  	<meta property="og:image:alt"     content="Ecobrick projects">
+	  <meta property="og:locale" content="<?php include 'lang.php';?>_<?php include 'lang.php';?>" >
+
+	  <meta name="author" content="Ecobricks.org">
+  
---
+++ meta/add-project-id.php
@@ -0,0 +1,24 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Pasang proyek ecobrick Anda</title>
+
+<meta name="keywords" content="post, share, submit, ecobrick, project">
+
+<meta name="description" content="Posting dan bagikan proyek ecobrick mengagumkan Anda kepada dunia">	
+
+<meta name="robots" content="noindex">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://ecobricks.org/<?php include 'lang.php';?>/add-project.php">
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="Pasang proyek ecobrick Anda">
+  	<meta property="og:description"   content="Posting dan bagikan proyek ecobrick mengagumkan Anda kepada dunia">
+  	<meta property="og:image"         content="https://ecobricks.org/webp/build-blue-450px.webp">
+  	<meta property="og:image:width" content="450px" >
+	<meta property="og:image:height" content="450px" >
+  	<meta property="og:image:alt"     content="Proyek Ecobrick">
+	  <meta property="og:locale" content="<?php include 'lang.php';?>_<?php include 'lang.php';?>" >
+
+	  <meta name="author" content="Ecobricks.org">
+  
