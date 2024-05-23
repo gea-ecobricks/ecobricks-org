60371ad757d2781dd92e516d8825e8b8789b2cf2
Russell Maier <russ@earthen.io>
2024-05-23 21:15:50 +0800

    ff

---
+++ id/side-modules/plastic-from-oil.php
@@ -0,0 +1,5 @@
+            <div class="side-module-desktop-only" style="margin-top: -30px;">
+				<img src="../webp/plastic-origins.webp" width="250" alt="Plastic comes from ancient carbon that has transformed into oil and that we've extracted and refined.">
+				<!-- <h4>Plastic's Origins</h4>
+				<h5>Plastic comes from ancient carbon that we've extracted and refined.</h5> -->
+			</div>
