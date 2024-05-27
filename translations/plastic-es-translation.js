ef5257bd0708b4370c7f281733b2f8612f6c2437
Russell Maier <russ@earthen.io>
2024-05-27 12:27:17 +0800

    go fr

--- translations/plastic-es-translation.js
+++ translations/plastic-es-translation.js
@@ -16,21 +16,70 @@ FRENCH SNIPPETS FOR ECOBRICKS.ORG
 
 
 const es_Page_Translations = {
+        "001-splash-title": "La Larga Historia del Plástico",
+        "002-splash-subtitle": "Conocer el pasado primordial del plástico es la clave para asegurar su futuro verde hoy.",
+        "003-splash-image-alt": "<img src=\"../svgs/the-biosphere.svg\" style=\"width: 85%\" alt=\"El plástico proviene de las profundidades de la tierra.\">",
+        "004-first-page-paragraph": "¿Qué es exactamente el plástico? ¿De dónde viene? Y, lo que nos preocupa a todos, ¿a dónde debería ir nuestro plástico?",
+        "005-second-page-paragraph": "Para gestionar nuestro plástico sabiamente y tomar decisiones seguras sobre su futuro, conocer su historia completa basada en la ciencia es esencial—lo que llamamos la <i>larga historia del plástico</i>. Solo conociendo los orígenes antiguos del plástico podemos asegurar que su futuro sea una contribución ecológica– en lugar de solo más contaminación.",
+        "005b-second-page-paragraph": "¿Listo para una inmersión profunda? Siéntate y regresa con nosotros al pasado primordial de nuestro planeta...",
+        "006-page-paragraph": "<span class=\"drop-cap\">H</span>ACE MÁS DE CUATRO MIL MILLONES DE AÑOS, el polvo cósmico y los desechos se arremolinaron juntos para formar nuestro planeta. Al igual que los otros planetas de nuestro sistema solar, la Tierra era al principio un lugar árido y desolado. Durante los primeros dos mil millones de años de nuestro planeta, su atmósfera estaba llena de dióxido de carbono, lo que hacía que su clima fuera caliente, duro e inestable.",
+        "007-page-paragraph": "Sin embargo, a medida que el Sol brillaba, las cosas comenzaron a cambiar. Poco a poco, el carácter único de nuestro planeta comenzó a desarrollarse. Los volcanes expulsaron, los ríos corrieron y las corrientes oceánicas y las placas tectónicas chocaron. A medida que la superficie de la tierra dispersaba el resplandor siempre presente del sol, los átomos de la Tierra comenzaron a girar en configuraciones cada vez más complejas. Lento pero seguro, pequeños sistemas moleculares descubrieron cómo convertir la energía del Sol en vida.",
+        "008-page-paragraph": "A medida que emergían las células y la fotosíntesis, siguieron las plantas. Al absorber el dióxido de carbono, se podían hacer nutrientes y construir cuerpos.",
+        "009-page-paragraph": "Poco a poco, las plantas y los árboles se extendieron por la superficie del planeta.",
 
-    
-        '001-splash-title': 'La largas historia del plástico',
-        '002-splash-subtitle': 'Comprender la historia antigua del plástico es esencial para volver a usarlo adecuadamente hoy.',
-        '003-splash-image-alt': '<img src="../webp/plastic-origins.webp" style="width: 85%" alt="El plástico proviene de lo profundo de la tierra.">',
-        
-        '004-lead-page-paragraph': '¿Qué es exactamente el plástico? ¿De dónde viene? ¿Y hacia dónde se dirige? Para usar el plástico de manera inteligente y tomar decisiones seguras sobre su futuro, es esencial conocer su historia completa, basada en la ciencia, lo que llamamos la \'larga historia del plástico\'.',
-        
-        '005-first-page-paragraph': 'Cada vez más, estamos aprendiendo que la historia del plástico no termina cuando lo tiramos. Según los científicos, una cantidad cada vez mayor de plástico se fabrica, se desecha y llega a la biosfera, donde daña a criaturas grandes y pequeñas.',
-        
-        '006-second-page-paragraph': 'Sin embargo, es igual de importante entender que la historia del plástico no comienza en una fábrica. En cambio, el plástico encuentra sus orígenes hace millones de años en una Tierra antigua y estéril que apenas reconoceríamos. De hecho, las moléculas de carbono de nuestro plástico moderno jugaron un papel clave en hacer de nuestro hogar común la biosfera próspera y diversa que conocemos hoy.'
-    
+
+
+
+        "010-page-paragraph": "Hechas del carbono que recolectaron, pronto vastos bosques cubrieron los continentes y enormes florecimientos de algas llenaron los mares bajos.",
+        "011-page-paragraph": "A medida que las plantas y los animales de estos ecosistemas vivían y morían, cada generación caída era cubierta por la siguiente. Ya sea en el suelo del bosque o en el fondo del océano, las capas de vida se enterraban unas sobre otras. Durante cientos de millones de años, las incontables toneladas de carbono que contenían se concentraron, compactaron y aseguraron bajo la tierra.",
+        "012-page-paragraph": "Con todo este carbono secuestrado fuera del aire, la atmósfera de la Tierra se calmó y se enfrió. Poco a poco, ¡la vida prosperó! Los ecosistemas y biomas se volvieron más vibrantes, diversos y abundantes.",
+        "013-page-paragraph": "Nutritado por los ecosistemas ahora frondosos y abundantes del planeta, un mamífero particular de dos patas surgió de los bosques hace unos 30 millones de años—¡y nosotros, los sapiens, aparecimos en escena!",
+        "014-page-paragraph": "Luego, en una noche fría y oscura de la Edad de Hielo, uno de nuestros primeros ancestros descubrió el fuego.",
+        "015-page-paragraph": "A pesar del molesto humo, nuestras cuevas se calentaron mientras el carbono de las ramitas y los palos se convertía en llamas.",
+        "016-page-paragraph": "A lo largo de los milenios, mejoramos cada vez más en hacer nuestros fuegos.",
+        "017-page-paragraph": "No pasó mucho tiempo hasta que descubrimos rocas negras que eran aún más densas en carbono que la madera más pesada. ¡Y ardían aún mejor! Poco sabíamos, estas piedras estaban hechas de las mismas plantas que hace mucho tiempo habían eliminado el carbono del aire de la Tierra, para hacer posible nuestra vida.",
+        "018-page-paragraph": "Durante los siglos siguientes, ciertos grupos de humanos (pero no todos) mejoraron cada vez más en extraer y quemar las rocas negras. También se descubrieron otros tipos de carbono compacto antiguo. Pronto estábamos extrayendo carbón, petróleo y gas natural en grandes cantidades.",
+        "019-page-paragraph": "A medida que las naciones modernas prosperaban, sus economías comenzaron a depender cada vez más de la energía fácil obtenida al quemar estos \"combustibles fósiles\".",
+        "020-page-paragraph": "Sin embargo, a pesar del nombre ingenioso, estos depósitos fósiles nunca fueron combustibles al principio. En el proceso de refinarlos en combustible, quedaba un residuo (5-15% dependiendo de la fuente) que simplemente no podía ser utilizado. Este lodo residual comenzó a acumularse.",
+        "021-page-paragraph": "Pronto nos dimos cuenta de que los residuos del refinamiento también podían ser utilizados. Con un poco de química, se podían producir polímeros—y con un poco más; una gama infinita de materiales maravillosos.",
+        "022-page-paragraph": "¡Los plásticos habían llegado!",
+        "023-page-paragraph": "Pronto los humanos estaban resolviendo todo tipo de problemas fabricando todo tipo de cosas asombrosas de plástico. Ya no era necesario matar elefantes por su marfil para fabricar bolas de billar. Ya no necesitabas costosos platos de plata para tomar una fotografía. La comida ya no se echaba a perder en unos pocos días.",
+        "024-page-paragraph": "A medida que se consumían más productos, se necesitaban más y más fábricas. A medida que más fábricas necesitaban energía, se extraían y refinaban más y más combustibles fósiles. El petróleo y el capital fluían mientras las economías crecían.",
+        "025-page-paragraph": "A medida que todo giraba más y más rápido, siempre había un pequeño porcentaje que no se podía procesar. Esto llevó a la industria a producir cada vez más plástico a un costo cada vez menor. Y así como quemar combustibles generaba cada vez más humo, refinar combustibles generaba cada vez más plástico.",
+        "026-page-paragraph": "A medida que consumíamos más y más, intentábamos hacer todo lo posible para reutilizar y reciclar nuestro plástico cuando terminábamos con él. Por supuesto, refinábamos tanto carbono fósil en combustible que ahora había una sobreabundancia de residuos perfectos para fabricar polímeros. Al final, era mucho más fácil (¡y barato!) producir más plástico nuevo desde cero.",
+
+                "028-page-paragraph": "Sin embargo, pronto hubo tanto plástico que comenzó a desbordarse de nuestros hogares, empresas e industrias. Con creciente alarma y ansiedad, observamos cómo nuestro plástico obstruía los ríos, ensuciaba las playas y se amontonaba en grandes montañas humeantes. Las partículas de plástico estaban por todas partes. Dentro de los cuerpos de peces y animales. Y en nuestros cuerpos también.",
+                "029-page-paragraph": "Sorprendidos y avergonzados, nuestras industrias y nuestras empresas prometieron hacerlo mejor. Y lo intentaron. Hicieron todo lo posible para convencer a todos de que había una solución a la vuelta de la esquina. Empresas, científicos, empresarios y políticos se esforzaron valientemente por hacer que sus procesos, productos y envases fueran menos dañinos, menos contaminantes y menos perjudiciales.",
+                "030-page-paragraph": "Sin embargo, después de innumerables giros, a pesar de ser menos dañinos y menos contaminantes, sus procesos seguían siendo contaminantes y dañinos.",
+                "031-page-paragraph": "Año tras año, la contaminación de los ríos y océanos continuó aumentando.",
+                "032-page-paragraph": "Y con ella, nuestra desesperación.",
+                "033-page-paragraph": "Estaba claro que reducir el daño no era suficiente.",
+                "035-page-paragraph": "¿Cómo podríamos asegurar que nuestros productos, procesos, envases y empresas no causaran contaminación?",
+                "037-page-paragraph": "¿Cómo podríamos asegurarnos de que nuestras formas no fueran grises y dañinas desde el principio, sino que en realidad ayudaran a hacer que los ecosistemas fueran vibrantes y verdes, diversos y abundantes?",
+                "036-page-paragraph": "¿Cómo podríamos gestionar nuestro plástico de una manera verdaderamente ecológica?",
+                "038-page-conclusion": "Resulta que las respuestas se pueden encontrar en la historia misma.",
+                "039-header-paragraph": "Viendo el Plástico de Nuevo",
+                "039b-header-paragraph": "A la luz de la antigua historia de la Tierra, podemos ver nuestro plástico moderno de manera diferente…",
+                "039c-page-paragraph": "Viendo nuestro plástico a través del lente de la historia de la Tierra, podemos observar que…",
+                "040a-list-paragraph": "¡Nuestro plástico está hecho del mismo carbono y del mismo proceso que la Tierra usó para verdear la superficie de nuestro planeta!",
+                "040b-list-paragraph": "Hay una conexión directa entre la quema de combustibles fósiles y la producción y dispersión del plástico.",
+                "040c-list-paragraph": "El plástico es la única forma de combustibles fósiles refinados que permanece densamente compactada.",
+                "040d-list-paragraph": "No hay solución industrial o económica para el plástico, porque la industria y nuestras economías están en la raíz del bajo costo subsidiado y la vasta disponibilidad del plástico.",
+                "041-opener-header-text": "<h4>Aprendiendo de la Tierra</h4><h5>Podemos aprender mucho de la forma en que la Tierra se transformó de estéril a biosfera.</h5><br>",
+                "042-page-paragraph": "Al observar de cerca el pasado primordial de la Tierra, podemos observar que…",
+                "043a-list-paragraph": "La Tierra usó el carbono a corto plazo como un bloque de construcción indefinido.",
+                "043b-list-paragraph": "La Tierra concentró, aseguró y secuestró el carbono de manera constante en almacenamiento a largo plazo fuera de la biosfera.",
+                "043c-list-paragraph": "Los procesos de la Tierra tendían a restar más carbono al almacenamiento del que devolvían a la atmósfera.",
+                "043d-list-paragraph": "Los procesos de la Tierra distribuían los nutrientes a través de su superficie y sistemas de manera cada vez más amplia y beneficiosa.",
+                "043e-list-paragraph": "Los procesos de la Tierra tendían hacia sistemas, organismos y ecosistemas cada vez más diversos.",
+                "043f-list-paragraph": "Los organismos y ecosistemas de la Tierra tendían hacia una mayor conciencia de sí mismos y de su interconexión.",
+                "044-page-paragraph": "Estas dinámicas terrestres proporcionan un poderoso ejemplo de lo que significa ser ecológico. Siguiendo el ejemplo de la Tierra, podemos estar seguros de que los resultados también serán enriquecedores ecológicamente.",
+                "045-page-paragraph": "De hecho, podemos aplicar el ejemplo de la Tierra directamente a la gestión de nuestro plástico moderno…",
+                "046a-list-paragraph": "Cuanto menos participemos en la economía petrocapitalista, menos combustibles fósiles serán extraídos y refinados, y menos plástico será producido."
+            
     
 
-    
-}
+
         
+    }; 
       
