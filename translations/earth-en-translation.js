543ef8621f3a578ae4d833da2309b24cc182ac38
Russell Maier <russ@earthen.io>
2024-05-29 12:41:49 +0800

    earth fix

--- translations/earth-en-translation.js
+++ translations/earth-en-translation.js
@@ -13,9 +13,23 @@ const en_Page_Translations = {
     "splash-image": '<img src="../svgs/tree-bench.svg" style="width: 85%">',
     "004-lead-page-paragraph": "Combining time-tested local building traditions with ecobricks, we can safely secure our plastic for the long-term in beautiful and functional earthen constructions.",
     "005-first-page-paragraph": "Earth and Ecobrick building methods are inspired by the natural building traditions of our ancestors. Countless cultures around the world have their ancestral traditions of working with the earth to build. Many of their structures have stood for centuries. Some for millenia! By adapting their ancient methods to our modern challenges, we can put our ecobricks to good and green use.",
-    "006-second-page-paragraph": "With earth and ecobrick methods not only can we build beautiful, strong and resilient structures, we can secure our plastic out of industry and out of the biosphere. Such constructions, rather than result in emissions, actually avoid them, while absorbing physical carbon within them. This ensures that we are not just creating homes, but so too a common home for all the others with which we share ecosystem and biome."
+    "006-second-page-paragraph": "With earth and ecobrick methods not only can we build beautiful, strong and resilient structures, we can secure our plastic out of industry and out of the biosphere. Such constructions, rather than result in emissions, actually avoid them, while absorbing physical carbon within them. This ensures that we are not just creating homes, but so too a common home for all the others with which we share ecosystem and biome.",
+
+    "007-opener-header": "What is Earth Building?",
+    "008-opener-subheader": "The organic traditions of yesterday have practical relevance for our green visions today.",
+    "009-photo-caption": "Mud Mosque in Djene, Mali",
+    "010-photo-caption": "16th Century Tudor Cob Home - England",
+    "011-photo-caption": "Ancient Navajo Dwelling -New Mexico, America",
+    "012-photo-caption": "A simple raised bed garden bench - Indonesia",
+    "013-photo-caption": "A spiral cob and ecobrick construction - UK",
+    "014-photo-caption": "An ecobrick snake play-bench for a public park - Philippines",
+    "015-paragraph": "Earth Building is known by different names around the world– “adobe”, “wattle-and-daub”, “cob”, “organic cement” etc. These low-energy and low-cost traditions have resulted in incredibly beautiful and varying structures around the world. Despite their unique shapes and forms they share certain fundamentals in common– such as using local materials, including the community, and enriching the local ecology. Our ancestors took these principles for granted, but today its more important than ever to learn from the wisdom embedded in their ways.",
+    "016-paragraph": "Earth building contrasts starkly with today’s conventional construction methods.",
+    "017-list-item": "Whereas commercial construction methods rely on energy-intensive, industrial processes and are ecologically depleting, earthen building is ecologically enriching.",
+    "018-list-item": "Whereas commercial construction is powered by petroleum energy and dependent on capital, earthen constructions uses local resources and community collaboration.",
+    "019-list-item": "Whereas, commercial construction emits Co2 and releases plastic, earth and ecobrick building strives to secure them.",
+    "020-list-item": "Whereas commercial construction has a top-down owner/boss/employee structure, earthen building strives to involve everyone in the community."
+
 
     
-
-
     };
