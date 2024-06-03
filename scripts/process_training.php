3981b34fc60d9cc686880d5e1e589c91331d6a6a
Russell Maier <russ@earthen.io>
2024-06-03 19:50:24 +0800

    dude

--- scripts/process_training.php
+++ scripts/process_training.php
@@ -103,6 +103,7 @@ if (isset($data['records']) && count($data['records']) > 0) {
             $training_photo2_main = isset($record['field_1329_raw']) ? $record['field_1329_raw']['url'] : '';
             $training_photo3_main = isset($record['field_2179_raw']) ? $record['field_2179_raw']['url'] : '';
             $training_photo4_main = isset($record['field_2178_raw']) ? $record['field_2178_raw']['url'] : '';
+            $training_photo5_main = isset($record['field_2178_raw']) ? $record['field_2178_raw']['url'] : '';
 
             // Check if the training ID already exists in the database
             $check_stmt = $conn->prepare("SELECT training_id FROM tb_trainings WHERE training_id = ?");
@@ -115,11 +116,11 @@ if (isset($data['records']) && count($data['records']) > 0) {
                 $errors[] = "A record with Training ID $training_id already exists.";
             } else {
                 // Prepare and bind
-                $stmt = $conn->prepare("INSERT INTO tb_trainings (training_id, training_title, training_logged, no_participants, lead_trainer, training_photo0_main, training_photo1_main, training_photo2_main, training_photo3_main, training_photo4_main, training_type, briks_made, est_plastic_packed, location_full, training_summary, training_agenda, training_success, training_challenges, training_lessons_learned) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                $stmt = $conn->prepare("INSERT INTO tb_trainings (training_id, training_title, training_logged, no_participants, lead_trainer, training_photo0_main, training_photo1_main, training_photo2_main, training_photo3_main, training_photo4_main, training_photo5_main, training_type, briks_made, est_plastic_packed, location_full, training_summary, training_agenda, training_success, training_challenges, training_lessons_learned) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
                 if ($stmt === false) {
                     die("<script>alert('Prepare failed: " . htmlspecialchars($conn->error) . "');</script>");
                 }
-                $stmt->bind_param("sssssssssssssssssss", $training_id, $training_title, $training_logged, $no_participants, $lead_trainer, $training_photo0_main, $training_photo1_main, $training_photo2_main, $training_photo3_main, $training_photo4_main, $training_type, $briks_made, $est_plastic_packed, $location_full, $training_summary, $training_agenda, $training_success, $training_challenges, $training_lessons_learned);
+                $stmt->bind_param("ssssssssssssssssssss", $training_id, $training_title, $training_logged, $no_participants, $lead_trainer, $training_photo0_main, $training_photo1_main, $training_photo2_main, $training_photo3_main, $training_photo4_main, $training_photo5_main, $training_type, $briks_made, $est_plastic_packed, $location_full, $training_summary, $training_agenda, $training_success, $training_challenges, $training_lessons_learned);
 
                 // Execute statement
                 if (!$stmt->execute()) {
@@ -157,6 +158,7 @@ if (isset($data['records']) && count($data['records']) > 0) {
                 <p><img src='$training_photo2_main' alt='Feature Photo 2' style='max-width: 400px;' title='$training_photo2_main' /></p>
                 <p><img src='$training_photo3_main' alt='Feature Photo 2' style='max-width: 400px;' title='$training_photo3_main' /></p>
                 <p><img src='$training_photo4_main' alt='Feature Photo 2' style='max-width: 400px;' title='$training_photo4_main' /></p>
+                <p><img src='$training_photo5_main' alt='Feature Photo 2' style='max-width: 400px;' title='$training_photo5_main' /></p>
             ";
             break;
         }
