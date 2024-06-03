1fffd59b22950c63c9f0b661e342c0d411e3702f
Russell Maier <russ@earthen.io>
2024-06-03 16:11:06 +0800

    26

--- scripts/process_training.php
+++ scripts/process_training.php
@@ -69,7 +69,6 @@ $record_details = "";
 
 
 
-
 // PART 2
 
 $data = json_decode($response, true);
@@ -104,18 +103,14 @@ if (isset($data['records']) && count($data['records']) > 0) {
 
             // Extract additional image URLs
             $training_photo1_main = isset($record['field_1328_raw']) ? $record['field_1328_raw']['url'] : null;
-            $training_photo2_main = isset($record['field_1329_raw']) ? $record['field_1329_raw']['url'] : null;
-            $training_photo3_main = isset($record['field_2179_raw']) ? $record['field_2179_raw']['url'] : null;
-            $training_photo4_main = isset($record['field_2178_raw']) ? $record['field_2178_raw']['url'] : null;
-            $training_photo5_main = isset($record['field_2180_raw']) ? $record['field_2180_raw']['url'] : null;
             $training_photo6_main = isset($record['field_2181_raw']) ? $record['field_2181_raw']['url'] : null;
 
             // Prepare and bind
-            $stmt = $conn->prepare("INSERT INTO tb_trainings (training_id, training_title, training_logged, no_participants, lead_trainer, training_photo1_main, training_type, briks_made, est_plastic_packed, location_full, training_summary, training_agenda, training_success, training_challenges, training_lessons_learned, training_photo2_main, training_photo3_main, training_photo4_main, training_photo5_main, training_photo6_main) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $stmt = $conn->prepare("INSERT INTO tb_trainings (training_id, training_title, training_logged, no_participants, lead_trainer, training_photo1_main, training_type, briks_made, est_plastic_packed, location_full, training_summary, training_agenda, training_success, training_challenges, training_lessons_learned, training_photo6_main) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
             if ($stmt === false) {
                 die("<script>alert('Prepare failed: " . htmlspecialchars($conn->error) . "');</script>");
             }
-            $stmt->bind_param("ssssssssssssssssssss", $training_id, $training_title, $training_logged, $no_participants, $lead_trainer, $training_photo0_main, $training_type, $briks_made, $est_plastic_packed, $location_full, $training_summary, $training_agenda, $training_success, $training_challenges, $training_lessons_learned, $training_photo2_main, $training_photo3_main, $training_photo4_main, $training_photo5_main, $training_photo6_main);
+            $stmt->bind_param("ssssssssssssssss", $training_id, $training_title, $training_logged, $no_participants, $lead_trainer, $training_photo1_main, $training_type, $briks_made, $est_plastic_packed, $location_full, $training_summary, $training_agenda, $training_success, $training_challenges, $training_lessons_learned, $training_photo6_main);
 
             // Execute statement
             if (!$stmt->execute()) {
@@ -143,10 +138,6 @@ if (isset($data['records']) && count($data['records']) > 0) {
                 <p>Training Challenges: $training_challenges</p>
                 <p>Training Lessons Learned: $training_lessons_learned</p>
                 <p>Training Photo 1: <img src='$training_photo1_main' alt='Training Photo 1' style='max-width: 350px;' /></p>
-                <p>Training Photo 2: <img src='$training_photo2_main' alt='Training Photo 2' style='max-width: 350px;' /></p>
-                <p>Training Photo 3: <img src='$training_photo3_main' alt='Training Photo 3' style='max-width: 350px;' /></p>
-                <p>Training Photo 4: <img src='$training_photo4_main' alt='Training Photo 4' style='max-width: 350px;' /></p>
-                <p>Training Photo 5: <img src='$training_photo5_main' alt='Training Photo 5' style='max-width: 350px;' /></p>
                 <p>Training Photo 6: <img src='$training_photo6_main' alt='Training Photo 6' style='max-width: 350px;' /></p>
             ";
             break;
@@ -165,6 +156,7 @@ if (isset($data['records']) && count($data['records']) > 0) {
 } else {
     echo "<script>alert('No records found in the Knack database.');</script>";
 }
+
 
 
 //PART 3
