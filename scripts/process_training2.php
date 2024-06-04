5e0f6528df25fa030401e1b093443a9869e85982
Russell Maier <russ@earthen.io>
2024-06-04 17:16:47 +0800

    bsadf

--- scripts/process_training2.php
+++ scripts/process_training2.php
@@ -101,9 +101,7 @@ if (isset($data['records']) && count($data['records']) > 0) {
             $training_photo4_main = isset($record['field_2178_raw']['url']) ? $record['field_2178_raw']['url'] : '';
             $training_photo5_main = isset($record['field_2180_raw']['url']) ? $record['field_2180_raw']['url'] : '';
             $training_photo6_main = isset($record['field_2181_raw']['url']) ? $record['field_2181_raw']['url'] : '';
-        }
-    }
-    
+      
 
             // Check if the training ID already exists in the database
             $check_stmt = $conn->prepare("SELECT training_id FROM tb_trainings WHERE training_id = ?");
