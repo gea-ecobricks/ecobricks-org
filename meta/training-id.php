e586180926b57b9fb02bbd89593e28e535ba8cbc
Russell Maier <russ@earthen.io>
2024-06-07 20:09:13 +0800

    id

--- meta/training-id.php
+++ meta/training-id.php
@@ -1,4 +1,4 @@
-<!-- ENGLISH ECOBRICK DETAILS PAGE -->
+<!-- HALAMAN RINCIAN ECOBRICK DALAM BAHASA INDONESIA -->
 
 <?php
 
@@ -15,36 +15,39 @@ if ($result->num_rows > 0) {
 	
     while($array = $result->fetch_assoc()) {
 
-        echo '<title>'. $array["training_title"] .' |  '. $array["no_participants"] .' peserta</title>';
+		echo '<title>'. $array["training_title"] .' |  '. $array["no_participants"] .' peserta</title>';
 
-echo '<meta name="description" content="'. $array["no_participants"] .' telah berlatih di '. $array["location_full"] .' dalam pelatihan GEA '. $array["training_type"] .'">';
+        echo '<meta name="description" content="'. $array["no_participants"] .' telah dilatih di '. $array["location_full"] .' dalam pelatihan '. $array["training_type"] .' dari Aliansi Ecobrick Global.">';
 
-echo '<meta name="keywords" content="pelatihan, lokakarya, edukasi plastik, ecobrick, '. $array["lead_trainer"] .', '. $array["training_type"] .','. $array["training_description"] .', sekuestrasi plastik, daur ulang, alternatif, sekuestrasi plastik, kompensasi plastik, plastik AES, sekuestrasi karbon. '. $array["location_full"] .'">';
 
-echo '<meta property="og:url" content="https://ecobricks.org/'. $lang .'/training.php?training_id='. $array["training_id"] .'">';
-echo '<meta property="og:title" content="'. $array["training_name"] .' |  '. $array["briks_used"] .' ecobrick">';
-echo '<meta property="og:description" content="'. $array["no_participants"] .' telah berlatih di '. $array["location_full"] .' dalam pelatihan GEA '. $array["training_type"] .'">';
-echo '<meta property="og:image" content="https://ecobricks.org/trainings/photos/training-'. $array["training_id"] .'-1.webp?v=2">';
-echo '<meta property="og:image:alt" content="Foto lokakarya pelatihan ecobrick kami">';
-echo '<meta property="og:locale" content="id_ID" >';
-echo '<meta property="og:type" content="website">';
+		echo '<meta name="keywords" content="pelatihan, lokakarya, edukasi plastik, ecobrick, '. $array["lead_trainer"] .', '. $array["training_type"] .', sekuester plastik, daur ulang, alternatif, sekuester plastik, pengimbangan plastik, aes plastik, sekuester karbon, '. $array["training_country"] .','. $array["training_location"] .'">';
 
-echo '<meta property="og:type" content="article" >
-<meta property="og:site_name" content="Ecobricks.org" >
-<meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" >
-<meta property="article:modified_time" content="'. $array["training_logged"] .'" >
-<meta property="og:image:type" content="image/webp" >
-<meta name="author" content="Aliansi Global Ecobrick" >
-<meta name="twitter:card" content="summary" >
-<meta name="twitter:label1" content="Estimasi waktu baca" >
-<meta name="twitter:data1" content="15 menit" > ';
+        echo '<meta property="og:url" content="https://ecobricks.org/'. $lang .'/training.php?training_id='. $array["training_id"] .'">' ;
+        echo '<meta property="og:title" content="'. $array["training_title"] .' |  '. $array["no_participants"] .' peserta">';
+        echo '<meta property="og:description"   content="'. $array["no_participants"] .' telah dilatih di '. $array["location_full"] .' dalam pelatihan '. $array["training_type"] .' dari Aliansi Ecobrick Global.">';
+        echo '<meta property="og:image" content="https://ecobricks.org/trainings/photos/training-'. $array["training_id"] .'-1.webp?v=2">';
+        echo '<meta property="og:image:alt"     content="Foto lokakarya pelatihan ecobrick kami">';
+        echo '<meta property="og:locale" content="id_ID" >';
+        echo '<meta property="og:type"          content="situs web">';
+ 	   
+
+        echo '<meta property="og:type" content="artikel" >
+        <meta property="og:site_name" content="Ecobricks.org" >
+        <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" >
+        <meta property="article:modified_time" content="'. $array["training_logged"] .'" >
+        <meta property="og:image:type" content="image/webp" >
+        <meta name="author" content="Aliansi Ecobrick Global" >
+        <meta name="twitter:card" content="summary" >
+        <meta name="twitter:label1" content="Perkiraan waktu membaca" >
+        <meta name="twitter:data1" content="15 menit" > ';
+
+    }
 
 } else {
-echo '<META NAME="robots" CONTENT="noindex">';
-echo '<title>Pelatihan Tidak Ditemukan | Ecobricks.org</title>';
-echo '<meta name="description" content="Tidak ada data yang ditemukan untuk nomor ID pelatihan ini. Anda mungkin memiliki tautan yang salah atau mungkin belum sepenuhnya dicatat oleh pelatih.">';
+    echo '<META NAME="robots" CONTENT="noindex">';
+    echo '<title>Tidak Ada Pelatihan Ditemukan | Ecobricks.org</title>';
+    echo '<meta name="description" content="Tidak ada data yang ditemukan untuk nomor ID pelatihan ini. Anda mungkin memiliki tautan yang salah atau mungkin belum sepenuhnya dicatat oleh pelatih."> ';
 }
 $conn->close();
 
-}
 ?>
--- meta/transition-es.php
+++ meta/transition-es.php
@@ -1,21 +1,53 @@
-<!-- Meta tags for page display and search engine listing-->
+<!-- PÁGINA DE DETALLES DE ECOBRICK EN ESPAÑOL -->
 
-<title>Transición Plástica y Ecobricks</title>
+<?php
 
-<meta name="keywords" content="transisi plastik, transisi petro-capital, move on, daur ulang, keberlanjutan, hijau, abu-abu.">
+include '../ecobricks_env.php';
 
-<meta name="description" content="Nuestro plástico es un portal fuera de nuestro viejo mundo gris y hacia el mundo verde que anhelamos ver.">
+
+$trainingId = $_GET['training_id'];
+
+$sql = "SELECT * FROM tb_trainings WHERE training_id = '" . $trainingId . "'";
+
+
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
 	
-	<!-- Facebook Open Graph Tags for social sharing-->
+    while($array = $result->fetch_assoc()) {
 
-	<meta property="og:url"           content="https://ecobricks.org/es/transition.php"/>
-	<meta property="og:type"          content="website">
- 	<meta property="og:title"         content="Transición Plástica y Ecobricks">
-  	<meta property="og:description"   content="Nuestro plástico es un portal fuera de nuestro viejo mundo gris y hacia el mundo verde que anhelamos ver."/>
-  	<meta property="og:image"         content="https://ecobricks.org/pngs/floating-dial-1000px.png"/>
-  	<meta property="og:image:width" content="1000" />
-	<meta property="og:image:height" content="1000" />
-  	<meta property="og:image:alt"     content="Plastic Transition Score Dial speeding away"/>
-	  <meta property="og:locale" content="es_ES" />
+		echo '<title>'. $array["training_title"] .' |  '. $array["no_participants"] .' participantes</title>';
 
-	
+        echo '<meta name="description" content="'. $array["no_participants"] .' han sido capacitados en '. $array["location_full"] .' en una capacitación '. $array["training_type"] .' de la Alianza Global de Ecoladrillos.">';
+
+
+		echo '<meta name="keywords" content="capacitación, taller, educación plástica, ecoladrillo, '. $array["lead_trainer"] .', '. $array["training_type"] .', secuestro de plástico, reciclaje, alternativa, secuestro de plástico, compensación de plástico, aes plástico, secuestro de carbono, '. $array["training_country"] .','. $array["training_location"] .'">';
+
+        echo '<meta property="og:url" content="https://ecobricks.org/'. $lang .'/training.php?training_id='. $array["training_id"] .'">' ;
+        echo '<meta property="og:title" content="'. $array["training_title"] .' |  '. $array["no_participants"] .' participantes">';
+        echo '<meta property="og:description"   content="'. $array["no_participants"] .' han sido capacitados en '. $array["location_full"] .' en una capacitación '. $array["training_type"] .' de la Alianza Global de Ecoladrillos.">';
+        echo '<meta property="og:image" content="https://ecobricks.org/trainings/photos/training-'. $array["training_id"] .'-1.webp?v=2">';
+        echo '<meta property="og:image:alt"     content="Una foto de nuestro taller de capacitación de ecoladrillos">';
+        echo '<meta property="og:locale" content="es_ES" >';
+        echo '<meta property="og:type"          content="sitio web">';
+ 	   
+
+        echo '<meta property="og:type" content="article" >
+        <meta property="og:site_name" content="Ecobricks.org" >
+        <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" >
+        <meta property="article:modified_time" content="'. $array["training_logged"] .'" >
+        <meta property="og:image:type" content="image/webp" >
+        <meta name="author" content="Alianza Global de Ecoladrillos" >
+        <meta name="twitter:card" content="summary" >
+        <meta name="twitter:label1" content="Tiempo de lectura estimado" >
+        <meta name="twitter:data1" content="15 minutos" > ';
+
+    }
+
+} else {
+    echo '<META NAME="robots" CONTENT="noindex">';
+    echo '<title>No se encontró la capacitación | Ecobricks.org</title>';
+    echo '<meta name="description" content="No se encontraron datos para este número de ID de capacitación. Puede que el enlace esté incorrecto o quizás aún no haya sido completamente registrado por el capacitador."> ';
+}
+$conn->close();
+
+?>
