36ecf4e71a205a09fc685a389785ccfd844e0c8b
russs <russ@earthen.io>
2024-06-14 17:47:35 +0800

    Earth up

--- en/movement.php
+++ en/movement.php
@@ -28,58 +28,57 @@
 	<div class="row">
 		<div class="main">
 
-		<div class="lead-page-paragraph">				
-			<p data-lang-id="004-lead-page-paragraph">Ecobricking is not just a technology, it’s a movement.  Hundreds of thousands of people around the world have turned to ecobricks as a way to take personal responsibility for their plastic.</p>
-		</div>
+            <div class="lead-page-paragraph">
+                <p data-lang-id="004-lead-page-paragraph">Ecobricking is not just a technology, it’s a movement.  Hundreds of thousands of people around the world have turned to ecobricks as a way to take personal responsibility for their plastic.</p>
+            </div>
 
-        <div class="page-paragraph-reg">
-			<p data-lang-id="005-first-page-paragraph">Plastic.  It’s everywhere.  And it unites us.  Taking personal responsibility for one’s waste is the embodiment of Ghandi’s famous words “Be the change you want to see in the world“.</p>
+            <div class="page-paragraph-reg">
+                <p data-lang-id="005-first-page-paragraph">Plastic.  It’s everywhere.  And it unites us.  Taking personal responsibility for one’s waste is the embodiment of Ghandi’s famous words “Be the change you want to see in the world“.</p>
 
-			<p data-lang-id="006-first-page-paragraph">United by plastic, ecobrickers around the world are now not just solving plastic for themselves, but for their neighbourhood, community, and cities.  In cases like Indonesia and the Philippines, they are fast on their way to solving plastic for their countries."</p>
+                <p data-lang-id="006-first-page-paragraph">United by plastic, ecobrickers around the world are now not just solving plastic for themselves, but for their neighbourhood, community, and cities.  In cases like Indonesia and the Philippines, they are fast on their way to solving plastic for their countries."</p>
 
-			<p data-lang-id="007-first-page-paragraph">The grassroots, people powered, Ecobrick movement is spreading around the world.  Hundreds of thousands of everyday folks, having seen through the illusions of Industrial ‘Recycling’, the failure of governments and corporations to solve solid ‘waste’, have turned to the abundance of plastic bottles for their personal solution.  Ecobricking has allowed them to take personal responsibility for their plastic, to keep it out of the failing systems and thus their local biosphere, and instead put the plastic to use locally.  The joy and pride from Ecobrickers who have made this transition is what propels the viral spread of Ecobricks around the world.</p>
+                <p data-lang-id="007-first-page-paragraph">The grassroots, people powered, Ecobrick movement is spreading around the world.  Hundreds of thousands of everyday folks, having seen through the illusions of Industrial ‘Recycling’, the failure of governments and corporations to solve solid ‘waste’, have turned to the abundance of plastic bottles for their personal solution.  Ecobricking has allowed them to take personal responsibility for their plastic, to keep it out of the failing systems and thus their local biosphere, and instead put the plastic to use locally.  The joy and pride from Ecobrickers who have made this transition is what propels the viral spread of Ecobricks around the world.</p>
 
-            <br><br>
-		<div class="links">
-			<h3 data-lang-id="008-header">Ecobrick groups around the world:</h3>
+                <br><br>
+            </div>
+		    <div class="links">
+			    <h3 data-lang-id="008-header">Ecobrick groups around the world:</h3>
 
-			<ul>
+			    <ul>
 	
 			        <li data-lang-id="009a-list-link"><a href="https://www.facebook.com/ecobricks.org">Plastic Solved Facebook Page</a> (GEA offcial page)</li>
-				<li data-lang-id="009b-list-link"><a href="https://www.instagram.com/ecobricks.plastic.solved/">Ecobricks Plastic Solved Instagram page</a> (GEA Official)</li>
-				<li data-lang-id="009c-list-link"><a href="https://www.facebook.com/groups/ecobricksUK/">UK Ecobricks Facebook Page</a></li>
-				<li data-lang-id="009d-list-link"><a href="https://www.facebook.com/Ecobricks-Indonesia-112129303527411/">Indonesia Ecobricks Facebook Page</a></li>
-				<li data-lang-id="009e-list-link"><a href="https://www.facebook.com/EcobricksUSA/">USA Ecobricks Facebook Page</a></li>
-				<li data-lang-id="009f-list-link"><a href="https://web.facebook.com/theplasticsolution/">The Plastic Solution (Philippines)</a></li>
-				<li data-lang-id="009g-list-link"><a href="https://web.facebook.com/EcoBrickExchange/">The Ecobrick Exchange (South Africa)</a></li>
-				<li data-lang-id="009h-list-link"><a href="https://web.facebook.com/groups/242330276319852/">Sri Lankan Ecobrick Movement Facebook page</a></li>
-				<li data-lang-id="009i-list-link"><a href="https://www.facebook.com/ecobricks.cavite/">Ecobricks Cavite (Philippines) Facebook page</a></li>
-				<li data-lang-id="009j-list-link"><a href="https://www.facebook.com/ecobricksg">Singapore Ecobricks Community</a></li>
-			</ul>
-	
-	    </ul>
-
+				    <li data-lang-id="009b-list-link"><a href="https://www.instagram.com/ecobricks.plastic.solved/">Ecobricks Plastic Solved Instagram page</a> (GEA Official)</li>
+                    <li data-lang-id="009c-list-link"><a href="https://www.facebook.com/groups/ecobricksUK/">UK Ecobricks Facebook Page</a></li>
+                    <li data-lang-id="009d-list-link"><a href="https://www.facebook.com/Ecobricks-Indonesia-112129303527411/">Indonesia Ecobricks Facebook Page</a></li>
+                    <li data-lang-id="009e-list-link"><a href="https://www.facebook.com/EcobricksUSA/">USA Ecobricks Facebook Page</a></li>
+                    <li data-lang-id="009f-list-link"><a href="https://web.facebook.com/theplasticsolution/">The Plastic Solution (Philippines)</a></li>
+                    <li data-lang-id="009g-list-link"><a href="https://web.facebook.com/EcoBrickExchange/">The Ecobrick Exchange (South Africa)</a></li>
+                    <li data-lang-id="009h-list-link"><a href="https://web.facebook.com/groups/242330276319852/">Sri Lankan Ecobrick Movement Facebook page</a></li>
+                    <li data-lang-id="009i-list-link"><a href="https://www.facebook.com/ecobricks.cavite/">Ecobricks Cavite (Philippines) Facebook page</a></li>
+                    <li data-lang-id="009j-list-link"><a href="https://www.facebook.com/ecobricksg">Singapore Ecobricks Community</a></li>
+                </ul>
 		    </div>
 
-			<br><br>
-            <h4 data-lang-id="xxx">How does it spread?</h4>
+            <div class="page-paragraph-reg">
+			    <br><br>
+                <h4 data-lang-id="xxx">How does it spread?</h4>
                  <p data-lang-id="060-principles-acknowledgement">The Ecobrick movement is people powered– and fundamentally non-capital.  This means that no one gets paid to make an Ecobrick, and that all of the resources that the GEA offers, from our site, to our books, to our app, were manifested through collaboration that received no financial remuneration.  The tremendous amount of work that goes into making an Ecobrick, or for that matter developing our resources, is powered by much more potent motivations than capital.</p>
                 <br><br>
 
 
-            <h4 data-lang-id="xxx">How does it spread?</h4>
+                <h4 data-lang-id="xxx">How does it spread?</h4>
                 <p data-lang-id="060-principles-acknowledgement">The Ecobrick movement is people powered– and fundamentally non-capital.  This means that no one gets paid to make an Ecobrick, and that all of the resources that the GEA offers, from our site, to our books, to our app, were manifested through collaboration that received no financial remuneration.  The tremendous amount of work that goes into making an Ecobrick, or for that matter developing our resources, is powered by much more potent motivations than capital.</p>
                 <br><br>
 
 
-            <h4 data-lang-id="xxx">How does it spread?</h4>
+                <h4 data-lang-id="xxx">How does it spread?</h4>
                 <p data-lang-id="060-principles-acknowledgement">The Ecobrick movement is people powered– and fundamentally non-capital.  This means that no one gets paid to make an Ecobrick, and that all of the resources that the GEA offers, from our site, to our books, to our app, were manifested through collaboration that received no financial remuneration.  The tremendous amount of work that goes into making an Ecobrick, or for that matter developing our resources, is powered by much more potent motivations than capital.</p>
                 <br><br>
 
-            <p><a class="action-btn" href="about.php" data-lang-id="062-about-ayyew-action">🍃 About the GEA</a></p>
-            <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="063-learn-indigenous-wisdom">Learn about the Global Ecobrick Alliance.</p>
+                <p><a class="action-btn" href="about.php" data-lang-id="062-about-ayyew-action">🍃 About the GEA</a></p>
+                <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="063-learn-indigenous-wisdom">Learn about the Global Ecobrick Alliance.</p>
             </div>
-
+                <br><br><br>
 		</DIV>
 		
 
@@ -87,19 +86,16 @@
 
             <?php include 'side-modules/transcaste-principle.php';?>
 
-		<?php include 'side-modules/good-use.php';?>
-
-
+		    <?php include 'side-modules/good-use.php';?>
 
 			<?php include 'side-modules/signup-now.php';?>
 
 		
 
                 <br>
-			</div>   
-		</div>
+        </div>
+    </div>
 
-	</div>
 </div>
 
 
