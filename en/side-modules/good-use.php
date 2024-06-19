85ff783ddbe7a990083f6dbf7f6f4f3315d5d0ca
russs <russ@earthen.io>
2024-06-19 20:53:09 +0800

    refactor?

--- en/side-modules/good-use.php
+++ en/side-modules/good-use.php
@@ -2,6 +2,8 @@
             <img src="../webp/earth-tree-400px.webp" width="250" style="full-width-image" loading="lazy" alt="eco brik and earth building can make circular benches with trees planted in the middle">
 				<br><h4>Good Use</h4>
 				<h5>Building with ecobricks puts plastic to good use in the short-term, while securing it out of both the biosphere and industry for the long-term.</h5><br>
-				<button type=button class="module-btn" onclick="location.href='what'" aria-label="Learn more" title="Learn more about ecobrick basics">Learn More</button>
+
+                <a class="module-btn" href="about.php">Learn More</a>
+
                 <br>
 			</div>  
