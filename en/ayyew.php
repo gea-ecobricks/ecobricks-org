cf1cd4810e7fc0d08c16aa47ac8decfc512d8cce
russs <russ@earthen.io>
2024-06-21 17:29:27 +0800

    russ merge 2

--- en/ayyew.php
+++ en/ayyew.php
@@ -4,7 +4,7 @@
 <HEAD>
 <META charset="UTF-8">
 <?php $lang='en';?>
-<?php $version='2.05';?>
+<?php $version='2.06';?>
 <?php $page='ayyew';?>
 
 <title>Ayyew | An ancient ecological ethos for our modern moment</title>
@@ -35,76 +35,64 @@
                 <p data-lang-id="004-lead-page-paragraph">The Asian ecobrick movement emerged from the land of the Igorot people  and is deeply inspired by their ancestral ecological ethos.</p>
             </div>
 
-            <div class="page-paragraph">
-
-            <p data-lang-id="005-first-page-paragraph">The Igorots, one of the few unconquered indigenous people of the South East Asia, have thrived for centuries in the remote mountains of Northern Luzon in the West Philippines Sea. While Spanish, Japanese and Americans colonized the remainder of the Philippines archipelago, they struggled in vain to dominate the various Igorot tribes that lived in the fertile valleys of the mountainous region.  However, lone explorers and anthropologists who reached the region were made welcome.  They documented a tapestry of fierce cultures that spun in profound sync with the ecological cycles of the region. To this day, the region is a hot bed of resistance to attempts to industrial projects that would tame ecological forces of the land.<p>
-
-<!--                <p data-lang-id="006-second-page-paragraph">While other cultures prided themselves on their paintings and sculpture, arms and architecture, the Igorots had their own art.  Like many other indigenous peoples around the world, the concept of waste does not exist in their language— and consequently in the world around them. Instead, their culture was guided by a virtue and concept known as <i>ayyew’</i></p>.-->
-
-
-
-
-		<!--PAGE CONTENT--->
-		
-
-
-
-            <br><br>
-                <h4 data-lang-id="058-ayyew-title">An ancient ecological ethos for our modern moment</h4>
-
-
-                <p>All aspects of Igorot life and culture are guided by the virtue of <i>ayyew</i>. Men, women, households and communities are admired and respected to the degree in which they embody the principle. Ayyew means to not just to be in sync with a cycle, but to tend to its spin.</p>
-
-                <p data-lang-id="059-regenerative-definition">Never  dominated by Spanish, Japanese or American colonialists, their fierce and wise ‘ayyew’ culture directly inspires and guides the ecobrick movement sweeping the globe. The ayyew concept guides the principles of the ecobrick movement. We feel it fills a pressing linguistic gap and helps us articulate our way forward.</p>
-
-<!--                <div class="row">-->
-<!--                    <div class="main2">-->
-<!--                        <p data-lang-id="059-regenerative-definition">Never  dominated by Spanish, Japanese or American colonialists, their fierce and wise ‘ayyew’ culture directly inspires and guides the ecobrick movement sweeping the globe. The ayyew concept guides the principles of the ecobrick movement. We feel it fills a pressing linguistic gap and helps us articulate our way forward.</p>-->
-<!--                    </div>-->
+<!--            <div class="page-paragraph">-->
 <!---->
-<!--                    <div class="side2" data-lang-id="078-side2-comparison">-->
-<!--                        <br><img src="../photos/maligcong-rice-terraces-300x300.jpg" width="300" style="width:100%" alt="green igorot rice terraces" loading="lazy" data-lang-id="079-sustaining-vs-regenerative-alt">-->
-<!--                        <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="080-ecobricking-thousand-years">Maligcong Rice Terraces – Bontoc, Philippines</p>-->
-<!--                    </div>-->
-<!--                </div>-->
+<!--            <p data-lang-id="005-first-page-paragraph">The Igorots, one of the few unconquered indigenous people of the South East Asia, have thrived for centuries in the remote mountains of Northern Luzon in the West Philippines Sea. While Spanish, Japanese and Americans colonized the remainder of the Philippines archipelago, they struggled in vain to dominate the various Igorot tribes that lived in the fertile valleys of the mountainous region.  However, lone explorers and anthropologists who reached the region were made welcome.  They documented a tapestry of fierce cultures that spun in profound sync with the ecological cycles of the region. To this day, the region is a hot bed of resistance to attempts to industrial projects that would tame ecological forces of the land.<p>-->
+<!---->
+<!-- <p data-lang-id="006-second-page-paragraph">While other cultures prided themselves on their paintings and sculpture, arms and architecture, the Igorots had their own art.  Like many other indigenous peoples around the world, the concept of waste does not exist in their language— and consequently in the world around them. Instead, their culture was guided by a virtue and concept known as <i>ayyew’</i></p>.-->
+<!---->
+<!---->
+<!---->
+<!---->
+<!--		-->
+<!---->
+<!---->
+<!---->
+<!--            <br><br>-->
+<!--                <h4 data-lang-id="058-ayyew-title">An ancient ecological ethos for our modern moment</h4>-->
+<!---->
+<!---->
+<!--                <p>All aspects of Igorot life and culture are guided by the virtue of <i>ayyew</i>. Men, women, households and communities are admired and respected to the degree in which they embody the principle. Ayyew means to not just to be in sync with a cycle, but to tend to its spin.</p>-->
+<!---->
+<!--                <p data-lang-id="059-regenerative-definition">Never  dominated by Spanish, Japanese or American colonialists, their fierce and wise ‘ayyew’ culture directly inspires and guides the ecobrick movement sweeping the globe. The ayyew concept guides the principles of the ecobrick movement. We feel it fills a pressing linguistic gap and helps us articulate our way forward.</p>-->
+<!--                -->
+<!---->
+<!--                <p data-lang-id="059-regenerative-definition">Phenomena such as ‘pollution’, ‘poverty’ and ‘exploitation’ also did not exist in their world prior to the arrival of European explorers. Never  dominated by Spanish, Japanese or American colonialists, their fierce and wise ‘Ayyew’ culture directly inspires and guides the ecobrick movement sweeping the globe. We consciously adapt the term to summarize and qualify the principles of the ecobrick movement. We feel it fills a pressing linguistic gap and helps us articulate our way forward.</p>-->
+<!---->
+<!--                <p data-lang-id="060-chief-virtue-description">As one of the chief virtues of the Igorot people, the concept of Ayyew guides the Igorot way of life.   In the Igorot culture, Ayyew ranked just as high as other virtues such as Courage, Honor or Loyalty.  When someone is ayyew, it means that they are striving towards ever-increasing harmony with the cycles of life.   A farmer who turns a barren hillside into a  fertile field is being ayyew.  A gardener who transforms a fertile field into a biodiverse food forest is being ayyew.</p>-->
+<!---->
+<!--                <p data-lang-id="061-daily-igorot-description">An example of ayyew in daily Igorot life is dealing with food left-overs.  Composting food leftovers to grow a garden would be more ayyew than just throwing it away. However, even more ayyew would be to feed the food leftovers to the pigs, who would both grow fat and provide even richer fertilizer for the garden.</p>-->
+<!---->
+<!--                <p data-lang-id="062-direct-equivalent-description">The ayyew term has no direct equivalent in Latin languages. Essentially it is the virtue of striving to be in ever more harmony with the surrounding ecological cycles.  Ayyew is not a state of being, but a process of being.  However, it is a directional process, one that leads to ever more abundance from ever more sync with the surrounding cycles of life.</p>-->
+<!---->
+<!--                <p data-lang-id="063-good-thing-description">Ayyew is a virtue or a “good thing” for Igorots, something that is good for individuals, households and community to strive for. In the same way as individual voices in a choir merge to attain otherwise impossible harmonies, their lives are integrated with the cycles of other species to achieve otherwise impossible health and abundance.</p>-->
+<!---->
+<!--                <p data-lang-id="064-non-judgemental-description">Because Ayyew is not a state but rather a process, it is non-judgemental of one’s present ecological state.  In this way, an industrial farmer with a giant mono-crop could be also be Ayyew (assuming they’ve shifted from striving from profit, to striving for ecological harmony.)</p>-->
+<!---->
+<!--                <p data-lang-id="065-perspective-plastic-description">Likewise, from an Ayyew perspective plastic is not bad in and of itself.   It is a material with certain properties and characteristics that can be put to use in different ways.</p>-->
+<!---->
+<!--                <p data-lang-id="066-language-description">Interestingly enough, in the Igorot language, there was never a word for “waste” or “trash”*.  From the ayyew perspective, nothing is innately useless.  Instead, every material presents an opportunity that can be embraced– or not.</p>-->
+<!---->
+<!--                <p data-lang-id="067-concept-description">The concept of ayyew thus has great relevance for us in the ecological downturn of the 21st century as we strive to solve ecological challenges.  Moving on from linear, black and white judgements, we can shift to sinking with cycles.</p>-->
+<!---->
+<!--                <p data-lang-id="068-modern-manifestation-description">Ecobricking is a modern manifestation of the Ayyew concept with regard to our personal plastic.</p>-->
+<!---->
+<!--                <p data-lang-id="069-ecobricking-one-description">For example, ecobricking one’s plastic is more Ayyew than recycling it: ecobricking puts the plastic to immediate local use whereas industrial recycling, involving transportation and far factories, is a longer more energy consuming process.</p>-->
+<!---->
+<!--                <p data-lang-id="070-ecobrick-alliance-description">The Global Ecobrick Alliance’s development of ecobrick applications further reflects our aspiration to the virtue of Ayyew. Our first innovation for building modules with ecobricks was to use silicone. However, silicone requires some (minimal) capital, and relies on non-local materials (the silicone and gun) and requires a specialized skill (using the caulking gun).</p>-->
+<!---->
+<!--                <p data-lang-id="071-motorcycle-description">The latest innovation for using ecobricks uses old motorcycle inner tubes– which syncs with the local cycle of consumed and disposed tires, using them to everyone’s advantage. This is what the Igorots would call ‘nagagut‘ (“being industrious”, or “reflecting of Ayyew”).</p>-->
+<!---->
+<!--                <p data-lang-id="072-strive-and-sync-description">Of course, it's possible to strive and sync even better with the surrounding ecological cycles.  An ayyew ecobricker strives to move from modules to gardens to <a href = "earth.php">earth and ecobrick</a> green space structures that <a href = "sequest.php">sequester plastic</a> and CO2.-->
+<!---->
+<!--                <p data-lang-id="073-deep-and-powerful-description">Inspired by this deep and powerful Igorot concept, the Global Ecobrick Alliance has developed our principles, techniques and applications around the concept of Ayyew. Ayyew fits in closely with regenerative philosophy and guided our development of the <a href = "https://www.ecobricks.org/principles">regenerative guiding principles</a>. that underlie the work of the <a href = "about.php">Global Ecobrick Alliance</a> and the <a href = "/movement">global ecobrick movement</a>.</p>-->
+<!--                <br><br>-->
+<!--    -->
+<!--                <p><a class="action-btn" href="principles.php" data-lang-id="076-principles-button">🍃 Our GEA Principles</a></p>-->
+<!--            <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="077-learn-indigenous-wisdom">The full ayyew inspired principles of the ecobrick movement</p>-->
+<!---->
+<!--            </div>-->
 
-
-                <p data-lang-id="059-regenerative-definition">Phenomena such as ‘pollution’, ‘poverty’ and ‘exploitation’ also did not exist in their world prior to the arrival of European explorers. Never  dominated by Spanish, Japanese or American colonialists, their fierce and wise ‘Ayyew’ culture directly inspires and guides the ecobrick movement sweeping the globe. We consciously adapt the term to summarize and qualify the principles of the ecobrick movement. We feel it fills a pressing linguistic gap and helps us articulate our way forward.</p>
-
-                <p data-lang-id="060-chief-virtue-description">As one of the chief virtues of the Igorot people, the concept of Ayyew guides the Igorot way of life.   In the Igorot culture, Ayyew ranked just as high as other virtues such as Courage, Honor or Loyalty.  When someone is ayyew, it means that they are striving towards ever-increasing harmony with the cycles of life.   A farmer who turns a barren hillside into a  fertile field is being ayyew.  A gardener who transforms a fertile field into a biodiverse food forest is being ayyew.</p>
-
-                <p data-lang-id="061-daily-igorot-description">An example of ayyew in daily Igorot life is dealing with food left-overs.  Composting food leftovers to grow a garden would be more ayyew than just throwing it away. However, even more ayyew would be to feed the food leftovers to the pigs, who would both grow fat and provide even richer fertilizer for the garden.</p>
-
-                <p data-lang-id="062-direct-equivalent-description">The ayyew term has no direct equivalent in Latin languages. Essentially it is the virtue of striving to be in ever more harmony with the surrounding ecological cycles.  Ayyew is not a state of being, but a process of being.  However, it is a directional process, one that leads to ever more abundance from ever more sync with the surrounding cycles of life.</p>
-
-                <p data-lang-id="063-good-thing-description">Ayyew is a virtue or a “good thing” for Igorots, something that is good for individuals, households and community to strive for. In the same way as individual voices in a choir merge to attain otherwise impossible harmonies, their lives are integrated with the cycles of other species to achieve otherwise impossible health and abundance.</p>
-
-                <p data-lang-id="064-non-judgemental-description">Because Ayyew is not a state but rather a process, it is non-judgemental of one’s present ecological state.  In this way, an industrial farmer with a giant mono-crop could be also be Ayyew (assuming they’ve shifted from striving from profit, to striving for ecological harmony.)</p>
-
-                <p data-lang-id="065-perspective-plastic-description">Likewise, from an Ayyew perspective plastic is not bad in and of itself.   It is a material with certain properties and characteristics that can be put to use in different ways.</p>
-
-                <p data-lang-id="066-language-description">Interestingly enough, in the Igorot language, there was never a word for “waste” or “trash”*.  From the ayyew perspective, nothing is innately useless.  Instead, every material presents an opportunity that can be embraced– or not.</p>
-
-                <p data-lang-id="067-concept-description">The concept of ayyew thus has great relevance for us in the ecological downturn of the 21st century as we strive to solve ecological challenges.  Moving on from linear, black and white judgements, we can shift to sinking with cycles.</p>
-
-                <p data-lang-id="068-modern-manifestation-description">Ecobricking is a modern manifestation of the Ayyew concept with regard to our personal plastic.</p>
-
-                <p data-lang-id="069-ecobricking-one-description">For example, ecobricking one’s plastic is more Ayyew than recycling it: ecobricking puts the plastic to immediate local use whereas industrial recycling, involving transportation and far factories, is a longer more energy consuming process.</p>
-
-                <p data-lang-id="070-ecobrick-alliance-description">The Global Ecobrick Alliance’s development of ecobrick applications further reflects our aspiration to the virtue of Ayyew. Our first innovation for building modules with ecobricks was to use silicone. However, silicone requires some (minimal) capital, and relies on non-local materials (the silicone and gun) and requires a specialized skill (using the caulking gun).</p>
-
-                <p data-lang-id="071-motorcycle-description">The latest innovation for using ecobricks uses old motorcycle inner tubes– which syncs with the local cycle of consumed and disposed tires, using them to everyone’s advantage. This is what the Igorots would call ‘nagagut‘ (“being industrious”, or “reflecting of Ayyew”).</p>
-
-                <p data-lang-id="072-strive-and-sync-description">Of course, it's possible to strive and sync even better with the surrounding ecological cycles.  An ayyew ecobricker strives to move from modules to gardens to <a href = "earth.php">earth and ecobrick</a> green space structures that <a href = "sequest.php">sequester plastic</a> and CO2.
-
-                <p data-lang-id="073-deep-and-powerful-description">Inspired by this deep and powerful Igorot concept, the Global Ecobrick Alliance has developed our principles, techniques and applications around the concept of Ayyew. Ayyew fits in closely with regenerative philosophy and guided our development of the <a href = "https://www.ecobricks.org/principles">regenerative guiding principles</a>. that underlie the work of the <a href = "about.php">Global Ecobrick Alliance</a> and the <a href = "/movement">global ecobrick movement</a>.</p>
-                <br><br>
-    
-                <p><a class="action-btn" href="principles.php" data-lang-id="076-principles-button">🍃 Our GEA Principles</a></p>
-            <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="077-learn-indigenous-wisdom">The full ayyew inspired principles of the ecobrick movement</p>
-
-            </div>
-        </div>
 
 		</DIV>  <!--main close-->
 		
