07c5814a502df4c7d265a7b2c87416e3664845ea
JosephOFlanagan <joseph.oflanagan@yahoo.co.uk>
2024-06-26 13:53:04 +0100

    Update earth-methods.php
    Added first content batch.

--- en/earth-methods.php
+++ en/earth-methods.php
@@ -8,7 +8,7 @@
  
 <title>Earth & Ecobrick Building Methods</title>
 
-<?php require_once ("../includes/earth-inc.php");?>
+<?php require_once ("../includes/earth-methods-inc.php");?>
 
     	 
 <div class="splash-content-block">
@@ -30,7 +30,19 @@
 
 <div class="row">
     <div class="main">
-      <p> </p>
+            <div class="lead-page-paragraph">
+                <p data-lang-id="004-lead-page-paragraph">se your ecobricks to launch your regenerative building journey!  The Global Ecobrick Alliance has developed best-practices  for building with earth and ecobricks.  Our methods are designed to ensure the long-term sequestration of plastic in solid and safe constructions that can be built by anyone anywhere.</p>
+            </div>
+            <div class="page-paragraph-reg">
+		<p data-lang-id="005-first-page-paragraph">The Global Ecobrick Alliance recommends the use of earth and ecobricks to build non-structural, non-load bearing green spaces such as benches, planters, and food forest play parks.  By using simple methods to build small-scale inspiring constructions that fully embody regenerative principles, we enable others to copy our constructions.  As more and more ecobrickers lead and assist in earth building, more and more plastic is sequestered.  And as more and more people get their hands dirty building with earth, more and more we reconnect to the Earth.  Its all part of our individual and collective journeys of plastic transition.</p>
+		<p data-lang-id="006-first-page-paragraph">Our recommended GEA earth building methods are designed to be both adaptable and scalable. Ecobrick and earth building is fundamentally about working with what resources are locally available– so we encourage you to adapt accordingly to your local context. We encourage you to start by learning from ancestral earth building methods in your region and starting small.  By starting small, you and your community can master methods and principles that apply equally to large projects as they do small. Once we have mastered small non-structural applications, we fully envisions homes and structures built with earth and ecobricks.</p>
+	    </div>
+	    <div class="caution-box-reg>
+		<p data-lang-id="007-header">Header</p>
+		<p data-lang-id="008-first-page-paragraph">Earth & Ecobrick building is fundamentally distinct from conventional commercial building.</p>
+		<p data-lang-id="009-first-page-paragraph">Today’s conventional construction is driven by capital and relies on industrial methods and materials with additive plastic and CO2 impacts (greying).  GEA recommended building methods are based on regenerative principles to ensure that their net CO2 and plastic impacts are subtractive (greening).</p>
+		<p data-lang-id="010-link-learn-more"><a href = "https://www.ecobricks.org/new-earth">Learn more about Ecobrick & Earth Building Principles</a></p>
+	    </div>
 </div>
 
 
