d0aba96f32dbfc14405bb21ab15b4914310fbf44
JosephOFlanagan <joseph.oflanagan@yahoo.co.uk>
2024-06-26 13:34:46 +0100

    Create earth-methods.php
    Initial page creation, lacks content.

---
+++ en/earth-methods.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<HTML lang="en">
+<HEAD>
+<META charset="UTF-8">
+<?php $lang='en';?>
+<?php $version='2.04';?>
+<?php $page='earth';?>
+ 
+<title>Earth & Ecobrick Building Methods</title>
+
+<?php require_once ("../includes/earth-inc.php");?>
+
+    	 
+<div class="splash-content-block">
+	<div class="splash-box">
+		<div class="splash-heading" data-lang-id="001-splash-title">Ecobrick & Earth Building Methods</div>
+	    <div class="splash-sub" data-lang-id="002-splash-subtitle">Step by step guidelines on building strong, circular and regenerative earth & ecobrick structures.</div>
+	</div> 
+	<div class="splash-image" data-lang-id="003-splash-alt"><img src="../svgs/tree-bench.svg" style="width: 85%" alt="Ecobrick bench and tree">
+    </div>	
+</div>
+<div id="splash-bar"></div>
+
+
+
+<!-- PAGE CONTENT-->
+
+
+<div id="main-content">
+
+<div class="row">
+    <div class="main">
+      <p> </p>
+</div>
+
+
+
+
+
+
+    
+	
+
+		<div class="side">
+	
+            <?php require_once ("side-modules/spiral-design.php");?>
+
+            <?php require_once ("side-modules/sequest-module.php");?>
+                
+            <?php require_once ("side-modules/good-use.php");?>
+
+            <?php require_once ("side-modules/earthen-ethics.php");?>
+
+            <?php require_once ("side-modules/transcaste-principle.php");?>
+
+            <?php require_once ("side-modules/plastic-carbon.php");?>
+
+            
+		</div>
+
+	</div>
+
+</div>
+
+
+
+	<!--FOOTER STARTS HERE-->
+
+	<?php require_once ("../footer-2024.php");?>
+
+
+<!-- CUSTOM PAGE SCRIPTS-->
+<script>
+
+
+</script>
+
+<!-- This script is for pages that use the accordion content system-->
+<script src="../accordion-scripts.js?v=2" defer></script>
+
+</div>
+</body>
+</html>
