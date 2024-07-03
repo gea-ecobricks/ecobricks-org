5959cc347083506acac1bb08e693b35215a40c3b
JosephOFlanagan <joseph.oflanagan@yahoo.co.uk>
2024-07-03 13:39:50 +0100

    Update earth-methods-inc.php

--- includes/earth-methods-inc.php
+++ includes/earth-methods-inc.php
@@ -9,7 +9,7 @@
 
 <!-- This loads the page's meta tags:  Be sure the page name is in place in English-->
 
-<?php require_once ("../meta/earth-$lang.php");?>
+<?php require_once ("../meta/earth-methods-$lang.php");?>
 
 
 
