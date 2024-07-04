dfbca8eb92ae3e0e6b9f83998e1d29cf5b3fae27
russs <russ@earthen.io>
2024-07-04 17:46:10 +0800

    valid score

--- scripts/process_ecobrick.php
+++ scripts/process_ecobrick.php
@@ -161,7 +161,8 @@ if (isset($data['records']) && count($data['records']) > 0) {
             $validator_2 = $record['field_662_raw'][0]['identifier'] ?? '';
             $validator_3 = $record['field_663_raw'][0]['identifier'] ?? '';
 //            $validation_score_avg = $record['field_568_raw'] ?? 0;
-            $validation_score_avg = isset($record['field_568_raw']) ? $record['field_568_raw'] : round($record['field_1435_raw'] ?? 0);
+            $validation_score_avg = ($record['field_568_raw'] !== '0.00' && isset($record['field_568_raw'])) ? $record['field_568_raw'] : round($record['field_1435_raw'] ?? 0);
+
 
             $knack_record_id = $record['id'] ?? '';
             $final_validation_score = $record['field_1435_raw'] ?? 0;
