c6db067e635f54f7c77a79775161ce1dfad8b8b5
russs <russ@earthen.io>
2024-07-06 10:47:46 +0800

    avg

--- scripts/process_ecobrick.php
+++ scripts/process_ecobrick.php
@@ -53,6 +53,56 @@
 </script>
 
 <div id="ecobrick-being-processed">
+
+
+    <div id="ecobricks-processed-gallery">
+
+
+
+        <?php
+
+        ini_set('display_errors', 1);
+        error_reporting(E_ALL);
+
+        include '../ecobricks_env.php';
+        $conn->set_charset("utf8mb4");
+
+        // SQL query to fetch the latest 100 authenticated ecobricks
+        $query = "SELECT serial_no, ecobrick_thumb_photo_url FROM tb_ecobricks 
+          WHERE status = 'authenticated' 
+          ORDER BY date_published_ts DESC 
+          LIMIT 100";
+
+        $result = $conn->query($query);
+        ?>
+
+
+        <body>
+
+        <h1>Latest Ecobrick Imports</h1>
+        <div class="gallery">
+            <?php
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $serial_no = $row['serial_no'];
+                    $thumb_url = $row['ecobrick_thumb_photo_url'];
+                    echo "<a href='https://ecobricks.org/en/details-ecobrick-page.php?serial_no=$serial_no' target='_blank'>
+                    <img src='$thumb_url' alt='Ecobrick $serial_no'>
+                  </a>";
+                }
+            } else {
+                echo "<p>No ecobricks found.</p>";
+            }
+            ?>
+        </div>
+
+
+
+        <?php
+        $conn->close();
+        ?>
+    </div>
+
     <?php
 
     // PART 1 of the code
@@ -456,54 +506,6 @@ if (!empty($error_message)) {
 
 </div>
 
-<div id="ecobricks-processed-gallery">
-
-
-
-    <?php
-
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
-
-    include '../ecobricks_env.php';
-    $conn->set_charset("utf8mb4");
-
-    // SQL query to fetch the latest 100 authenticated ecobricks
-    $query = "SELECT serial_no, ecobrick_thumb_photo_url FROM tb_ecobricks 
-          WHERE status = 'authenticated' 
-          ORDER BY date_published_ts DESC 
-          LIMIT 100";
-
-    $result = $conn->query($query);
-    ?>
-
-
-    <body>
-
-    <h1>Latest Ecobrick Imports</h1>
-    <div class="gallery">
-        <?php
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $serial_no = $row['serial_no'];
-                $thumb_url = $row['ecobrick_thumb_photo_url'];
-                echo "<a href='https://ecobricks.org/en/details-ecobrick-page.php?serial_no=$serial_no' target='_blank'>
-                    <img src='$thumb_url' alt='Ecobrick $serial_no'>
-                  </a>";
-            }
-        } else {
-            echo "<p>No ecobricks found.</p>";
-        }
-        ?>
-    </div>
-
-    </body>
-    </html>
-
-    <?php
-    $conn->close();
-    ?>
-</div>
 
 
 
