2b37ba2f93b249cda0b3f854a3e64bb51e67378b
Russell Maier <russ@earthen.io>
2024-07-15 18:08:01 +0800

    2.45

--- en/upload-success.php
+++ en/upload-success.php
@@ -1,3 +1,4 @@
+
 <?php
 
 ini_set('display_errors', 1);
@@ -25,72 +26,58 @@ if (isset($_GET['ecobrick_unique_id'])) {
 
 ?>
 
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Upload Success</title>
-    <style>
-        .photo-container {
-            margin: 20px 0;
-            text-align: center;
-        }
-        .photo-container img {
-            max-width: 150px;
-            margin: 10px;
-        }
-        .confirm-button {
-            display: inline-block;
-            padding: 10px 20px;
-            background: #4CAF50;
-            color: white;
-            text-decoration: none;
-            border-radius: 5px;
-        }
-    </style>
-</head>
-<body>
-<div class="splash-content-block"></div>
-<div id="splash-bar"></div>
-<div id="photos-submission-box" style="display:flex;flex-flow:column;">
-    <div class="form-container" id="upload-photo-form">
-        <div class="step-graphic" style="width:fit-content;margin:auto;">
-            <img src="../svgs/step3-log-project.svg" style="height:30px;margin-bottom:25px;" alt="Upload Successful!">
+<HTML lang="en">
+<HEAD>
+    <META charset="UTF-8">
+    <?php $lang='en';?>
+    <?php $version='2.42';?>
+    <?php $page='log';?>
+
+    <?php require_once ("../includes/log-inc.php");?>
+
+    <div class="splash-content-block"></div>
+    <div id="splash-bar"></div>
+    <div id="photos-submission-box" style="display:flex;flex-flow:column;">
+        <div class="form-container" id="upload-photo-form">
+            <div class="step-graphic" style="width:fit-content;margin:auto;">
+                <img src="../svgs/step3-log-project.svg" style="height:30px;margin-bottom:25px;" alt="Upload Successful!">
+            </div>
+            <div class="splash-form-content-block" style="text-align:center; display:flex;flex-flow:column;">
+                <div class="splash-image-2" data-lang-id="003-weigh-plastic-image-alt">
+                    <img src="../svgs/Happy-turtle-dolphin-opti.svg" style="width:55%; margin:auto" alt="The Earth Thanks You">
+                </div>
+                <div>
+                    <h2 data-lang-id="001-form-title">Your Ecobrick <?php echo $serial_no; ?> has been successfully logged!</h2>
+                </div>
+                <div id="upload-success-message">
+                    <?php if ($ecobrick_full_photo_url): ?>
+                        <div class="photo-container">
+                            <img src="<?php echo $ecobrick_thumb_photo_url; ?>" alt="Basic Ecobrick Photo">
+                            <p>Basic Ecobrick Photo</p>
+                        </div>
+                    <?php endif; ?>
+                    <?php if ($selfie_photo_url): ?>
+                        <div class="photo-container">
+                            <img src="<?php echo $selfie_thumb_url; ?>" alt="Ecobrick Selfie Photo">
+                            <p>Ecobrick Selfie Photo</p>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <p data-lang-id="002-form-description2" style="text-align: center;">The Earth thanks You for your plastic sequestration and plastic transition!</p>
+                <a class="confirm-button" href="ecobrick.php?ecobrick_unique_id=<?php echo $ecobrick_unique_id; ?>" data-lang-id="013-view-ecobrick-post">🎉 View Ecobrick Post</a>
+                <form id="deleteForm" action="" method="POST">
+                    <input type="hidden" name="ecobrick_unique_id" value="<?php echo htmlspecialchars($ecobrick_unique_id); ?>">
+                    <input type="hidden" name="action" value="delete_ecobrick">
+                    <a class="confirm-button" style="background:red; cursor:pointer;" id="deleteButton" data-lang-id="014-delete-ecobrick">❌ Delete Ecobrick</a>
+                </form>
+                <br>
+            </div>
         </div>
-        <div class="splash-form-content-block" style="text-align:center; display:flex;flex-flow:column;">
-            <div class="splash-image-2" data-lang-id="003-weigh-plastic-image-alt">
-                <img src="../svgs/Happy-turtle-dolphin-opti.svg" style="width:55%; margin:auto" alt="The Earth Thanks You">
-            </div>
-            <div>
-                <h2 data-lang-id="001-form-title">Your Ecobrick <?php echo $serial_no; ?> has been successfully logged!</h2>
-            </div>
-            <div id="upload-success-message">
-                <?php if ($ecobrick_full_photo_url): ?>
-                    <div class="photo-container">
-                        <img src="<?php echo $ecobrick_thumb_photo_url; ?>" alt="Basic Ecobrick Photo">
-                        <p>Basic Ecobrick Photo</p>
-                    </div>
-                <?php endif; ?>
-                <?php if ($selfie_photo_url): ?>
-                    <div class="photo-container">
-                        <img src="<?php echo $selfie_thumb_url; ?>" alt="Ecobrick Selfie Photo">
-                        <p>Ecobrick Selfie Photo</p>
-                    </div>
-                <?php endif; ?>
-            </div>
-            <p data-lang-id="002-form-description2" style="text-align: center;">The Earth thanks You for your plastic sequestration and plastic transition!</p>
-            <a class="confirm-button" href="ecobrick.php?ecobrick_unique_id=<?php echo $ecobrick_unique_id; ?>" data-lang-id="013-view-ecobrick-post">🎉 View Ecobrick Post</a>
-            <form id="deleteForm" action="" method="POST">
-                <input type="hidden" name="ecobrick_unique_id" value="<?php echo htmlspecialchars($ecobrick_unique_id); ?>">
-                <input type="hidden" name="action" value="delete_ecobrick">
-                <a class="confirm-button" style="background:red; cursor:pointer;" id="deleteButton" data-lang-id="014-delete-ecobrick">❌ Delete Ecobrick</a>
-            </form>
-            <br>
-        </div>
+        <br><br>
     </div>
-    <br><br>
-</div>
-<!--FOOTER STARTS HERE-->
-<?php require_once ("../footer-2024.php");?>
-</body>
+    <!--FOOTER STARTS HERE-->
+    <?php require_once ("../footer-2024.php");?>
+    </body>
 </html>
