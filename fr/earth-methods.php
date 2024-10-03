f2f9cd3d8e83aa340ebfb624d04ea92e6f106e94
Russell Maier <russ@earthen.io>
2024-10-03 10:58:04 -0400

    redo

--- fr/earth-methods.php
+++ fr/earth-methods.php
@@ -3,7 +3,7 @@
 <HEAD>
 <META charset="UTF-8">
 <?php $lang='fr';?>
-<?php $version='2.24';?>
+<?php $version='2.25';?>
 <?php $page='earth-methods';?>
  
 <title>Earth & Ecobrick Building Methods</title>
