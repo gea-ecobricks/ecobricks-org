da10a9aa5b275eb11614f690b3da3f13da52c3fb
Russell Maier <russ@earthen.io>
2024-10-11 17:02:12 -0400

    earth methods

--- meta/welcome-fr.php
+++ meta/welcome-fr.php
@@ -1,5 +1,7 @@
 
-<meta name="keywords" content="ecobriques, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-briques, briques, eco brique, brik, briks, plastique, gestion du plastique, séquestration du carbone, éthique de la terre, plastique, alternatives de recyclage, incinération, compensations plastiques, solutions plastiques, upcycling, séquestration du plastique, alternative de recyclage, alternative d'incinération, prévention de la pollution plastique, pollution plastique, solution plastique régénérative, déchets, gestion des déchets , déchets plastiques, non capitaux, zéro déchet, net zéro, séquestration du plastique"> 
+<title>Bienvenue a Ecobricks.org</title>
+
+<meta name="keywords" content="ecobriques, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-briques, briques, eco brique, brik, briks, plastique, gestion du plastique, séquestration du carbone, éthique de la terre, plastique, alternatives de recyclage, incinération, compensations plastiques, solutions plastiques, upcycling, séquestration du plastique, alternative de recyclage, alternative d'incinération, prévention de la pollution plastique, pollution plastique, solution plastique régénérative, déchets, gestion des déchets , déchets plastiques, non capitaux, zéro déchet, net zéro, séquestration du plastique">
 
 <meta name="description" content="Du plastique problématique aux solutions régénératives">
 	
