72251fc981d7ebb2f0b9fc089c330ca7a9aa1a10
Russell Maier <russ@earthen.io>
2024-10-11 11:52:06 -0400

    earth methods

---
+++ includes/gobrik-inc.php
@@ -0,0 +1,51 @@
+
+
+<!--Image files to preload that are unique to this page-->
+
+<link rel="preload" as="image" href="../svgs/eb-blue-no-clouds.svg">
+<link rel="preload" as="image" href="../svgs/top-clouds-mobile2.svg?v=2">
+<link rel="preload" as="image" href="../svgs/top-clouds-animated-desktop.svg?v=2">
+<!--This loads CSS specific to this page-->
+
+<STYLE>
+
+@media screen and (max-width: 700px) {
+	.splash-content-block {
+		background-color: #00E6FF;
+		background: url(../svgs/top-clouds-mobile2.svg?v=2) top;
+
+	}
+}
+
+
+@media screen and (min-width: 700px) {
+	.splash-content-block {
+
+        	background-color: #00E6FF;
+		background: url(../svgs/top-clouds-animated-desktop.svg?v=2) top #00E6FF;
+
+
+}
+}
+
+
+#splash-bar {
+
+	background-color: #00E6FF;
+
+    }
+
+
+    .module-btn {
+  background: var(--emblem-blue);
+}
+
+.module-btn:hover {
+  background: var(--emblem-blue-over);
+}
+
+
+	</STYLE>
+
+
+<?php require_once ("../header-2024.php");?>
