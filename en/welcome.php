ab5d8f5f54d29eb2643bada8dbd33bed5ef7efb8
Russell Maier <russ@earthen.io>
2024-10-20 10:29:37 -0400

    earth methods

--- en/welcome.php
+++ en/welcome.php
@@ -45,7 +45,7 @@ https://github/globalecobrickalliance/ecobricks.org
         <div class="gallery-flex-container">
         <?php
     // Updated SQL query to include a WHERE clause and a LIMIT
-    $sql = "SELECT * FROM tb_projects WHERE ready_to_show = 1 ORDER BY project_id DESC LIMIT 45;";
+    $sql = "SELECT * FROM tb_projects WHERE ready_to_show = 1 ORDER BY project_id DESC LIMIT 20;";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -87,7 +87,7 @@ https://github/globalecobrickalliance/ecobricks.org
     <div class="gallery-flex-container">
         <?php
         // Updated SQL query to include a WHERE clause and a LIMIT
-        $sql = "SELECT * FROM tb_trainings WHERE ready_to_show = 1 ORDER BY training_id DESC LIMIT 40;";
+        $sql = "SELECT * FROM tb_trainings WHERE ready_to_show = 1 ORDER BY training_id DESC LIMIT 20;";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
