f79406ae5d40bbda979971a7f8edfb0b288c80a8
Russell Maier <russ@earthen.io>
2024-12-06 11:58:12 +0700

    earth methods

--- scripts/ajax-ecobricks.php
+++ scripts/ajax-ecobricks.php
@@ -94,13 +94,8 @@ $columns = array(
     */
 
  
-// SQL server connection information
-$sql_details = array(
-    'user' => 'ecobricks_russs',
-    'pass' => '1ecobricks!',
-    'db'   => 'ecobricks_gobrik_msql_db',
-    'host' => 'localhost'
-);
+<?php include '../gobrikconn_env.php';?>
+
  
 
 
