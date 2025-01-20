f530d862bc9088673d85e9211a8d37bfa9405424
Russell Maier <russ@earthen.io>
2025-01-20 19:14:33 +0700

    open books version 2.0

---
+++ en/side-modules/about-gea-accounting.php
@@ -0,0 +1,9 @@
+<div class="side-module-desktop-mobile">
+	<img src="../webp/gea-logo-400px.webp" width="300" style="width:90%" alt="The Global Eco Brick Alliance" loading="lazy">
+
+    <h4>Global Ecobrick Alliance</h4>
+	<h5>As a for-Earth enterprise we account and disclose all of our finances to meet our not-for-profit and net-green mandates.</h5><br>
+
+    <a class="module-btn" href="about.php">Learn More</a>
+
+</div>
