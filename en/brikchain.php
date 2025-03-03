6e779e5c103706aa182aebf6f060bfba7d6b2355
Russell Maier <russ@earthen.io>
2025-03-03 13:13:57 +0700

    open books version 2.0

--- en/brikchain.php
+++ en/brikchain.php
@@ -61,7 +61,10 @@
 		</div>
 
 	</div>
-  <?php
+
+
+	<!--LIVE DATA-->
+	<?php
 // Include the GoBrik database connection credentials
 require_once '../gobrikconn_env.php';
 
@@ -71,10 +74,10 @@ try {
                    brick_count,
                    total_brk,
                    weight,
-                   tot_usd_exp_amt,
-                   tot_usd_rev_amt,
-                   final_aes_plastic_cost
-            FROM vw_detail_sums_by_year";
+                   tot_idr_exp_amt,
+                   tot_idr_rev_amt,
+                   final_aes_plastic_cost_idr
+            FROM vw_detail_sums_by_year_idr";
 
     $result = $gobrik_conn->query($sql);
 
@@ -97,9 +100,9 @@ try {
         $sum_ecobricks += (float)str_replace(',', '', $row['brick_count']);
         $sum_brikcoins += (float)str_replace(',', '', $row['total_brk']);
         $sum_weight += (float)str_replace(',', '', $row['weight']);
-        $sum_expenses += (float)str_replace(',', '', $row['tot_usd_exp_amt']);
-        $sum_revenue += (float)str_replace(',', '', $row['tot_usd_rev_amt']);
-        $sum_costs += (float)str_replace(',', '', $row['final_aes_plastic_cost']);
+        $sum_expenses += (float)str_replace(',', '', $row['tot_idr_exp_amt']);
+        $sum_revenue += (float)str_replace(',', '', $row['tot_idr_rev_amt']);
+        $sum_costs += (float)str_replace(',', '', $row['final_aes_plastic_cost_idr']);
         $row_count++;
     }
 
@@ -116,19 +119,19 @@ try {
                 <img src="../svgs/aes-brk-vertical.svg?v=2" style="width:95%; margin-top:20px;"><p style="font-size: 1.0em; margin-top:5px;">
             </div>
         <div id="brikchain-totals">
-            <p><span class="blink">◉  </span> ' . number_format($aes_rolling, 2) . ' &#8202;$ USD per 1 Kg of AES Plastic</p>
+            <p><span class="blink">◉  </span> ' . number_format($aes_rolling, 2) . ' &#8202; IDR per 1 Kg of AES Plastic</p>
             <ul>
-                <li>Total Ecobricks Authenticated: ' . number_format($sum_ecobricks) . '</li>
+                <li>Total ecobricks authenticated: ' . number_format($sum_ecobricks) . '</li>
                 <li>Total Brikcoins Generated: ' . number_format($sum_brikcoins) . '&#8202;ß</li>
                 <li>Total Authenticatd Sequestered Plastic: ' . number_format($sum_weight, 2) . '&#8202;kg</li>
-                <li>Total System Expenses: $' . number_format($sum_expenses, 2) . '&#8202;USD</li>
+                <li>Total System Expenses: ' . number_format($sum_expenses, 2) . '&#8202;IDR</li>
 
                 <!--
-                <li>Total AES Sales: $' . number_format($sum_revenue, 2) . '</li>
-                <li>Avg AES Price: $' . number_format($avg_cost, 2) . '</li>-->
+                <li>Total AES Sales: ' . number_format($sum_revenue, 2) . ' IDR</li>
+                <li>Avg AES Price: ' . number_format($avg_cost, 2) . ' IDR</li>-->
             </ul>
             <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="006-current-pricing">
-                The price per kg of <a href="offset.php">of AES plastic offsets</a> is a function of system authenticataed plastic and GEA system expenses.
+                The price per kg of <a href="offsets.php">of AES plastic offsets</a> is a function of system authenticataed plastic and GEA system expenses.
             </p>
         </div>
     </div>';
--- meta/gobrik-en.php
+++ meta/gobrik-en.php
@@ -2,16 +2,17 @@
 
 <title>GoBrik Project</title>
 
-<meta name="keywords" content="ecobricks, eco-bricks, eco bricks, eco briks, briks, plastic, plastic management, carbon sequestration, earthen ethics, plastic, recycling alternatives, incineration, plastic offsets, plastic solutions, upcycling, plastic sequestration, recycling alternative, incineration alternative, preventing plastic pollution, plastic pollution, regenerative plastic solution, waste, waste management, plastic waste">
+<meta name="keywords" content="gobrik app, gobrik.com, gobrik project, development team, global ecobrick alliance project, brikchain, open source">
 
-<meta name="description" content="Ecobricks are a plastic transition solution to make reusable buidling blocks of sequestered plastic.">
+<meta name="description" content="A Social Action Platform Serving the People, Serving the Planet.
+The basics of ecobricking">
 	
 	<!-- Facebook Open Graph Tags for social sharing-->
 
-	<meta property="og:url"           content="https://ecobricks.org/en/what.php">
+	<meta property="og:url"           content="https://ecobricks.org/en/gobrik.php">
 	<meta property="og:type"          content="website">
  	<meta property="og:title"         content="What is an ecobrick? | Ecobricks.org">
-  	<meta property="og:description"   content="Ecobricks are a plastic transition solution to make reusable buidling blocks of sequestered plastic.">
+  	<meta property="og:description"   content="A Social Action Platform Serving the People, Serving the Planet.">
   	<meta property="og:image"         content="https://ecobricks.org/pngs/what-page-ecobrick-banner-1024px.png">
   	<meta property="og:image:width" content="1024">
 	<meta property="og:image:height" content="1017">
@@ -23,10 +24,10 @@
   <meta property="og:type" content="article">
  <meta property="og:site_name" content="Ecobricks.org">
  <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org">
- <meta property="article:modified_time" content="2022-09-07T09:14:13+00:00">
+ <meta property="article:modified_time" content="2025-03-02T09:14:13+00:00">
  <meta property="og:image:type" content="image/png">
  <meta name="author" content="Global Ecobrick Alliance">
  <meta name="twitter:card" content="summary">
  <meta name="twitter:label1" content="Est. reading time">
- <meta name="twitter:data1" content="3 minutes"> 
+ <meta name="twitter:data1" content="10 minutes">
 	
