7489e3018ea33c024e41a67576e8b596eb608fd9
Russell Maier <russ@earthen.io>
2025-04-21 15:59:37 +0700

    open books version 2.0

--- testing/side-modules/earthen-ethics-principle.php
+++
@@ -1,6 +0,0 @@
-<div class="side-module-desktop-mobile">
-				<img src="../icons/earthen-ethics-icon-ecobricks-org.svg" width="250" alt="For-Earth Enterprise through eco bricking">
-				<h4>Earthen Ethics</h4>
-				<h5>Ecobricks are based on the five principles of Earth's example.</h5><br>
-				<a class="module-btn" href="https://book.earthen.io/en/" target="_blank">Learn More</a>
-			</div>
