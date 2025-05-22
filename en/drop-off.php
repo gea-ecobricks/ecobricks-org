8a8efdbce55a555189735bcb3d2301fb7fbc1cf0
JosephOFlanagan <joseph.oflanagan@yahoo.co.uk>
2025-05-22 20:40:47 +0100

    Update drop-off.php
    Added the script and style to update the page to the new format.

--- en/drop-off.php
+++ en/drop-off.php
@@ -1,13 +1,23 @@
 <!--PAGE LANGUAGE:  ENGLISH
 Content Page template: v.1.0.1-->
-  
+
+<!DOCTYPE html>
+<HTML lang="en">
+<HEAD>
+<META charset="UTF-8">
+<?php $lang='en';?>
+<?php $version='1.1';?>
+<?php $page='drop-off';?>
+
 <!-- TRANSLATORS:   Look for untranslated text inside HTML tags.  In other words <a tag>any content text between markers like these</a tag>.  Don't worry about translating these comments.  Be sure NOT to translate english page names, file names, div names, div class names, or html syntax.-->
  
 <?php require_once ("../includes/drop-off-inc.php");?>
-
+	
 <!--Once translation is completed, you can remove the line below to take the translation notice off the page-->
 <!--<?php require_once ("translate-please.php");?>-->
 
+<title>Drop Off</title>
+	
 <!--TOP PAGE BANNER-->
 
 <div class="splash-content-block">
@@ -53,8 +63,46 @@ Content Page template: v.1.0.1-->
 <!-- CUSTOM PAGE SCRIPTS-->
 
 <!-- This script is for pages that use the accordion content system-->
-<script src="../scripts/accordion-scripts.js" defer></script>
+<!-- This script is for pages that use the accordion content system
 
+<script src="../accordion-scripts.js?v=<?php echo ($version); ;?>" defer></script>
+
+
+-->
+
+<!-- CUSTOM PAGE SCRIPTS-->
+<script>
+
+  function toggleAccordion(blockNumber) {
+    // Toggle the display of the content block
+    var contentBlock = document.getElementById("preclosed" + blockNumber);
+    if (contentBlock.style.display === "block") {
+        contentBlock.style.display = "none";
+    } else {
+        contentBlock.style.display = "block";
+    }
+
+    // Toggle the styling of the main block
+    var mainBlock = document.getElementById("block" + blockNumber);
+    if (mainBlock.style.display === "grid") {
+        mainBlock.style.background = "var(--accordion-background)";
+        mainBlock.style.display = "block";
+    } else {
+        mainBlock.style.background = "var(--main-background)";
+        mainBlock.style.display = "grid";
+    }
+
+    // Toggle the button text
+    var toggleButton = document.getElementById("block-toggle-show" + blockNumber);
+    if (toggleButton.innerHTML === "+") {
+        toggleButton.innerHTML = "-";
+    } else {
+        toggleButton.innerHTML = "+";
+    }
+}
+
+
+</script>
 
 </div>
 </body>
