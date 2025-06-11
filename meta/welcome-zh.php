96b71bec8ccf82b432fe4dfacc0150835561d679
Russell Maier <russ@earthen.io>
2025-06-11 19:03:51 +0700

    Fix training preview

---
+++ meta/welcome-zh.php
@@ -0,0 +1,29 @@
+<title>欢迎访问 Ecobricks.org</title>
+
+<meta name="keywords" content="生态砖，生态 砖，生态砖块，eco砖，生态积木，塑料，塑料管理，碳封存，土本伦理，塑料，回收替代方案，焚烧，塑料抵消，塑料解决方案，再利用，塑料封存，回收替代，焚烧替代，防止塑料污染，塑料污染，塑料的再生解决方案，垃圾，废物管理，塑料垃圾，非资本化，零废弃，净零，塑料封存">
+
+<meta name="description" content="生态砖防止塑料成为垃圾、废物、回收品、焚烧物或污染物。生态砖实现非资本化、零废弃、净零的塑料封存。">
+
+<!-- Facebook Open Graph Tags for social sharing -->
+
+<meta property="og:url" content="https://ecobricks.org/zh/">
+<meta property="og:type" content="website">
+<meta property="og:title" content="生态砖推动塑料转型">
+<meta property="og:description" content="生态砖防止塑料成为垃圾、废物、回收品、焚烧物或污染物。生态砖实现非资本化、零废弃、净零的塑料封存。">
+<meta property="og:image" content="https://www.ecobricks.org/pngs/social-banner2.png">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="638">
+<meta property="og:image:alt" content="一条象征性道路蜿蜒延伸，道路两旁是各种生态砖和土建筑结构。">
+<meta property="og:locale" content="zh_CN">
+
+<meta name="author" content="Ecobricks.org">
+
+<meta property="og:type" content="article">
+<meta property="og:site_name" content="Ecobricks.org">
+<meta property="article:publisher" content="https://web.facebook.com/ecobricks.org">
+<meta property="article:modified_time" content="2024-04-3T09:14:13+00:00">
+<meta property="og:image:type" content="image/png">
+<meta name="author" content="全球生态砖联盟">
+<meta name="twitter:card" content="summary">
+<meta name="twitter:label1" content="预计阅读时间">
+<meta name="twitter:data1" content="3分钟">
---
+++ translations/welcome-zh-translation.js
@@ -0,0 +1,49 @@
+/*-----------------------------------
+
+CHINESE TRANSLATION FOR ECOBRICKS.ORG
+
+-----------------------------------*/
+
+const zh_Page_Translations = {
+    "300-featured-content-1-title": "六月免费课程",
+    "301-featured-content-1-subtitle": "加入我们的免费直播入门课程。通过GEA联合创始人Russell Maier和GEA培训师Paula Apollonia的授课，学习塑料、生态圈与生态砖的科学、哲学和核心技术。",
+    "302-featured-content-1-button": "↗️ 在GoBrik上查看课程",
+
+    "300-featured-content-3-title": "塑料的星球故事",
+    "301-featured-content-3-subtitle": "塑料到底从哪里来？",
+    "302-featured-content-3-button": "🌎 深入了解！",
+
+    "300-featured-content-4-title": "地球与生态砖方法",
+    "301-featured-content-4-subtitle": "深入的建筑指南与最佳实践。",
+    "302-featured-content-4-button": "⚒️ 学习",
+
+    "500-training-feed": "<span class=\"blink\">⬤  </span> 实时培训信息流...",
+    "500-featured-training-heading": "GEA 培训",
+    "too-featured-live-training-subheading": "我们600多位培训师的团队在全球开展培训。",
+    "500-training-join-button": "➕ 参加工作坊",
+
+    "403-featured-live-brikchain": "<span class=\"blink\">⬤  </span> 实时项目信息流。点击预览。",
+    "404-featured-live-heading": "生态砖应用",
+    "405-featured-live-subheading": "全球生态砖爱好者记录的项目。",
+    "308-featured-2-heading": "保持塑料安全",
+    "309-featured-2-subheading": "生态砖是一种简单、非资本化的方法，可安全地储存塑料，并将其用于绿色用途。",
+    "310-featured-2-button": "快速介绍",
+    "311-featured-2-references": "五张幻灯片。45秒。",
+    "303-featured-live-brikchain": "<span class=\"blink\">⬤  </span> 生态砖认证实时链。点击预览。",
+    "304-featured-live-heading": "生态砖。实时。",
+    "305-featured-live-subheading": "生态砖正在全球范围内被制作、记录和验证。",
+    "306-featured-live-button": "⛓️ Brikchain 链",
+    "307-featured-live-links": "经过验证的生态砖信息流和存档",
+    "312-top10s-title": "每月前十",
+    "313-top10s-subtitle": "每月，全球数千块生态砖被记录并进行同行评审！",
+    "313-top10s-button": "🏆 每月排名",
+    "314-top10s-references": "本月最佳生态砖",
+    "315-featured-3-heading": "善用塑料",
+    "316-featured-3-subheading": "生态砖将问题塑料转化为可重复使用的积木。塑料是个问题，但也是构建绿色解决方案的方式。",
+    "317-featured-3-button": "🔨 生态砖建造",
+    "318-featured-3-references": "<a href=\"earth.php\">土建筑</a> | <a href=\"modules.php\">家具模块</a>",
+    "319-featured-4-heading": "追随地球的榜样",
+    "320-featured-4-subheading": "我们的生态砖灵感来自吕宋北部的伊戈洛特人——全球生态砖联盟的发源地。受其Ayyew精神的启发，生态砖根本区别于西方的可持续性与零废弃理念。",
+    "321-featured-4-button": "🌏 我们的土原则",
+    "322-featured-4-references": "<a href=\"spiral.php\">螺旋设计</a> | <a href=\"/ayyew/\">Ayyew</a> | <a href=\"about.php\">关于我们</a>",
+};
