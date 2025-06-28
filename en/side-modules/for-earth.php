c353aa61e0a05cd95251c040b17f326ee7eb69fe
Russell Maier <russ@earthen.io>
2025-06-28 11:18:05 +0700

    2025 footer and header update

--- en/side-modules/for-earth.php
+++ en/side-modules/for-earth.php
@@ -1,6 +1,6 @@
 <div class="side-module-desktop-mobile">
 					<img src="../webp/for-earth500px.webp" width="80%" alt="For-Earth Enterprise through eco bricking">
-					<h4>For-Earth Enterprise</h4>
-					<h5>The GEA commits to a not-for-profit business model that redistributes financial benefit towards ecological benefit.  It does this by disclosing its carbon, plastic and biodiversity impacts and by ensuring that they are net-green.</h5><br>
+					<h4 data-lang-id="000-for-earth-enterprise">For-Earth Enterprise</h4>
+					<h5 data-lang-id="000-gea-for-earth">The GEA commits to a not-for-profit business model that redistributes financial benefit towards ecological benefit.  It does this by disclosing its carbon, plastic and biodiversity impacts and by ensuring that they are net-green.</h5><br>
 					
 				</div>
