43405ca6585f50445b64eb0de5ea3a89466b3b62
Russell Maier <68637813+russs95@users.noreply.github.com>
2025-08-02 22:01:28 +0700

    Add alias translation file for Earthwands page (#57)

---
+++ meta/earthwand-zh.php
@@ -0,0 +1,31 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Earthwand 地球魔杖</title>
+
+<meta name="keywords" content="earthwands, ecobrick, stick, dowl, bamboo, packing, wood, earth, wand, ecobricking, make, 地球棒, 魔杖, 生态砖, 竹子, 藤条">
+
+<meta name="description" content="用终极生态砖工具让你的塑料消失。">
+
+        <!-- Facebook Open Graph Tags for social sharing-->
+
+        <meta property="og:url"           content="https://ecobricks.org/zh/earth-wands.php">
+        <meta property="og:type"          content="website">
+        <meta property="og:title"         content="Earthwand 地球魔杖">
+        <meta property="og:description"   content="用终极生态砖工具让你的塑料消失。">
+        <meta property="og:image"         content="https://ecobricks.org/photos/earthwand-photo-1-500px.webp">
+        <meta property="og:image:width" content="500">
+        <meta property="og:image:height" content="500" >
+        <meta property="og:image:alt"     content="一支 Earthwand、一些塑料和带有 GoBrik 应用的手机。">
+          <meta property="og:locale" content="zh_CN" >
+
+          <meta name="author" content="Ecobricks.org">
+
+  <meta property="og:type" content="article" >
+ <meta property="og:site_name" content="Ecobricks.org" >
+ <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" >
+ <meta property="article:modified_time" content="2024-02-16T09:14:13+00:00" >
+ <meta property="og:image:type" content="image/png" >
+ <meta name="author" content="Global Ecobrick Alliance" >
+ <meta name="twitter:card" content="summary" >
+ <meta name="twitter:label1" content="Est. reading time" >
+ <meta name="twitter:data1" content="2 minutes" >
