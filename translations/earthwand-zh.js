c3001aec02045502adee095cd1ff80adb940f587
Russell Maier <68637813+russs95@users.noreply.github.com>
2025-08-02 22:01:07 +0700

    Add alias translation file for Earthwands page

---
+++ translations/earthwand-zh.js
@@ -0,0 +1,35 @@
+/*-----------------------------------
+
+ZH SNIPPETS FOR ECOBRICKS.ORG/EARTHWANDS
+
+-----------------------------------*/
+
+const zh_Page_Translations = {
+
+    '001-splash-title': '地球魔杖',
+    '002-splash-subtitle': '让你的塑料消失。',
+    '003-splash-image-alt': '<img src="../icons/earthwand-shadow.svg" style="width: 85%" alt="EarthWand 是一款从头开始设计的再生产品，是完美的塑料压实和生态砖制作工具。">',
+
+    '004-lead-page-paragraph': '为什么不用最好的工具来压实你的生态砖呢？',
+
+    '005-first-page-paragraph': 'Earthwand 是一款再生产品，从零开始设计为完美的塑料压实和生态砖制作工具。它简单却非常实用，符合人体工程学，坚固而强壮。最棒的是，它不含塑料——完全由竹子、藤条和椰纤手工制作。',
+
+    '006-second-page-paragraph': '经过三年多生态砖制作和原型设计，Earthwand 被设计成不仅可以压实塑料，还能通过销售支持本地和全球生态砖运动。EarthWand 由 GEA 培训师制作并销售，作为资助其生态工作的方式。<br><br>请参阅我们的 <a href="https://nextcloud.ecobricks.org/index.php/s/pmLJjGJGr3qzdBF" target="_blank">Earthwand 再生财务文件</a> 了解销售如何促进塑料转变运动。',
+
+    '007-virtue-icons':'<img src="../webp/earthwand-virtue-icons.webp" style="width:100%;" alt="Earthwand 设计原则">',
+
+    '008-page-paragraph': '最重要的是，我们精心设计了 Earthwand 的生产、消费和处置，使其整个生命周期的所有影响相加后，最终使世界更加绿色。查看 EarthWand 的产品影响评估（<a href="https://nextcloud.ecobricks.org/index.php/s/gLfD7rTXAWAszie" target="_blank">英国报告</a> | <a href="https://nextcloud.ecobricks.org/index.php/s/ScfWXzxcP58YEaN" target="_blank">印尼报告</a>）。这样，EarthWand 体现了指导全球塑料转变运动的 <a href="/ayyew">Ayyew</a>、<a href="https://book.earthen.io" target="_blank">Earthen</a> 和 <a href="principles">再生原则</a>。',
+
+    '009-page-paragraph': '不仅仅是一个产品',
+    '010-page-paragraph': 'EarthWand 由 GEA 负责人兼发明家 Russell Maier 开发。他的目标是创造一个体现推动生态砖运动的地球原则并为之赋能的产品。他的愿景不仅是打造一个优秀的绿色产品，还要创造一个能够在经济上支持本地和全球塑料转变运动的收益模式。',
+
+    '011-page-paragraph': 'GEA 培训师是 Earthwand 的官方销售者。销售收益用于继续他们的工作——例如在社区开展尽可能多的生态砖工作坊！培训师承诺至少将零售价的 25% 返还给 GEA 以支持其全球工作。销售交易及这些资金的使用在 <a href="open-books.php">GEA 公开账目系统</a> 上完全可追踪。',
+
+    '011-header-paragraph':'Earthwand 的益处',
+    '012-page-paragraph': '<li>最适合生态砖的工具。</li><li>由 100% 有机材料制成。</li><li>Earthwand 由经全球生态砖联盟审核的小规模制造伙伴制作，并被要求追踪制造的生态影响。</li><li>制造、销售和运输的所有生态影响都得到核算并公开。</li><li>Earthwand 的制造、销售和运输的净影响是绿色的。</li><li>Earthwand 利润的 75% 用于支持销售它的 GEA 培训师的生态领导力。</li><li>Earthwand 利润的 24% 用于支持全球生态砖联盟的工作。</li><li>Earthwand 利润的 1% 用于支持设计和模式的发明者的工作。</li>',
+    '013-page-paragraph': '购买 Earthway 是生态砖者支持本地生态砖培训师、全球生态砖联盟和再生制作者的一种方式，从而为塑料转变运动本身注入活力。',
+    '013-earthwand-buy-button': '🛒 GoBrik 商店',
+
+}
+
+export default zh_Page_Translations;
