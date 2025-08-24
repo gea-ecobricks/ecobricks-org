cfba0d713753c063c57e65c85bbfb92270711771
ubuntusitubondo <russ@earthen.io>
2025-08-24 19:47:39 +0700

    Ecojoiner page update

--- en/ecojoiners.php
+++ en/ecojoiners.php
@@ -63,13 +63,13 @@
                         <p data-lang-id="043-block-4-main-p1">Ecojoiners leverage the versatility of bamboo to create a powerful new way to build with ecobricks.  Both ecobricks and ecojoiners sequester carbon in terms of CO2 and plastic.  Both are subaltern technologies that follow the principles of Earthen design: non-capital, open-source, modular, circular, and carbon+plastic negative.</p>
                     </div>
                     <div class="side2" data-lang-id="044-block-4-img1-alt">
-                        <img src="../webp/carbon-cube-500px.webp" width="300" style="width:100%;" alt="A cubic matrix made with bottles and bamboo" loading="lazy" >
+                        <img src="../webp/carbon-cube-500px.webp" width="360" style="width:100%;" alt="A cubic matrix made with bottles and bamboo" loading="lazy" >
                     </div>
                 </div>
 
                 <div class="row2">
                     <div class="main3">
-                        <p data-lang-id="045-block-4-main-p2">T<p>And because the combination of ecojoiners and ecobricks is indefinitely cubic and completely modular, simple chairs and tables are just the beginning of the great green things we can make together.</p></p>
+                        <p data-lang-id="045-block-4-main-p2"><p>And because the combination of ecojoiners and ecobricks is indefinitely cubic and completely modular, simple chairs and tables are just the beginning of the great green things we can make together.</p></p>
                     </div>
                     <div class="side3" data-lang-id="046-block-3-img2-alt">
                         <img src="../webp/carbon-cube-500px.webp" width="120" style="width:100%;" alt="A cubic matrix made with bottles and bamboo" loading="lazy" >
@@ -100,15 +100,17 @@
                 <div class="row">
                     <div class="main2">
                         <p data-lang-id="009-block-1-main-p1-x">Ecobricks are great.  They sequester plastic and you can build all sorts of stuff with them.  Consequently, folks around the world pack their plastic into bottles so that they can build their projects with them.</p>
+
+                        <p>But what if the stuff you could build was even more awesome?</p>
                     </div>
                     <div class="side2" data-lang-id="044-block-4-img1-alt">
-                        <img src="../webp/carbon-cube-chair-400px.webp" width="300" style="width:100%;" alt="A cubic matrix made with bottles and bamboo" loading="lazy" >
+                        <img src="../webp/carbon-cube-chair-400px.webp" width="360" style="width:100%;" alt="A cubic matrix made with bottles and bamboo" loading="lazy" >
                     </div>
                 </div>
 
                 <div class="row2">
                     <div class="main3">
-                        <p data-lang-id="045-block-1-main-p2"><p>But what if the stuff you could build was even more awesome?</p></p>
+                        <p data-lang-id="045-block-1-main-p2">For the last three years our R&D team, led by GEA co-founder Russell Maier, has been working on the situation that the true potential for ecobrick construction remained untapped.</p>
                     </div>
                     <div class="side3" data-lang-id="046-block-3-img2-alt">
                         <img src="../webp/carbon-cube-chair-400px.webp" width="120" style="width:100%;" alt="A cubic matrix made with bottles and bamboo" loading="lazy" >
@@ -116,7 +118,7 @@
                 </div>
 
                 <p data-lang-id="010-block-1-main-p2-x">
-				For the last three years our R&D team, led by GEA co-founder Russell Maier, has been working on the situation that the true potential for ecobrick construction remained untapped.  Afterall, ecobricks are solid, resilient and strong.  Surely, we could do better than connecting into hexagons and triangular modules-- and if we could, it would provide a powerful and compelling incentive for folks around the world, concerned about plastic pollution, to ecobrick their plastic more-- and to dump, recycle and burn their plastic less.</p>
+				Afterall, ecobricks are solid, resilient and strong.  Surely, we could do better than connecting into hexagons and triangular modules-- and if we could, it would provide a powerful and compelling incentive for folks around the world, concerned about plastic pollution, to ecobrick their plastic more-- and to dump, recycle and burn their plastic less.</p>
 
 				<p>With the stakes high, the GEA team explored dozens of ways in which we could connect ecobricks.  Of course, our design process wasn't just based on function-- so too were we guided and constrained by the Earthen principles that guide the regenerative plastic transition movement.</p>
 
