c94d930ed1f2cf05ba4f98a8f813ccede1b25499
ubuntusitubondo <russ@earthen.io>
2025-08-24 19:40:10 +0700

    Ecojoiner page update

--- en/ecojoiners.php
+++ en/ecojoiners.php
@@ -32,7 +32,7 @@
                         <div class="page-paragraph">
                                 <p data-lang-id="005-first-page-paragraph-x">We're thrilled to introduce a whole new way to connect your ecobricks.  After three years of research and development, the Global Ecobrick Alliance is launching <b>ecojoiners</b> as an open source (non-patented!) and fully Earthen technology (biodegradeable, spiral, modular, carbon sequestering) to create cubic latices.  That's a fancy way to say... if you can imagine it, you can use ecojoiners and ecobricks to build it green and great.  </p>
 
-
+                         <img src="../webps/cubic-matrix.webp" style="width:100%">
 
                         </div>
 
@@ -57,9 +57,30 @@
         <button onclick="preclosed4()" class="block-toggle" id="block-toggle-show4">+</button>
     </div>
     <div id="preclosed4">
-         <p>Ecojoiners leverage the versatility of bamboo to create a powerful new way to build with ecobricks.  Both ecobricks and ecojoiners sequester carbon in terms of CO2 and plastic.  Both are subaltern technologies that follow the principles of Earthen design: non-capital, open-source, modular, circular, and carbon+plastic negative.</p>
 
-        <p>And because the combination of ecojoiners and ecobricks is indefinitely cubic and completely modular, simple chairs and tables are just the beginning of the great green things we can make together.</p>
+                <div class="row">
+                    <div class="main2">
+                        <p data-lang-id="043-block-4-main-p1">Ecojoiners leverage the versatility of bamboo to create a powerful new way to build with ecobricks.  Both ecobricks and ecojoiners sequester carbon in terms of CO2 and plastic.  Both are subaltern technologies that follow the principles of Earthen design: non-capital, open-source, modular, circular, and carbon+plastic negative.</p>
+                    </div>
+                    <div class="side2" data-lang-id="044-block-4-img1-alt">
+                        <img src="../webps/carbon-cube-500px.webp" width="300" style="width:100%;" alt="A cubic matrix made with bottles and bamboo" loading="lazy" >
+                    </div>
+                </div>
+
+                <div class="row2">
+                    <div class="main3">
+                        <p data-lang-id="045-block-4-main-p2">T<p>And because the combination of ecojoiners and ecobricks is indefinitely cubic and completely modular, simple chairs and tables are just the beginning of the great green things we can make together.</p></p>
+                    </div>
+                    <div class="side3" data-lang-id="046-block-3-img2-alt">
+                        <img src="../webps/carbon-cube-500px.webp" width="120" style="width:100%;" alt="A cubic matrix made with bottles and bamboo" loading="lazy" >
+                    </div>
+                </div>
+
+
+
+
+
+
     </div>
 </div>
 
@@ -76,9 +97,24 @@
 
     <div id="preclosed1">
 
-                <p data-lang-id="009-block-1-main-p1-x">Ecobricks are great.  They sequester plastic and you can build all sorts of stuff with them.  Consequently, folks around the world pack their plastic into bottles so that they can build their projects with them.</p>
+                <div class="row">
+                    <div class="main2">
+                        <p data-lang-id="009-block-1-main-p1-x">Ecobricks are great.  They sequester plastic and you can build all sorts of stuff with them.  Consequently, folks around the world pack their plastic into bottles so that they can build their projects with them.</p>
+                    </div>
+                    <div class="side2" data-lang-id="044-block-4-img1-alt">
+                        <img src="../webps/carbon-cube-chair-400px.webp" width="300" style="width:100%;" alt="A cubic matrix made with bottles and bamboo" loading="lazy" >
+                    </div>
+                </div>
 
-                <p>But what if the stuff you could build was even more awesome?</p>
+                <div class="row2">
+                    <div class="main3">
+                        <p data-lang-id="045-block-1-main-p2"><p>But what if the stuff you could build was even more awesome?</p></p>
+                    </div>
+                    <div class="side3" data-lang-id="046-block-3-img2-alt">
+                        <img src="../webps/carbon-cube-chair-400px.webp" width="120" style="width:100%;" alt="A cubic matrix made with bottles and bamboo" loading="lazy" >
+                    </div>
+                </div>
+
                 <p data-lang-id="010-block-1-main-p2-x">
 				For the last three years our R&D team, led by GEA co-founder Russell Maier, has been working on the situation that the true potential for ecobrick construction remained untapped.  Afterall, ecobricks are solid, resilient and strong.  Surely, we could do better than connecting into hexagons and triangular modules-- and if we could, it would provide a powerful and compelling incentive for folks around the world, concerned about plastic pollution, to ecobrick their plastic more-- and to dump, recycle and burn their plastic less.</p>
 
@@ -113,85 +149,42 @@
             </div>
         </div>
 
-        <p data-lang-id="026-block-2-p2-x">Completed ecobricks are packed solid enough that they can bear the weight of a person without deforming and to prohibit easy <a href="/fire">flammability</a> while maximizing its durability and re-usability.</p>
-
         <div class="row2">
             <div class="main3">
-                <p data-lang-id="027-block-2-main-p3">To be an ecobrick, the following standards must be met:</p>
+                <p data-lang-id="045-block-1-main-p2"><p>So far, we've built chairs, shelves and tables.  What more could we put together?  The answer is up to you</p>
+
+
             </div>
-            <div class="side3" data-lang-id="028-block-2-img2-alt">
-                <img src="https://ecobricks.org/svgs/eb-blue.svg" width="120" style="width:100%;" alt="An ecobrick being made" loading="lazy" >
+            <div class="side3" data-lang-id="046-block-3-img2-alt">
+                <img src="../ecojoiner-table-400px.webp" width="120" style="width:100%;" alt="A cubic matrix made with bottles and bamboo" loading="lazy" >
             </div>
         </div>
 
-        <ul>
-            <li data-lang-id="029-block-2-li-1">The ecobrick is made using a transparent plastic (PET) bottle</li>
-            <li data-lang-id="030-block-2-li-2">The ecobrick is only packed with used plastics that are clean and dry.</li>
-            <li data-lang-id="031-block-2-li-3">The ecobrick’s density is higher than 0.33g/ml.</li>
-            <li data-lang-id="032-block-2-li-4">The ecobrick must have a density of 0.70g/ml or less</li>
-            <li data-lang-id="033-block-2-li-5">The ecobrick is sealed tight with a screw down lid.</li>
-            <li data-lang-id="034-block-2-li-6">Care has been taken taken to prepare the ecobrick as a reusable building block:
-                <ul>
-                    <li data-lang-id="035-block-2-li-6-1">The label has been removed and ecobricked.</li>
-                    <li data-lang-id="036-block-2-li-6-2">A uniform colored plastic has been given to the ecobrick's bottom.</li>
-                    <li data-lang-id="037-block-2-li-6-3">The weight has been permanently recorded on the outside.</li>
-                </ul>
-            </li>
-        </ul>
+    <p data-lang-id="045-block-1-main-p3"><p>Best of all ecojoiners enable spiral designs:  when your done with your creation you can take and part and build another.  But, there's more to it than that.  When ecojoiners break or can't be used anymore, then they can safely composted as bamboo is entirely organic.  Meanwhile, the ecobricks can be put to use in longer term constructions like modules or earthen constructions.  </p>
 
-        <p data-lang-id="038-block-2-p3">Only when these ecobrick standards are  met can the plastic inside be considered to be sequestered.</p>
-		<br><br>
-        <a class="action-btn" href="how.php" data-lang-id="039-block-2-btn">ℹ️ How to Make</a>
-        <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="040-block-2-p4">View our 10 Step Guide on Making an Ecobrick.</p>
+                    <p>Is all about having a plan for your cycles!</p>
+
+        <a class="action-btn" href="spiral.php" data-lang-id="039-block-2-btn">ℹ️ Spiral Design</a>
+        <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="040-block-2-p4">Learn about the Earthen principle of Spiral Design</p>
     </div>
 </div>
 
 <div class="reg-content-block" id="block3">
     <div class="opener-header">
         <div class="opener-header-text">
-            <h4 data-lang-id="041-block-3-header">Principles behind the Design</h4>
-            <h5 data-lang-id="042-block-3-subheader">Imagine structures that sequester more carbon and plastic than their making creates.</h5>
+            <h4 data-lang-id="041-block-3-header">Open Source Designs/h4>
+            <h5 data-lang-id="042-block-3-subheader">Download the plans to build and make your own ecojoiners from Bamboo</h5>
         </div>
         <button onclick="preclosed3()" class="block-toggle" id="block-toggle-show3" aria-label="Plus">+</button>
     </div>
 
     <div id="preclosed3">
         <div class="row">
-            <div class="main2">
-                <p data-lang-id="043-block-3-main-p1">Cigbricks are a class of ecobrick to secure the potentially toxic plastic/acetate of the cigarette filters-- one of the most prevalent and damaging of all plastic pollutants. Cigbricks are made exclusively from the packed acetate filters of cigarette butts once the paper and ash has been fully removed.</p>
-            </div>
-            <div class="side2" data-lang-id="044-block-3-img1-alt">
-                <img src="../svgs/Cigbrick.svg" width="300" style="width:100%;" alt="Cigbricks are for cigarettes" loading="lazy" >
-            </div>
-        </div>
 
-        <div class="row2">
-            <div class="main3">
-                <p data-lang-id="045-block-3-main-p2">To be a Cigbrick the following standards must be met:</p>
-            </div>
-            <div class="side3" data-lang-id="046-block-3-img2-alt">
-                <img src="../svgs/Cigbrick.svg" width="120" style="width:100%;" alt="Cigbricks are for cigarettes" loading="lazy" >
-            </div>
-        </div>
+        <p data-lang-id="055-block-3-p1">We're still working hard on the ecojoiner core concept!  We have a little more testing to do, then we will release the designs here.  </p>
 
-        <ul>
-            <li data-lang-id="047-block-3-li-1">A density higher than 0.29 g/ml</li>
-            <li data-lang-id="048-block-3-li-2">A maximum density of 0.70g/ml</li>
-            <li data-lang-id="049-block-3-li-3">Only packed with acetate cigarette butts – the paper and ash have been removed.</li>
-            <li data-lang-id="050-block-3-li-4">The cigbrick is sealed tight with a screw down lid.</li>
-            <li data-lang-id="051-block-3-li-5">Care has been taken to prepare the cigbrick as a reusable building block:
-                <ul>
-                    <li data-lang-id="052-block-3-li-5-1">The label has been removed and ecobricked.</li>
-                    <li data-lang-id="053-block-3-li-5-2">A bottom color has been added</li>
-                    <li data-lang-id="054-block-3-li-5-3">The weight has been recorded on the outside.</li>
-                </ul>
-            </li>
-        </ul>
-
-        <p data-lang-id="055-block-3-p1">Only cigbricks that meet these standards can be authenticated and count as sequestered plastic.</p>
-
-        <a class="action-btn" href="/cigbricks" data-lang-id="056-block-3-btn" style="margin-top:20px">ℹ️ About Cigbricks</a>
-        <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="057-block-3-p2">Learn more about Cibricks.</p>
+        <!--<a class="action-btn" href="/cigbricks" data-lang-id="056-block-3-btn" style="margin-top:20px">ℹ️ 600ml Aqua Bottle 6-Face-Cubic Ecojoiner v.1.0.1</a>
+        <p style="font-size: 0.85em; margin-top:20px;" data-lang-id="057-block-3-p2">Download the plans & dimensions</p>-->
     </div>
 </div>
 
