b1b305ec9c2546d2a9472fa20703defab4941b14
Russell Maier <68637813+russs95@users.noreply.github.com>
2025-08-30 14:08:12 +0700

    Add ocean meta translations (#61)

---
+++ meta/ocean-id.php
@@ -0,0 +1,31 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>Ecobrick Laut</title>
+
+<meta name="keywords" content="pencemaran laut, ecobrick laut, OEB, OEBs, pengambilan sampah pantai, pembersihan pantai, bersih-bersih, alternatif Sungai Watch, blok bangunan, batu bata, brik, ecobrick laut, ecobrick, OEB, sekuestrasi plastik, plastik basah, plastik kotor, plastik pantai, polusi plastik, solusi regeneratif, bangunan tanah, konstruksi cob, bangunan adobe, trash hero, eco bricks, pembersihan laut, bangunan berkelanjutan, penggunaan ulang plastik, botol PET, blok plastik, plastik tersekuester, nol sampah, mikroplastik, biosfer, konstruksi sirkular, kesadaran ekologis, transisi ekologis, cradle to cradle, desain regeneratif, ruang hijau, plastik laut, literasi ekologis, ekosistem, keberlanjutan laut, plastik dapat digunakan kembali, solusi teknologi rendah, sekuestrasi karbon, transisi plastik, jaring ikan, alat hantu, konservasi laut, untuk bumi, etika bumi, desain spiral, pemadatan plastik, prinsip regeneratif, tanggung jawab lingkungan, pengayaan ekologis, pantai bersih, racun plastik, ecobrick laut">
+
+<meta name="description" content="Ecobrick laut adalah solusi sekuestrasi sederhana untuk plastik yang dikumpulkan dari sungai, pantai, dan lautan.">
+
+        <!-- Facebook Open Graph Tags for social sharing-->
+
+        <meta property="og:url"           content="https://ecobricks.org/id/ocean.php">
+        <meta property="og:type"          content="website">
+        <meta property="og:title"         content="Ecobrick Laut">
+        <meta property="og:description"   content="Ecobrick adalah solusi transisi plastik untuk membuat blok bangunan yang dapat digunakan kembali dari plastik tersekuester.">
+        <meta property="og:image"         content="https://ecobricks.org/photos/ocean-examples4.png">
+        <meta property="og:image:width" content="588">
+        <meta property="og:image:height" content="865">
+        <meta property="og:image:alt"     content="Ecobrick laut adalah solusi sekuestrasi sederhana untuk plastik yang dikumpulkan dari sungai, pantai, dan lautan.">
+          <meta property="og:locale" content="id_ID">
+
+          <meta name="author" content="Ecobricks.org">
+
+  <meta property="og:type" content="article">
+ <meta property="og:site_name" content="Ecobricks.org">
+ <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org">
+<meta property="article:modified_time" content="2025-08-30T09:14:13+00:00">
+ <meta property="og:image:type" content="image/png">
+ <meta name="author" content="Global Ecobrick Alliance">
+ <meta name="twitter:card" content="summary">
+ <meta name="twitter:label1" content="Est. reading time">
+ <meta name="twitter:data1" content="8 minutes">
