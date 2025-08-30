8e6dc6020f2e5642a312ac94dad71160b1b17a21
Russell Maier <68637813+russs95@users.noreply.github.com>
2025-08-30 14:00:47 +0700

    Add ocean meta translations

---
+++ meta/ocean-zh.php
@@ -0,0 +1,31 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>海洋生态砖</title>
+
+<meta name="keywords" content="海洋污染，海洋生态砖，OEB，OEBs，海滩捡拾，海滩清理，清理，Sungai Watch 替代，建筑块，砖，brik，海洋生态砖，生态砖，OEB，塑料封存，湿塑料，脏塑料，海滩塑料，塑料污染，再生解决方案，土建，夯土建筑，土坯建筑，垃圾英雄，生态砖，海洋清理，可持续建筑，塑料再利用，PET 瓶，塑料块，被封存的塑料，零废弃，微塑料，生物圈，循环建造，生态意识，生态转型，循环再生，再生设计，绿色空间，海洋塑料，生态素养，生态系统，海洋可持续性，可重复使用塑料，低技术解决方案，碳封存，塑料转型，渔网，幽灵渔具，海洋保护，为地球，地球伦理，螺旋设计，塑料压实，再生原则，环境责任，生态丰富，干净海滩，塑料毒素，海洋生态砖">
+
+<meta name="description" content="海洋生态砖是从河流、海滩和海洋中回收塑料的简单封存解决方案。">
+
+        <!-- Facebook Open Graph Tags for social sharing-->
+
+        <meta property="og:url"           content="https://ecobricks.org/zh/ocean.php">
+        <meta property="og:type"          content="website">
+        <meta property="og:title"         content="海洋生态砖">
+        <meta property="og:description"   content="生态砖是塑料转型的解决方案，将封存的塑料制成可重复使用的建筑块。">
+        <meta property="og:image"         content="https://ecobricks.org/photos/ocean-examples4.png">
+        <meta property="og:image:width" content="588">
+        <meta property="og:image:height" content="865">
+        <meta property="og:image:alt"     content="海洋生态砖是从河流、海滩和海洋中回收塑料的简单封存解决方案。">
+          <meta property="og:locale" content="zh_CN">
+
+          <meta name="author" content="Ecobricks.org">
+
+  <meta property="og:type" content="article">
+ <meta property="og:site_name" content="Ecobricks.org">
+ <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org">
+<meta property="article:modified_time" content="2025-08-30T09:14:13+00:00">
+ <meta property="og:image:type" content="image/png">
+ <meta name="author" content="全球生态砖联盟">
+ <meta name="twitter:card" content="summary">
+ <meta name="twitter:label1" content="预计阅读时间">
+ <meta name="twitter:data1" content="8分钟">
