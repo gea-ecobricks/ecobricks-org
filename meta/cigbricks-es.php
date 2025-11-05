5fa3a0f04bad04df652f174c2dff912a201f8ef9
JosephOFlanagan <joseph.oflanagan@yahoo.co.uk>
2025-11-05 15:42:23 +0000

    Created the meta files for the cigrbricks page.

---
+++ meta/cigbricks-es.php
@@ -0,0 +1,32 @@
+<!-- Meta tags for page display and search engine listing-->
+
+<title>The Cigbrick</title>
+
+<meta name="keywords" content="ocean pollution, ocean ecobrick, oeb, oebs, beach pickup, beach cleanup, clean up, sungai watch alternative, building block, brick, brik, ocean ecobricks, ecobrick, OEB, plastic sequestration, wet plastic, dirty plastic, beach plastic, plastic pollution, regenerative solution, earth building, cob construction, adobe building, trash hero, eco bricks, ocean cleanup, sustainable building, plastic reuse, PET bottles, plastic blocks, sequestered plastic, zero waste, microplastics, biosphere, circular construction, ecological consciousness, eco transition, cradle to cradle, regenerative design, green spaces, ocean plastic, eco literacy, eco systems, ocean sustainability, reusable plastic, low tech solution, carbon sequestration, plastic transition, fishing nets, ghost gear, ocean conservation, for-earth, earth ethics, spiral design, plastic compaction, regenerative principles, environmental responsibility, ecological enrichment, clean beaches, plastic toxins, oceanic ecobricks
+">
+
+<meta name="description" content="Many people don’t realize that cigarette filters are made from a form of plastic called acetate.  When acetate gets into the environment it can cause all sorts of problems. Ecobricks are designed to keep plastic (like filters!) out of the environment in order to make a reusable building block.">
+
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://ecobricks.org/es/cigbricks.php">
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="The Cigbrick">
+  	<meta property="og:description"   content="a simple solution for cigarette pollution">
+  	<meta property="og:image"         content="https://cdn.ecobricks.org/wp-content/uploads/2018/10/Characters-Albatross-Cigar.gif">
+  	<meta property="og:image:width" content="588">
+	<meta property="og:image:height" content="865">
+  	<meta property="og:image:alt"     content="Many people don’t realize that cigarette filters are made from a form of plastic called acetate.  When acetate gets into the environment it can cause all sorts of problems. Ecobricks are designed to keep plastic (like filters!) out of the environment in order to make a reusable building block.">
+	  <meta property="og:locale" content="es_ES">
+
+	  <meta name="author" content="Ecobricks.org">
+
+  <meta property="og:type" content="article">
+ <meta property="og:site_name" content="Ecobricks.org">
+ <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org">
+<meta property="article:modified_time" content="2025-08-30T09:14:13+00:00">
+ <meta property="og:image:type" content="image/png">
+ <meta name="author" content="Global Ecobrick Alliance">
+ <meta name="twitter:card" content="summary">
+ <meta name="twitter:label1" content="Est. reading time">
+ <meta name="twitter:data1" content="8 minutes">
