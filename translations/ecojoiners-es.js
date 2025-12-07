446a2c69a7c1b821553445608b16e75cf974277c
Russell Maier <68637813+russs95@users.noreply.github.com>
2025-12-07 14:05:06 +0700

    Update ecojoiners regen link and translations

--- translations/ecojoiners-es.js
+++ translations/ecojoiners-es.js
@@ -9,5 +9,6 @@ const es_Page_Translations = {
     '002-splash-subtitle': 'Usa bambú para construir muebles, estructuras y más con tus ecobricks',
     '003-splash-image-alt': '<img src="../webp/ecojoiner-1000px-banner.webp" style="width:95%" alt="Ecobricks unidos con ecojoiners"/>',
     '004-lead-page-paragraph': 'Los Ecojoiners son conectores de bambú que unen ecobricks en marcos cúbicos resistentes para mesas, sillas y mucho más.',
-    '005-first-page-paragraph': 'Al igual que el Lego, los Ecojoiners abren posibilidades infinitas para un diseño regenerativo. La tecnología es de código abierto y pronto compartiremos aquí los planos. <b>Esta página está en construcción.</b>'
+    '005-first-page-paragraph': 'Al igual que el Lego, los Ecojoiners abren posibilidades infinitas para un diseño regenerativo. La tecnología es de código abierto y pronto compartiremos aquí los planos. <b>Esta página está en construcción.</b>',
+    '058-block-5-report-link': 'Consulta nuestro <a href="../pdfs/600ml-Aqua-Bottle-Ecojoiner-v1.0.pdf">Informe de regeneración para los ecojoiners Aqua de 600 ml</a> para ver el desglose completo de nuestras estimaciones de impacto de carbono y plástico'
 };
