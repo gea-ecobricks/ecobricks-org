0ed96ec376d3eeedf4d37e355e8d71ca42c4cca4
ubuntusitubondo <russ@earthen.io>
2026-04-15 19:52:43 +0700

    main page update for new March course

--- en/ocean.php
+++ en/ocean.php
@@ -1,5 +1,5 @@
 <?php $lang = basename(dirname($_SERVER['SCRIPT_NAME']));?>
-<?php $version='1.2';?>
+<?php $version='1.3';?>
 <?php $page='ocean';?>
 <!DOCTYPE html>
 <html lang="<?php echo $lang; ?>">
--- translations/ocean-en.js
+++ translations/ocean-en.js
@@ -1,75 +1,82 @@
 /*-----------------------------------
 
-FRENCH SNIPPETS FOR OCEAN ECOBRICKS PAGE
+ENGLISH SNIPPETS FOR OCEAN ECOBRICKS PAGE
 
 
 -----------------------------------*/
 
-// 🌊 Ocean Ecobricks Introduction & Sequestration
+// 🌊 Ocean Ecobricks Splash & Introduction
 const en_Page_Translations = {
-    "004-lead-page-paragraph": "Plastics collected on the beach or from a river tend to be large, dirty, and wet and are not suitable for normal ecobricking.  Nor are they attractive for recyclers.  However, as they degrade into microplastics and toxins they create significant ecological problems.",
-    "005-first-page-paragraph": "Ocean ecobricks (OEBs) are a low-tech <a href =\"/sequest\">plastic sequestration technology.</a>   OEBs allow the packing of dirty and wet plastics (unlike normal ecobricks) in order to secure them from <a href=\"/why\">degrading into toxins and microplastics.</a> In so doing a zero-carbon reusable building block is made.",
-    "006-second-page-paragraph": "OEBs were developed in partnership with <a href =\"www.trashhero.org\" >TrashHero.org</a>, a global beach clean up movement, as a local and <a href =\"/principles\">regenerative</a> solution to the ocean plastic that they recovered.  Rather than returning the collected plastic to be dumped, recycled or incinerated, trash heroes often use OEBs to secure the plastic that they pick up.",
-    "007-third-page-paragraph": "An ocean ecobrick is made by packing solid two cut bottles halves with plastic. By definition the making of an ocean ecobrick is manual and does not require machines, special skills or equipment.  OEBs can be used to make make <a href=\"/modules\">modular</a> furniture.  However they are especially suited for building green spaces using <a href =\"/earth-methods\">earth building techniques</a> and <a href =\"/earth\">principles.</a>",
+    "001-splash-title": "Ocean Ecobricks",
+    "002-splash-subtitle": "A Sequestration Solution for Dirty Plastics",
+    "003-splash-image-alt": "The basics of ecobricking",
+    "004-lead-page-paragraph": "Ocean ecobricks are a low-tech sequestration method for transforming the problem of beached plastics into a simple building block solution.",
+    "005-first-page-paragraph": "Plastics collected on the beach or from a river tend to be large, dirty, and wet and are not suitable for normal ecobricking.  Nor are they attractive for recyclers.  However, as they degrade into microplastics and toxins they create significant ecological problems.",
+    "006-second-page-paragraph": "Ocean ecobricks (OEBs for short!) were developed in partnership with <a href=\"https://www.trashhero.org\" target=\"_blank\" rel=\"noopener\">TrashHero.org</a>, a global beach clean up movement, as a local and <a href =\"principles.php\">regenerative</a> solution to the ocean plastic that they recovered.  Rather than returning the collected plastic to be dumped, recycled or incinerated, beach cleanups began using OEBs to secure the plastic that they pick up.",
+    "007-third-page-paragraph": "Making an OEB is super simple.  It doesn't require any special skils or machines! When many are collected, a range of applications await. OEBs can be used to make make <a href=\"/modules\">modular</a> furniture.  However they are especially suited for building green spaces using <a href =\"earth-methods.php\">earth building techniques</a> and <a href =\"earth.php\">principles.</a>",
 
     // 📦 Section: Plastic Sequestration
-    "008-block-1-opener-header": "A Regenerative Solution",
+    "008-block-1-opener-header": "A Sequestration Solution",
     "008b-block-1-opener-subheader": "No capital, machines or special skills needed to sequester recovered plastic.",
-    "009-block-1-paragraph1": "Like normal ecobricks, OEBs are a collaboration powered technology, grounded in <a href=\"/principles\">regenerative principles.</a>",
-    "010-block-1-paragraph2": "The primary purpose of ocean ecobricks is <a href =\"/sequest\">plastic sequestration</a>— securing potentially toxic plastic out of the biosphere.   Once made, ocean ecobricks can be used as reusable building blocks.  Earth building methods further secure the contained plastic into long term constructions.",
-    "011-block-1-paragraph3": "<a href=\"/sequest\">Learn more about plastic sequestration</a>",
+    "009-block-1-paragraph1": "Like normal ecobricks, OEBs are a collaboration powered technology, grounded in following <a href=\"plast.php\">Earth's example</a> of capturing and compacting carbon.",
+    "010-block-1-paragraph2": "The primary purpose of ocean ecobricks is what we call <a href=\"sequest.php\">plastic sequestration</a>— securing potentially toxic plastic out of the biosphere.   Once made, ocean ecobricks prevent the plastic they contain from photodegrading into microplastics and from leaching out chemicals into the environment.  Earth building with OEBs further secure the contained plastic into long term sequestration..",
+    "011-block-1-paragraph3": "<a href=\"sequest.php\">Learn more about plastic sequestration</a>",
 
     // 🛠️ Section: Making an Ocean Ecobrick
     "012-block-2-opener-header": "Making an Ocean Ecobrick",
     "013-block-2-opener-subheader": "Making an Ocean Ecobrick (OEB) is straightforward– no special tools, machines or skills are required.",
-    "014-block-2-paragraph-1": "An OEB is essentially two PET bottles, cut, packed with plastic, then sealed bottom to bottom. By cutting a bottle’s top off, one creates a wide opening for packing the chunky, large and solid plastics found on the beach.",
+    "014-block-2-paragraph-1": "An OEB is essentially two PET bottles, cut, packed with plastic, then sealed bottom to bottom. By cutting a bottle's top off, one creates a wide opening for packing the chunky, large and solid plastics found on the beach.",
     "014c-block-2-main-p1": "Click to enlarge instructions",
     "015-block-2-paragraph-2": "For the lining of the bottom and the top of the OEB a soft plastic is used to give the bottom a color.  Ideally both the top and bottom of the OEB are given the same color.  This adds the ability to create designs and patterns in OEB building applications.",
-    "016-block-2-paragraph-3": "A simple bamboo stick is then used to pack the bottle as full and as solid as possible of ocean plastic. Once the bottom is 80% complete, the top is silvered, then packed 30% full.  Packing anymore than this will make it difficult to join the top and bottom together later.",
-    "017-block-2-paragraph-4": "The techniques of making a good normal ecobrick apply to ocean ecobrick making. <a href=\"/how\">See our 10 Step Guide to Ecobrick Making to learn mow to make an awesome OEB.</a>",
+    "016-block-2-paragraph-3": "A simple bamboo stick is then used to pack the bottle as full and as solid as possible of ocean plastic. Once the bottom is 80% complete, the top half is packed 30% full.  Packing anymore than this will make it difficult to join the top and bottom together later.",
+    "017-block-2-paragraph-4": "The techniques of making a good normal ecobrick apply to ocean ecobrick making. <a href=\"how.php\">See our 10 Step Guide to Ecobrick Making to learn how to make an awesome OEB.</a>",
 
-// 🧴 Section: Sealing an Ocean Ecobrick
+    // 🧴 Section: Sealing an Ocean Ecobrick
     "018-block-3-opener-header": "Sealing an Ocean Ecobrick",
     "019-block-3-opener-subheader": "An Ocean Ecobrick is sealed shut by pressing together and twisting tight the top and bottom.",
     "020c-block-3-main-p1": "Click to enlarge instructions",
-    "020-block-3-paragraph-1": "An Ocean Ecobrick is unlike a regular ecobrick as it is <b>not</b> sealed air-tight.  This is important as OEBs are made from wet and soiled plastic that will eventually generate methane. The lack of a seal between the two cut bottles, allows gas to escape, avoiding the dangers of gas accumulation (this is why normal ecobricks are made with clean and dry plastic).",
-    "021-block-3-paragraph-2": "Some ecobricks still choose to use silicone to secure the top to the bottom.  If you choose to do this, be sure that your seal is not perfect and that air can still escape.",
+    "020-block-3-paragraph-1": "An ocean ecobrick is unlike a regular ecobrick as it is <b>not</b> sealed air-tight.  This is important as OEBs are made from wet and soiled plastic that will eventually generate methane. The lack of a seal between the two cut bottles, allows gas to escape, avoiding the dangers of gas accumulation (this is why normal ecobricks are made with clean and dry plastic).",
+    "021-block-3-paragraph-2": "Some ecobrickers choose to use silicone to secure the top to the bottom.  This is not absolutely necessary as two well cut bottles should twist together in a tight bond.  However, if you choose to silicone your OEB together, be sure that your seal is not perfect and that air can still escape.",
 
-// 🎣 Section: Fishing Line & Nets
+    // 🎣 Section: Fishing Line & Nets
     "022-block-4-opener-header": "Fishing Line & Nets",
     "023-block-4-opener-subheader": "Fishing nets and gear is one of the most prevalent and problematic plastic found on beaches and in the ocean.",
-    "024-block-4-paragraph-1": "Over 10% of the plastic in the oceans is ‘ghost gear’– that’s over <a href=\"https://static1.squarespace.com/static/5b987b8689c172e29293593f/t/5bd6e9e00d9297316b69bc38/1540811271735/GGGI+who+we+are.NOBLEED.PDF\">640,000 tons of fishing nets and equipement.</a> This plastic not only degrades into micro-plastics and toxins but causes a persistent dangers to marine life.   Ocean ecobricks are a great place to put fishing nets and gear found on the beach!",
+    "024-block-4-paragraph-1": "OEBs are a great solution to fishing line, nets and rope found on the beach.  Apparently, over 10% of the plastic in the oceans is 'ghost gear'– that's over <a href=\"https://static1.squarespace.com/static/5b987b8689c172e29293593f/t/5bd6e9e00d9297316b69bc38/1540811271735/GGGI+who+we+are.NOBLEED.PDF\">640,000 tons of fishing nets and equipement.</a> This plastic not only degrades into micro-plastics and toxins but causes a persistent dangers to marine life.   ",
     "025-block-4-paragraph-2": "By packing them into an ocean ecobrick we keep them from causing all sorts of problems for ecologies and marine life.",
 
-// 🌱 Section: Raising Ecological Consciousness
+    // 🌱 Section: Raising Ecological Consciousness
     "026-block-5-opener-header": "Raising Ecological Consciousness",
     "027-block-5-opener-subheader": "Caring for local ecosystems unites people across class, age, economy and continents.",
-    "028-block-5-paragraph-1": "Not only do ocean ecobricks <a href=\"/sequest\">secure and sequester plastic</a>, they provide a launch pad to <a href=\"/ayyew\">ayyew living</a>, <a href=\"/principles\">regenerative principles</a> and <a>plastic transition</a>.",
+    "028-block-5-paragraph-1": "Not only do ocean ecobricks <a href=\"sequest.php\">secure and sequester plastic</a>, they provide a launch pad to <a href=\"ayyew.php\">ayyew living</a>, <a href=\"principles.php\">regenerative principles</a> and <a href=\"transition.php\">plastic transition</a>.",
     "029-block-5-paragraph-2": "By packing them into an ocean ecobrick we keep them from causing all sorts of problems for ecologies and marine life.",
-    "030-block-5-paragraph-3": "With many ocean ecobricks completed, the process of building with them is likewise an experience of <a href=\"/circular\">circular</a> and <a href=\"/principles\">regenerative principles.</a>",
+    "030-block-5-paragraph-3": "With many ocean ecobricks completed, the process of building with them is likewise an experience of <a href=\"spiral.php\">spiral</a> and <a href=\"principles.php\">regenerative principles.</a>",
 
-// 🧱 Section: Building with Ocean Ecobricks
+    // 🧱 Section: Building with Ocean Ecobricks
     "031-block-6-opener-header": "Building with Ocean Ecobricks",
     "032-block-6-opener-subheader": "Building with ocean ecobricks the last step in securing the plastic that has been collected.",
-    "033-block-6-paragraph-1": "Ocean ecobricks are sometimes combined together with silicone to make ecobrick <a href =\"/modules\">modules</a>.  However, the best application is in making and enhancing <b>green spaces:</b>  home garden walls, play parks, tree ring benches, and forest walkways. Ocean ecobricks are laid down horizontally and combined with <a href=\"/earth\">earth / cob /adobe.</a> In this way they can used to create benches, garden planters, walkways, walls and more.",
-    "034-block-6-paragraph-2": "Building with ocean ecobricks applies <a href=\"/circular\">circular</a> principles to ensure that we plan for the end of the construction.  In this way, when the construction comes to its end the ocean ecobricks can be extricated and used for another construction.",
-    "035-block-6-paragraph-3": "<a href\"/earth\">Learn more about Ecobrick & Earth Building</a>",
-// ⚖️ Section: Ocean vs Normal Ecobricks
+    "033-block-6-paragraph-1": "Ocean ecobricks are sometimes combined together with silicone to make ecobrick <a href=\"modules.php\">modules</a>.  However, the best application is in making and enhancing <b>green spaces:</b>  home garden walls, play parks, tree ring benches, and forest walkways. Ocean ecobricks are laid down horizontally and combined with <a href=\"earth.php\">earth / cob /adobe.</a> In this way they can used to create benches, garden planters, walkways, walls and more.",
+    "034-block-6-paragraph-2": "Building with ocean ecobricks applies <a href=\"spiral.php\">spiral</a> principles to ensure that we plan for the end of the construction.  In this way, when the construction comes to its end the ocean ecobricks can be extricated and used for another construction.",
+    "035-block-6-paragraph-3": "<a href=\"earth.php\">Learn more about Ecobrick &amp; Earth Building</a>",
+
+    // ⚖️ Section: Ocean vs Normal Ecobricks
     "036-block-7-opener-header": "Ocean vs Normal Ecobricks",
     "037-block-7-opener-subheader": "The Global Ecobrick Alliance promotes ocean ecobricks only as a last resort for plastic.",
-    "038-block-7-paragraph-1": "Normal ecobricks are the best way to deal with our daily plastic. Compared to <a href=\"/what\">regular ecobricks</a>, Ocean Ecobricks are more wasteful, fragile, and much less elegant than regular Ecobricks.  They are not ideal for making <a href=\"/modules\">milstein modules</a> or lego– mainly for <a href=\"/earth\">earth and ecobrick constructions.</a> Nonetheless, Ocean Ecobricks remain <a href=\"/cradle-to-cradle/\">cradle to cradle.</a>",
-    "039-block-7-paragraph-2": "In other words, making an ecobrick by packing your own clean and dry plastic into an uncut bottle is ideal!",
+    "038-block-7-paragraph-1": "Normal ecobricks are the best way to deal with our daily plastic. Compared to <a href=\"what.php\">regular ecobricks</a>, Ocean Ecobricks are more wasteful, fragile, and much less elegant than regular Ecobricks.  They are not ideal for making <a href =\"modules.php\">milstein modules</a> or lego.  However, they are great for  for <a href=\"earth.php\">earth and ecobrick constructions!</a>",
+    "039-block-7-paragraph-2": "In other words, for normal plastic, stick to making a regular ecobrick made with an uncut bottle.",
     "040-block-7-paragraph-3": "Ocean ecobricks miss out on the most value aspect of ecobricking:  that of taking personal responsibility for our plastic.",
-    "041-block-7-paragraph-4": "Also, unlike normal ecobricks, OEB’s generate waste in their making. Bottles must be cut, and the bottle tops and caps are left over. (Note that even though cut, the tops and caps can still be recycled.  PET and HDPE are the most sought after plastics. In most South East Asian countries these will be collected by recyclers.  Simply pack bottle cuttings into an intact PET bottle, cap and recycle.)",
+    "041-block-7-paragraph-4": "Also, unlike normal ecobricks, OEB's generate waste in their making. Bottles must be cut, and the bottle tops and caps are left over. (Note that even though cut, the tops and caps can still be recycled.  PET and HDPE are the most sought after plastics. In most South East Asian countries these will be collected by recyclers.  Simply pack bottle cuttings into an intact PET bottle, cap and recycle.)",
     "042-block-7-paragraph-5": "Ocean Ecobricks are therefore ideal for communities near ocean, lake or river shores that are overloaded with plastic.",
 
-// 🌍 Section: Final Solution for Plastic?
+    // 🌍 Section: Final Solution for Plastic?
     "043-block-8-opener-header": "Are Ocean Ecobricks Really a Final Solution for Plastic?",
-    "044-block-8-opener-subheader": "This is a common question, but one with a false premise. In the circular systems of the biosphere, there’s never such thing as ‘final’.",
-    "045-block-8-paragraph-1": "Ecological cycles can be very short and they can be very long– but they are never final.   The Earth’s sequestration of carbon is an example of a long cycle– measured in millions of years.   However, as carbon moves from plants to trees and back again, the cycles can be short.",
-    "046-block-7-paragraph-2": "By concentrating our carbon/plastic into building blocks like ocean ecobricks, we secure it from getting into the environment in the short-term.  Then when we put ocean ecobricks into long-term earth constructions, the plastic is further protected.  Covered by earth it cannot be degraded by the sun, fire, or friction– much like the carbon the earth put under ground.",
-    "047-block-7-paragraph-3": "Even if these structures last decades or centuries, when the structure comes to its end, the plastic can be removed and put to use again.  In this way we copy how the earth removed plastic from ecology and into long-term geology.",
-    "048-block-7-paragraph-4": "Although we don’t know how it will be used, we do know that it makes a better gift for the future compacted and concentrated than scattered and loose in the biosphere.  In the same way the Earth’s carbon was a gift to our age, we can gift our carbon to another.",
+    "044-block-8-opener-subheader": "In the spiral systems of the biosphere, there's never such thing as 'final'...",
+    "045-block-8-paragraph-1": "Ecological cycles can be very short and they can be very long– but they are never final.   The Earth's sequestration of carbon is an example of a long cycle– measured in millions of years.   However, as carbon moves from plants to trees and back again, the cycles can be short.",
+    "046-block-8-paragraph-2": "By concentrating our carbon/plastic into building blocks like ocean ecobricks, we secure it from getting into the environment in the short-term.  Then when we put ocean ecobricks into long-term earth constructions, the plastic is further protected.  Covered by earth it cannot be degraded by the sun, fire, or friction– much like the carbon the earth put under ground.",
+    "047-block-8-paragraph-3": "Even if these structures last decades or centuries, when the structure comes to its end, the plastic can be removed and put to use again.  In this way we copy how the earth removed plastic from ecology and into long-term geology.",
+    "048-block-8-paragraph-4": "Although we don't know how it will be used, we do know that it makes a better gift for the future compacted and concentrated than scattered and loose in the biosphere.  In the same way the Earth's carbon was a gift to our age, we can gift our carbon to another.",
+
+    // 📸 Page End Gallery
+    "048-page-end-header": "Examples of Ocean Ecobricks",
+    "049-end-page-paragraph": "Ocean Ecobricks come in all sizes and shapes– it all depends on what you plan to build!",
 
 };
-
