c9c769e8d13ef48dfa64230ab7e50f191125f313
ubuntusitubondo <russ@earthen.io>
2026-04-15 20:38:29 +0700

    main page update for new March course

--- en/ocean.php
+++ en/ocean.php
@@ -16,7 +16,7 @@
 		<div class="splash-heading" data-lang-id="001-splash-title">Ocean Ecobricks</div>
 	    <div class="splash-sub" data-lang-id="002-splash-subtitle">A Sequestration Solution for Dirty Plastics</div>
 	</div>
-	<div class="splash-image"><img src="../svgs/oebs.svg" style="width: 95%" alt="The basics of ecobricking" data-lang-id="003-splash-image-alt">
+	<div class="splash-image"><img src="../svgs/oebs.svg" style="width: 95%" alt="Ocean Ecobrick" data-lang-id="003-splash-image-alt">
     </div>
 </div>
 <div id="splash-bar"></div>
@@ -54,7 +54,7 @@
 					</div>
 					
 					<div id="preclosed1">
-                        <img src="../photos/oebs/making-oebs.webp" style="width:100%" alt="Making ocean ecobricks" loading="lazy">
+                        <img src="../photos/oebs/making-oebs.webp" style="width:100%" alt="Ocean Ecobrick" loading="lazy">
 						<p data-lang-id="009-block-1-paragraph1">Like normal ecobricks, OEBs are a collaboration powered technology, grounded in following <a href="plast.php">Earth's example</a> of capturing and compacting carbon.</p>
 						<p data-lang-id="010-block-1-paragraph2">The primary purpose of ocean ecobricks is what we call <a href="sequest.php">plastic sequestration</a>— securing potentially toxic plastic out of the biosphere.   Once made, ocean ecobricks prevent the plastic they contain from photodegrading into microplastics and from leaching out chemicals into the environment.  Earth building with OEBs further secure the contained plastic into long term sequestration..</p>
 						<p data-lang-id="011-block-1-paragraph3"><a href="sequest.php">Learn more about plastic sequestration</a></p>
@@ -73,7 +73,7 @@
                         <button onclick="toggleAccordion(2)" class="block-toggle" id="block-toggle-show2" aria-label="Toggle Section Two">+</button>
                     </div>
                     <div id="preclosed2">
-                        <img src="../photos/oebs/many-oebs.webp" style="width:100%" alt="Ocean ecobricks" loading="lazy">
+                        <img src="../photos/oebs/many-oebs.webp" style="width:100%" alt="Ocean Ecobrick" loading="lazy">
                         <p data-lang-id="014-block-2-paragraph-1">An OEB is essentially two PET bottles, cut, packed with plastic, then sealed bottom to bottom. By cutting a bottle’s top off, one creates a wide opening for packing the chunky, large and solid plastics found on the beach.</p>
                         <p data-lang-id="015-block-2-paragraph-2">For the lining of the bottom and the top of the OEB a soft plastic is used to give the bottom a color.  Ideally both the top and bottom of the OEB are given the same color.  This adds the ability to create designs and patterns in OEB building applications.</p>
                         <p data-lang-id="016-block-2-paragraph-3">A simple bamboo stick is then used to pack the bottle as full and as solid as possible of ocean plastic. Once the bottom is 80% complete, the top half is packed 30% full.  Packing anymore than this will make it difficult to join the top and bottom together later.</p>
@@ -81,8 +81,8 @@
                         <p data-lang-id="020-block-3-paragraph-1">An ocean ecobrick is unlike a regular ecobrick as it is <b>not</b> sealed air-tight.  This is important as OEBs are made from wet and soiled plastic that will eventually generate methane. The lack of a seal between the two cut bottles, allows gas to escape, avoiding the dangers of gas accumulation (this is why normal ecobricks are made with clean and dry plastic).</p>
                         <p data-lang-id="021-block-3-paragraph-2">Some ecobrickers choose to use silicone to secure the top to the bottom.  This is not absolutely necessary as two well cut bottles should twist together in a tight bond.  However, if you choose to silicone your OEB together, be sure that your seal is not perfect and that air can still escape.</p>
                         <div style="display:flex; gap:10px; margin-top:1em;">
-                            <img src="../photos/oebs/oeb-1.jpg" style="width:48%" alt="Ocean ecobrick example 1" loading="lazy">
-                            <img src="../photos/oebs/oeb-2.jpg" style="width:48%" alt="Ocean ecobrick example 2" loading="lazy">
+                            <img src="../photos/oebs/oeb-1.jpg" style="width:48%" alt="Ocean Ecobrick" loading="lazy">
+                            <img src="../photos/oebs/oeb-2.jpg" style="width:48%" alt="Ocean Ecobrick" loading="lazy">
                         </div>
                     </div>
                 </div>
@@ -100,7 +100,7 @@
                         <button onclick="toggleAccordion(4)" class="block-toggle" id="block-toggle-show4" aria-label="Toggle Section Four">+</button>
                     </div>
                     <div id="preclosed4">
-                        <img src="../photos/ocean-ecobrick.jpg" style="width:100%" loading="lazy">
+                        <img src="../photos/ocean-ecobrick.jpg" style="width:100%" alt="Ocean Ecobrick" loading="lazy">
                         <p data-lang-id="024-block-4-paragraph-1">OEBs are a great solution to fishing line, nets and rope found on the beach.  Apparently, over 10% of the plastic in the oceans is ‘ghost gear’– that’s over <a href="https://static1.squarespace.com/static/5b987b8689c172e29293593f/t/5bd6e9e00d9297316b69bc38/1540811271735/GGGI+who+we+are.NOBLEED.PDF">640,000 tons of fishing nets and equipement.</a> This plastic not only degrades into micro-plastics and toxins but causes a persistent dangers to marine life.   </p>
                         <p data-lang-id="025-block-4-paragraph-2">By packing them into an ocean ecobrick we keep them from causing all sorts of problems for ecologies and marine life.</p>
                     </div>
@@ -117,7 +117,7 @@
                         <button onclick="toggleAccordion(6)" class="block-toggle" id="block-toggle-show6" aria-label="Toggle Section Six">+</button>
                     </div>
                     <div id="preclosed6">
-                        <img src="../photos/oebs/build-with-oebs.webp" style="width:100%" alt="Building with ocean ecobricks" loading="lazy">
+                        <img src="../photos/oebs/build-with-oebs.webp" style="width:100%" alt="Ocean Ecobrick" loading="lazy">
                         <p data-lang-id="033-block-6-paragraph-1">Ocean ecobricks are sometimes combined together with silicone to make ecobrick <a href="modules.php">modules</a>.  However, the best application is in making and enhancing <b>green spaces:</b>  home garden walls, play parks, tree ring benches, and forest walkways. Ocean ecobricks are laid down horizontally and combined with <a href="earth.php">earth / cob /adobe.</a> In this way they can used to create benches, garden planters, walkways, walls and more.</p>
                         <p data-lang-id="034-block-6-paragraph-2">Building with ocean ecobricks applies <a href="spiral.php">spiral</a> principles to ensure that we plan for the end of the construction.  In this way, when the construction comes to its end the ocean ecobricks can be extricated and used for another construction.</p>
                         <p data-lang-id="035-block-6-paragraph-3"><a href="earth.php">Learn more about Ecobrick &amp; Earth Building</a></p>
@@ -135,7 +135,7 @@
                         <button onclick="toggleAccordion(7)" class="block-toggle" id="block-toggle-show7" aria-label="Toggle Section Seven">+</button>
                     </div>
                     <div id="preclosed7">
-                        <img src="../photos/oebs/example-oeb.webp" style="width:100%" alt="Ocean ecobrick example" loading="lazy">
+                        <img src="../photos/oebs/example-oeb.webp" style="width:100%" alt="Ocean Ecobrick" loading="lazy">
                         <p data-lang-id="038-block-7-paragraph-1">Normal ecobricks are the best way to deal with our daily plastic. Compared to <a href="what.php">regular ecobricks</a>, Ocean Ecobricks are more wasteful, fragile, and much less elegant than regular Ecobricks.  They are not ideal for making <a href ="modules.php">milstein modules</a> or lego.  However, they are great for  for <a href="earth.php">earth and ecobrick constructions!</a></p>
                         <p data-lang-id="039-block-7-paragraph-2">In other words, for normal plastic, stick to making a regular ecobrick made with an uncut bottle.</p>
                         <p data-lang-id="040-block-7-paragraph-3">Ocean ecobricks miss out on the most value aspect of ecobricking:  that of taking personal responsibility for our plastic.</p>
@@ -169,23 +169,24 @@
 
 
         	<div class="page-paragraph">
-    			<h4 data-lang-id="048-page-end-header">Examples of Ocean Ecobricks</h4>
+                <br><br>
+    			<h4 data-lang-id="048-page-end-header">Examples of ocean ecobricks</h4>
     			<p data-lang-id="049-end-page-paragraph">
     			Ocean Ecobricks come in all sizes and shapes– it all depends on what you plan to build!</p>
     		</div>
             <div id="three-column-gal-srs1" class="three-column-gal">
                 <!--photo1-->
                 <div class="gal-photo" onclick="viewGalleryImage('../photos/ocean-examples1.jpg', 'Soil test')">
-                    <img src="../photos/ocean-examples1.jpg" alt="Soil test" loading="lazy">
+                    <img src="../photos/ocean-examples1.jpg" alt="Ocean Ecobrick" loading="lazy">
                 </div>
 
                 <!--photo2-->
                 <div class="gal-photo" onclick="viewGalleryImage('../photos/ocean-examples2.jpeg', 'Find a smooth, flat surface (i.e large tile)')">
-                    <img src="../photos/ocean-examples2.jpeg" alt="Find a smooth, flat surface (i.e large tile)" loading="lazy">
+                    <img src="../photos/ocean-examples2.jpeg" alt="Ocean Ecobrick" loading="lazy">
                 </div>
                 <!--photo3-->
                 <div class="gal-photo" onclick="viewGalleryImage('../photos/ocean-examples3.jpeg', 'Put on some oil')">
-                    <img src="../photos/ocean-examples3.jpeg" alt="Put on some oil">
+                    <img src="../photos/ocean-examples3.jpeg" alt="Ocean Ecobrick">
                 </div>
             </div>
             <br>
@@ -193,51 +194,51 @@
 
                 <!--photo1-->
                 <div class="gal-photo" onclick="viewGalleryImage('../photos/ocean-examples4.png', 'Mix a small batch of 1:6 cement')">
-                    <img src="../photos/ocean-examples4-preview.png" alt="Mix a small batch of 1:6 cement">
+                    <img src="../photos/ocean-examples4.png" alt="Ocean Ecobrick">
                 </div>
                 
                 <!--photo2-->
                 <div class="gal-photo" onclick="viewGalleryImage('../photos/ocean-examples7.jpg', 'Lay out your cement foundation/top')">
-                    <img src="../photos/ocean-examples7.jpg" alt="Lay out your cement foundation/top">
+                    <img src="../photos/ocean-examples7.jpg" alt="Ocean Ecobrick">
                 </div>
 
                 <!--photo3-->
                 <div class="gal-photo" onclick="viewGalleryImage('../photos/ocean-examples6.jpg', 'Create a hole for the ecobrick')">
-                    <img src="../photos/ocean-examples6.jpg" alt="Create a hole for the ecobrick">
+                    <img src="../photos/ocean-examples6.jpg" alt="Ocean Ecobrick">
                 </div>
             </div>
             <br>
             <div id="three-column-gal-srs3" class="three-column-gal">
                 <!--photo1-->
                 <div class="gal-photo" onclick="viewGalleryImage('../photos/ocean-examples5.jpg', 'Add first ecobrick')">
-                    <img src="../photos/ocean-examples5.jpg" alt="Add first ecobrick">
+                    <img src="../photos/ocean-examples5.jpg" alt="Ocean Ecobrick">
                 </div>
 
                 <!--photo2-->
                 <div class="gal-photo" onclick="viewGalleryImage('../photos/ocean-examples8.jpg', 'Continue adding ecobricks')">
-                    <img src="../photos/ocean-examples8.jpg" alt="Continue adding ecobricks">
+                    <img src="../photos/ocean-examples8.jpg" alt="Ocean Ecobrick">
                 </div>
 
                 <!--photo3-->
                 <div class="gal-photo" onclick="viewGalleryImage('../photos/ocean-examples9.jpg', 'Add all bottles, let dry.')">
-                    <img src="../photos/ocean-examples9.jpg" alt="Add all bottles, let dry.">
+                    <img src="../photos/ocean-examples9.jpg" alt="Ocean Ecobrick">
                 </div>
             </div>
             <br>
             <div id="three-column-gal-srs4" class="three-column-gal">
                 <!--photo1-->
                 <div class="gal-photo" onclick="viewGalleryImage('../photos/ocean-examples10.jpg', 'Mix your cob mixture to your ratio.')">
-                    <img src="../pngs/ocean-examples10-preview.png" alt="Mix your cob mixture to your ratio.">
+                    <img src="../pngs/ocean-examples10-preview.png" alt="Ocean Ecobrick">
                 </div>
                     
                 <!--photo2-->
                 <div class="gal-photo" onclick="viewGalleryImage('../photos/ocean-examples11.jpg', 'Layer cob on top of dry cement')">
-                    <img src="../photos/ocean-examples11.jpg" alt="Layer cob on top of dry cement">
+                    <img src="../photos/ocean-examples11.jpg" alt="Ocean Ecobrick">
                 </div>
                     
                 <!--photo3-->
                 <div class="gal-photo" onclick="viewGalleryImage('../photos/ocean-examples12.jpg', 'Finish with a thin layer of cob/cement')">
-                    <img src="../photos/ocean-examples12.jpg" alt="Finish with a thin layer of cob/cement">
+                    <img src="../photos/ocean-examples12.jpg" alt="Ocean Ecobrick">
                 </div>
             </div>
         </div>
