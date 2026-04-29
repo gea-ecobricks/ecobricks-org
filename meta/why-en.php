6c52e4c909ab6c7018683ca164a076d22a66783a
JosephOFlanagan <joseph.oflanagan@yahoo.co.uk>
2026-04-29 13:31:23 +0100

    Added missing why-inc.php page, the absence of which was causing the why page to not be able to be displayed.

---
+++ meta/why-en.php
@@ -0,0 +1,32 @@
+<!-- Meta tags for page display and search engine listing-->
+ 
+<title>Why Make Ecobricks?</title>
+
+<meta name="keywords" content="why make, ten step guide, making, packing, ecobrick, why make ecobricks, eco brick, eco, brick, brik, method, ecobricking method, technique, advanced, guide, best practice, clean, plastic, density, minimum bottle, PET, bottle brick">
+
+<meta name="description" content="Our 10 Step Guide to Making an Ecobrick: best practices, standards and advanced techniques.">
+	
+	<!-- Facebook Open Graph Tags for social sharing-->
+
+	<meta property="og:url"           content="https://ecobricks.org/en/why.php">
+	<meta property="og:type"          content="website">
+ 	<meta property="og:title"         content="Why Make Ecobricks? | Ecobricks.org">
+  	<meta property="og:description"   content="Why bother with the hard work of ecobricking plastic?">
+  	<meta property="og:image"         content="https://www.ecobricks.org/webp/sad_albatross-1.webp">
+  	<meta property="og:image:width" content="1000">
+	<meta property="og:image:height" content="1000" >
+  	<meta property="og:image:alt"     content="Why bother with the hard work of ecobricking plastic?">
+	  <meta property="og:locale" content="en_GB" >
+
+	  <meta name="author" content="Ecobricks.org">
+  
+  <meta property="og:type" content="article" >
+ <meta property="og:site_name" content="Ecobricks.org" >
+ <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" >
+ <meta property="article:modified_time" content="2023-11-1T09:14:13+00:00" >
+ <meta property="og:image:type" content="image/png" >
+ <meta name="author" content="Global Ecobrick Alliance" >
+ <meta name="twitter:card" content="summary" >
+ <meta name="twitter:label1" content="Est. reading time" >
+ <meta name="twitter:data1" content="7 minutes" > 
+	 
