e87ffae3ad64805335be136d50a89ab6cc03526d
JosephOFlanagan <joseph.oflanagan@yahoo.co.uk>
2026-04-29 13:39:49 +0100

    Updated the why-inc page to the new structure.

--- includes/why-inc.php
+++ includes/why-inc.php
@@ -1,42 +1,24 @@
-<?php require_once ("lang.php");
-echo <<<_END
-<html lang="$lang">
-_END;?>
-<HEAD>
-<?php require_once ("../header.php");?>
-
-<meta http-equiv="Content-Security-Policy" content="
-    default-src 'self';
-    script-src 'self' https://api.websitecarbon.com 'unsafe-inline' https://www.youtube.com https://s.ytimg.com https://unpkg.com https://api.earthen.io;
-    frame-src https://www.youtube.com;
-    connect-src 'self';
-    img-src 'self' https://www.youtube.com;
-    style-src 'self' 'unsafe-inline' https://api.earthen.io;
-    font-src 'self' https://www.ecobricks.org/fonts/Arvo-regular.ttf;
-    object-src 'none';
-    worker-src 'self';
-">
-
-
 
 
 <!--Image files to preload that are unique to this page-->
 
-<link rel="preload" as="image" href="../logos/gea-horizontal.svg">
+<!-- <link rel="preload" as="image" href="../logos/gea-horizontal.svg"> -->
 <link rel="preload" as="image" href="../webp/eb-sky-400px.webp">
 <link rel="preload" as="image" href="../webp/build-banner-1400px.webp">
 
-<!--This loads CSS specific to this page
-<link rel="stylesheet" type="text/css" href="http://ecobricks.org/css/what.css?v1.1">-->
+<!-- <link rel="stylesheet" type="text/css" href="../stylesheet-content-2024.css?v=2.3">  -->
 
 <!-- This loads the page's meta tags:  Be sure the page name is in place in English-->
 
 <?php require_once ("../meta/why-$lang.php");?>
 
- 
+
 <STYLE>
 
 
+img {
+  max-width: 100%;
+}
 
 #play-button {
     width: 60px;
@@ -85,199 +67,45 @@ _END;?>
   margin-bottom: -10px;
 }
 
-@media screen and (max-width: 700px) { 
+@media screen and (max-width: 700px) {
 	.splash-content-block {
-		text-align: left;
-		height: 73vh;
-		padding: 50px 5% 25px 5%;
-		margin-bottom: 0px;
-		z-index: 5;
-       
-		background: url(../webp/build-banner-1400px.webp) bottom;
+
+		    background: url(../webp/build-banner-1400px.webp) bottom;
         background-color: #FA8D04;
-		
-    	background-repeat: no-repeat;
-		background-size: cover;
-		margin-top: 45px;
-		width: 100%;
-		display: flex;
- 		box-sizing: border-box;
-		flex-direction: column;
-	position: relative;
+        /* filter: hue-rotate(-30deg); */
+
 
 	}
 }
 
 
-@media screen and (min-width: 700px) { 
+@media screen and (min-width: 700px) {
 	.splash-content-block {
-		text-align: left;
-		min-height: 67vh;
-		padding: 50px 7% 50px 7%;
-		z-index: 5;
-		position: relative;
-		
-        
-		background: url(../webp/build-banner-1400px.webp) bottom;
-        background-color: #FA8D04 ;
-		
-    	background-repeat: no-repeat;
-		margin: -3px 0 -20px 0;
-		display: flex;
- 		 flex-wrap: wrap;
- 		 box-sizing: border-box;
-		  flex-direction: row;
-		  width: 100%;
-		  background-size: cover;
 
-	position: relative;
-    margin: auto;
-
+		    background: url(../webp/build-banner-1400px.webp) bottom;
+        background-color: #FA8D04;
+        /* filter: hue-rotate(-30deg); */
 
 }
-} 
-
-
-@media screen and (min-width: 700px) { 
-.splash-image {
-  z-index: 5;
-  position: relative;
-  text-align: center;
-  flex: 35%;
-	padding: 0px;
-	box-sizing: border-box;
-	margin-top: 0px;
-}
-}
-
-
-@media screen and (min-width: 700px) { 
-.splash-box {
-  z-index: 5;
-  position: relative;
-  flex: 65%;
-	padding: 10px 30px 0px 0px;
-	box-sizing: border-box;
-	text-align: right;
-    margin: auto;
-}
-}
-
-
-
-@media screen and (max-width: 700px) { 
-.splash-image {
-  z-index: 5;
-  position: relative;
-  text-align: left;
-  flex: 25%;
-  width: 250px;
-	padding: 0px;
-	box-sizing: border-box;
-	margin: 0px 0px 0px 10px;
-}
-}
-
-
-
-@media screen and (max-width: 700px) { 
-.splash-box {
-	position: relative;
-  flex: 100%;
-	padding: 10px 10px 0px 0px;
-	box-sizing: border-box;
-	text-align: right;
-}
-}
-
-
-
-.splash-heading { 
-    /*font-family: 'Mulish', Arial, Helvetica, sans-serif;*/
-    font-family: Arvo, serif;
-  color: white;
-  font-weight: 500;
-  text-shadow: 0 0 8px black;
-
-}
-
-@media screen and (max-width: 700px) {
-	.splash-heading {
-      font-size: 3.0em;
-      line-height: 1.1;
-      margin: 10px 0;
-  }
-}
-
-@media screen and (min-width: 700px) {
-	.splash-heading {
-      font-size: 4em;
-      line-height: 1.3;
-      margin: 0px 0px 10px 0px;
-  }
-}
-
-
-.splash-sub {
-  font-family: 'Mulish', Arial, Helvetica, sans-serif;
-  color: #fff;
-  /*text-shadow: 0px 0px 10px #fff;*/
-  margin: 15px 0;
-  text-shadow: 0 0 7px black;
-}
-
-@media screen and (max-width: 700px) {
-	.splash-sub {
-		font-size: 1.9em;
-		line-height: 1.3;
-		font-weight: 400;
-  }
-}
-@media screen and (min-width: 700px) {
-	.splash-sub {
-		font-size: 2.5em;
-		line-height: 1.3;
-		font-weight: 400;
-  }
 }
 
 
 #splash-bar {
-	margin-top: -50px;
-	width: 100%;
-	
-	height:80px;	
-	
-	position: relative;
-	z-index: 0;
 
-box-shadow: 0 8px 7px rgba(85, 84, 84, 0.4);
     background-color: #FA8D04;
-
-	-webkit-transform: skewY(-3deg);
-  -moz-transform: skewY(-3deg);
-  -ms-transform: skewY(-3deg);
-  -o-transform: skewY(-3deg);
-  transform: skewY(-3deg);
-	margin-bottom: 40px;
-
+    /* filter: hue-rotate(-30deg); */
 }
 
-hr {border: gray;
-border-style: dashed;
-border-width: 1px;
-margin-top: 31px;
-margin-bottom: 31px;}
-	
-</style>	
 
-</head>
-							  
-											  
-<BODY id="full-page">
+.module-btn {
+  background: var(--emblem-orange);
+}
 
-	<div id="load-background">
-        
-	<!-- This loads the page's language specific menu -->
+.module-btn:hover {
+  background: var(--emblem-orange-over);
+}
 
-    <?php require_once ("../menus/menu-$lang.php");?>
+
+</style>
+
+<?php require_once ("../header-2025.php");?>
